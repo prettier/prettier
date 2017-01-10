a0edc2a52013e41c07ec22de721d381b8a259087
James Long <longster@gmail.com>
2017-01-09 23:22:58 -0500

    Document more CLI flags in usage text

--- src/options.js
+++ src/options.js
@@ -1,32 +1,17 @@
 var defaults = {
-  // Number of spaces the pretty-printer should use per tab for
-  // indentation. If you do not pass this option explicitly, it will be
-  // (quite reliably!) inferred from the original code.
+  // Number of spaces the pretty-printer should use per tab
   tabWidth: 2,
 
-  // Fit code within this line limit.
+  // Fit code within this line limit
   printWidth: 80,
 
   // If true, will use single instead of double quotes
   singleQuote: false,
 
-  // Controls the printing of trailing commas in object literals,
-  // array expressions and function parameters.
-  //
-  // This option could either be:
-  // * Boolean - enable/disable in all contexts (objects, arrays and function params).
-  // * Object - enable/disable per context.
-  //
-  // Example:
-  // trailingComma: {
-  //   objects: true,
-  //   arrays: true,
-  //   parameters: false,
-  // }
+  // Controls the printing of trailing commas wherever possible
   trailingComma: false,
 
   // Controls the printing of spaces inside array and objects
-  // See: http://eslint.org/docs/rules/array-bracket-spacing
   bracketSpacing: true
 };
 
