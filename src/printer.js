fc521c4d03fb6f1e4017694cf429e848712acabe
Michael Ficarra <mficarra@shapesecurity.com>
2017-01-10 12:03:36 -0800

    fixes #56: indent cases inside switch statements

--- src/printer.js
+++ src/printer.js
@@ -996,8 +996,10 @@ function genericPrintNoParens(path, options, print) {
       "switch (",
       path.call(print, "discriminant"),
       ") {",
-      hardline,
-      join(hardline, path.map(print, "cases")),
+      indent(
+        options.tabWidth,
+        concat([ hardline, join(hardline, path.map(print, "cases"))])
+      ),
       hardline,
       "}"
     ]);
