867b8299652464a4301e12bd71b96065e7306c13
Dan Harper <intouch@danharper.me>
2017-01-10 18:42:03 +0000

    Print Flow types with no name (fixes #38)

--- src/printer.js
+++ src/printer.js
@@ -1329,7 +1329,7 @@ function genericPrintNoParens(path, options, print) {
     return concat([
       path.call(print, "name"),
       n.optional ? "?" : "",
-      ": ",
+      n.name ? ": " : "",
       path.call(print, "typeAnnotation")
     ]);
   case "GenericTypeAnnotation":
@@ -1399,7 +1399,7 @@ function genericPrintNoParens(path, options, print) {
       variance,
       "[",
       path.call(print, "id"),
-      ": ",
+      n.id ? ": " : "",
       path.call(print, "key"),
       "]: ",
       path.call(print, "value")
