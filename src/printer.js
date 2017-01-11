2e1e6eece2a6c8dc5b539384cbdb36b95a6147f5
Alex Rattray <rattray.alex@gmail.com>
2017-01-11 15:35:51 -0800

    JSX maintains spaces that matter (fixes #30 and thus part of #73)

--- src/printer.js
+++ src/printer.js
@@ -2034,7 +2034,7 @@ function printJSXElement(path, options, print) {
 
             children.push(
               beginBreak ? hardline : "",
-              child.value.replace(/^\s+|\s+$/g, ""),
+              child.value.replace(/^\s+|\s+$/g, endBreak ? "" : " "),
               endBreak ? hardline : ""
             );
           } else if (/\n/.test(child.value)) {
