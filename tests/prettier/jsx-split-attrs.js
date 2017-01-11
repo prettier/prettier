85a07d446d4fb21a08328de25b06db4a81843014
Alex Rattray <rattray.alex@gmail.com>
2017-01-11 15:07:22 -0800

    Break JSXOpeningElement between attributes (fixes #15)

---
+++ tests/prettier/jsx-split-attrs.js
@@ -0,0 +1,15 @@
+long_closed =
+  <BaseForm url="/auth/google" method="GET" colour="blue" size="large" submitLabel="Sign in with Google"/>
+
+long_open =
+  <BaseForm url="/auth/google" method="GET" colour="blue" size="large" submitLabel="Sign in with Google">
+    hello
+  </BaseForm>
+
+short_closed =
+  <BaseForm url="/auth/google" method="GET"/>
+
+short_open =
+  <BaseForm url="/auth/google" method="GET">
+    hello
+  </BaseForm>
