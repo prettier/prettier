5075fa602ee2ec4ae5c8541e76fc4835756ebcd9
Alex Rattray <rattray.alex@gmail.com>
2017-01-12 12:45:47 -0800

    Multiline JSX opening tag breaks children out too (for #73)

--- tests/prettier/jsx-stateless-arrow-fn.js
+++ tests/prettier/jsx-stateless-arrow-fn.js
@@ -14,4 +14,13 @@ const render4 = ({ styles }) => <div style={styles} key="something">Create wrapp
 
 const render5 = ({ styles }) => <div>Keep it on one line.</div>;
 
+const render6 = ({ styles }) => (
+  <div attr1="aaaaaaaaaaaaaaaaa" attr2="bbbbbbbbbbb" attr3="cccccccccccc">
+    <div attr1="aaaaaaaaaaaaaaaaa" attr2="bbbbbbbbbbb" attr3="cccccccccccc" attr4>ddd d dd d d dddd dddd <strong>hello</strong></div>
+    <div attr1="aaaaaaaaaaaaaaaaa" attr2="bbbbbbbbbbb" attr3="cccccccccccc" attr4>ddd d dd d d dddd dddd <strong>hello</strong></div>
+    <div attr1="aaaaaaaaaaaaaaaaa" attr2="bbbbbbbbbbb" attr3="cccccccccccc" attr4>
+      <div attr1="aaaaaaaaaaaaaaaaa" attr2="bbbbbbbbbbb" attr3="cccccccccccc" attr4>ddd d dd d d dddd dddd <strong>hello</strong></div> <strong>hello</strong></div>
+  </div>
+)
+
 const notJSX = (aaaaaaaaaaaaaaaaa, bbbbbbbbbbb) => this.someLongCallWithParams(aaaaaa, bbbbbbb).anotherLongCallWithParams(cccccccccccc, dddddddddddddddddddddd)
