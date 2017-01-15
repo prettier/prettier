15cc3bb7783adcb51ebce629808ae23ad6234648
Christopher Chedeau <vjeuxx@gmail.com>
2017-01-14 20:40:26 -0800

    Fix key quotes omission for flow parser (#203)
    ```js
    echo 'var { "key": val } = 1;' | ./bin/prettier.js --stdin --flow-parser
    var { key: val } = 1;
    ```

--- src/printer.js
+++ src/printer.js
@@ -1587,7 +1587,11 @@ function printStatementSequence(path, options, print) {
 
 function printPropertyKey(path, print) {
   var node = path.getNode().key;
-  if (node.type === "StringLiteral" && isIdentifierName(node.value)) {
+  if (
+    (node.type === "StringLiteral" ||
+      node.type === "Literal" && typeof node.value === "string") &&
+    isIdentifierName(node.value)
+  ) {
     // 'a' -> a
     return node.value;
   }
