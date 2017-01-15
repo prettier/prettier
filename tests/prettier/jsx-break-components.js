5966f519118c30101f1dd6b3e6934e8d36edc74c
Davy Duperron <yamafaktory@gmail.com>
2017-01-15 22:06:05 +0100

    Extended the test :rocket:.

--- tests/prettier/jsx-break-components.js
+++ tests/prettier/jsx-break-components.js
@@ -1,4 +1,7 @@
 <div>
-  <p>foo<span>bar</span></p><h1><span><em>yep</em></span></h1>
+  <Foo />
+  <Bar>
+    <p>foo<span>bar</span></p><h1><span><em>yep</em></span></h1>
+  </Bar>
   <h2>nope</h2>
 </div>
