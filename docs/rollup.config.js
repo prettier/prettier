581338273d1944525754534f91e539bf07303c44
Christopher Chedeau <vjeux@fb.com>
2017-01-18 14:11:29 -0800

    Fix docs
    It doesn't compile cleanly anymore:
    - esutils checks for Buffer.isBuffer without ensuring that Buffer exists.
    - for version, we require a json file, need a rollup extension for that.
    
    I no longer hardcode the version number.
    
    Note: we need to merge this pull request after we have done the release so that the version number is correct and need to rebuild the minified version in a subsequent commit.

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -2,12 +2,14 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import builtins from 'rollup-plugin-node-builtins';
 import babili from 'rollup-plugin-real-babili';
+import json from 'rollup-plugin-json';
 
 export default {
   entry: 'index.js',
   dest: 'docs/prettier.min.js',
   format: 'iife',
   plugins: [
+    json(),
     resolve(),
     commonjs(),
     builtins(),
