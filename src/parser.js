aef3e387f8c9c9995a640c89c6fe65c6b7be9598
Christopher Chedeau <vjeuxx@gmail.com>
2017-01-19 13:23:04 -0800

    Extract parser.js (#338)
    Before, we would parse things inline in both index.js and run_spec. Extracting it should make it easier to manage and understand what is going on.

---
+++ src/parser.js
@@ -0,0 +1,53 @@
+"use strict";
+
+function parseWithFlow(text, filename) {
+  // Inline the require to avoid loading all the JS if we don't use it
+  const flowParser = require("flow-parser");
+
+  const ast = flowParser.parse(text, {
+    esproposal_class_instance_fields: true,
+    esproposal_class_static_fields: true,
+    esproposal_export_star_as: true
+  });
+
+  if (ast.errors.length > 0) {
+    let msg = ast.errors[0].message +
+      " on line " +
+      ast.errors[0].loc.start.line;
+    if (filename) {
+      msg += " in file " + filename;
+    }
+    throw new Error(msg);
+  }
+
+  return ast;
+}
+
+function parseWithBabylon(text) {
+  // Inline the require to avoid loading all the JS if we don't use it
+  const babylon = require("babylon");
+
+  return babylon.parse(text, {
+    sourceType: "module",
+    allowImportExportEverywhere: false,
+    allowReturnOutsideFunction: false,
+    plugins: [
+      "jsx",
+      "flow",
+      "doExpressions",
+      "objectRestSpread",
+      "decorators",
+      "classProperties",
+      "exportExtensions",
+      "asyncGenerators",
+      "functionBind",
+      "functionSent",
+      "dynamicImport"
+    ]
+  });
+}
+
+module.exports = {
+  parseWithFlow,
+  parseWithBabylon
+};
