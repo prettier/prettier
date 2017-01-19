5affe76c7d61988c4c5cd8b1ab9fe71d1bdbdb33
Davy Duperron <yamafaktory@gmail.com>
2017-01-19 22:54:53 +0100

    Updated tests.

--- tests/prettier/object-prop-break-in-binary-exp.js
+++
@@ -1,7 +0,0 @@
-const foo = classnames({
-  "some-prop": this.state.longLongLongLongLongLongLongLongLongTooLongProp
-});
-
-const bar = classnames({
-  "some-prop": this.state.longLongLongLongLongLongLongLongLongTooLongProp === true
-});
