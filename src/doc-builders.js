b76fc894cd84e8f2d4799a83346a9e5570ca9db0
Christopher Chedeau <vjeux@fb.com>
2017-01-19 12:56:35 -0800

    Fix node 4
    It complained that it couldn't recognize const outside of `"use strict";`. It's a good idea to add this anyway.

--- src/doc-builders.js
+++ src/doc-builders.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const assert = require("assert");
 const utils = require("./doc-utils");
 const hasHardLine = utils.hasHardLine;
--- src/doc-utils.js
+++ src/doc-utils.js
@@ -1,3 +1,4 @@
+"use strict";
 
 function iterDoc(topDoc, func) {
   const docs = [ topDoc ];
