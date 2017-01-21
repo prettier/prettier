d4442357c013bfeadab0ca9a5b591254ceef465f
Simon Lydell <simon.lydell@gmail.com>
2017-01-21 18:20:47 +0100

    Warn if using deprecated API options

--- src/options.js
+++ src/options.js
@@ -20,6 +20,7 @@ function normalize(options) {
 
   // For backward compatibility. Deprecated in 0.0.10
   if ("useFlowParser" in normalized) {
+    console.warn('The `"useFlowParser": true/false` option is deprecated. Use `parser: "flow"` instead.');
     normalized.parser = normalized.useFlowParser ? "flow" : "babylon";
     delete normalized.useFlowParser;
   }
