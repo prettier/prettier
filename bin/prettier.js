08167360b5a3a93a5d5e3320b95d29e8c8788f26
Simon Lydell <simon.lydell@gmail.com>
2017-01-21 14:53:29 +0100

    Warn if using deprecated CLI options

--- bin/prettier.js
+++ bin/prettier.js
@@ -50,6 +50,7 @@ if (!filenames.length && !stdin) {
 function getParser() {
   // For backward compatibility. Deprecated in 0.0.10
   if (argv["flow-parser"]) {
+    console.warn("`--flow-parser` is deprecated. Use `--parser flow` instead.");
     return "flow";
   }
 
