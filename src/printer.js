1f92218a06767fb530cc174dcde3179a5128ff0f
Christopher Chedeau <vjeuxx@gmail.com>
2017-01-22 12:34:27 -0800

    Fix empty labels (#383)
    We actually need this `;` for EmptyStatement, otherwise it applies to the next block of code. (Creating a label with an empty statement is completely useless, but it triggers a lot in the fuzz testing tool)
    
    Fixes #376

--- src/printer.js
+++ src/printer.js
@@ -921,6 +921,13 @@ function genericPrintNoParens(path, options, print) {
 
       return concat(parts);
     case "LabeledStatement":
+      if (n.body.type === "EmptyStatement") {
+        return concat([
+          path.call(print, "label"),
+          ":;"
+        ]);
+      }
+
       return concat([
         path.call(print, "label"),
         ":",
