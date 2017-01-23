9751da8f79699cdbdae1b0a02ac052b9d37c34fb
Michał Pierzchała <thymikee@gmail.com>
2017-01-23 17:37:51 +0100

    Remove chalk reference

--- src/deprecated.js
+++ src/deprecated.js
@@ -1,17 +1,12 @@
 "use strict";
 
-const chalk = require('chalk'); // "chalk" comes with jest-validate
-const bold = chalk
-  ? chalk.bold
-  : value => value; // fallback when chalk is not available (e.g. for npm 2)
-
 const deprecated = {
   useFlowParser: config =>
-`  The ${bold("\"useFlowParser\"")} option is deprecated. Use ${bold("\"parser\"")} instead.
+`  The ${"\"useFlowParser\""} option is deprecated. Use ${"\"parser\""} instead.
 
   Prettier now treats your configuration as:
   {
-    ${bold("\"parser\"")}: ${bold(config.seFlowParser ? "\"flow\"" : "\"babylon\"")}
+    ${"\"parser\""}: ${config.seFlowParser ? "\"flow\"" : "\"babylon\""}
   }`
 };
 
