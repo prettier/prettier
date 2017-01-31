d0bc2991f637aa0c5257bc8fa3b895cd3cdbaa27
Christopher Chedeau <vjeuxx@gmail.com>
2017-01-31 12:32:42 -0800

    Reimplement MemberExpression printing (#469)
    There are currently three issues related to suboptimal rendering of MemberExpression chains. The previous implementation was trying to flatten only a single group at the same time, but it didn't work well because we didn't have the full context to be able to make decisions.
    
    In this implementation, I'm going through the entire chain at the same time and group it into logical units and make decisions based on this. It solves all the problems I can think of and if we need to tweak it in the future, it should be easy.
    
    Fixes #268
    Fixes #212
    Fixes #21

---
+++ tests/method-chain/multiple-members.js
@@ -0,0 +1,16 @@
+if (testConfig.ENABLE_ONLINE_TESTS === "true") {
+  describe("POST /users/me/pet", function() {
+    it("saves pet", function() {
+      function assert(pet) {
+        expect(pet).to.have.property("OwnerAddress").that.deep.equals({
+          AddressLine1: "Alexanderstrasse",
+          AddressLine2: "",
+          PostalCode: "10999",
+          Region: "Berlin",
+          City: "Berlin",
+          Country: "DE"
+        });
+      }
+    });
+  });
+}
