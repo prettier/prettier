8c66e44db15dd324d44dbbe8ef1f3bafa91825db
Davy Duperron <yamafaktory@gmail.com>
2017-02-04 22:16:24 +0100

    Fix use strict as expression statement (#602)
    * Add parentheses if node is StringLiteral surrounded by ExpressionStatement :tools:.
    
    * Add new test.
    
    * Update tests to run against Babylon, plus no regression case.
    
    * Split actual test and no regression test in two files.
    
    * Disable Flow till facebook/flow#3234 is merged.

---
+++ tests/expression_statement/jsfmt.spec.js
@@ -0,0 +1,2 @@
+// TODO: Re-enable Flow when the following fix is merged facebook/flow#3234.
+run_spec(__dirname, {parser: 'babylon'});
---
+++ tests/expression_statement/use_strict.js
@@ -0,0 +1,2 @@
+// Parentheses around expression statement should be preserved in this case.
+("use strict");
