7c3d77e4867e5defb3c5d6362dba597897d2dbf1
Christopher Chedeau <vjeux@fb.com>
2017-02-10 20:40:45 -0800

    Add option for putting > on the last line in jsx
    Feel free to suggest a better name for the option. Otherwise it works great :)
    
    Fixes #654

--- src/printer.js
+++ src/printer.js
@@ -1219,9 +1219,9 @@ function genericPrintNoParens(path, options, print) {
                 path.map(attr => concat([line, print(attr)]), "attributes")
               )
             ),
-            n.selfClosing ? line : softline
+            n.selfClosing ? line : (options.jsxBracketSameLine ? ">" : softline)
           ]),
-          n.selfClosing ? "/>" : ">"
+          n.selfClosing ? "/>" : (options.jsxBracketSameLine ? "" : ">")
         ])
       );
     }
