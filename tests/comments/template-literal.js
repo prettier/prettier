1c0339da9a3715f8487d4e7846fdea21f57a65ff
Davy Duperron <yamafaktory@gmail.com>
2017-02-14 19:52:24 +0100

    Update tests to match new implementation.

--- tests/comments/template-literal.js
+++ tests/comments/template-literal.js
@@ -1,6 +1,13 @@
 `
 ${// $FlowFixMe found when converting React.createClass to ES6
-ExampleStory.getFragment('story')
-// Another comment
+ExampleStory.getFragment("story")
 }
-`
+
+${// Another comment
+foo()
+}
+
+${// And another one
+Bar()
+}
+`;
