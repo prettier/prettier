2b8da48d64462a578ff38909eba54de8a2797c35
James Long <longster@gmail.com>
2017-02-17 22:39:40 -0500

    Add deprecation warning to API

--- src/options.js
+++ src/options.js
@@ -21,6 +21,17 @@ var exampleConfig = Object.assign({}, defaults, {
 
 // Copy options and fill in default values.
 function normalize(options) {
+  if(typeof options.trailingComma === "boolean") {
+    // Support a deprecated boolean type for the trailing comma config
+    // for a few versions. This code can be removed later.
+    options.trailingComma =  "es5";
+
+    console.warn(
+      "Warning: `trailingComma` without any argument is deprecated. " +
+      'Specify "none", "es5", or "all".'
+    );
+  }
+
   validate(options, { exampleConfig, deprecatedConfig });
   const normalized = Object.assign({}, options || {});
 
