a4f34bf530da3f2cd2f4ced3a09b9391e88c8e2f
Davy Duperron <yamafaktory@gmail.com>
2017-02-19 21:57:36 +0100

    Update tests.

--- tests/async/conditional-expression.js
+++ tests/async/conditional-expression.js
@@ -1,3 +1,9 @@
 async function f() {
   const result = typeof fn === 'function' ? await fn() : null;
 }
+
+(async function() {
+  console.log(
+    await (true ? Promise.resolve("A") : Promise.resolve("B"))
+  );
+})()
