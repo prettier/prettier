bbac75f3bbd19d9ba8ff1c7085610f5d776e8665
Davy Duperron <yamafaktory@gmail.com>
2017-02-25 12:38:46 +0100

    Update tests according.

--- tests/break-calls/break.js
+++ tests/break-calls/break.js
@@ -1,3 +1,20 @@
 h(f(g(() => {
   a
 })))
+
+deepCopyAndAsyncMapLeavesA(
+  { source: sourceValue, destination: destination[sourceKey] },
+  { valueMapper, overwriteExistingKeys }
+)
+
+deepCopyAndAsyncMapLeavesB(
+  1337,
+  { source: sourceValue, destination: destination[sourceKey] },
+  { valueMapper, overwriteExistingKeys }
+)
+
+deepCopyAndAsyncMapLeavesC(
+  { source: sourceValue, destination: destination[sourceKey] },
+  1337,
+  { valueMapper, overwriteExistingKeys }
+)
