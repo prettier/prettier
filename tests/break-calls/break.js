c1eea1dece6fae57352ff44ade8bce60cdd731fc
Davy Duperron <yamafaktory@gmail.com>
2017-02-25 18:35:15 +0100

    Add new tests for #682, #677, #253.

--- tests/break-calls/break.js
+++ tests/break-calls/break.js
@@ -18,3 +18,18 @@ deepCopyAndAsyncMapLeavesC(
   1337,
   { valueMapper, overwriteExistingKeys }
 )
+
+function someFunction(url) {
+  return get(url)
+    .then(
+      json => dispatch(success(json)),
+      error => dispatch(failed(error))
+    );
+}
+
+const mapChargeItems = fp.flow(
+  l => l < 10 ? l: 1,
+  l => Immutable.Range(l).toMap()
+);
+
+expect(new Range([0, 0], [0, 0])).toEqualAtomRange(new Range([0, 0], [0, 0]));
