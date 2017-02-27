edf2bde5904744da69b5e38ac8766366bbb504ca
Christopher Chedeau <vjeuxx@gmail.com>
2017-02-27 14:02:41 -0800

    Fix trailing commas in docs (#825)
    I also added support for parser dropdown and stop minifying the bundle so that the flow parser actually works.
    
    Fixes #816

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -2,7 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
-import babili from 'rollup-plugin-real-babili';
 import json from 'rollup-plugin-json';
 
 export default {
@@ -15,7 +14,6 @@ export default {
     commonjs(),
     globals(),
     builtins(),
-    babili({comments: false, sourceMap: false}),
   ],
   useStrict: false,
   moduleName: 'prettier',
