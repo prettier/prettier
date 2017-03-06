b91d6384a6db769789ecd332cdf3b26e1e4d59c2
Simon Lydell <simon.lydell@gmail.com>
2017-03-06 12:44:05 +0100

    Fix 0.5e0 (#911)
    * Don't generate invalid scientific notation
    
    Fixes #902.
    
    * Remove unnecessary scientific notation (1e0)

--- src/printer.js
+++ src/printer.js
@@ -2942,7 +2942,9 @@ function printNumber(rawNumber) {
   return rawNumber
     .toLowerCase()
     // Remove unnecessary plus and zeroes from scientific notation.
-    .replace(/^([\d.]+e)(?:\+|(-))?0*/, "$1$2")
+    .replace(/^([\d.]+e)(?:\+|(-))?0*(\d)/, "$1$2$3")
+    // Remove unnecessary scientific notation (1e0).
+    .replace(/^([\d.]+)e[+-]?0+$/, "$1")
     // Make sure numbers always start with a digit.
     .replace(/^\./, "0.")
     // Remove trailing dot.
