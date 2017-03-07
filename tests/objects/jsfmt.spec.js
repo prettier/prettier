4252d1f22638da2520cb0cbae257deed096e40c9
James Henry <james@henry.sc>
2017-03-07 21:47:59 +0000

    typescript parser is drop-in replacement for flow parser

--- tests/objects/jsfmt.spec.js
+++ tests/objects/jsfmt.spec.js
@@ -1,1 +1,3 @@
 run_spec(__dirname, {parser: 'babylon'});
+run_spec(__dirname, {parser: 'typescript'});
+
--- tests/trailing_comma/jsfmt.spec.js
+++ tests/trailing_comma/jsfmt.spec.js
@@ -1,3 +1,7 @@
-run_spec(__dirname);
-run_spec(__dirname, { trailingComma: "all" });
-run_spec(__dirname, { trailingComma: "es5" });
+// run_spec(__dirname);
+// run_spec(__dirname, { trailingComma: "all" });
+// run_spec(__dirname, { trailingComma: "es5" });
+// 
+run_spec(__dirname, {parser: 'typescript'});
+run_spec(__dirname, { parser: 'typescript', trailingComma: "all" });
+run_spec(__dirname, { parser: 'typescript', trailingComma: "es5" });
