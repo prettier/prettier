d871c1ecb5a81e5f82409bebc94f3d4ac76c6054
James Henry <james@henry.sc>
2017-03-08 20:19:33 +0000

    Remove annotated issues

--- tests/objects/jsfmt.spec.js
+++ tests/objects/jsfmt.spec.js
@@ -1,8 +1,1 @@
 run_spec(__dirname, {parser: 'babylon'});
-
-/**
- * TS currently broken
- * 
- * - expression.js potential future JS bind syntax (::) was added which is not supported by TypeScript
- */
-// run_spec(__dirname, {parser: 'typescript'});
