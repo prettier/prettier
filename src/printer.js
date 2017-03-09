7ce54cf5c03e81ce7418a6b81ea661b729e689f1
Christopher Chedeau <vjeux@fb.com>
2017-03-09 08:25:46 -0800

    Only allow same-line arrow-less body for explicit nodes
    In practice, trying to allow calls to be inlined is causing a lot of code to look very weird and unstable as seen by the four issues this is fixing. It also requires us to add a conditional group and do hackery around it.
    
    Fixes #959
    Fixes #760
    Fixes #615
    Fixes #625

--- src/printer.js
+++ src/printer.js
@@ -341,25 +341,18 @@ function genericPrintNoParens(path, options, print) {
         n.body.type === "ArrayExpression" ||
         n.body.type === "ObjectExpression" ||
         n.body.type === "JSXElement" ||
-        n.body.type === "BlockStatement"
+        n.body.type === "BlockStatement" ||
+        n.body.type === "TaggedTemplateExpression" ||
+        n.body.type === "TemplateElement"
       ) {
         return group(collapsed);
       }
 
-      // These nested groups are a little wonky, but because
-      // `conditionalGroup` suppresses break propagation, we want to
-      // re-propagate it. We still want to allow the printer to choose
-      // the more collapsed version, but still break parents if there
-      // are any hard breaks in the content.
       return group(
-        conditionalGroup([
-          collapsed,
-          concat([
-            concat(parts),
-            group(indent(options.tabWidth, concat([line, body])))
-          ])
-        ]),
-        { shouldBreak: willBreak(body) }
+        concat([
+          concat(parts),
+          group(indent(options.tabWidth, concat([line, body])))
+        ])
       );
     case "MethodDefinition":
       if (n.static) {
