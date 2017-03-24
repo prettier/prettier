b5a754a036fe9d4da002545c109650ef4f59b34e
Christian Hersevoort <christian.hersevoort@gmail.com>
2017-03-24 21:29:22 +0100

    Formatted run_spec.js with prettier

--- tests_config/run_spec.js
+++ tests_config/run_spec.js
@@ -1,12 +1,14 @@
 "use strict";
-const fs = require('fs');
+const fs = require("fs");
 const prettier = require("../");
 const types = require("ast-types");
-const parser = require('../src/parser');
+const parser = require("../src/parser");
 
 const RUN_AST_TESTS = process.env["AST_COMPARE"];
 const VERIFY_ALL_PARSERS = process.env["VERIFY_ALL_PARSERS"] || false;
-const ALL_PASERS = process.env["ALL_PASERS"] ? JSON.parse(process.env["ALL_PASERS"]) : ["flow", "babylon", "typescript"];
+const ALL_PASERS = process.env["ALL_PASERS"]
+  ? JSON.parse(process.env["ALL_PASERS"])
+  : ["flow", "babylon", "typescript"];
 
 // Ignoring empty statements that are added into the output removes a
 // lot of noise from test failures and let's us focus on the real
@@ -22,48 +24,56 @@ function removeEmptyStatements(ast) {
 
 function run_spec(dirname, options = {}, additionalParsers = []) {
   fs.readdirSync(dirname).forEach(filename => {
-    if ((filename.endsWith('.js') || filename.endsWith('.ts')) && filename !== 'jsfmt.spec.js') {
-      const path = dirname + '/' + filename;
+    if (
+      (filename.endsWith(".js") || filename.endsWith(".ts")) &&
+      filename !== "jsfmt.spec.js"
+    ) {
+      const path = dirname + "/" + filename;
 
       if (!RUN_AST_TESTS) {
-        const source = read(path).replace(/\r\n/g, '\n');
+        const source = read(path).replace(/\r\n/g, "\n");
         const mergedOptions = mergeDefaultOptions(options);
         const output = prettyprint(source, path, mergedOptions);
         test(`${mergedOptions.parser} - ${parser.parser}-verify`, () => {
-          expect(source + '~'.repeat(80) + '\n' + output).toMatchSnapshot(filename);
+          expect(source + "~".repeat(80) + "\n" + output).toMatchSnapshot(
+            filename
+          );
         });
 
-        getParsersToVerify(mergedOptions.parser, additionalParsers).forEach(parserName => {
-          test(`${filename} - ${parserName}-verify` , () => {
-            const verifyOptions = Object.assign(mergedOptions, { parser: parserName });
+        getParsersToVerify(
+          mergedOptions.parser,
+          additionalParsers
+        ).forEach(parserName => {
+          test(`${filename} - ${parserName}-verify`, () => {
+            const verifyOptions = Object.assign(mergedOptions, {
+              parser: parserName
+            });
             const verifyOutput = prettyprint(source, path, verifyOptions);
-            expect(output).toEqual(verifyOutput)
+            expect(output).toEqual(verifyOutput);
           });
         });
       }
 
       if (RUN_AST_TESTS) {
-        const source = read(dirname + '/' + filename);
+        const source = read(dirname + "/" + filename);
         const ast = removeEmptyStatements(parse(source));
         let prettyprinted = false;
         let ppast;
         let pperr = null;
         try {
           ppast = removeEmptyStatements(parse(prettyprint(source, path)));
-        }
-        catch(e) {
+        } catch (e) {
           pperr = e.stack;
         }
 
-        test(path + ' parse', () => {
+        test(path + " parse", () => {
           expect(pperr).toBe(null);
           expect(ppast).toBeDefined();
-          if(ast.errors.length === 0) {
+          if (ast.errors.length === 0) {
             expect(ast).toEqual(ppast);
           }
         });
       }
-
     }
   });
 }
@@ -73,10 +83,12 @@ function stripLocation(ast) {
   if (Array.isArray(ast)) {
     return ast.map(e => stripLocation(e));
   }
-  if (typeof ast === 'object') {
+  if (typeof ast === "object") {
     const newObj = {};
     for (var key in ast) {
-      if (key === 'loc' || key === 'range' || key === 'raw' || key === 'comments') {
+      if (
+        key === "loc" || key === "range" || key === "raw" || key === "comments"
+      ) {
         continue;
       }
       newObj[key] = stripLocation(ast[key]);
@@ -91,20 +103,29 @@ function parse(string) {
 }
 
 function prettyprint(src, filename, options) {
-  return prettier.format(src, Object.assign({
-    filename,
-  }, options));
+  return prettier.format(
+    src,
+    Object.assign(
+      {
+        filename
+      },
+      options
+    )
+  );
 }
 
 function read(filename) {
-  return fs.readFileSync(filename, 'utf8');
+  return fs.readFileSync(filename, "utf8");
 }
 
 function mergeDefaultOptions(parserConfig) {
-  return Object.assign({
-    parser: 'flow',
-    printWidth: 80,
-  }, parserConfig);
+  return Object.assign(
+    {
+      parser: "flow",
+      printWidth: 80
+    },
+    parserConfig
+  );
 }
 
 function getParsersToVerify(parser, additionalParsers) {
