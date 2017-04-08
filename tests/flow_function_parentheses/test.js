b6083d72d9919dcbe59cde61d1a52ba64b7f7a99
Simon Lydell <simon.lydell@gmail.com>
2017-04-08 13:57:53 +0200

    Extract custom tests from tests/flow/
    Approach:
    
    1. Remove all .js files in tests/flow except .snap.js files.
    2. Copy over all .js files from tests/ in the flow repo.
    3. Go through the diff looking for deletions.
       - It was easy to see which deletions were due to changes in the tests
         due to updates in the flow repo.
       - For the rest of the deletions, I used `git blame` to verify that
         they had been added by us since the flow tests were copied over.
    
    This makes tests/flow/ simply a copy of the tests from the flow repo,
    making it easier to sync with the upstream flow tests in the future.

---
+++ tests/flow_function_parentheses/test.js
@@ -0,0 +1,31 @@
+type Banana = {
+  eat: string => boolean,
+};
+
+type Hex = {n: 0x01};
+
+type T = { method: (a) => void };
+
+type T = { method(a): void };
+
+declare class X { method(a): void }
+
+declare function f(a): void;
+
+var f: (a) => void;
+
+interface F { m(string): number }
+
+interface F { m: (string) => number }
+
+function f(o: { f: (string) => void }) {}
+
+function f(o: { f(string): void }) {}
+
+type f = (...arg) => void;
+
+type f = (/* comment */ arg) => void;
+
+type f = (arg /* comment */) => void;
+
+type f = (?arg) => void;
