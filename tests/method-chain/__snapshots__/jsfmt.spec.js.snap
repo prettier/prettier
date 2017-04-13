prettier/prettier

commit 523f64ff14bb0714c95195d23458147b39882dd1

Add ability to break for top member expression (#1036)
It turns out that the top member expression doesn't go through the member chain logic. Let's give it the ability to break for now.

Fixes #1031

src/printer.js
@@ -232,9 +232,22 @@ function genericPrintNoParens(path, options, print, args) {
         path.call(print, "right")
       ]);
     case "MemberExpression": {
+      const parent = path.getParentNode();
+      const shouldInline =
+        n.computed ||
+        (n.object.type === "Identifier" &&
+          n.property.type === "Identifier" &&
+          parent.type !== "MemberExpression");
+
       return concat([
         path.call(print, "object"),
-        printMemberLookup(path, options, print)
+        shouldInline
+          ? printMemberLookup(path, options, print)
+          : group(
+              indent(
+                concat([softline, printMemberLookup(path, options, print)])
+              )
+            )
       ]);
     }
     case "MetaProperty":

tests/method-chain/__snapshots__/jsfmt.spec.js.snap
@@ -119,6 +119,32 @@ it(\\"should group messages with same created time\\", () => {
 "
 `;
 
+exports[`break-last-member.js 1`] = `
+"SomeVeryLongUpperCaseConstant.someVeryLongCallExpression().some_very_long_member_expression
+weNeedToReachTheEightyCharacterLimitXXXXXXXXXXXXXXXXX.someNode
+  .childrenInAnArray[0];
+superSupersuperSupersuperSupersuperSupersuperSuperLong.exampleOfOrderOfGetterAndSetterReordered;
+superSupersuperSupersuperSupersuperSupersuperSuperLong.exampleOfOrderOfGetterAndSetterReordered[0];
+
+expect(
+  findDOMNode(component.instance()).getElementsByClassName(styles.inner)[0].style.paddingRight
+).toBe('1000px');
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SomeVeryLongUpperCaseConstant.someVeryLongCallExpression()
+  .some_very_long_member_expression;
+weNeedToReachTheEightyCharacterLimitXXXXXXXXXXXXXXXXX.someNode
+  .childrenInAnArray[0];
+superSupersuperSupersuperSupersuperSupersuperSuperLong.exampleOfOrderOfGetterAndSetterReordered;
+superSupersuperSupersuperSupersuperSupersuperSuperLong
+  .exampleOfOrderOfGetterAndSetterReordered[0];
+
+expect(
+  findDOMNode(component.instance()).getElementsByClassName(styles.inner)[0]
+    .style.paddingRight
+).toBe(\\"1000px\\");
+"
+`;
+
 exports[`comment.js 1`] = `
 "function f() {
   return observableFromSubscribeFunction()

tests/method-chain/break-last-member.js
@@ -1,0 +1,9 @@
+SomeVeryLongUpperCaseConstant.someVeryLongCallExpression().some_very_long_member_expression
+weNeedToReachTheEightyCharacterLimitXXXXXXXXXXXXXXXXX.someNode
+  .childrenInAnArray[0];
+superSupersuperSupersuperSupersuperSupersuperSuperLong.exampleOfOrderOfGetterAndSetterReordered;
+superSupersuperSupersuperSupersuperSupersuperSuperLong.exampleOfOrderOfGetterAndSetterReordered[0];
+
+expect(
+  findDOMNode(component.instance()).getElementsByClassName(styles.inner)[0].style.paddingRight
+).toBe('1000px');

tests/flow/declare_export/__snapshots__/jsfmt.spec.js.snap
@@ -1117,7 +1117,8 @@ var numberValue6 = require(\\"ES6_ExportFrom_Intermediary2\\").numberValue1;
 var ap1: number = numberValue6;
 var ap2: string = numberValue6; // Error: number ~> string
 
-var numberValue2_renamed2 = require(\\"ES6_ExportFrom_Intermediary2\\").numberValue2_renamed2;
+var numberValue2_renamed2 = require(\\"ES6_ExportFrom_Intermediary2\\")
+  .numberValue2_renamed2;
 var aq1: number = numberValue2_renamed2;
 var aq2: string = numberValue2_renamed2; // Error: number ~> string
 

tests/flow/es6modules/__snapshots__/jsfmt.spec.js.snap
@@ -1246,7 +1246,8 @@ var numberValue6 = require(\\"ES6_ExportFrom_Intermediary2\\").numberValue1;
 var ap1: number = numberValue6;
 var ap2: string = numberValue6; // Error: number ~> string
 
-var numberValue2_renamed2 = require(\\"ES6_ExportFrom_Intermediary2\\").numberValue2_renamed2;
+var numberValue2_renamed2 = require(\\"ES6_ExportFrom_Intermediary2\\")
+  .numberValue2_renamed2;
 var aq1: number = numberValue2_renamed2;
 var aq2: string = numberValue2_renamed2; // Error: number ~> string
 

commit ba36e3190c4d5c02c9faf289987f2912ca5ebba0

Do not inline member expressions as part of assignments
This is a regression from #1036 and #1188.

Fixes #1241
Fixes #1236

src/printer.js
@@ -185,6 +185,7 @@ function genericPrintNoParens(path, options, print, args) {
       return concat(["(", path.call(print, "expression"), ")"]);
     case "AssignmentExpression":
       return printAssignment(
+        n.left,
         path.call(print, "left"),
         n.operator,
         n.right,
@@ -232,7 +233,22 @@ function genericPrintNoParens(path, options, print, args) {
       ]);
     case "MemberExpression": {
       const parent = path.getParentNode();
+      let firstNonMemberParent;
+      let i = 0;
+      do {
+        firstNonMemberParent = path.getParentNode(i);
+        i++;
+      } while (
+        firstNonMemberParent &&
+        firstNonMemberParent.type === "MemberExpression"
+      );
+
       const shouldInline =
+        firstNonMemberParent && (
+          (firstNonMemberParent.type === "VariableDeclarator" &&
+            firstNonMemberParent.id.type === "ObjectPattern") ||
+          (firstNonMemberParent.type === "AssignmentExpression" &&
+            firstNonMemberParent.left.type === "ObjectPattern")) ||
         n.computed ||
         (n.object.type === "Identifier" &&
           n.property.type === "Identifier" &&
@@ -964,6 +980,7 @@ function genericPrintNoParens(path, options, print, args) {
       return group(concat(parts));
     case "VariableDeclarator":
       return printAssignment(
+        n.id,
         path.call(print, "id"),
         "=",
         n.init,
@@ -3161,6 +3178,7 @@ function printBinaryishExpressions(path, print, options, isNested) {
 }
 
 function printAssignment(
+  leftNode,
   printedLeft,
   operator,
   rightNode,
@@ -3176,9 +3194,10 @@ function printAssignment(
     printed = indent(concat([hardline, printedRight]));
   } else if (
     (isBinaryish(rightNode) && !shouldInlineLogicalExpression(rightNode)) ||
-    rightNode.type === "StringLiteral" ||
-    (rightNode.type === "Literal" && typeof rightNode.value === "string") ||
-    isMemberExpressionChain(rightNode)
+    leftNode.type !== "ObjectPattern" && (
+      rightNode.type === "StringLiteral" ||
+      (rightNode.type === "Literal" && typeof rightNode.value === "string") ||
+      isMemberExpressionChain(rightNode))
   ) {
     printed = indent(concat([line, printedRight]));
   } else {

tests/method-chain/__snapshots__/jsfmt.spec.js.snap
@@ -129,6 +129,17 @@ superSupersuperSupersuperSupersuperSupersuperSuperLong.exampleOfOrderOfGetterAnd
 expect(
   findDOMNode(component.instance()).getElementsByClassName(styles.inner)[0].style.paddingRight
 ).toBe('1000px');
+
+const { course, conflicts = [], index, scheduleId, studentId, something } = a.this.props;
+
+const { course, conflicts = [], index, scheduleId, studentId, something } = this.props;
+
+const {
+  updated,
+  author: { identifier: ownerId },
+  location,
+  category: categories,
+} = rawAd.entry;
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SomeVeryLongUpperCaseConstant.someVeryLongCallExpression()
   .some_very_long_member_expression;
@@ -142,6 +153,31 @@ expect(
   findDOMNode(component.instance()).getElementsByClassName(styles.inner)[0]
     .style.paddingRight
 ).toBe("1000px");
+
+const {
+  course,
+  conflicts = [],
+  index,
+  scheduleId,
+  studentId,
+  something
+} = a.this.props;
+
+const {
+  course,
+  conflicts = [],
+  index,
+  scheduleId,
+  studentId,
+  something
+} = this.props;
+
+const {
+  updated,
+  author: { identifier: ownerId },
+  location,
+  category: categories
+} = rawAd.entry;
 
 `;
 

tests/method-chain/break-last-member.js
@@ -7,3 +7,14 @@ superSupersuperSupersuperSupersuperSupersuperSuperLong.exampleOfOrderOfGetterAnd
 expect(
   findDOMNode(component.instance()).getElementsByClassName(styles.inner)[0].style.paddingRight
 ).toBe('1000px');
+
+const { course, conflicts = [], index, scheduleId, studentId, something } = a.this.props;
+
+const { course, conflicts = [], index, scheduleId, studentId, something } = this.props;
+
+const {
+  updated,
+  author: { identifier: ownerId },
+  location,
+  category: categories,
+} = rawAd.entry;