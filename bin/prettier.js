e65ac11dfb984742bfff7d89a7e4cfa3a2173c72
Pi Lanningham <pi.lanningham@gmail.com>
2017-04-17 21:57:07 -0400

    Add typescript as a valid parser value
    Now that typescript development has started, each contributor has been adding this as a supported flag themselves.
    
    This just adds it as an allowed option, with a warning that support for this parser is experimental.
    
    I also tried to structure this a bit future proof, in case other experimental parsers come along.

--- bin/prettier.js
+++ bin/prettier.js
@@ -66,19 +66,27 @@ function getParserOption() {
     return "flow";
   }
 
-  if (value === "flow" || value === "babylon") {
+  const defaultParser = "babylon";
+  const supportedParsers = ["babylon", "flow"];
+  const experimentalParsers = ["typescript"];
+  if (supportedParsers.indexOf(value) >= 0) {
+    return value;
+  }
+  if(experimentalParsers.indexOf(value) >= 0) {
+    console.warn("WARNING: " + value + " parser is experimental!");
     return value;
   }
 
+  const allParsers = supportedParsers.concat(experimentalParsers);
   console.warn(
     "Ignoring unknown --" +
       optionName +
-      ' value, falling back to "babylon":\n' +
-      '  Expected "flow" or "babylon", but received: ' +
+      ' value, falling back to "' + defaultParser + '":\n' +
+      '  Expected one of [' + allParsers.join(", ") + '] but received: ' +
       JSON.stringify(value)
   );
 
-  return "babylon";
+  return defaultParser;
 }
 
 function getIntOption(optionName) {
