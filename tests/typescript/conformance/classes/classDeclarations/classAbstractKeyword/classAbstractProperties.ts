9482515904208e1ea70199af20ae938a0267a536
Lucas Azzola <derflatulator@gmail.com>
2017-04-26 14:49:43 +1000

    test(typescript): remove unimplemented abstract properties from tests

--- tests/typescript/conformance/classes/classDeclarations/classAbstractKeyword/classAbstractProperties.ts
+++ tests/typescript/conformance/classes/classDeclarations/classAbstractKeyword/classAbstractProperties.ts
@@ -1,11 +1,4 @@
-abstract class A {
-    abstract x : number;
-    public abstract y : number;
-    protected abstract z : number;
-    private abstract w : number;
-    
-    abstract m: () => void; 
-    
+abstract class A {    
     abstract foo_x() : number;
     public abstract foo_y() : number;
     protected abstract foo_z() : number;
