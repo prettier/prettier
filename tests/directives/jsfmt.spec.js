2aba6231271f6985a395c31e3df9323e8f3da115
Joseph Frazier <1212jtraceur@gmail.com>
2017-05-09 03:05:47 -0400

    WIP Disable Flow/Typescript for ./tests/directives
    We don't yet handle escaped directives for them, but Babylon works.
    
    (similar to https://github.com/prettier/prettier/pull/602/commits/90bf93713c78a6a6b3f55e52d7be172ece9b56df#diff-0de18284f37da79ab8af4e4690919abaR1)

--- tests/directives/jsfmt.spec.js
+++ tests/directives/jsfmt.spec.js
@@ -1,1 +1,3 @@
-run_spec(__dirname, null, ["typescript", "babylon"]);
+// TODO: Re-enable Flow/TypeScript when they work with ./escaped.js
+// run_spec(__dirname, null, ["typescript", "babylon"]);
+run_spec(__dirname, {parser: "babylon"});
