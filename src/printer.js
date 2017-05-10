7151246faecf26ebabd30455f9d271c2e93f3107
Joseph Frazier <1212jtraceur@gmail.com>
2017-05-10 10:56:23 -0400

    Fix test for unnecessarily escaped character at not-beginning of string

--- src/printer.js
+++ src/printer.js
@@ -3861,7 +3861,7 @@ function makeString(rawContent, enclosingQuote) {
 
   // Matches any unnecessarily escaped character.
   // Adapted from https://github.com/eslint/eslint/blob/de0b4ad7bd820ade41b1f606008bea68683dc11a/lib/rules/no-useless-escape.js#L27
-  const regexUnnecessaryStringEscapes = /(?:^|[^\\])\\([^\\nrvtbfux\r\n\u2028\u2029"'0])/;
+  const regexUnnecessaryStringEscapes = /(^|[^\\])\\([^\\nrvtbfux\r\n\u2028\u2029"'0])/;
 
   // Escape and unescape single and double quotes as needed to be able to
   // enclose `rawContent` with `enclosingQuote`.
@@ -3884,7 +3884,7 @@ function makeString(rawContent, enclosingQuote) {
     return match;
   })
   // Unescape unnecessarily escaped characters.
-  .replace(regexUnnecessaryStringEscapes, (match, escaped) => escaped);
+  .replace(regexUnnecessaryStringEscapes, (match, prev, escaped) => prev + escaped);
 
   return enclosingQuote + newContent + enclosingQuote;
 }
