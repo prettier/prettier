7e940f43b62fb34d71fc3015ed14f9c59cf61d1a
Joseph Frazier <1212jtraceur@gmail.com>
2017-05-12 23:43:19 -0400

    Extract makeBlockComment() helper

--- src/comments.js
+++ src/comments.js
@@ -520,14 +520,18 @@ function handleTemplateLiteralComments(enclosingNode, comment) {
       comment
     );
     // Enforce all comments to be leading block comments.
-    if (!util.isBlockComment(comment)) {
-      comment.value += ' '
-    }
-    comment.type = "CommentBlock";
+    makeBlockComment(comment);
     addTrailingComment(enclosingNode.expressions[expressionIndex], comment);
     return true;
   }
   return false;
+}
+
+function makeBlockComment(comment) {
+  if (!util.isBlockComment(comment)) {
+    comment.value += " ";
+  }
+  comment.type = "CommentBlock";
 }
 
 function handleCommentInEmptyParens(text, enclosingNode, comment) {
