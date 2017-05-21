a823ef52a98e3d99d65e27dd668a3f9978b58989
Joseph Frazier <1212jtraceur@gmail.com>
2017-05-21 16:14:46 -0400

    Test extending formatted range to entire node

--- tests/range/range.js
+++ tests/range/range.js
@@ -3,7 +3,7 @@ function ugly ( {a=1,     b     =   2     }      ) {
     function ugly ( {a=1,     b     =   2     }      ) {
              `multiline template string
               with too much indentation`
-      // The [165, 246) range selects the above two lines, including the second newline
+      // The [224, 227) range selects "too" above
     }
   }
 }
