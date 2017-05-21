01152dcd509369ffacfc2c9c3b498a261e3d3b37
Joseph Frazier <1212jtraceur@gmail.com>
2017-05-21 19:53:31 -0400

    Test that external indentation is left alone when formatting a range

---
+++ tests/range/ignore-indentation.js
@@ -0,0 +1,9 @@
+function ugly ( {a=1,     b     =   2     }      ) {
+  function ugly ( {a=1,     b     =   2     }      ) {
+    function ugly ( {a=1,     b     =   2     }      ) {
+  	  	     `multiline template string
+              with too much indentation`
+      // The [222, 225) range selects "too" above
+    }
+  }
+}
--- tests/range/jsfmt.spec.js
+++ tests/range/jsfmt.spec.js
@@ -1,2 +1,6 @@
 run_file(__dirname, "range.js", { rangeStart: 224, rangeEnd: 227 });
 run_file(__dirname, "try-catch.js", { rangeStart: 7, rangeEnd: 8 });
+run_file(__dirname, "ignore-indentation.js", {
+  rangeStart: 222,
+  rangeEnd: 225
+});
