aeed7840e2e6f8975cc820780ffecc6fcad08728
Christopher Chedeau <vjeuxx@gmail.com>
2017-05-22 06:40:19 -0700

    Do not inline arrow when argument has a leading comment (#1660)
    Fixes #1614

---
+++ tests/arrows/comment.js
@@ -0,0 +1,23 @@
+/**
+ * Curried function that ends with a BEM CSS Selector
+ *
+ * @param {String} block - the BEM Block you'd like to select.
+ * @returns {Function}
+ */
+export const bem = block =>
+  /**
+   * @param {String} [element] - the BEM Element within that block; if undefined, selects the block itself.
+   * @returns {Function}
+   */
+  element =>
+    /**
+     * @param {?String} [modifier] - the BEM Modifier for the Block or Element; if undefined, selects the Block or Element unmodified.
+     * @returns {String}
+     */
+    modifier =>
+      [
+        ".",
+        css(block),
+        element ? `__${css(element)}` : "",
+        modifier ? `--${css(modifier)}` : ""
+      ].join("");
