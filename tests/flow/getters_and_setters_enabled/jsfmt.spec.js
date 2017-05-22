d608b7ba28b42671b67c6935c68139b0bd697dc4
Brian Ng <bng412@gmail.com>
2017-05-22 17:31:53 -0500

    Bump Babylon and enable additional tests (#1668)

--- tests/flow/getters_and_setters_enabled/jsfmt.spec.js
+++ tests/flow/getters_and_setters_enabled/jsfmt.spec.js
@@ -1,5 +1,1 @@
-run_spec(__dirname);
-
-// TODO: These tests used to be run in babylon as well, but currently babylon
-// fails to parse them.
-// run_spec(__dirname, null, ["babylon"]);
+run_spec(__dirname, null, ["babylon"]);
