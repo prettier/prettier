c0b38333154a40061678ae6229c5d501867a64ff
Joseph Frazier <1212jtraceur@gmail.com>
2017-05-22 15:31:41 -0400

    Remove unnecessary comments from range fixtures

--- tests/range/function-declaration.js
+++ tests/range/function-declaration.js
@@ -1,2 +1,1 @@
 function ugly ( <<<PRETTIER_RANGE_START>>>{a=1,     b     =   2     }<<<PRETTIER_RANGE_END>>>      ) {}
-// the range [16, 43) selects the argument object above
--- tests/range/range.js
+++ tests/range/range.js
@@ -3,7 +3,6 @@ function ugly ( {a=1,     b     =   2     }      ) {
     function ugly ( {a=1,     b     =   2     }      ) {
              `multiline template string
               with <<<PRETTIER_RANGE_START>>>too<<<PRETTIER_RANGE_END>>> much indentation`
-      // The [224, 227) range selects "too" above
     }
   }
 }
