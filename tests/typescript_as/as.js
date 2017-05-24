7cd0b78fe7422395e81568b8ca7860dd6ee91339
Christopher Chedeau <vjeuxx@gmail.com>
2017-05-23 18:36:14 -0700

    Properly add parenthesis for as inside of binary expression (#1698)
    The binary logic doesn't work if the node isn't a binary or logical expression

--- tests/typescript_as/as.js
+++ tests/typescript_as/as.js
@@ -1,3 +1,6 @@
 const name = (description as DescriptionObject).name || (description as string);
 this.isTabActionBar((e.target || e.srcElement) as HTMLElement);
 (originalError ? wrappedError(errMsg, originalError) : Error(errMsg)) as InjectionError;
+'current' in (props.pagination as Object)
+start + (yearSelectTotal as number)
+scrollTop > (visibilityHeight as number)
