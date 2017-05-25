fe49650700e95d4df9b5cf29c780ed00ba934bf0
Christopher Chedeau <vjeuxx@gmail.com>
2017-05-25 16:26:40 -0700

    Inline | null and | void (#1734)
    TypeScript doesn't have the concept of `?` for nullable options and instead you have to write `| null` and `| void`. This is annoying to have it use the long form, so we're now inlining them.
    
    While working on this, I found out a few issues with the way we deal with those:
    - We only align objects if the parent is a union. This means that if you have `Array<{ object }>`, the object is not aligned properly. The fix is to move the alignment logic to the union, and not the child.
    - When doing so, it messes up with the comment alignment, so we have to manually handle children comment printing in the union code.
    
    It doesn't yet fix #1727 because the hardcoded type names are different, i'll follow up in a PR.

---
+++ tests/flow_union/union.js
@@ -0,0 +1,11 @@
+interface RelayProps {
+  articles: Array<{
+    __id: string,
+  } | null> | null | void | 1,
+}
+
+interface RelayProps {
+  articles: Array<{
+    __id: string,
+  } | null> | null | void,
+}
