b11b89d2af9f2797c0f641881423ac530ee87fbf
Christopher Chedeau <vjeuxx@gmail.com>
2017-05-26 13:45:15 -0700

    Inline url() even if unquoted (#1752)

--- tests/css_inline_url/inline_url.js
+++ tests/css_inline_url/inline_url.js
@@ -1,3 +1,4 @@
 .breadItem {
   background-image: url('/images/product/simple_product_manager/breadcrumb/chevron_right.png');
+  background-image: url(/images/product/simple_product_manager/breadcrumb/chevron_right.png);
 }
