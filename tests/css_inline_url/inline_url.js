7f5031f5c37031a1ad680daf805e7f3c145c8bb8
Christopher Chedeau <vjeux@fb.com>
2017-05-27 14:08:32 -0700

    Only inline urls with a single argument

--- tests/css_inline_url/inline_url.js
+++
@@ -1,4 +0,0 @@
-.breadItem {
-  background-image: url('/images/product/simple_product_manager/breadcrumb/chevron_right.png');
-  background-image: url(/images/product/simple_product_manager/breadcrumb/chevron_right.png);
-}
