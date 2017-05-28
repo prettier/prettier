afa8bd6fce77445f1e73dcc45818be1b2e99201d
Lucas Azzola <derflatulator@gmail.com>
2017-05-28 10:21:03 +1000

    test(typescript): enable typescript-verify on most tests

--- tests/expression_statement/jsfmt.spec.js
+++ tests/expression_statement/jsfmt.spec.js
@@ -1,2 +1,1 @@
-// TODO: Re-enable Flow when the following fix is merged facebook/flow#3234.
-run_spec(__dirname, { parser: "babylon" });
+run_spec(__dirname, null, ["babylon", "typescript"]);
--- tests/tabWith/jsfmt.spec.js
+++ tests/tabWith/jsfmt.spec.js
@@ -1,2 +1,2 @@
-run_spec(__dirname);
-run_spec(__dirname, { tabWidth: 4 });
+run_spec(__dirname, null, ["typescript"]);
+run_spec(__dirname, { tabWidth: 4 }, ["typescript"]);
