2a58e6bc06dbdde64ba026c96feb0dd680a75879
Lucas Azzola <derflatulator@gmail.com>
2017-05-28 10:57:07 +1000

    test(typescript): enable typescript-verify on most tests (#1781)

--- tests/arrow-call/jsfmt.spec.js
+++ tests/arrow-call/jsfmt.spec.js
@@ -1,2 +1,2 @@
-run_spec(__dirname);
-run_spec(__dirname, { trailingComma: "all" });
+run_spec(__dirname, null, ["typescript"]);
+run_spec(__dirname, { trailingComma: "all" }, ["typescript"]);
--- tests/es6modules/jsfmt.spec.js
+++ tests/es6modules/jsfmt.spec.js
@@ -1,3 +1,3 @@
 run_spec(__dirname);
 // FIXME export_default_function_declaration_async.js flow != babylon output
-run_spec(__dirname, { parser: "babylon" });
+run_spec(__dirname, { parser: "babylon" }, ["typescript"]);
