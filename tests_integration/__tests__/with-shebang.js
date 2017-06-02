43b75267e66b51c76590bb24263d10400180f01d
Joseph Frazier <1212jtraceur@gmail.com>
2017-06-02 15:48:17 -0400

    Add regression test for CLI shebang preservation
    Following up on https://github.com/prettier/prettier/pull/1899

---
+++ tests_integration/__tests__/with-shebang.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const runPrettier = require("../runPrettier");
+
+test("preserves shebang", () => {
+  const result = runPrettier("cli/with-shebang", ["issue1890.js"]);
+
+  expect(result.stdout).toMatchSnapshot();
+});
