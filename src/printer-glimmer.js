d70e0c04498c9ba30537af876bd9f2b68154565e
Lucas Azzola <derflatulator@gmail.com>
2017-06-04 17:33:18 +1000

    feat(glimmer): implement all AST types

--- src/printer-glimmer.js
+++ src/printer-glimmer.js
@@ -10,6 +10,8 @@ const hardline = docBuilders.softline;
 // const group = docBuilders.group;
 const indent = docBuilders.indent;
 
+// https://github.com/glimmerjs/glimmer-vm/blob/master/packages/%40glimmer/syntax/lib/generation/print.ts
+
 function genericPrint(path, options, print) {
   const n = path.getValue();
 
@@ -25,57 +27,52 @@ function genericPrint(path, options, print) {
     case "Program": {
       return join(softline, path.map(print, "body"));
     }
-    case "AttrNode": {
-      return concat([n.name, '="', path.call(print, "value"), '"']);
-    }
-    case "BlockStatement": {
-      return concat([
-        "{{#",
-        path.call(print, "path"),
-        " ",
-        join(" ", path.map(print, "params")),
-        n.hash.pairs.length ? " " : "",
-        path.call(print, "hash"),
-        n.program.blockParams && n.program.blockParams.length
-          ? concat([" as |", n.program.blockParams.join(" "), "|"])
-          : "",
-        "}}",
-        indent(concat([hardline, path.call(print, "program")])),
-        softline,
-        "{{/",
-        path.call(print, "path"),
-        "}}"
-      ]);
-    }
-    case "ConcatStatement": {
-      return concat(path.map(print, "parts"));
-    }
-    case "CommentStatement": {
-      return concat(["<!--", n.value, "-->"]);
-    }
     case "ElementNode": {
       return concat([
         "<",
         n.tag,
         n.attributes.length ? " " : "",
         join(" ", path.map(print, "attributes")),
+
+        n.modifiers.length ? " " : "",
+        join(" ", path.map(print, "modifiers")),
+
+        n.comments.length ? " " : "",
+        join(" ", path.map(print, "comments")),
+
         n.children.length ? ">" : " />",
+
         indent(concat([softline, join(softline, path.map(print, "children"))])),
         n.children.length ? concat([softline, "</", n.tag, ">"]) : ""
       ]);
     }
-    case "Hash": {
-      return join(" ", path.map(print, "pairs"));
+    case "AttrNode": {
+      const quote = n.value.type === "TextNode" ? '"' : "";
+      return concat([n.name, "=", quote, path.call(print, "value"), quote]);
     }
-    case "HashPair": {
-      return concat([n.key, "=", path.call(print, "value")]);
+    case "ConcatStatement": {
+      return concat([
+        '"',
+        concat(
+          path.map(partPath => {
+            const part = partPath.getValue();
+            if (part.type === "StringLiteral") {
+              return part.original;
+            }
+            return print(partPath);
+          }, "parts")
+        ),
+        '"'
+      ]);
     }
+    case "TextNode": {
+      return n.chars.trim();
+    }
+    case "ElementModifierStatement":
     case "MustacheStatement": {
       return concat([
         n.escaped ? "{{" : "{{{",
-        path.call(print, "path"),
-        n.params.length ? " " : "",
-        join(" ", path.map(print, "params")),
+        printPathParams(path, print),
         n.escaped ? "}}" : "}}}"
       ]);
     }
@@ -84,17 +81,107 @@ function genericPrint(path, options, print) {
       return concat(["{{!", dashes, n.value, dashes, "}}"]);
     }
     case "PathExpression": {
-      return concat([n.data ? "@" : "", join(" ", path.map(print, "parts"))]);
+      return n.original;
+    }
+    case "SubExpression": {
+      return concat(["(", printPathParams(path, print), ")"]);
+    }
+    case "BooleanLiteral": {
+      return String(n.value);
+    }
+    case "BlockStatement": {
+      return concat([
+        printOpenBlock(path, print),
+        indent(concat([hardline, path.call(print, "program")])),
+        softline,
+        printCloseBlock(path, print)
+      ]);
+    }
+    case "PartialStatement": {
+      return concat(["{{>", printPathParams(path, print), "}}"]);
+    }
+    case "CommentStatement": {
+      return concat(["<!--", n.value, "-->"]);
     }
     case "StringLiteral": {
-      return concat(['"', n.value, '"']);
+      return `"${n.value}"`;
     }
-    case "TextNode": {
-      return n.chars.trim();
+    case "NumberLiteral": {
+      return String(n.value);
     }
+    case "UndefinedLiteral": {
+      return "undefined";
+    }
+    case "NullLiteral": {
+      return "null";
+    }
+    case "Hash": {
+      return concat([
+        n.pairs.length ? " " : "",
+        join(" ", path.map(print, "pairs"))
+      ]);
+    }
+    case "HashPair": {
+      return concat([n.key, "=", path.call(print, "value")]);
+    }
+
     default:
       throw new Error("unknown glimmer type: " + JSON.stringify(n.type));
   }
+}
+
+function isLiteral(node) {
+  return node.type.endsWith("Literal");
+}
+
+function printPathParams(path, print) {
+  const node = path.getValue();
+  const parts = [];
+
+  switch (node.type) {
+    case "MustacheStatement":
+    case "SubExpression":
+    case "ElementModifierStatement":
+    case "BlockStatement":
+      if (isLiteral(node.path)) {
+        return String(node.path.value);
+      }
+      parts.push(path.call(print, "path"));
+      break;
+    case "PartialStatement":
+      parts.push(path.call(print, "name"));
+      break;
+    default:
+      return "";
+  }
+
+  parts.push(
+    node.params.length ? " " : "",
+    concat(path.map(print, "params")),
+    path.call(print, "hash")
+  );
+  return concat(parts);
+}
+
+function printBlockParams(path) {
+  const block = path.getValue();
+  if (!block.program || !block.program.blockParams.length) {
+    return "";
+  }
+  return concat([" as |", block.program.blockParams.join(" "), "|"]);
+}
+
+function printOpenBlock(path, print) {
+  return concat([
+    "{{#",
+    printPathParams(path, print),
+    printBlockParams(path, print),
+    "}}"
+  ]);
+}
+
+function printCloseBlock(path, print) {
+  return concat(["{{/", path.call(print, "path"), "}}"]);
 }
 
 module.exports = genericPrint;
