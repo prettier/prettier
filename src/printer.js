79849761c85498b8da8c233dc1ac0f71ffba6570
threehams <threehams@gmail.com>
2017-06-05 09:13:32 -0700

    Force break when argument list contains function call.

--- src/printer.js
+++ src/printer.js
@@ -3062,6 +3062,10 @@ function shouldGroupFirstArg(args) {
   );
 }
 
+function shouldBreakArgs(args) {
+  return args.length > 1 && args.some(arg => arg.type === "CallExpression");
+}
+
 function printArgumentsList(path, options, print) {
   const printed = path.map(print, "arguments");
   if (printed.length === 0) {
@@ -3076,6 +3080,7 @@ function printArgumentsList(path, options, print) {
   // This is just an optimization; I think we could return the
   // conditional group for all function calls, but it's more expensive
   // so only do it for specific forms.
+
   const shouldGroupFirst = shouldGroupFirstArg(args);
   const shouldGroupLast = shouldGroupLastArg(args);
   if (shouldGroupFirst || shouldGroupLast) {
@@ -3138,16 +3143,18 @@ function printArgumentsList(path, options, print) {
     ]);
   }
 
-  return group(
-    concat([
-      "(",
-      indent(concat([softline, join(concat([",", line]), printed)])),
-      ifBreak(shouldPrintComma(options, "all") ? "," : ""),
-      softline,
-      ")"
-    ]),
-    { shouldBreak: printed.some(willBreak) }
-  );
+  const children = [
+    "(",
+    indent(concat([softline, join(concat([",", line]), printed)])),
+    ifBreak(shouldPrintComma(options, "all") ? "," : ""),
+    softline,
+    ")"
+  ];
+
+  if (shouldBreakArgs(args)) {
+    children.push(breakParent);
+  }
+  return group(concat(children), { shouldBreak: printed.some(willBreak) });
 }
 
 function printFunctionTypeParameters(path, options, print) {
