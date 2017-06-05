128d73402b14a3336630595dab9f9e495bf3cf90
Joseph Frazier <1212jtraceur@gmail.com>
2017-06-05 18:28:12 -0400

    Add test demonstrating SourceElement-relative cursor translation
    See https://github.com/prettier/prettier/issues/1981#issuecomment-306326739

--- tests/cursor/jsfmt.spec.js
+++ tests/cursor/jsfmt.spec.js
@@ -6,3 +6,11 @@ test("translates cursor correctly in basic case", () => {
     cursorOffset: 1
   });
 });
+
+test("positions cursor relative to closest node, not SourceElement", () => {
+  const code = "return         15";
+  expect(prettier.formatWithCursor(code, { cursorOffset: 15 })).toEqual({
+    formatted: "return 15;\n",
+    cursorOffset: 15
+  });
+});
