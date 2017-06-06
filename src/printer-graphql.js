14a66c92c583680f406a471280852bf9bb1eb961
Sashko Stubailo <sashko@stubailo.com>
2017-06-05 21:44:24 -0700

    Remove extra space

--- src/printer-graphql.js
+++ src/printer-graphql.js
@@ -53,7 +53,7 @@ function genericPrint(path, options, print) {
                     ])
                   ),
                   softline,
-                  ") "
+                  ")"
                 ])
               )
             : "",
