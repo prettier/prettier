prettier/prettier

commit 26e829b987faeae1865737c77bebc2504f77cfea

Upgrade prettier to v1.4.0, fix lint issues (#1888)

src/comments.js
@@ -68,7 +68,12 @@ function getSortedChildNodes(node, text, resultArray) {
     });
   }
 
-  for (let i = 0, nameCount = names.length; i < nameCount; ++i) {
+  for (
+    let i = 0,
+      nameCount = names.length;
+    i < nameCount;
+    ++i
+  ) {
     getSortedChildNodes(node[names[i]], text, resultArray);
   }
 
@@ -82,7 +87,8 @@ function decorateComment(node, comment, text) {
   const childNodes = getSortedChildNodes(node, text);
   let precedingNode, followingNode;
   // Time to dust off the old binary search robes and wizard hat.
-  let left = 0, right = childNodes.length;
+  let left = 0,
+    right = childNodes.length;
   while (left < right) {
     const middle = (left + right) >> 1;
     const child = childNodes[middle];

src/parser-babylon.js
@@ -38,7 +38,8 @@ function parse(text) {
       throw createError(
         // babel error prints (l:c) with cols that are zero indexed
         // so we need our custom error
-        originalError.message.replace(/ \(.*\)/, ""),
+        originalError.message
+          .replace(/ \(.*\)/, ""),
         originalError.loc.line,
         originalError.loc.column + 1
       );

src/doc-printer.js
@@ -189,7 +189,6 @@ function printDocToString(doc, options) {
               }
             // fallthrough
 
-
             case MODE_BREAK: {
               shouldRemeasure = false;
 
@@ -371,7 +370,6 @@ function printDocToString(doc, options) {
                 shouldRemeasure = true;
               }
             // fallthrough
-
 
             case MODE_BREAK:
               if (lineSuffix.length) {

src/parser-typescript.js
@@ -44,7 +44,7 @@ function isProbablyJsx(text) {
     [
       "(^[^\"'`]*</)", // Contains "</" when probably not in a string
       "|",
-      "(^[^/]{2}.*\/>)" // Contains "/>" on line not starting with "//"
+      "(^[^/]{2}.*/>)" // Contains "/>" on line not starting with "//"
     ].join(""),
     "m"
   ).test(text);

src/printer.js
@@ -867,8 +867,10 @@ function genericPrintNoParens(path, options, print, args) {
 
       const lastElem = util.getLast(n[propertiesField]);
 
-      const canHaveTrailingSeparator = !(lastElem &&
-        (lastElem.type === "RestProperty" || lastElem.type === "RestElement"));
+      const canHaveTrailingSeparator = !(
+        lastElem &&
+        (lastElem.type === "RestProperty" || lastElem.type === "RestElement")
+      );
 
       let content;
       if (props.length === 0 && !n.typeAnnotation) {
@@ -996,8 +998,9 @@ function genericPrintNoParens(path, options, print, args) {
         }
       } else {
         const lastElem = util.getLast(n.elements);
-        const canHaveTrailingComma = !(lastElem &&
-          lastElem.type === "RestElement");
+        const canHaveTrailingComma = !(
+          lastElem && lastElem.type === "RestElement"
+        );
 
         // JavaScript allows you to have empty elements in an array which
         // changes its length based on the number of commas. The algorithm
@@ -1882,13 +1885,14 @@ function genericPrintNoParens(path, options, print, args) {
       const parentParentParent = path.getParentNode(2);
       let isArrowFunctionTypeAnnotation =
         n.type === "TSFunctionType" ||
-        !((parent.type === "ObjectTypeProperty" &&
-          !getFlowVariance(parent) &&
-          !parent.optional &&
-          util.locStart(parent) === util.locStart(n)) ||
+        !(
+          (parent.type === "ObjectTypeProperty" &&
+            !getFlowVariance(parent) &&
+            !parent.optional &&
+            util.locStart(parent) === util.locStart(n)) ||
           parent.type === "ObjectTypeCallProperty" ||
-          (parentParentParent &&
-            parentParentParent.type === "DeclareFunction"));
+          (parentParentParent && parentParentParent.type === "DeclareFunction")
+        );
 
       let needsColon =
         isArrowFunctionTypeAnnotation && parent.type === "TypeAnnotation";
@@ -2015,9 +2019,11 @@ function genericPrintNoParens(path, options, print, args) {
       const shouldIndent =
         parent.type !== "TypeParameterInstantiation" &&
         parent.type !== "GenericTypeAnnotation" &&
-        !((parent.type === "TypeAlias" ||
-          parent.type === "VariableDeclarator") &&
-          hasLeadingOwnLineComment(options.originalText, n));
+        !(
+          (parent.type === "TypeAlias" ||
+            parent.type === "VariableDeclarator") &&
+          hasLeadingOwnLineComment(options.originalText, n)
+        );
 
       // {
       //   a: string
@@ -3254,8 +3260,10 @@ function printFunctionParams(path, print, options, expandArg) {
     fun[paramsField][0].typeAnnotation &&
     flowTypeAnnotations.indexOf(fun[paramsField][0].typeAnnotation.type) !==
       -1 &&
-    !(fun[paramsField][0].typeAnnotation.type === "GenericTypeAnnotation" &&
-      fun[paramsField][0].typeAnnotation.typeParameters) &&
+    !(
+      fun[paramsField][0].typeAnnotation.type === "GenericTypeAnnotation" &&
+      fun[paramsField][0].typeAnnotation.typeParameters
+    ) &&
     !fun.rest;
 
   if (isFlowShorthandWithOneArg) {
@@ -4593,7 +4601,9 @@ function classChildNeedsASIProtection(node) {
 
   if (!node.computed) {
     const name = node.key && node.key.name;
-    if (name === "in" || name === "instanceof") return true;
+    if (name === "in" || name === "instanceof") {
+      return true;
+    }
   }
   switch (node.type) {
     case "ClassProperty":

commit 959ce0da749122a869b112134965b7160da7c820

Upgrade prettier to v1.4.2, fix lint issues
This corrects the regressions seen in commit
26e829b (https://github.com/prettier/prettier/pull/1888)

src/comments.js
@@ -68,12 +68,7 @@ function getSortedChildNodes(node, text, resultArray) {
     });
   }
 
-  for (
-    let i = 0,
-      nameCount = names.length;
-    i < nameCount;
-    ++i
-  ) {
+  for (let i = 0, nameCount = names.length; i < nameCount; ++i) {
     getSortedChildNodes(node[names[i]], text, resultArray);
   }
 

src/parser-babylon.js
@@ -38,8 +38,7 @@ function parse(text) {
       throw createError(
         // babel error prints (l:c) with cols that are zero indexed
         // so we need our custom error
-        originalError.message
-          .replace(/ \(.*\)/, ""),
+        originalError.message.replace(/ \(.*\)/, ""),
         {
           start: {
             line: originalError.loc.line,