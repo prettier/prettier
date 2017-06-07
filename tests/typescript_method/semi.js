0df0114d6ca56743bc4330a3dc1b0245dbf2c6c2
Christopher Chedeau <vjeux@fb.com>
2017-06-07 09:54:06 -0700

    Add semi for functions without body
    We don't need to be overly restrictive and should add the semi if there's no body
    
    Fixes #2003

--- tests/typescript_method/semi.js
+++ tests/typescript_method/semi.js
@@ -2,3 +2,6 @@ declare module 'foo' {
   function foo(namespace: string): void;
   function bar(namespace: string): void;
 }
+
+function pickCard(x: {suit: string; card: number; }[]): number;
+function pickCard(x: number): {suit: string; card: number; };
