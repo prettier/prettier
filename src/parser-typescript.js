3726067923c72f5349c3cc5fb0ff6f7a53c5412e
James Henry <james@henry.sc>
2017-06-14 22:53:05 +0100

    Override tsep loggerFn with noop (#2144)
    * Override tsep loggerFn with noop
    
    * Extra function to keep Vjeux happy :P

--- src/parser-typescript.js
+++ src/parser-typescript.js
@@ -35,7 +35,10 @@ function tryParseTypeScript(text, jsx) {
     tokens: true,
     comment: true,
     useJSXTextNode: true,
-    ecmaFeatures: { jsx }
+    ecmaFeatures: { jsx },
+    // Override logger function with noop,
+    // to avoid unsupported version errors being logged
+    loggerFn: () => {}
   });
 }
 
