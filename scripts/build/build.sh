33a1c713d562deb194c6f14b097ff4f9e2f3d18f
Joseph Frazier <1212jtraceur@gmail.com>
2017-06-17 17:39:30 -0400

    Dedupe copy/pasted code in build.sh (#2179)

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -15,23 +15,10 @@ echo 'Bundling lib bin...';
 node_modules/.bin/rollup -c scripts/build/rollup.bin.config.js
 chmod +x ./dist/bin/prettier.js
 
-echo 'Bundling lib babylon...';
-node_modules/.bin/rollup -c scripts/build/rollup.parser.config.js --environment parser:babylon
-
-echo 'Bundling lib flow...';
-node_modules/.bin/rollup -c scripts/build/rollup.parser.config.js --environment parser:flow
-
-echo 'Bundling lib graphql...';
-node_modules/.bin/rollup -c scripts/build/rollup.parser.config.js --environment parser:graphql
-
-echo 'Bundling lib typescript...';
-node_modules/.bin/rollup -c scripts/build/rollup.parser.config.js --environment parser:typescript
-
-echo 'Bundling lib parse5...';
-node_modules/.bin/rollup -c scripts/build/rollup.parser.config.js --environment parser:parse5
-
-echo 'Bundling lib json...';
-node_modules/.bin/rollup -c scripts/build/rollup.parser.config.js --environment parser:json
+for parser in babylon flow graphql typescript parse5 json; do
+  echo "Bundling lib $parser...";
+  node_modules/.bin/rollup -c scripts/build/rollup.parser.config.js --environment parser:$parser
+done
 
 echo 'Bundling lib postcss...';
 # PostCSS has dependency cycles and won't work correctly with rollup :(
@@ -53,23 +40,10 @@ echo 'Bundling docs babylon...';
 node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-babylon.js
 node_modules/babel-cli/bin/babel.js docs/lib/parser-babylon.js --out-file docs/lib/parser-babylon.js --presets=es2015
 
-echo 'Bundling docs flow...';
-node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-flow.js
-
-echo 'Bundling docs graphql...';
-node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-graphql.js
-
-echo 'Bundling docs typescript...';
-node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-typescript.js
-
-echo 'Bundling docs postcss...';
-node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-postcss.js
-
-echo 'Bundling docs parse5...';
-node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-parse5.js
-
-echo 'Bundling docs json...';
-node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-json.js
+for parser in flow graphql typescript postcss parse5 json; do
+  echo "Bundling docs $parser...";
+  node_modules/.bin/rollup -c scripts/build/rollup.docs.config.js --environment filepath:parser-$parser.js
+done
 
 echo;
 
