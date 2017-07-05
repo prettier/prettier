f3b568d5bd48a742460cb0f805d1be38d8f61a40
Joseph Frazier <1212jtraceur@gmail.com>
2017-07-05 15:41:39 -0400

    lint: Disallow yoda conditions
    http://eslint.org/docs/rules/yoda

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ function calculateRange(text, opts, ast) {
 }
 
 function formatRange(text, opts, ast) {
-  if (0 < opts.rangeStart || opts.rangeEnd < text.length) {
+  if (opts.rangeStart > 0 || opts.rangeEnd < text.length) {
     const range = calculateRange(text, opts, ast);
     const rangeStart = range.rangeStart;
     const rangeEnd = range.rangeEnd;
