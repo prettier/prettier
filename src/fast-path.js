03842d23780c5acaa6d220f2aa61886ae8adc658
Lucas Azzola <derflatulator@gmail.com>
2017-07-17 19:57:46 +1000

    Preserve parens inside keyof operator

--- src/fast-path.js
+++ src/fast-path.js
@@ -346,13 +346,15 @@ FastPath.prototype.needsParens = function(options) {
       }
 
     case "TSParenthesizedType": {
+      const grandParent = this.getParentNode(1);
       if (
         (parent.type === "TypeParameter" ||
           parent.type === "VariableDeclarator" ||
           parent.type === "TypeAnnotation" ||
           parent.type === "GenericTypeAnnotation") &&
         (node.typeAnnotation.type === "TypeAnnotation" &&
-          node.typeAnnotation.typeAnnotation.type !== "TSFunctionType")
+          node.typeAnnotation.typeAnnotation.type !== "TSFunctionType" &&
+          grandParent.type !== "TSTypeOperator")
       ) {
         return false;
       }
