9567d0f1ce2fb1a7e8f6afa6ea96d1ca71832780
jackyho112 <jackyho112dev@gmail.com>
2017-08-20 17:09:09 -0700

    Remove flow test modification

--- tests/flow/interface/interface.js
+++ tests/flow/interface/interface.js
@@ -4,10 +4,6 @@ var x: string = new C().x;
 
 interface I { x: number; }
 
-interface A { 'x': string; }
-
-interface B { "x": boolean; }
-
 var i = new I(); // error
 
 function testInterfaceName(o: I) {
