d0ac34b6b741323f3451641627731615421ca048
Lucas Azzola <derflatulator@gmail.com>
2017-08-27 15:43:43 +1000

    Fallback from "decorators2" to "decorators" plugin

--- src/parser-babylon.js
+++ src/parser-babylon.js
@@ -2,61 +2,85 @@
 
 const createError = require("./parser-create-error");
 
+const basePlugins = [
+  "jsx",
+  "flow",
+  "doExpressions",
+  "objectRestSpread",
+  "classProperties",
+  "exportExtensions",
+  "asyncGenerators",
+  "functionBind",
+  "functionSent",
+  "dynamicImport",
+  "numericSeparator",
+  "importMeta",
+  "optionalCatchBinding",
+  "optionalChaining"
+];
+
+function babylonOptions(extraOptions, extraPlugins) {
+  return Object.assign(
+    {
+      sourceType: "module",
+      allowImportExportEverywhere: true,
+      allowReturnOutsideFunction: true,
+      plugins: basePlugins.concat(extraPlugins)
+    },
+    extraOptions
+  );
+}
+
 function parse(text, parsers, opts) {
   // Inline the require to avoid loading all the JS if we don't use it
   const babylon = require("babylon");
 
-  const babylonOptions = {
-    sourceType: "module",
-    allowImportExportEverywhere: true,
-    allowReturnOutsideFunction: true,
-    plugins: [
-      "jsx",
-      "flow",
-      "doExpressions",
-      "objectRestSpread",
-      "decorators2",
-      "classProperties",
-      "exportExtensions",
-      "asyncGenerators",
-      "functionBind",
-      "functionSent",
-      "dynamicImport",
-      "numericSeparator",
-      "importMeta",
-      "optionalCatchBinding",
-      "optionalChaining"
-    ]
-  };
+  const combinations = [
+    babylonOptions({ strictMode: true }, ["decorators2"]),
+    babylonOptions({ strictMode: false }, ["decorators2"]),
+    babylonOptions({ strictMode: true }, ["decorators"]),
+    babylonOptions({ strictMode: false }, ["decorators"])
+  ];
 
   const parseMethod =
     opts && opts.parser === "json" ? "parseExpression" : "parse";
 
-  let ast;
-  try {
-    ast = babylon[parseMethod](text, babylonOptions);
-  } catch (originalError) {
-    try {
-      ast = babylon[parseMethod](
-        text,
-        Object.assign({}, babylonOptions, { strictMode: false })
-      );
-    } catch (nonStrictError) {
-      throw createError(
-        // babel error prints (l:c) with cols that are zero indexed
-        // so we need our custom error
-        originalError.message.replace(/ \(.*\)/, ""),
-        {
-          start: {
-            line: originalError.loc.line,
-            column: originalError.loc.column + 1
-          }
+  const parseFunction = options => babylon[parseMethod](text, options);
+
+  const result = tryCombinations(combinations, parseFunction);
+  if (!result.success) {
+    const originalError = result.errors[0];
+    throw createError(
+      // babel error prints (l:c) with cols that are zero indexed
+      // so we need our custom error
+      originalError.message.replace(/ \(.*\)/, ""),
+      {
+        start: {
+          line: originalError.loc.line,
+          column: originalError.loc.column + 1
         }
-      );
-    }
+      }
+    );
   }
+
+  const ast = result.value;
   delete ast.tokens;
   return ast;
+}
+
+function tryCombinations(combinations, fn) {
+  const errors = [];
+  for (const combination of combinations) {
+    try {
+      return {
+        success: true,
+        value: fn(combination)
+      };
+    } catch (error) {
+      errors.push(error);
+    }
+  }
+  return { success: false, errors };
 }
 
 module.exports = parse;
