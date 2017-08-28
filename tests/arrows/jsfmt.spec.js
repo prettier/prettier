ad5d5a4123e4d98a3634f1d9607d1bb86a471e2a
Alex Rattray <rattray.alex@gmail.com>
2017-08-27 18:29:34 -0700

    Run tests with functional option

--- tests/arrows/jsfmt.spec.js
+++ tests/arrows/jsfmt.spec.js
@@ -1,3 +1,3 @@
 run_spec(__dirname, { arrowFnParens: "avoid", parser: "babylon" }, ["typescript"]);
-run_spec(__dirname, { arrowFnParens: "avoid", parser: "babylon" }, ["typescript"]);
+run_spec(__dirname, { arrowFnParens: "functional", parser: "babylon" }, ["typescript"]);
 run_spec(__dirname, { arrowFnParens: "always", parser: "babylon" }, ["typescript"]);
