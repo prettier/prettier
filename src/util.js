87401de0a56c185e62a339f3460afe9144f7012d
Christian Wesselhoeft <christian.wesselhoeft@gmail.com>
2017-08-30 12:26:11 -0600

    Handle +/- before numbers in CSS (#2713)
    * src/util: Fix negative number handling
    
    * src/util: Handle decimal numbers with leading plus sign
    
    * src/util: Handle sign symbols before numbers in scientific notation

--- src/util.js
+++ src/util.js
@@ -572,11 +572,11 @@ function printNumber(rawNumber) {
     rawNumber
       .toLowerCase()
       // Remove unnecessary plus and zeroes from scientific notation.
-      .replace(/^([\d.]+e)(?:\+|(-))?0*(\d)/, "$1$2$3")
+      .replace(/^([+-]?[\d.]+e)(?:\+|(-))?0*(\d)/, "$1$2$3")
       // Remove unnecessary scientific notation (1e0).
-      .replace(/^([\d.]+)e[+-]?0+$/, "$1")
+      .replace(/^([+-]?[\d.]+)e[+-]?0+$/, "$1")
       // Make sure numbers always start with a digit.
-      .replace(/^\./, "0.")
+      .replace(/^([+-])?\./, "$10.")
       // Remove extraneous trailing decimal zeroes.
       .replace(/(\.\d+?)0+(?=e|$)/, "$1")
       // Remove trailing dot.
