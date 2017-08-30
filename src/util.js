b049e0fc2760e52f2460aa1eded930db98640346
Christian Wesselhoeft <cwesselhoeft@enbala.com>
2017-08-30 12:11:44 -0600

    src/util: Handle sign symbols before numbers in scientific notation

--- src/util.js
+++ src/util.js
@@ -572,9 +572,9 @@ function printNumber(rawNumber) {
     rawNumber
       .toLowerCase()
       // Remove unnecessary plus and zeroes from scientific notation.
-      .replace(/^([\d.]+e)(?:\+|(-))?0*(\d)/, "$1$2$3")
+      .replace(/^([+-]?[\d.]+e)(?:\+|(-))?0*(\d)/, "$1$2$3")
       // Remove unnecessary scientific notation (1e0).
-      .replace(/^([\d.]+)e[+-]?0+$/, "$1")
+      .replace(/^([+-]?[\d.]+)e[+-]?0+$/, "$1")
       // Make sure numbers always start with a digit.
       .replace(/^([+-])?\./, "$10.")
       // Remove extraneous trailing decimal zeroes.
