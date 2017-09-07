7303e1dbeb4c3bbd79d145b5993bf5daa3b234be
Lucas Duailibe <lucasds@gmail.com>
2017-09-07 15:28:12 -0300

    Fix chained logical expressions with objects/array/etc inlined

--- tests/binary-expressions/inline-object-array.js
+++ tests/binary-expressions/inline-object-array.js
@@ -6,9 +6,39 @@ prevState = prevState || {
   selectedCatalog: null,
 };
 
+prevState = prevState ||
+  defaultState || {
+    catalogs: [],
+    loadState: LOADED,
+    opened: false,
+    searchQuery: '',
+    selectedCatalog: null,
+  };
+
 this.steps = steps || [
   {
     name: 'mock-module',
     path: '/nux/mock-module',
   },
 ];
+
+const create = () => {
+  const result = doSomething();
+  return (
+    shouldReturn &&
+    result.ok && {
+      status: "ok",
+      createdAt: result.createdAt,
+      updatedAt: result.updatedAt
+    }
+  );
+}
+
+const obj = {
+  state: shouldHaveState &&
+    stateIsOK && {
+      loadState: LOADED,
+      opened: false
+    },
+  loaded: true
+}
