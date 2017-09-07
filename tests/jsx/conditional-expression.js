7e294b34c3bf20057762c11b81d609f4a26612e4
Lucas Duailibe <lucasds@gmail.com>
2017-09-07 13:26:32 -0300

    Change how JSX mode is activated in ternaries

--- tests/jsx/conditional-expression.js
+++ tests/jsx/conditional-expression.js
@@ -17,84 +17,70 @@
 //
 //  test ? consequent : alternate;
 //
-// We print a conditional expression in JSX mode if any of the following are
-// true:
-// * Its parent is a JSXExpressionContainer
-// * Its test, consequent, or alternate are JSXElements
-// * It is in a chain with other ConditionalExpressions, and the outermost
-//   one's parent is a JSXExpressionContainer
-// * It is in a chain with other ConditionalExpressions, and any of the
-//   tests, consequents, or alternates of any of the ConditionalExpressions in
-//   the chain are JSXElements.
+// We only print a conditional expression in JSX mode if its test,
+// consequent, or alternate are JSXElements.
 // Otherwise, we print in normal mode.
 
-// This ConditionalExpression does not meet any of the other criteria for
-// printing in JSX mode, so it prints in normal mode. The line does not break.
+// This ConditionalExpression has no JSXElements so it prints in normal mode.
+// The line does not break.
 normalModeNonBreaking ? "a" : "b";
 
-// This ConditionalExpression does not meet any of the criteria to print in JSX
-// mode, so it prints in normal mode. Its consequent is very long, so it breaks
-// out to multiple lines.
+// This ConditionalExpression has no JSXElements so it prints in normal mode.
+// Its consequent is very long, so it breaks out to multiple lines.
 normalModeBreaking
   ? johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
   : "c";
 
-// This ConditionalExpression prints in JSX mode because its parent is a
+// This ConditionalExpression prints in normal mode even though its parent is a
 // JSXExpressionContainer. The line does not break, so it does not contain
 // parens.
 <div>
   {a ? "b" : "c"}
 </div>;
 
-// This ConditionalExpression prints in JSX mode because its parent is a
-// JSXExpressionContainer. Because the consequent is long enough to break the
-// line, both its consequent and alternate break out and are wrapped in parens.
+// This ConditionalExpression prints in normal mode even though its parent is a
+// JSXExpressionContainer. Because the consequent is very long, they are broken in
+// multiple lines but no parens are added.
 <div>
-  {a ? (
-    johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
-  ) : (
-    "c"
-  )}
+  {a
+    ? johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
+    : "c"}
 </div>;
 
-// This ConditionalExpression prints in JSX mode because its parent is a
-// JSXExpressionContainer. The consequent is long enough to break the line, but
-// because the alternate is null, only the consequent is wrapped in parens.
+// This ConditionalExpression prints in normal mode even though its parent is a
+// JSXExpressionContainer. Because the consequent is very long, they are broken in
+// multiple lines but no parens are added.
 <div>
-  {a ? (
-    johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
-  ) : null}
+  {a
+    ? johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
+    : null}
 </div>;
 
-// This ConditionalExpression prints in JSX mode because its parent is a
-// JSXExpressionContainer. Because the alternate is long enough to break the
-// line, both its consequent and alternate break out and are wrapped in parens.
+// This ConditionalExpression prints in normal mode even though its parent is a
+// JSXExpressionContainer. Because the alternate is very long, they are broken in
+// multiple lines but no parens are added.
 <div>
-  {a ? (
-    "b"
-  ) : (
-    johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
-  )}
+  {a
+    ? "b"
+    : johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa}
 </div>;
 
-// This ConditionalExpression prints in JSX mode because its parent is a
-// JSXExpressionContainer. The alternate is long enough to break the line, but
-// because the consequent is null, only the alternate is wrapped in parens.
+// This ConditionalExpression prints in normal mode even though its parent is a
+// JSXExpressionContainer. Because the alternate is very long, they are broken in
+// multiple lines but no parens are added.
 <div>
-  {a ? null : (
-    johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
-  )}
+  {a
+    ? null
+    : johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa}
 </div>;
 
-// This ConditionalExpression prints in JSX mode because its parent is a
-// JSXExpressionContainer. Because the test is long enough to break the
-// line, both its consequent and alternate break out and are wrapped in parens.
+// This ConditionalExpression prints in normal mode even though its parent is a
+// JSXExpressionContainer. Because the test is very long, they are broken in
+// multiple lines but no parens are added.
 <div>
-  {johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa ? (
-    "b"
-  ) : (
-    "c"
-  )}
+  {johnJacobJingleHeimerSchmidtHisNameIsMyNameTooWheneverWeGoOutThePeopleAlwaysShoutThereGoesJohnJacobJingleHeimerSchmidtYaDaDaDaDaDaDa
+    ? "b"
+    : "c"}
 </div>;
 
 // This ConditionalExpression prints in JSX mode because its test is a
@@ -150,17 +136,14 @@ jsxModeFromElementBreaking ? (
   {a ? "a" : b ? "b" : "c"}
 </div>;
 
-// This chain of ConditionalExpressions prints in JSX mode because the parent of
-// the outermost ConditionalExpression is a JSXExpressionContainer. It is
-// breaking.
+// This chain of ConditionalExpressions prints in normal mode even though its parent is a
+// JSXExpressionContainer. It is breaking.
 <div>
-  {a ? (
-    "a"
-  ) : b ? (
-    "b"
-  ) : (
-    thisIsASongAboutYourPoorSickPenguinHeHasAFeverAndHisToesAreBlueButIfISingToYourPoorSickPenguinHeWillFeelBetterInADayOrTwo
-  )}
+  {a
+    ? "a"
+    : b
+      ? "b"
+      : thisIsASongAboutYourPoorSickPenguinHeHasAFeverAndHisToesAreBlueButIfISingToYourPoorSickPenguinHeWillFeelBetterInADayOrTwo}
 </div>;
 
 // This chain of ConditionalExpressions prints in JSX mode because there is a
@@ -194,3 +177,17 @@ cable ? (
 ) : affairs ? (
   "network"
 ) : "dunno";
+
+// This chain of ConditionalExpressions prints in JSX mode because there is a
+// JSX element somewhere in the chain. It is breaking; notice the consequents
+// and alternates in the entire chain get wrapped in parens.
+<div>
+  {properties.length > 1 ||
+  (properties.length === 1 && properties[0].apps.size > 1) ? (
+    draggingApp == null || newPropertyName == null ? (
+      <MigrationPropertyListItem />
+    ) : (
+      <MigrationPropertyListItem apps={Immutable.List()} />
+    )
+  ) : null}
+</div>;
