df5482008ef97ce9172557e1ea200997aaeb3f39
长天之云 <ambar.lee@gmail.com>
2017-09-11 00:21:56 +0800

    fix(prettierignore): support absolute filename (#2783)

--- src/cli-util.js
+++ src/cli-util.js
@@ -292,7 +292,14 @@ function eachFilename(argv, patterns, callback) {
   }
 
   try {
-    const filePaths = globby.sync(patterns, { dot: true });
+    const filePaths = globby
+      .sync(patterns, { dot: true })
+      .map(
+        filePath =>
+          path.isAbsolute(filePath)
+            ? path.relative(process.cwd(), filePath)
+            : filePath
+      );
     if (filePaths.length === 0) {
       console.error(`No matching files. Patterns tried: ${patterns.join(" ")}`);
       process.exitCode = 2;
