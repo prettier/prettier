2425d5a80f61d017930c8228493efbc99f8b00c6
Lucas Duailibe <lucasds@gmail.com>
2017-09-10 16:55:00 -0300

    Use same logic for binary chain

--- tests/method-chain/logical.js
+++ tests/method-chain/logical.js
@@ -1,2 +1,11 @@
 (veryLongVeryLongVeryLong || e).map(tickets =>
   TicketRecord.createFromSomeLongString());
+
+(veryLongVeryLongVeryLong || e).map(tickets =>
+  TicketRecord.createFromSomeLongString()).filter(obj => !!obj);
+
+(veryLongVeryLongVeryLong || anotherVeryLongVeryLongVeryLong || veryVeryVeryLongError).map(tickets =>
+  TicketRecord.createFromSomeLongString());
+
+(veryLongVeryLongVeryLong || anotherVeryLongVeryLongVeryLong || veryVeryVeryLongError).map(tickets =>
+  TicketRecord.createFromSomeLongString()).filter(obj => !!obj);
