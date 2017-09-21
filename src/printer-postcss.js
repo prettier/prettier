cd4d4e7273b1732caff2b8006edd9c69336a4504
Simon Lydell <simon.lydell@gmail.com>
2017-09-21 20:23:22 +0200

    Don't lowercase SCSS placeholder selectors (#2876)
    Fixes #2872.

--- src/printer-postcss.js
+++ src/printer-postcss.js
@@ -495,6 +495,7 @@ function maybeToLowerCase(value) {
   return value.includes("$") ||
     value.includes("@") ||
     value.includes("#") ||
+    value.startsWith("%") ||
     value.startsWith("--")
     ? value
     : value.toLowerCase();
