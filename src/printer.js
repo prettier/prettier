b7a8ee9eb075cbefeb1f4176a2e2b5b8178be1f9
Lucas Duailibe <lucasds@gmail.com>
2017-10-20 13:20:42 -0200

    Fix indentation of intersection object types

--- src/printer.js
+++ src/printer.js
@@ -2177,14 +2177,23 @@ function genericPrintNoParens(path, options, print, args) {
     case "IntersectionTypeAnnotation": {
       const types = path.map(print, "types");
       const result = [];
+      let wasIndented = false;
       for (let i = 0; i < types.length; ++i) {
         if (i === 0) {
           result.push(types[i]);
+        } else if (isObjectType(n.types[i - 1]) && isObjectType(n.types[i])) {
+          // If both are objects, don't indent
+          result.push(
+            concat([" & ", wasIndented ? indent(types[i]) : types[i]])
+          );
         } else if (!isObjectType(n.types[i - 1]) && !isObjectType(n.types[i])) {
           // If no object is involved, go to the next line if it breaks
           result.push(indent(concat([" &", line, types[i]])));
         } else {
           // If you go from object to non-object or vis-versa, then inline it
+          if (i > 1) {
+            wasIndented = true;
+          }
           result.push(" & ", i > 1 ? indent(types[i]) : types[i]);
         }
       }
