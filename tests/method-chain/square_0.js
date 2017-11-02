979b86944d4c3b9611df8a20148a63efc6c38882
Lucas Duailibe <lucasds@gmail.com>
2017-11-02 10:35:34 -0200

    Consider [0] to be part of the first member group

--- tests/method-chain/square_0.js
+++ tests/method-chain/square_0.js
@@ -3,3 +3,7 @@ const version = someLongString
   .pop()
   .split(EOL)[0]
   .trim();
+
+const component = find('.org-lclp-edit-copy-url-banner__link')[0]
+  .getAttribute('href')
+  .indexOf(this.landingPageLink);
