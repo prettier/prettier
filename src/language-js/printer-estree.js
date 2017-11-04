a6095eb7d6f6b6b98f734e8d527064329e6c22f7
Jarda Snajdr <jsnajdr@gmail.com>
2017-11-04 22:31:43 +0100

    [prettierx] Fix parens around breaking expression in MemberExpression

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -188,7 +188,7 @@ function genericPrint(path, options, printPath, args) {
       node.trailingComments[0].printed = true;
     }
 
-    parts.push(parenSpace, ")");
+    parts.push(hasAddedLine(linesWithoutParens) ? "" : parenSpace, ")");
   }
 
   if (decorators.length > 0) {
@@ -435,12 +435,12 @@ function printTernaryOperator(path, options, print, operatorOptions) {
   // We want a whole chain of ConditionalExpressions to all
   // break if any of them break. That means we should only group around the
   // outer-most ConditionalExpression.
-  const maybeGroup = doc =>
+  const maybeGroup = (doc, options) =>
     operatorOptions.breakNested
       ? parent === firstNonConditionalParent
-        ? group(doc)
+        ? group(doc, options)
         : doc
-      : group(doc);
+      : group(doc, options); // Always group in normal mode.
 
   // Break the closing paren to keep the chain right after it:
   // (a
@@ -475,7 +475,8 @@ function printTernaryOperator(path, options, print, operatorOptions) {
         forceNoIndent ? concat(parts) : indent(concat(parts)),
         operatorOptions.afterParts(breakClosingParen)
       )
-    )
+    ),
+    { addedLine: breakClosingParen }
   );
 }
 
@@ -1755,7 +1756,7 @@ function printPathNoParens(path, options, print, args) {
     case "ConditionalExpression":
       return printTernaryOperator(path, options, print, {
         beforeParts: () => [path.call(print, "test")],
-        afterParts: breakClosingParen => [breakClosingParen ? softline : ""],
+        afterParts: breakClosingParen => [breakClosingParen ? parenLine : ""],
         shouldCheckJsx: true,
         conditionalNodeType: "ConditionalExpression",
         consequentNodePropertyName: "consequent",
