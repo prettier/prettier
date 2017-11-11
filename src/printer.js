5663ee668cc82776256404fff136f35cf8858305
Lucas Duailibe <lucasds@gmail.com>
2017-11-11 18:39:28 -0200

    Refactor JSXOpeningElement code

--- src/printer.js
+++ src/printer.js
@@ -1791,6 +1791,9 @@ function genericPrintNoParens(path, options, print, args) {
     case "JSXOpeningElement": {
       const n = path.getValue();
 
+      const nameHasComments =
+        n.name && n.name.comments && n.name.comments.length;
+
       // don't break up opening elements with a single long text attribute
       if (
         n.attributes.length === 1 &&
@@ -1805,10 +1808,8 @@ function genericPrintNoParens(path, options, print, args) {
         //   attr="value"
         //   // comment
         // >
-        !(
-          (n.name && n.name.comments && n.name.comments.length) ||
-          (n.attributes[0].comments && n.attributes[0].comments.length)
-        )
+        !nameHasComments &&
+        !(n.attributes[0].comments && n.attributes[0].comments.length)
       ) {
         return group(
           concat([
@@ -1821,6 +1822,11 @@ function genericPrintNoParens(path, options, print, args) {
         );
       }
 
+      const lastAttrHasTrailingComments =
+        n.attributes &&
+        n.attributes.length &&
+        hasTrailingComment(util.getLast(n.attributes));
+
       const bracketSameLine =
         options.jsxBracketSameLine &&
         // We should print the bracket in a new line for the following cases:
@@ -1830,15 +1836,8 @@ function genericPrintNoParens(path, options, print, args) {
         // <div
         //   attr // comment
         // >
-        !(
-          (n.name &&
-            !(n.attributes && n.attributes.length) &&
-            n.name.comments &&
-            n.name.comments.length) ||
-          (n.attributes &&
-            n.attributes.length &&
-            hasTrailingComment(util.getLast(n.attributes)))
-        );
+        (!nameHasComments || (n.attributes && n.attributes.length)) &&
+        !lastAttrHasTrailingComments;
 
       return group(
         concat([
