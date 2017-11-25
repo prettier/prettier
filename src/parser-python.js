cbfcc86dd2c0816dd5043207314f215fa202b5fc
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-25 23:42:02 +0000

    Use astexport to get real ast

--- src/parser-python.js
+++ src/parser-python.js
@@ -1,89 +1,22 @@
 "use strict";
 
-function parse(text /*, parsers, opts*/) {
-  // TODO: implement parsing using astexport
-  // https://github.com/fpoli/python-astexport
+const { spawnSync } = require("child_process");
 
-  // This is a sample AST used for testing purpose.
-  const ast = {
-    ast_type: "Module",
-    body: [
-      {
-        args: {
-          args: [
-            {
-              annotation: null,
-              arg: "x",
-              ast_type: "arg",
-              col_offset: 10,
-              lineno: 1
-            },
-            {
-              annotation: null,
-              arg: "y",
-              ast_type: "arg",
-              col_offset: 13,
-              lineno: 1
-            }
-          ],
-          ast_type: "arguments",
-          defaults: [],
-          kw_defaults: [],
-          kwarg: null,
-          kwonlyargs: [],
-          vararg: null
-        },
-        ast_type: "FunctionDef",
-        body: [
-          {
-            ast_type: "Expr",
-            col_offset: 4,
-            lineno: 2,
-            value: {
-              args: [
-                {
-                  ast_type: "Str",
-                  col_offset: 10,
-                  lineno: 2,
-                  s: "hello world"
-                },
-                {
-                  ast_type: "Name",
-                  col_offset: 25,
-                  ctx: { ast_type: "Load" },
-                  id: "x",
-                  lineno: 2
-                },
-                {
-                  ast_type: "Name",
-                  col_offset: 28,
-                  ctx: { ast_type: "Load" },
-                  id: "y",
-                  lineno: 2
-                }
-              ],
-              ast_type: "Call",
-              col_offset: 4,
-              func: {
-                ast_type: "Name",
-                col_offset: 4,
-                ctx: { ast_type: "Load" },
-                id: "print",
-                lineno: 2
-              },
-              keywords: [],
-              lineno: 2
-            }
-          }
-        ],
-        col_offset: 0,
-        decorator_list: [],
-        lineno: 1,
-        name: "hello",
-        returns: null
-      }
-    ]
-  };
+function parse(text /*, parsers, opts*/) {
+  // TODO: return an useful error when https://github.com/fpoli/python-astexport
+  // is not installed or when it fails
+
+  const executionResult = spawnSync("astexport", [], { input: text });
+
+  const error = executionResult.stderr.toString();
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  const ast = JSON.parse(executionResult.stdout.toString());
+
+  // TODO: add comments
 
   ast.comments = [];
   return ast;
