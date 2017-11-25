a7e2e3de90304cd178aee2a3e4d6ae2da7a10c29
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-25 21:27:56 +0000

    Throw an error when ast type is unknown

--- src/printer-python.js
+++ src/printer-python.js
@@ -20,8 +20,6 @@ function genericPrint(path, options, print) {
     return n;
   }
 
-  console.log(n.ast_type);
-
   switch (n.ast_type) {
     case "Module": {
       return concat([
@@ -44,10 +42,11 @@ function genericPrint(path, options, print) {
       // keyword only arguments
       return join(", ", n.args.map(a => a.arg));
     }
-  }
 
-  // TODO: implement all the ast types
-  return "hello world";
+    default:
+      /* istanbul ignore next */
+      throw new Error("unknown python type: " + JSON.stringify(n.ast_type));
+  }
 }
 
 module.exports = genericPrint;
