c986f45ec7f60295e73d52b8c94661937d17a158
Lucas Duailibe <lucasds@gmail.com>
2017-12-05 17:48:10 -0200

    fix lint

--- tests_integration/__tests__/skip-folders.js
+++ tests_integration/__tests__/skip-folders.js
@@ -13,7 +13,10 @@ describe("skips folders in glob", () => {
 
 describe("skip folders passed specifically", () => {
   runPrettier("cli/skip-folders", [
-    "a", "a/file.js", "b", "b/file.js",
+    "a",
+    "a/file.js",
+    "b",
+    "b/file.js",
     "-l"
   ]).test({ status: 1, stderr: "" });
 });
