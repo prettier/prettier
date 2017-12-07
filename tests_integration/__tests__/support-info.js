669db3b8168580269876e3cc99462397fbc773ab
Ika <ikatyang@gmail.com>
2017-12-07 17:47:57 +0800

    test: snapshot diff

--- tests_integration/__tests__/support-info.js
+++ tests_integration/__tests__/support-info.js
@@ -2,18 +2,32 @@
 
 const prettier = require("../../tests_config/require_prettier");
 const runPrettier = require("../runPrettier");
+const snapshotDiff = require("snapshot-diff");
 
 describe("API getSupportInfo()", () => {
-  test("no arguments", () => {
-    expect(getCoreInfo(prettier.getSupportInfo())).toMatchSnapshot();
-  });
+  const testVersions = [
+    "0.0.0",
+    "1.0.0",
+    "1.4.0",
+    "1.5.0",
+    "1.7.1",
+    "1.8.0",
+    undefined
+  ];
 
-  const testVersions = ["0.0.0", "1.0.0", "1.4.0", "1.5.0", "1.7.1", "1.8.0"];
-
-  testVersions.forEach(version => {
-    test(`with version ${version}`, () => {
-      expect(getCoreInfo(prettier.getSupportInfo(version))).toMatchSnapshot();
-    });
+  testVersions.forEach((version, index) => {
+    const info = getCoreInfo(version);
+    if (index === 0) {
+      test(`with version ${version}`, () => {
+        expect(info).toMatchSnapshot();
+      });
+    } else {
+      const previousVersion = testVersions[index - 1];
+      const previousInfo = getCoreInfo(previousVersion);
+      test(`with version ${previousVersion} -> ${version}`, () => {
+        expect(snapshotDiff(previousInfo, info)).toMatchSnapshot();
+      });
+    }
   });
 });
 
@@ -21,7 +35,8 @@ describe("CLI --support-info", () => {
   runPrettier("cli", "--support-info").test({ status: 0 });
 });
 
-function getCoreInfo(supportInfo) {
+function getCoreInfo(version) {
+  const supportInfo = prettier.getSupportInfo(version);
   const languages = supportInfo.languages.reduce(
     (obj, language) =>
       Object.assign({ [language.name]: language.parsers }, obj),
