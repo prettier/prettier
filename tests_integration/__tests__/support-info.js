644b7333db0d0944ed66bdb1a88e654ec9ac2798
Ika <ikatyang@gmail.com>
2017-12-07 17:04:46 +0800

    test: reduce unnecessary data

--- tests_integration/__tests__/support-info.js
+++ tests_integration/__tests__/support-info.js
@@ -5,22 +5,14 @@ const runPrettier = require("../runPrettier");
 
 describe("API getSupportInfo()", () => {
   test("no arguments", () => {
-    expect(prettier.getSupportInfo()).toMatchSnapshot();
+    expect(getCoreInfo(prettier.getSupportInfo())).toMatchSnapshot();
   });
 
   const testVersions = ["0.0.0", "1.0.0", "1.4.0", "1.5.0", "1.7.1", "1.8.0"];
 
   testVersions.forEach(version => {
     test(`with version ${version}`, () => {
-      expect(
-        prettier
-          .getSupportInfo(version)
-          .languages.reduce(
-            (obj, language) =>
-              Object.assign({ [language.name]: language.parsers }, obj),
-            {}
-          )
-      ).toMatchSnapshot();
+      expect(getCoreInfo(prettier.getSupportInfo(version))).toMatchSnapshot();
     });
   });
 });
@@ -28,3 +20,12 @@ describe("API getSupportInfo()", () => {
 describe("CLI --support-info", () => {
   runPrettier("cli", "--support-info").test({ status: 0 });
 });
+
+function getCoreInfo(supportInfo) {
+  const languages = supportInfo.languages.reduce(
+    (obj, language) =>
+      Object.assign({ [language.name]: language.parsers }, obj),
+    {}
+  );
+  return { languages };
+}
