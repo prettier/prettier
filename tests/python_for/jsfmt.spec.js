335a038311bd3d979be142f18c77587e446ab49b
Patrick Arminio <patrick.arminio@gmail.com>
2017-12-10 20:54:29 +0000

    Fix lint

--- tests/python_for/jsfmt.spec.js
+++ tests/python_for/jsfmt.spec.js
@@ -1,1 +1,2 @@
-run_spec(__dirname, ["python"], { pythonVersion: "2" }); run_spec(__dirname, ["python"], { pythonVersion: "3" });
+run_spec(__dirname, ["python"], { pythonVersion: "2" });
+run_spec(__dirname, ["python"], { pythonVersion: "3" });
