8cca6f1604166f7ba34b18b92c7ee8486e033ce2
Mike Grip <mgrip@Mikes-MacBook-Pro.local>
2017-12-16 12:08:27 -0500

    adding group to function

--- src/printer-php.js
+++ src/printer-php.js
@@ -1,150 +1,150 @@
-"use strict";
+'use strict'
 
-const docBuilders = require("./doc-builders");
-const concat = docBuilders.concat;
-const join = docBuilders.join;
-const line = docBuilders.line;
-const group = docBuilders.group;
-const indent = docBuilders.indent;
-const hardline = docBuilders.hardline;
-const softline = docBuilders.softline;
+const docBuilders = require('./doc-builders')
+const concat = docBuilders.concat
+const join = docBuilders.join
+const line = docBuilders.line
+const group = docBuilders.group
+const indent = docBuilders.indent
+const hardline = docBuilders.hardline
+const softline = docBuilders.softline
 
 function genericPrint(path) {
-  const n = path.getValue();
+  const n = path.getValue()
   if (!n) {
-    return "";
-  } else if (typeof n === "string") {
-    return n;
+    return ''
+  } else if (typeof n === 'string') {
+    return n
   }
-  return handleNode(n);
+  return handleNode(n)
 }
 
 function handleLiteral(node) {
   switch (node.kind) {
-    case "boolean":
-      return node.value ? "true" : "false";
-    case "string":
-      return "'" + node.value + "'";
-    case "number":
-      return node.value;
-    case "inline":
-    case "magic":
-    case "nowdoc":
-    case "encapsed":
+    case 'boolean':
+      return node.value ? 'true' : 'false'
+    case 'string':
+      return "'" + node.value + "'"
+    case 'number':
+      return node.value
+    case 'inline':
+    case 'magic':
+    case 'nowdoc':
+    case 'encapsed':
     default:
-      return "Not yet accounted for";
+      return 'Not yet accounted for'
   }
 }
 
 function handleNode(node) {
   switch (node.kind) {
-    case "program":
+    case 'program':
       return concat([
-        "<?php",
+        '<?php',
         concat(node.children.map(child => concat([line, handleNode(child)])))
-      ]);
-    case "assign":
+      ])
+    case 'assign':
       return concat([
-        join(" = ", [handleNode(node.left), handleNode(node.right)]),
-        ";"
-      ]);
-    case "variable":
-      return "$" + node.name;
-    case "identifier":
+        join(' = ', [handleNode(node.left), handleNode(node.right)]),
+        ';'
+      ])
+    case 'variable':
+      return '$' + node.name
+    case 'identifier':
       // @TODO: do we need to conider node.resolution?
-      return node.name;
+      return node.name
 
     // literals
-    case "boolean":
-    case "string":
-    case "number":
-    case "inline":
-    case "magic":
-    case "nowdoc":
-    case "encapsed":
-      return handleLiteral(node);
+    case 'boolean':
+    case 'string':
+    case 'number':
+    case 'inline':
+    case 'magic':
+    case 'nowdoc':
+    case 'encapsed':
+      return handleLiteral(node)
 
     // operation
-    case "pre":
-      return concat([node.type + node.type, handleNode(node.what), ";"]);
-    case "post":
-      return concat([handleNode(node.what), node.type + node.type, ";"]);
-    case "bin":
+    case 'pre':
+      return concat([node.type + node.type, handleNode(node.what), ';'])
+    case 'post':
+      return concat([handleNode(node.what), node.type + node.type, ';'])
+    case 'bin':
       return concat([
         handleNode(node.left),
-        " ",
+        ' ',
         node.type,
-        " ",
+        ' ',
         handleNode(node.right)
-      ]);
-    case "parenthesis":
-      return concat(["(", handleNode(node.inner), ")"]);
-    case "unary":
-      return "unary needs to be implemented";
-    case "cast":
-      return "cast needs to be implemented";
+      ])
+    case 'parenthesis':
+      return concat(['(', handleNode(node.inner), ')'])
+    case 'unary':
+      return 'unary needs to be implemented'
+    case 'cast':
+      return 'cast needs to be implemented'
 
     // statements
-    case "do":
+    case 'do':
       return concat([
-        "do {",
+        'do {',
         indent(concat([line, handleNode(node.body)])),
         line,
-        group(concat(["} while (", handleNode(node.test), ");"]))
-      ]);
-    case "for":
+        group(concat(['} while (', handleNode(node.test), ');']))
+      ])
+    case 'for':
       return concat([
-        "for (",
+        'for (',
         concat(node.init.map(init => handleNode(init))),
         concat(node.test.map(test => handleNode(test))),
-        ";",
+        ';',
         concat(node.increment.map(increment => handleNode(increment))),
-        ") {",
+        ') {',
         indent(concat([line, handleNode(node.body)])),
         line,
-        "}"
-      ]);
-    case "foreach":
+        '}'
+      ])
+    case 'foreach':
       return concat([
-        "foreach (",
+        'foreach (',
         handleNode(node.source),
-        " as ",
+        ' as ',
         node.key
-          ? join(" => ", [handleNode(node.key), handleNode(node.value)])
+          ? join(' => ', [handleNode(node.key), handleNode(node.value)])
           : handleNode(node.value),
-        ") {",
+        ') {',
         indent(concat([line, handleNode(node.body)])),
         line,
-        "}"
-      ]);
-    case "if":
+        '}'
+      ])
+    case 'if':
       const handleIfAlternate = alternate => {
         if (!alternate) {
-          return "}";
+          return '}'
         }
-        if (alternate.kind === "if") {
-          return concat(["} else", handleNode(alternate)]);
+        if (alternate.kind === 'if') {
+          return concat(['} else', handleNode(alternate)])
         }
         return concat([
-          "} else {",
+          '} else {',
           indent(concat([line, handleNode(alternate)])),
           line,
-          "}"
-        ]);
-      };
+          '}'
+        ])
+      }
       return concat([
-        "if (",
+        'if (',
         handleNode(node.test),
-        ") {",
+        ') {',
         indent(concat([line, handleNode(node.body)])),
         line,
         handleIfAlternate(node.alternate)
-      ]);
-    case "switch":
+      ])
+    case 'switch':
       return concat([
-        "switch (",
+        'switch (',
         handleNode(node.test),
-        ") {",
+        ') {',
         indent(
           concat(
             node.body.children.map(caseChild =>
@@ -153,67 +153,79 @@ function handleNode(node) {
           )
         ),
         line,
-        "}"
-      ]);
-    case "case":
+        '}'
+      ])
+    case 'case':
       return concat([
-        node.test ? concat(["case ", handleNode(node.test), ":"]) : "default:",
+        node.test ? concat(['case ', handleNode(node.test), ':']) : 'default:',
         indent(concat([line, handleNode(node.body)]))
-      ]);
-    case "break":
-      return "break;";
-    case "while":
+      ])
+    case 'break':
+      return 'break;'
+    case 'while':
       return concat([
-        "while (",
+        'while (',
         handleNode(node.test),
-        ") {",
+        ') {',
         indent(concat([line, handleNode(node.body)])),
         line,
-        "}"
-      ]);
-    case "block":
+        '}'
+      ])
+    case 'block':
       return concat(
         node.children.map((child, i) => {
           if (i === 0) {
-            return handleNode(child);
+            return handleNode(child)
           }
-          return concat([line, handleNode(child)]);
+          return concat([line, handleNode(child)])
         })
-      );
-    case "return":
+      )
+    case 'return':
       if (node.expr) {
-        concat(["return", handleNode(node.expr), ";"]);
+        concat(['return', handleNode(node.expr), ';'])
       } else {
-        return "return;";
+        return 'return;'
       }
-      return concat(["return ", handleNode(node.expr), ";"]);
+      return concat(['return ', handleNode(node.expr), ';'])
     // functions
-    case "function":
+    case 'function':
       return concat([
-        group(concat(["function ", node.name, "("])),
-        group(join(", ", node.arguments.map(argument => handleNode(argument)))),
-        ") {",
+        group(concat(['function ', node.name, '('])),
+        group(
+          concat([
+            indent(
+              join(
+                ', ',
+                node.arguments.map(argument =>
+                  concat([softline, handleNode(argument)])
+                )
+              )
+            ),
+            softline
+          ])
+        ),
+        group(') {'),
         indent(concat([hardline, handleNode(node.body)])),
-        concat([hardline, "}"])
-      ]);
-    case "parameter":
+        concat([hardline, '}'])
+      ])
+    case 'parameter':
       if (node.value) {
         return group(
-          join(" = ", [concat(["$", node.name]), handleNode(node.value)])
-        );
+          join(' = ', [concat(['$', node.name]), handleNode(node.value)])
+        )
       }
-      return concat(["$", node.name]);
-    case "call":
+      return concat(['$', node.name])
+    case 'call':
       return concat([
         handleNode(node.what),
-        "(",
-        join(", ", node.arguments.map(argument => handleNode(argument))),
-        ");"
-      ]);
+        '(',
+        join(', ', node.arguments.map(argument => handleNode(argument))),
+        ');'
+      ])
     // we haven't implemented this type of node yet
     default:
-      return concat(["whoops " + node.kind + " hasn't been implemented yet"]);
+      return concat(['whoops ' + node.kind + " hasn't been implemented yet"])
   }
 }
 
-module.exports = genericPrint;
+module.exports = genericPrint
