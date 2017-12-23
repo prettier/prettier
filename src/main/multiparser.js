f211d10cdae07b1368aa66f51c5839023a8a3368
Lucas Azzola <derflatulator@gmail.com>
2017-12-24 00:19:56 +1100

    Flatten closure

--- src/main/multiparser.js
+++ src/main/multiparser.js
@@ -4,26 +4,30 @@ const comments = require("./comments");
 
 function printSubtree(printer, path, print, options) {
   if (printer.embed) {
-    return printer.embed(path, print, createTextToDoc(options), options);
+    return printer.embed(
+      path,
+      print,
+      (text, partialNextOptions) =>
+        textToDoc(text, partialNextOptions, options),
+      options
+    );
   }
 }
 
-function createTextToDoc(parentOptions) {
-  return function textToDoc(text, partialNextOptions) {
-    const nextOptions = Object.assign({}, parentOptions, partialNextOptions, {
-      parentParser: parentOptions.parser,
-      originalText: text
-    });
-    if (nextOptions.parser === "json") {
-      nextOptions.trailingComma = "none";
-    }
+function textToDoc(text, partialNextOptions, parentOptions) {
+  const nextOptions = Object.assign({}, parentOptions, partialNextOptions, {
+    parentParser: parentOptions.parser,
+    originalText: text
+  });
+  if (nextOptions.parser === "json") {
+    nextOptions.trailingComma = "none";
+  }
 
-    const ast = require("./parser").parse(text, nextOptions);
-    const astComments = ast.comments;
-    delete ast.comments;
-    comments.attach(astComments, ast, text, nextOptions);
-    return require("./ast-to-doc")(ast, nextOptions);
-  };
+  const ast = require("./parser").parse(text, nextOptions);
+  const astComments = ast.comments;
+  delete ast.comments;
+  comments.attach(astComments, ast, text, nextOptions);
+  return require("./ast-to-doc")(ast, nextOptions);
 }
 
 function fromVue(path, print, options) {
