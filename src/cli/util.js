b63c669ed9821a0e0d6918243ef4f4a091b222a9
Jimmy Jia <tesrin@gmail.com>
2017-12-28 01:08:58 -0800

    Support plugins field in config (#3584)
    * Actually support plugins field in config
    
    * Resolve plugin parsers
    
    * Fix default from CLI for "plugins"

--- src/cli/util.js
+++ src/cli/util.js
@@ -37,9 +37,12 @@ function getOptions(argv) {
   );
 }
 
-function dashifyObject(object) {
+function cliifyOptions(object) {
   return Object.keys(object || {}).reduce((output, key) => {
-    output[dashify(key)] = object[key];
+    const apiOption = constant.apiDetailedOptionMap[key];
+    const cliKey = apiOption ? apiOption.name : key;
+
+    output[dashify(cliKey)] = object[key];
     return output;
   }, {});
 }
@@ -202,8 +205,8 @@ function parseArgsToOptions(argv, overrideDefaults) {
           boolean: constant.minimistOptions.boolean,
           default: Object.assign(
             {},
-            dashifyObject(apiDefaultOptions),
-            dashifyObject(overrideDefaults)
+            cliifyOptions(apiDefaultOptions),
+            cliifyOptions(overrideDefaults)
           )
         })
       ),
@@ -619,7 +622,10 @@ function normalizeConfig(type, rawConfig, options) {
       return;
     }
 
-    const option = constant.detailedOptionMap[key];
+    let option = constant.detailedOptionMap[key];
+    if (type === "api" && option === undefined) {
+      option = constant.apiDetailedOptionMap[key];
+    }
 
     // unknown option
     if (option === undefined) {
