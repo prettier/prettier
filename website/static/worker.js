0af436cd47f9c419774fea0d9e47a683b9e74330
Ika <ikatyang@gmail.com>
2017-12-30 21:54:36 +0800

    fix(playground): use `prVersion` (#3613)

--- website/static/worker.js
+++ website/static/worker.js
@@ -103,7 +103,7 @@ self.onmessage = function(message) {
     doc: doc,
     ast: ast,
     formatted2: formatted2,
-    version: prettier.version
+    version: prettier.prVersion || prettier.version
   });
 };
 
