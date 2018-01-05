b165329bf62503e80d4420983be9aa2d06fd1d49
Jed Fox <git@twopointzero.us>
2018-01-05 11:11:35 -0500

    Add TypeScript example

--- website/static/playground.js
+++ website/static/playground.js
@@ -379,6 +379,28 @@ function getExample(parser) {
         "",
         "}"
       ].join("\n");
+    case "typescript":
+      return [
+        "interface MyInterface {"
+        "  foo(): string,",
+        "  bar: Array<number>,",
+        "}",
+        "",
+        "export abstract class Foo implements MyInterface {",
+        "  foo() {",
+        "            // TODO: return an actual value here",
+        "        return 'hello'",
+        "      }",
+        "  get bar() {",
+        "    return [  1,",
+        "",
+        "      2, 3,",
+        "    ]",
+        "  }",
+        "}",
+        "",
+        "type RequestType = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'OPTIONS' | 'CONNECT' | 'DELETE' | 'TRACE'"
+      ].join("\n");
     default:
       return "";
   }
