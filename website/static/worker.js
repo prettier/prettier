5e2e7a2881d07ff0aa4b4673becc9cb4ce2f6cae
Ika <ikatyang@gmail.com>
2018-01-07 19:59:14 +0800

    feat(playground): add example placeholder (#3657)
    * feat(playground): add example placeholder
    
    * Add TypeScript example
    
    * fix: mock process.binding
    
    * fix: add missing comma
    
    * refactor: use legacy function
    
    * fix: mock return `{}`
    
    * Return the error stack when there’s a non-SyntaxError thrown
    
    * Code style
    
    * fix: mock path.resolve
    
    * chore: cache placeholder.js
    
    * fix mock path.resolve attempt 2
    
    * fix: mock path.{parse,join,dirname}
    
    * fix: mock process.cwd
    
    * fix: mock path.parse root

--- website/static/worker.js
+++ website/static/worker.js
@@ -5,6 +5,12 @@
 self.global = self;
 self.util = {};
 self.path = {};
+self.path.resolve = self.path.join = self.path.dirname = function() {
+  return "";
+};
+self.path.parse = function() {
+  return { root: "" };
+};
 self.Buffer = {
   isBuffer: function() {
     return false;
@@ -12,13 +18,23 @@ self.Buffer = {
 };
 self.constants = {};
 // eslint-disable-next-line
-module$1 = module = path = os = crypto = {};
+module$1 = module = os = crypto = {};
 self.fs = { readFile: function() {} };
 // eslint-disable-next-line no-undef
 os.homedir = function() {
   return "/home/prettier";
 };
-self.process = { argv: [], env: { PRETTIER_DEBUG: true }, version: "v8.5.0" };
+self.process = {
+  argv: [],
+  env: { PRETTIER_DEBUG: true },
+  version: "v8.5.0",
+  binding: function() {
+    return {};
+  },
+  cwd: function() {
+    return "";
+  }
+};
 self.assert = { ok: function() {}, strictEqual: function() {} };
 self.require = function require(path) {
   if (path === "stream") {
@@ -111,6 +127,12 @@ function formatCode(text, options) {
   try {
     return prettier.format(text, options);
   } catch (e) {
-    return String(e);
+    if (e.constructor && e.constructor.name === "SyntaxError") {
+      // Likely something wrong with the user's code
+      return String(e);
+    }
+    // Likely a bug in Prettier
+    // Provide the whole stack for debugging
+    return e.stack || String(e);
   }
 }
