67008198d4d708d779523b022bede9e2d24db16b
Lucas Azzola <derflatulator@gmail.com>
2018-01-09 23:33:03 +1100

    Add tests for preprocessing

---
+++ tests_integration/plugins/preprocess/plugin.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const concat = require("../../../src/doc").builders.concat;
+
+module.exports = {
+  languages: [
+    {
+      name: "foo",
+      parsers: ["foo-parser"],
+      extensions: [".foo"],
+      since: "1.0.0"
+    }
+  ],
+  parsers: {
+    "foo-parser": {
+      preprocess: text => `preprocessed:${text}`,
+      parse: text => ({ text }),
+      astFormat: "foo-ast"
+    }
+  },
+  printers: {
+    "foo-ast": {
+      print: path => path.getValue().text
+    }
+  }
+};
