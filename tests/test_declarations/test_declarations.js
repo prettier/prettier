aa0437cc60e944542f0c0bcfdb3134426364ddb8
Quinn Hoyer <quinn@themightyquinn.com>
2018-01-12 18:02:31 -0800

    Add support for skip() (#3706) (#3735)
    * Add support for skip() (#3706)
    
    * Fix regex & add tests

--- tests/test_declarations/test_declarations.js
+++ tests/test_declarations/test_declarations.js
@@ -85,6 +85,10 @@ ftest("does something really long and complicated so I have to write a very long
 
 xtest("does something really long and complicated so I have to write a very long name for the describe block", () => {});
 
+skip(`does something really long and complicated so I have to write a very long name for the test`, () => {});
+
+skip("does something really long and complicated so I have to write a very long name for the test", () => {});
+
 // Should break
 
 it.only("does something really long and complicated so I have to write a very long name for the test", 10, () => {
@@ -98,3 +102,5 @@ it.only.only("does something really long and complicated so I have to write a ve
 it.only.only("does something really long and complicated so I have to write a very long name for the test", (a, b, c) => {
   console.log("hello!");
 });
+
+xskip("does something really long and complicated so I have to write a very long name for the test", () => {});
