81673b567d02b97e5f79eaeb372556a8babbb229
Ika <ikatyang@gmail.com>
2018-01-15 10:58:57 +0800

    test: add test cases

--- tests/ternaries/indent.js
+++ tests/ternaries/indent.js
@@ -1,1 +1,25 @@
 aaaaaaaaaaaaaaa ? bbbbbbbbbbbbbbbbbb : ccccccccccccccc ? ddddddddddddddd : eeeeeeeeeeeeeee ? fffffffffffffff : gggggggggggggggg
+
+a
+    ? {
+        a: 0
+      }
+    : {
+        a: {
+             a: 0
+           }
+            ? {
+                a: 0
+              }
+            : {
+                y: {
+                    a: 0
+                }
+                    ? {
+                        a: 0
+                    }
+                    : {
+                        a: 0
+                    }
+            }
+      }
