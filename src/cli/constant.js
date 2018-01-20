7fcf4f1a593674b94bf6341035803c1849f61bbc
Ika <ikatyang@gmail.com>
2018-01-20 19:08:28 +0800

    style: remove trailing whitespace

--- src/cli/constant.js
+++ src/cli/constant.js
@@ -190,7 +190,7 @@ const options = {
 
 const usageSummary = dedent`
   Usage: prettier [options] [file/glob ...]
-  
+
   By default, output is written to stdout.
   Stdin is read if it is piped to Prettier and no files are given.
 `;
