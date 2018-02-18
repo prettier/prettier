13a1f59b16dcef2c9f64ba0dee006550772134bd
Skubie Doo <sean@skube.com>
2018-02-18 11:31:20 -0500

    Add JSON example
    Not sure if this is a good or useful example case.

--- website/static/playground.js
+++ website/static/playground.js
@@ -439,6 +439,23 @@ function getExample(parser) {
         "  }",
         "}"
       ].join("\n");
+    case "json":
+      // Excerpted & adapted from Wikipedia, under the Creative Commons Attribution-ShareAlike License
+      return [
+        '{"allOn": "Single", "Line": "example",',
+        '"noSpace":true,',
+        '  "quote": {',
+        "    'singleQuote': 'example',",
+        '                  "indented": true,',
+        "  },",
+        '  "phoneNumbers": [',
+        '    {"type": "home",',
+        '      "number": "212 555-1234"},',
+        '    {"type": "office",',
+        '      "trailing": "commas by accident"},',
+        "  ],",
+        "}"
+      ].join("\n");
     default:
       return "";
   }
