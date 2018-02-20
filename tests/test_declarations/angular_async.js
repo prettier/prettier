e067e7ad2266f5573274cd042b4dfe7cc61fea9d
Lucas Duailibe <lucasds@gmail.com>
2018-02-20 15:29:08 -0300

    Inline Angular async tests

---
+++ tests/test_declarations/angular_async.js
@@ -0,0 +1,15 @@
+beforeEach(async(() => {
+  // code
+}));
+
+it('should create the app', async(() => {
+  // code
+}));
+
+it(`should have as title 'app'`, async(() => {
+  // code
+}));
+
+it("should render title in a h1 tag", async(() => {
+  // code
+}));
