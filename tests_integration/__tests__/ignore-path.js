762f57323f139fe734f1d790c2ca8d4891b92544
Lucas Duailibe <lucasds@gmail.com>
2018-02-27 12:59:57 -0300

    Fix printing ignored files with --debug-check

--- tests_integration/__tests__/ignore-path.js
+++ tests_integration/__tests__/ignore-path.js
@@ -19,6 +19,12 @@ describe("support .prettierignore", () => {
   });
 });
 
+describe("ignore file when using --debug-check", () => {
+  runPrettier("cli/ignore-path", ["**/*.js", "--debug-check"]).test({
+    status: 0
+  });
+});
+
 describe("outputs files as-is if no --write", () => {
   runPrettier("cli/ignore-path", ["regular-module.js"]).test({
     status: 0
