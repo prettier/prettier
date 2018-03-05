f54bb1458423821eeb0dd457d2c7c7da92e44e00
Simon Lydell <simon.lydell@gmail.com>
2018-03-05 09:01:07 +0100

    playground: Add Flow example

--- website/static/playground.js
+++ website/static/playground.js
@@ -379,6 +379,17 @@ function getExample(parser) {
         "",
         "}"
       ].join("\n");
+    case "flow":
+      return [
+        "declare export function graphql<Props, Variables, Component: React$ComponentType<Props>>",
+        "  (query: GQLDocument, config?: Config<Props, QueryConfigOptions<Variables>>):",
+        "  (Component: Component) => React$ComponentType<$Diff<React$ElementConfig<Component>, {",
+        "    data: Object|void,",
+        "    mutate: Function|void",
+        "  }>>",
+        "",
+        'declare type FetchPolicy = "cache-first" | "cache-and-network" | "network-only" | "cache-only"'
+      ].join("\n");
     case "typescript":
       return [
         "interface MyInterface {",
