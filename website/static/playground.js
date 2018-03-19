feb41a5516bca96977a2ff5f8eac88c74147c5c3
Lipis <lipiridis@gmail.com>
2018-03-19 16:32:06 +0200

    Richer markdown example in the playground (#4176)
    * Richer markdown example in the playground
    
    * Link is back

--- website/static/playground.js
+++ website/static/playground.js
@@ -496,18 +496,31 @@ function getExample(parser) {
       ].join("\n");
     case "markdown":
       return [
+        "Header",
+        "======",
+        "",
         "_Look,_ code blocks are formatted *too!*",
         "",
         "``` js",
         "function identity(x) { return x }",
         "```",
         "",
+        "Pilot|Airport|Hours",
+        "--|:--:|--:",
+        "John Doe|SKG|1338",
+        "Jane Roe|JFK|314",
+        "",
+        "- - - - - - - - - - - - - - -",
+        "",
         "+ List",
         " + with a [link] (/to/somewhere)",
         "+ and [another one]",
         "",
         "",
-        "  [another one]:  http://example.com 'Example title'"
+        "  [another one]:  http://example.com 'Example title'",
+        "",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Curabitur consectetur maximus risus, sed maximus tellus tincidunt et."
       ].join("\n");
     case "vue":
       return [
