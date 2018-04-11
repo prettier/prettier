7819a8f010292f95a6b0a0cdead03b48be79f257
Christopher Chedeau <vjeuxx@gmail.com>
2018-04-10 18:03:34 -0700

    [flow] Upgrade flow to 0.69 and enable ?. support (#4296)
    Flow now support ?. so this PR enables it in prettier

--- src/language-js/parser-flow.js
+++ src/language-js/parser-flow.js
@@ -12,7 +12,8 @@ function parse(text /*, parsers, opts*/) {
   const ast = flowParser.parse(text, {
     esproposal_class_instance_fields: true,
     esproposal_class_static_fields: true,
-    esproposal_export_star_as: true
+    esproposal_export_star_as: true,
+    esproposal_optional_chaining: true
   });
 
   if (ast.errors.length > 0) {
