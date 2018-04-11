a3be9f5505bee692721a14f0286dd15828bafd7a
Lucas Duailibe <lucasds@gmail.com>
2018-04-11 14:26:48 -0300

    fix lint

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,25 +1,23 @@
 "use strict";
 
-const webpack = require('webpack')
-
 module.exports = {
   entry: {
-    playground: './playground/index.js'
+    playground: "./playground/index.js"
   },
   output: {
-    filename: '[name].js',
-    path: __dirname + '/static/'
+    filename: "[name].js",
+    path: __dirname + "/static/"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['env']
+          presets: ["env"]
         }
       }
     ]
   }
-}
+};
