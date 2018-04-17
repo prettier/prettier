9d437ded930363088c8fba3da1e0def6fadda695
Tom MacWright <tom@macwright.org>
2018-04-17 16:07:42 -0700

    Simplify node_options vs options

--- src/main/node_options.js
+++ src/main/node_options.js
@@ -1,101 +1,15 @@
 "use strict";
 
-const inferParser = require("./infer-parser");
-const getSupportInfo = require("../common/support").getSupportInfo;
-const normalizer = require("./options-normalizer");
+const { normalize: baseNormalize, hiddenDefaults } = require("./options");
 const loadPlugins = require("../common/load-plugins");
-const resolveParser = require("./parser").resolveParser;
-const getPlugin = require("./get-plugin");
 
-const hiddenDefaults = {
-  astFormat: "estree",
-  printer: {},
-  locStart: null,
-  locEnd: null
-};
-
-// Copy options and fill in default values.
 function normalize(options, opts) {
-  opts = opts || {};
-
-  const rawOptions = Object.assign({}, options);
-
-  const plugins = loadPlugins(rawOptions.plugins);
-  rawOptions.plugins = plugins;
-
-  const supportOptions = getSupportInfo(null, {
-    plugins,
-    pluginsLoaded: true,
-    showUnreleased: true,
-    showDeprecated: true
-  }).options;
-  const defaults = supportOptions.reduce(
-    (reduced, optionInfo) =>
-      Object.assign(reduced, { [optionInfo.name]: optionInfo.default }),
-    Object.assign({}, hiddenDefaults)
-  );
-
-  if (opts.inferParser !== false) {
-    if (
-      rawOptions.filepath &&
-      (!rawOptions.parser || rawOptions.parser === defaults.parser)
-    ) {
-      const inferredParser = inferParser(
-        rawOptions.filepath,
-        rawOptions.plugins
-      );
-      if (inferredParser) {
-        rawOptions.parser = inferredParser;
-      }
-    }
-  }
-
-  const parser = resolveParser(
-    !rawOptions.parser
-      ? rawOptions
-      : // handle deprecated parsers
-        normalizer.normalizeApiOptions(
-          rawOptions,
-          [supportOptions.find(x => x.name === "parser")],
-          { passThrough: true, logger: false }
-        )
-  );
-  rawOptions.astFormat = parser.astFormat;
-  rawOptions.locEnd = parser.locEnd;
-  rawOptions.locStart = parser.locStart;
-
-  const plugin = getPlugin(rawOptions);
-  rawOptions.printer = plugin.printers[rawOptions.astFormat];
-
-  const pluginDefaults = supportOptions
-    .filter(
-      optionInfo =>
-        optionInfo.pluginDefaults && optionInfo.pluginDefaults[plugin.name]
-    )
-    .reduce(
-      (reduced, optionInfo) =>
-        Object.assign(reduced, {
-          [optionInfo.name]: optionInfo.pluginDefaults[plugin.name]
-        }),
-      {}
-    );
-
-  const mixedDefaults = Object.assign({}, defaults, pluginDefaults);
-
-  Object.keys(mixedDefaults).forEach(k => {
-    if (rawOptions[k] == null) {
-      rawOptions[k] = mixedDefaults[k];
-    }
-  });
-
-  if (rawOptions.parser === "json") {
-    rawOptions.trailingComma = "none";
-  }
-
-  return normalizer.normalizeApiOptions(
-    rawOptions,
-    supportOptions,
-    Object.assign({ passThrough: Object.keys(hiddenDefaults) }, opts)
+  options = options || {};
+  return baseNormalize(
+    Object.assign({}, options, {
+      plugins: loadPlugins(options.plugins)
+    }),
+    opts
   );
 }
 
