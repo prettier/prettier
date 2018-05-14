5142a37d6401d7bc944521dc53eb0a4a1a7645ad
Brian Ng <bng412@gmail.com>
2018-05-14 09:51:41 -0500

    Add a few more do-expression tests

--- tests/do/do.js
+++ tests/do/do.js
@@ -5,3 +5,33 @@ const envSpecific = {
       else if(env === 'development') 'http://localhost:4000';
     }
 };
+
+let x = do {
+  let tmp = f();
+  tmp * tmp + 1
+};
+
+let x = do {
+  if (foo()) { f() }
+  else if (bar()) { g() }
+  else { h() }
+};
+
+function foo() {
+  return (
+    <nav>
+      <Home />
+      {
+        do {
+          if (loggedIn) {
+            <LogoutButton />
+          } else {
+            <LoginButton />
+          }
+        }
+      }
+    </nav>
+  );
+}
+
+(do {})
