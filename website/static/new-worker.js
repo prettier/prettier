dd689674dda14f2ca1750f878c79cdfba9106afe
Lucas Duailibe <lucasds@gmail.com>
2018-05-24 19:49:09 -0300

    Fix playground

--- website/static/new-worker.js
+++ website/static/new-worker.js
@@ -92,7 +92,8 @@ function handleMessage(message) {
     delete options.doc;
     delete options.output2;
 
-    options.plugins = [{ parsers: parsers }];
+    var plugins = [{ parsers: parsers }];
+    options.plugins = plugins;
 
     var response = {
       formatted: formatCode(message.code, options),
@@ -115,7 +116,7 @@ function handleMessage(message) {
 
       if (!errored) {
         try {
-          ast = formatCode(ast, { parser: "json" });
+          ast = formatCode(ast, { parser: "json", plugins: plugins });
         } catch (e) {
           ast = JSON.stringify(ast, null, 2);
         }
