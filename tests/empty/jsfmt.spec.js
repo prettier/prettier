2fd58352882c516fdf93bd41cd6eb118a0f425c3
Jed Fox <git@twopointzero.us>
2018-05-27 07:09:41 -0400

    Add a single test suite for blank files in all parsers
    The parsers that don’t yet support blank files have been filtered out of the list until they do.

--- tests/empty/jsfmt.spec.js
+++ tests/empty/jsfmt.spec.js
@@ -1,1 +1,12 @@
-run_spec(__dirname, ["flow", "typescript"]);
+const core = require("../../src/main/core-options");
+
+run_spec(
+  __dirname,
+  core.options.parser.choices
+    .filter(choice => !choice.deprecated)
+    .map(choice => choice.value)
+    .filter(
+      parser =>
+        ["json", "json5", "json-stringify", "graphql"].indexOf(parser) === -1
+    )
+);
