61d35dd6364d6614616d153e57e316ed3c51973b
Lynn Smeria <ae@cephea.de>
2018-06-11 03:29:10 +0200

    fix(typescript): preserve type parameters of import-types (#4656) (#4662)

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -2940,7 +2940,8 @@ function printPathNoParens(path, options, print, args) {
         "import(",
         path.call(print, "argument"),
         ")",
-        !n.qualifier ? "" : concat([".", path.call(print, "qualifier")])
+        !n.qualifier ? "" : concat([".", path.call(print, "qualifier")]),
+        printTypeParameters(path, options, print, "typeParameters")
       ]);
     case "TSLiteralType":
       return path.call(print, "literal");
