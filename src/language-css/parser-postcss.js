1622369109e2ea846e2c16d342e953055de8ccd8
Ika <ikatyang@gmail.com>
2018-06-13 23:49:28 +0800

    refactor: try to fix node 4

--- src/language-css/parser-postcss.js
+++ src/language-css/parser-postcss.js
@@ -3,7 +3,7 @@
 const createError = require("../common/parser-create-error");
 const parseFrontMatter = require("../utils/front-matter");
 const lineColumnToIndex = require("../utils/line-column-to-index");
-const { hasPragma } = require("./pragma");
+const hasPragma = require("./pragma").hasPragma;
 
 // utils
 const utils = require("./utils");
