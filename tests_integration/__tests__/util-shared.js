6c482e2e6c4d771407302fcc999bb3e2b491cbd3
Warren Seine <warren.seine@gmail.com>
2018-06-26 07:20:00 +0200

    Expose `isPreviousLineEmpty` to plugins (#4747). (#4748)

--- tests_integration/__tests__/util-shared.js
+++ tests_integration/__tests__/util-shared.js
@@ -5,6 +5,7 @@ const sharedUtil = require("../../src/common/util-shared");
 test("shared util has correct structure", () => {
   expect(typeof sharedUtil.isNextLineEmpty).toEqual("function");
   expect(typeof sharedUtil.isNextLineEmptyAfterIndex).toEqual("function");
+  expect(typeof sharedUtil.isPreviousLineEmpty).toEqual("function");
   expect(typeof sharedUtil.getNextNonSpaceNonCommentCharacterIndex).toEqual(
     "function"
   );
