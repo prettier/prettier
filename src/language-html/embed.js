0bc075c6e546c8cb995104cee881d10b4165a43a
evilebottnawi <sheo13666q@gmail.com>
2018-06-27 16:53:36 +0300

    feat: better handle embed

--- src/language-html/embed.js
+++ src/language-html/embed.js
@@ -3,7 +3,7 @@
 const { hasNewlineInRange } = require("../common/util");
 const {
   builders: { hardline, concat },
-  utils: { stripTrailingHardline, removeLines }
+  utils: { removeLines }
 } = require("../doc");
 
 function embed(path, print, textToDoc, options) {
@@ -11,45 +11,81 @@ function embed(path, print, textToDoc, options) {
 
   switch (node.type) {
     case "text": {
-      const parent = path.getParentNode();
-      // Inline JavaScript
-      if (
-        parent.type === "script" &&
-        ((!parent.attribs.lang && !parent.attribs.lang) ||
-          parent.attribs.type === "text/javascript" ||
-          parent.attribs.type === "application/javascript")
-      ) {
-        const parser = options.parser === "flow" ? "flow" : "babylon";
-        const doc = textToDoc(getText(options, node), { parser });
-        return concat([hardline, stripTrailingHardline(doc)]);
+      const node = path.getValue();
+      const parentNode = path.getParentNode();
+
+      if (!parentNode) {
+        return null;
       }
 
-      // Inline TypeScript
-      if (
-        parent.type === "script" &&
-        (parent.attribs.type === "application/x-typescript" ||
-          parent.attribs.lang === "ts")
-      ) {
-        const doc = textToDoc(
-          getText(options, node),
-          { parser: "typescript" },
-          options
-        );
-        return concat([hardline, stripTrailingHardline(doc)]);
+      let parser;
+
+      if (parentNode.type === "style") {
+        if (Object.keys(parentNode.attribs).length === 0) {
+          parser = "css";
+        }
+
+        const typeAttr = parentNode.attribs["type"];
+
+        if (typeAttr === "text/css") {
+          parser = "css";
+        } else if (typeAttr === "text/x-scss") {
+          parser = "scss";
+        } else if (typeAttr === "text/less") {
+          parser = "less";
+        } else {
+          const langAttr = parentNode.attribs["lang"];
+
+          if (langAttr === "postcss") {
+            parser = "css";
+          } else if (langAttr === "scss") {
+            parser = "scss";
+          } else if (langAttr === "less") {
+            parser = "less";
+          } else {
+            parser = "css";
+          }
+        }
       }
 
-      // Inline Styles
-      if (parent.type === "style") {
-        const doc = textToDoc(getText(options, node), { parser: "css" });
-        return concat([hardline, stripTrailingHardline(doc)]);
+      if (parentNode.type === "script") {
+        if (Object.keys(parentNode.attribs).length === 0) {
+          parser = "babylon";
+        }
+
+        const typeAttr = parentNode.attribs["type"];
+
+        if (
+          typeAttr === "text/javascript" ||
+          typeAttr === "application/javascript"
+        ) {
+          parser = "babylon";
+        } else if (typeAttr === "application/x-typescript") {
+          parser = "typescript";
+        } else {
+          const langAttr = parentNode.attribs["lang"];
+
+          if (langAttr === "ts" || langAttr === "tsx") {
+            parser = "typescript";
+          } else {
+            parser = "babylon";
+          }
+        }
       }
 
-      break;
+      if (!parser) {
+        return null;
+      }
+
+      return concat([
+        hardline,
+        textToDoc(getText(options, node), { parser: parser }, options)
+      ]);
     }
 
     case "attribute": {
       /*
-       * Vue binding sytax: JS expressions
+       * Vue binding syntax: JS expressions
        * :class="{ 'some-key': value }"
        * v-bind:id="'list-' + id"
        * v-if="foo && !bar"
