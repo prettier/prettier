0d88be36d38f3645a60fc40884828dbe1d862c0f
Ika <ikatyang@gmail.com>
2018-06-27 13:07:16 +0800

    feat(typescript): support TSOptionalType and TSRestType

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -2489,6 +2489,10 @@ function printPathNoParens(path, options, print, args) {
 
       return group(concat(parts));
     }
+    case "TSRestType":
+      return concat(["...", path.call(print, "typeAnnotation")]);
+    case "TSOptionalType":
+      return concat([path.call(print, "typeAnnotation"), "?"]);
     case "FunctionTypeParam":
       return concat([
         path.call(print, "name"),
