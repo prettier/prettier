b4a92c6ee54d51ef769b22bc271ab2741887a5b5
Ika <ikatyang@gmail.com>
2018-07-01 16:17:25 +0800

    feat(playground): use real code sample (#4770)
    * feat(playground): double click to insert example
    
    * fix: correct condition
    
    * feat: real code sample
    
    * fix: remove unnecessary updateValue
    
    * feat: auto-select on focus

--- website/static/service-worker.js
+++ website/static/service-worker.js
@@ -28,7 +28,6 @@ toolbox.precache([
   "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/mode/jsx/jsx.js",
   "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/mode/css/css.js",
   "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/mode/markdown/markdown.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/display/placeholder.js",
   "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/display/rulers.js",
   "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/search/searchcursor.js",
   "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/edit/matchbrackets.js",
