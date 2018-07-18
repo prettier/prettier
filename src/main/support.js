1a2c4e663e7665e623de88bc57063920f20a02f8
Ika <ikatyang@gmail.com>
2018-07-18 09:38:26 +0800

    chore: use pre-release version number on master (#4855)
    * chore: use pre-release version number on master
    
    * docs: tweak comment

--- src/main/support.js
+++ src/main/support.js
@@ -17,7 +17,9 @@ function getSupportInfo(version, opts) {
   );
 
   if (!version) {
-    version = currentVersion;
+    // pre-release version is smaller than the normal version in semver,
+    // we need to treat it as the normal one so as to test new features.
+    version = currentVersion.split("-", 1)[0];
   }
 
   const plugins = opts.plugins;
