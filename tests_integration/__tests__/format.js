bcde3a76a55c8781fbe0a6c4f2971531bbadbc56
Ika <ikatyang@gmail.com>
2018-07-19 20:49:34 +0800

    test: fix typo

--- tests_integration/__tests__/format.js
+++ tests_integration/__tests__/format.js
@@ -2,7 +2,7 @@
 
 const prettier = require("prettier/local");
 
-test("yaml parser should handle CRLF correclty", () => {
+test("yaml parser should handle CRLF correctly", () => {
   const input = "a:\r\n  123\r\n";
   expect(prettier.format(input, { parser: "yaml" })).toMatchSnapshot();
 });
