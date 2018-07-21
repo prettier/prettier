930853516692d001ffaaa83e1a7d0e342602de2b
Ika <ikatyang@gmail.com>
2018-07-21 20:45:32 +0800

    fix: correct range format

--- tests_integration/__tests__/format.js
+++ tests_integration/__tests__/format.js
@@ -6,25 +6,3 @@ test("yaml parser should handle CRLF correctly", () => {
   const input = "a:\r\n  123\r\n";
   expect(prettier.format(input, { parser: "yaml" })).toMatchSnapshot();
 });
-
-test("vue parser should work with range format", () => {
-  const input = `<template>
-  <p>Templates are not formatted yet ...
-    </p>
-</template>
-
-<script>
-let Prettier = format => { your.js('though') }
-</script>
-
-<style>
-.and { css: too! important }
-</style>`;
-  expect(() =>
-    prettier.format(input, {
-      parser: "vue",
-      rangeStart: 1,
-      rangeEnd: input.length
-    })
-  ).not.toThrowError();
-});
