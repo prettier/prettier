8e88fb1c16846afb02ae3bcb366bee8066bdf78c
Jorge Gonzalez <jrgqhd@gmail.com>
2018-07-29 14:37:15 -0400

    Create snapshot for load-toml

--- tests_integration/__tests__/load-toml.js
+++ tests_integration/__tests__/load-toml.js
@@ -20,28 +20,9 @@ enabled = true
 
   const wrongToml = "///ERROR///";
 
-  const expectedObject = {
-    title: "TOML Example",
-    owner: {
-      name: "Tom Preston-Werner",
-      dob: new Date("1979-05-27T15:32:00.000Z")
-    },
-    database: {
-      server: "192.168.1.1",
-      ports: [8001, 8001, 8002],
-      connection_max: 5000,
-      enabled: true
-    }
-  };
-
   test("loads toml successfully", () => {
     const parsedToml = loadToml(currentFile, exampleToml);
-
-    expect(parsedToml.title).toBe("TOML Example");
-    expect(parsedToml).toEqual(expectedObject);
-    expect(Object.keys(parsedToml).sort()).toEqual(
-      Object.keys(expectedObject).sort()
-    );
+    expect(parsedToml).toMatchSnapshot();
   });
 
   test("throws error on incorrect toml", () => {
