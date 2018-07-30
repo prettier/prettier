f55cf6185ebc95cef548cba0567988834fe385b0
Jorge Gonzalez <jrgqhd@gmail.com>
2018-07-29 21:31:54 -0400

    Use @iarna/toml/parse-string

--- src/utils/load-toml.js
+++ src/utils/load-toml.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const toml = require("@iarna/toml");
+const parse = require("@iarna/toml/parse-string");
 
 module.exports = function(filePath, content) {
   try {
-    return toml.parse(content);
+    return parse(content);
   } catch (error) {
     error.message = `TOML Error in ${filePath}:\n${error.message}`;
     throw error;
