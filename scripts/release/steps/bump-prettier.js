2c38431b17183e1e0e3549ab309a53384b48836a
Ika <ikatyang@gmail.com>
2018-08-03 08:29:49 +0800

    chore: improve release scripts

--- scripts/release/steps/bump-prettier.js
+++ scripts/release/steps/bump-prettier.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const execa = require("execa");
-const { logPromise } = require("../utils");
+const semver = require("semver");
+const { logPromise, readJson, writeJson } = require("../utils");
 
 async function format() {
   await execa("yarn", ["lint", "--fix"]);
@@ -17,7 +18,19 @@ async function commit(version) {
   await execa("git", ["push"]);
 }
 
-module.exports = async function({ dry, version }) {
+async function bump({ version, previousVersion, previousVersionOnMaster }) {
+  const pkg = await readJson("package.json");
+  if (semver.diff(version, previousVersion) === "patch") {
+    pkg.version = previousVersionOnMaster; // restore the `-dev` version
+  } else {
+    pkg.version = semver.inc(version, "minor") + "-dev";
+  }
+  await writeJson("package.json", pkg, { spaces: 2 });
+}
+
+module.exports = async function(params) {
+  const { dry, version } = params;
+
   if (dry) {
     return;
   }
@@ -28,5 +41,6 @@ module.exports = async function({ dry, version }) {
   );
 
   await logPromise("Updating files", format());
+  await logPromise("Bump master version", bump(params));
   await logPromise("Committing changed files", commit(version));
 };
