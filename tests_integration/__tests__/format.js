51f3b02e50cd5508876b34cf7c79199a60bc21b1
Ika <ikatyang@gmail.com>
2018-08-09 00:58:05 +0800

    test: update test case

--- tests_integration/__tests__/format.js
+++ tests_integration/__tests__/format.js
@@ -4,5 +4,8 @@ const prettier = require("prettier/local");
 
 test("yaml parser should handle CRLF correctly", () => {
   const input = "a:\r\n  123\r\n";
-  expect(prettier.format(input, { parser: "yaml" })).toMatchSnapshot();
+  expect(
+    // use JSON.stringify to observe CRLF
+    JSON.stringify(prettier.format(input, { parser: "yaml" }))
+  ).toMatchSnapshot();
 });
