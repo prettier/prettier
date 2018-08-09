53c83f97f58163040bcfc7a9b05425498353b407
Ika <ikatyang@gmail.com>
2018-08-09 10:51:08 +0800

    fix: guess line ending before preprocess (#4957)

--- tests_integration/__tests__/format.js
+++ tests_integration/__tests__/format.js
@@ -4,7 +4,10 @@ const prettier = require("prettier/local");
 
 test("yaml parser should handle CRLF correctly", () => {
   const input = "a:\r\n  123\r\n";
-  expect(prettier.format(input, { parser: "yaml" })).toMatchSnapshot();
+  expect(
+    // use JSON.stringify to observe CRLF
+    JSON.stringify(prettier.format(input, { parser: "yaml" }))
+  ).toMatchSnapshot();
 });
 
 test("typescript parser should throw the first error when both JSX and non-JSX mode failed", () => {
