cf683c9f43409e4fc3b0044c1b528e443dbb13f3
Eric Sakmar <eric.sakmar@gmail.com>
2018-08-23 13:28:09 -0400

    format jest calls like regular calls

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -5771,10 +5771,7 @@ function isTestCall(n, parent) {
     }
 
     if (isUnitTestSetUp(n)) {
-      return (
-        isFunctionOrArrowExpression(n.arguments[0].type) ||
-        isAngularTestWrapper(n.arguments[0])
-      );
+      return isAngularTestWrapper(n.arguments[0]);
     }
   } else if (n.arguments.length === 2) {
     if (
