442e4c63dd1761160060892200cb2a8349f406a1
Ika <ikatyang@gmail.com>
2018-09-01 16:28:45 +0800

    chore: make rollup happy

--- src/language-markdown/utils.js
+++ src/language-markdown/utils.js
@@ -1,37 +1,15 @@
 "use strict";
 
-const cjkRegex = require("cjk-regex");
-const regexpUtil = require("regexp-util");
-const unicodeRegex = require("unicode-regex");
-const { getLast } = require("../common/util");
+const {
+  cjkPattern,
+  kPattern,
+  punctuationPattern
+} = require("./constants.evaluate");
 
-const cjkPattern = cjkRegex().toString();
-const kRegex = unicodeRegex({ Script: ["Hangul"] }).toRegExp();
-
-// http://spec.commonmark.org/0.25/#ascii-punctuation-character
-const asciiPunctuationCharset = /* prettier-ignore */ regexpUtil.charset(
-  "!", '"', "#",  "$", "%", "&", "'", "(", ")", "*",
-  "+", ",", "-",  ".", "/", ":", ";", "<", "=", ">",
-  "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|",
-  "}", "~"
-);
-
-// http://spec.commonmark.org/0.25/#punctuation-character
-const punctuationCharset = unicodeRegex({
-  // http://unicode.org/Public/5.1.0/ucd/UCD.html#General_Category_Values
-  General_Category: [
-    /* Pc */ "Connector_Punctuation",
-    /* Pd */ "Dash_Punctuation",
-    /* Pe */ "Close_Punctuation",
-    /* Pf */ "Final_Punctuation",
-    /* Pi */ "Initial_Punctuation",
-    /* Po */ "Other_Punctuation",
-    /* Ps */ "Open_Punctuation"
-  ]
-}).union(asciiPunctuationCharset);
-
-const punctuationPattern = punctuationCharset.toString();
+const kRegex = new RegExp(kPattern);
 const punctuationRegex = new RegExp(punctuationPattern);
+
+const { getLast } = require("../common/util");
 
 /**
  * split text into whitespaces and words
