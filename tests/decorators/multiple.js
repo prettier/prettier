2fa09f71d9870cd98fd94e6650fd582582701449
Lucas Duailibe <lucasds@gmail.com>
2018-10-04 13:42:24 -0300

    another test

--- tests/decorators/multiple.js
+++ tests/decorators/multiple.js
@@ -2,9 +2,14 @@ const dog = {
   @readonly
   @nonenumerable
   @doubledValue
-  legs: 4
+  legs: 4,
+
+  @readonly
+  @nonenumerable
+  @doubledValue
+  eyes: 2
 };
 
-const dog = {
-  @readonly @nonenumerable @doubledValue legs: 4
+const foo = {
+  @multipleDecorators @inline @theyWontAllFitInOneline aVeryLongPropName: "A very long string as value"
 };
