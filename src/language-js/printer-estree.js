b5fd382fe82fa5afe53173578fe34a9d52e2352c
Lucas Duailibe <lucasds@gmail.com>
2018-10-08 19:28:58 -0300

    Inline the arguments of an arrow function in the body is conditional expression (#5209)

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -3420,6 +3420,7 @@ function couldGroupArg(arg) {
         arg.body.type === "ArrayExpression" ||
         arg.body.type === "CallExpression" ||
         arg.body.type === "OptionalCallExpression" ||
+        arg.body.type === "ConditionalExpression" ||
         isJSXNode(arg.body)))
   );
 }
