5f40fc27bcd36eb1e0852298585e928f7241c122
Jed Fox <git@twopointzero.us>
2018-10-08 14:42:10 -0400

    Fix a bug

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -5976,7 +5976,9 @@ function isJsDocComment(comment) {
   const lines = comment.value.split("\n");
   return (
     lines.length > 1 &&
-    lines.slice(1, lines.length - 1).every(line => line.trim()[0] === "*")
+    lines.slice(1, lines.length - 1).every(line => line.trim()[0] === "*") &&
+    (lines[lines.length - 1].trim() === "" ||
+      lines[lines.length - 1].trim()[0] === "*")
   );
 }
 
