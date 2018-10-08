d3565e986b13a9f781bc41cf197fb4d7275c5384
Lucas Duailibe <lucasds@gmail.com>
2018-10-08 11:42:45 -0300

    fixes some other cases

--- tests/try/catch.js
+++ tests/try/catch.js
@@ -3,3 +3,23 @@ catch(
   // comment
   foo
 ) {}
+
+try {}
+catch(foo //comment
+) {}
+
+try {}
+catch(
+  /* comment */ foo
+) {}
+
+try {}
+catch(
+  foo /* comment */
+) {}
+
+try {}
+catch(
+  foo
+  /* comment */
+) {}
