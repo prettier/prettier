8c827882e53bdc3b4d0c04c1b5b569384eeb883e
Aquib Master <aquibmaster@gmail.com>
2018-10-11 22:43:52 +1300

    Fix flow extends not breaking out into new lines

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -2583,7 +2583,11 @@ function printPathNoParens(path, options, print, args) {
         parts.push(
           group(
             indent(
-              concat([line, "extends ", join(", ", path.map(print, "extends"))])
+              concat([
+                line,
+                "extends ",
+                indent(join(concat([",", line]), path.map(print, "extends")))
+              ])
             )
           )
         );
