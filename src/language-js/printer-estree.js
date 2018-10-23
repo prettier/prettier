6a174654a49a6df47fd75eecda44e22a3fa3723e
Jed Fox <git@twopointzero.us>
2018-10-23 15:04:24 -0400

    Fix indentation of block comments where every line starts with a star (#5206)
    Previously this was done only for JSDoc comments. Now it's done in a few additional cases.

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -6058,8 +6058,8 @@ function printComment(commentPath, options) {
   switch (comment.type) {
     case "CommentBlock":
     case "Block": {
-      if (isJsDocComment(comment)) {
-        const printed = printJsDocComment(comment);
+      if (isIndentableBlockComment(comment)) {
+        const printed = printIndentableBlockComment(comment);
         // We need to prevent an edge case of a previous trailing comment
         // printed as a `lineSuffix` which causes the comments to be
         // interleaved. See https://github.com/prettier/prettier/issues/4412
@@ -6093,15 +6093,16 @@ function printComment(commentPath, options) {
   }
 }
 
-function isJsDocComment(comment) {
-  const lines = comment.value.split("\n");
-  return (
-    lines.length > 1 &&
-    lines.slice(0, lines.length - 1).every(line => line.trim()[0] === "*")
-  );
+function isIndentableBlockComment(comment) {
+  // If the comment has multiple lines and every line starts with a star
+  // we can fix the indentation of each line. The stars in the `/*` and
+  // `*/` delimiters are not included in the comment value, so add them
+  // back first.
+  const lines = `*${comment.value}*`.split("\n");
+  return lines.length > 1 && lines.every(line => line.trim()[0] === "*");
 }
 
-function printJsDocComment(comment) {
+function printIndentableBlockComment(comment) {
   const lines = comment.value.split("\n");
 
   return concat([
@@ -6110,7 +6111,7 @@ function printJsDocComment(comment) {
       hardline,
       lines.map(
         (line, index) =>
-          (index > 0 ? " " : "") +
+          (index === 0 && line[0] === "*" ? "" : " ") +
           (index < lines.length - 1 ? line.trim() : line.trimLeft())
       )
     ),
