9e3374a376ba64a0e932ee3fb8f0169caa6c8324
Alexander Kachkaev <alexander@kachkaev.ru>
2018-11-04 21:12:21 +0000

    Mitigate use of os.EOL by jest-docblock

--- src/language-js/pragma.js
+++ src/language-js/pragma.js
@@ -8,12 +8,14 @@ function hasPragma(text) {
 }
 
 function insertPragma(text) {
-  const parsedDocblock = docblock.parseWithComments(docblock.extract(text).replace(/\r\n?/g, "\n"));
+  const parsedDocblock = docblock.parseWithComments(docblock.extract(text));
   const pragmas = Object.assign({ format: "" }, parsedDocblock.pragmas);
-  const newDocblock = docblock.print({
-    pragmas,
-    comments: parsedDocblock.comments.replace(/^(\s+?\r?\n)+/, "") // remove leading newlines
-  });
+  const newDocblock = docblock
+    .print({
+      pragmas,
+      comments: parsedDocblock.comments.replace(/^(\s+?\r?\n)+/, "") // remove leading newlines
+    })
+    .replace(/(\r\n|\r)/g, "\n"); // normalise newlines (mitigate use of os.EOL by jest-docblock)
   const strippedText = docblock.strip(text);
   const separatingNewlines = strippedText.startsWith("\n") ? "\n" : "\n\n";
   return newDocblock + separatingNewlines + strippedText;
