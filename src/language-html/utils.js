557b482d7077393cc9b108842a092686d85cc7db
Ika <ikatyang@gmail.com>
2018-11-08 15:50:45 +0800

    feat(html): treat capital element as custom element

--- src/language-html/utils.js
+++ src/language-html/utils.js
@@ -252,7 +252,11 @@ function isCustomElementWithSurroundingLineBreak(node) {
 }
 
 function isCustomElement(node) {
-  return node.type === "element" && !node.namespace && node.name.includes("-");
+  return (
+    node.type === "element" &&
+    !node.namespace &&
+    (node.name.includes("-") || /[A-Z]/.test(node.name[0]))
+  );
 }
 
 function hasSurroundingLineBreak(node) {
