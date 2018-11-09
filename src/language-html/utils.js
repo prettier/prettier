6f6b634948ff2e5713078b5bf289fc4acb604a65
Ika <ikatyang@gmail.com>
2018-11-09 19:19:03 +0800

    fix(html): preserve content for `<template>` with unknown lang (#5388)

--- src/language-html/utils.js
+++ src/language-html/utils.js
@@ -34,10 +34,16 @@ function mapObject(object, fn) {
   return newObject;
 }
 
-function hasPrettierIgnore(path) {
+function shouldPreserveElementContent(path) {
   const node = path.getValue();
-  if (node.type === "attribute" || node.type === "text") {
-    return false;
+
+  if (
+    node.type === "element" &&
+    node.fullName === "template" &&
+    node.attrMap.lang &&
+    node.attrMap.lang !== "html"
+  ) {
+    return true;
   }
 
   // TODO: handle non-text children in <pre>
@@ -48,6 +54,15 @@ function hasPrettierIgnore(path) {
     )
   ) {
     return true;
+  }
+
+  return false;
+}
+
+function hasPrettierIgnore(path) {
+  const node = path.getValue();
+  if (node.type === "attribute" || node.type === "text") {
+    return false;
   }
 
   const parentNode = path.getParentNode();
@@ -614,5 +629,6 @@ module.exports = {
   preferHardlineAsLeadingSpaces,
   preferHardlineAsTrailingSpaces,
   replaceDocNewlines,
-  replaceNewlines
+  replaceNewlines,
+  shouldPreserveElementContent
 };
