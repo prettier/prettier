5bf0b09abc294fe69e81e00f4010e4fac1efe4d0
Ika <ikatyang@gmail.com>
2018-11-20 12:01:09 +0800

    test: remove unnecessary tests

--- tests/end_of_line/jsfmt.spec.js
+++ tests/end_of_line/jsfmt.spec.js
@@ -1,4 +1,3 @@
-run_spec(__dirname, ["babylon"], { endOfLine: "auto" });
 run_spec(__dirname, ["babylon"], { endOfLine: "cr" });
 run_spec(__dirname, ["babylon"], { endOfLine: "crlf" });
 run_spec(__dirname, ["babylon"], { endOfLine: "lf" });
