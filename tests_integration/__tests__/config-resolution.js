c08159efdfb4476fea971281e11eb73edc940454
Wei-Wei Wu <wuxx1045@umn.edu>
2018-11-26 23:53:42 -0800

    adding test

--- tests_integration/__tests__/config-resolution.js
+++ tests_integration/__tests__/config-resolution.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const path = require("path");
+const tempy = require("tempy");
+const fs = require("fs");
 
 const runPrettier = require("../runPrettier");
 const prettier = require("prettier/local");
@@ -251,4 +253,31 @@ test("API resolveConfig resolves relative path values based on config filepath",
     plugins: [path.join(parentDir, "path-to-plugin")],
     pluginSearchDirs: [path.join(parentDir, "path-to-plugin-search-dir")]
   });
+});
+
+test("API resolveConfig should pick up .js config changes", () => {
+  const tempDir = tempy.directory();
+  const tempConfigFile = `${tempDir}/prettier.config.js`;
+  const oldConfig = `
+    "use strict";
+
+    module.exports = {
+      tabWidth: 8
+    };
+  `;
+
+  const newConfig = `
+    "use strict";
+
+    module.exports = {
+      tabWidth: 16
+    };
+  `;
+  fs.writeFileSync(tempConfigFile, oldConfig);
+  prettier.resolveConfig.sync(tempConfigFile, { useCache: false });
+  fs.writeFileSync(tempConfigFile, newConfig);
+  prettier.clearConfigCache();
+  expect(
+    prettier.resolveConfig.sync(tempConfigFile, { useCache: false })
+  ).toEqual({ tabWidth: 16 });
 });
