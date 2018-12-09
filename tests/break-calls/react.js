89365cd725433635e95d519956ab16ecff82cb4d
Jed Fox <git@twopointzero.us>
2018-12-09 12:17:24 -0500

    Add tests for `React.useEffect` form

--- tests/break-calls/react.js
+++ tests/break-calls/react.js
@@ -2,10 +2,16 @@ function helloWorld() {
   useEffect(() => {
     // do something
   }, [props.value])
+  useEffect(() => {
+    // do something
+  }, [props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value])
 }
 
-function helloWorld() {
-  useEffect(() => {
+function helloWorldWithReact() {
+  React.useEffect(() => {
+    // do something
+  }, [props.value])
+  React.useEffect(() => {
     // do something
   }, [props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value, props.value])
 }
