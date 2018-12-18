48254a7efb76e87bb7d6d9d1ac19df1c19858724
Ika <ikatyang@gmail.com>
2018-12-18 22:42:20 +0800

    test: add tests

--- tests/html_angular/jsfmt.spec.js
+++ tests/html_angular/jsfmt.spec.js
@@ -2,3 +2,4 @@ run_spec(__dirname, ["angular"]);
 run_spec(__dirname, ["angular"], { trailingComma: "es5" });
 run_spec(__dirname, ["angular"], { printWidth: 1 });
 run_spec(__dirname, ["angular"], { htmlWhitespaceSensitivity: "ignore" });
+run_spec(__dirname, ["angular"], { bracketSpacing: false });
