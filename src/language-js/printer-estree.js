3a8927a9e3f69777ee225cb1f8aad1debf2332d0
Christopher J. Brody <chris.brody@gmail.com>
2018-12-30 18:39:35 -0500

    Resolve issue with generics
    (with test updates)

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -3738,10 +3738,6 @@ function printMethod(path, options, print) {
 
   parts.push(key);
 
-  if (options.spaceBeforeFunctionParen && !node.value.async) {
-    parts.push(" ");
-  }
-
   parts.push(
     concat(
       path.call(
@@ -3749,6 +3745,7 @@ function printMethod(path, options, print) {
           printFunctionTypeParameters(valuePath, options, print),
           group(
             concat([
+              options.spaceBeforeFunctionParen ? " " : "",
               printFunctionParams(valuePath, print, options),
               printReturnType(valuePath, print, options)
             ])
@@ -4235,14 +4232,11 @@ function printFunctionDeclaration(path, print, options) {
     parts.push(" ", path.call(print, "id"));
   }
 
-  if (options.spaceBeforeFunctionParen) {
-    parts.push(" ");
-  }
-
   parts.push(
     printFunctionTypeParameters(path, options, print),
     group(
       concat([
+              options.spaceBeforeFunctionParen ? " " : "",
         printFunctionParams(path, print, options),
         printReturnType(path, print, options)
       ])
