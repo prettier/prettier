5ab7ccd05427606a3a83bd499453ae0afc32e3b8
Christopher J. Brody <chris.brody@gmail.com>
2019-01-01 18:48:06 -0500

    XXX TBD tests/align-object-properties/json - XXX TODO
    DOES NOT LOOK RIGHT

---
+++ tests/align-object-properties/json/jsfmt.spec.js
@@ -0,0 +1,5 @@
+run_spec(__dirname, ["json"]);
+run_spec(__dirname, ["json"], { trailingComma: "all" });
+run_spec(__dirname, ["json5"]);
+run_spec(__dirname, ["json5"], { trailingComma: "all" });
+run_spec(__dirname, ["json-stringify"]);
