62ee60176998fcc427c9984976a027a3ac867cce
Christopher J. Brody <chris.brody@gmail.com>
2019-01-06 15:25:58 -0500

    standard formatting tests
    as originally proposed in prettier/prettier#5723
    
    Co-authored-by: Adam Stankiewicz <sheerun@sher.pl>
    Co-authored-by: Christopher J. Brody <chris.brody@gmail.com>

---
+++ tests/standard/jsfmt.spec.js
@@ -0,0 +1,8 @@
+run_spec(__dirname, ["babel", "flow", "typescript"], {
+  yieldStarSpacing: true,
+  generatorStarSpacing: true,
+  spaceBeforeFunctionParen: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false
+});
