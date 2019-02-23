9bc6a1d4441fa37e5da97e1ffee4b0dbe8dc6c00
Jed Fox <git@twopointzero.us>
2019-02-23 16:37:13 -0500

    Update the language display on the homepage

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -112,7 +112,7 @@ TldrSection.propTypes = {
   language: PropTypes.string
 };
 
-const Language = ({ name, showName, image, variants }) => (
+const Language = ({ name, nameLink, showName, image, variants }) => (
   <div
     className="languageCategory"
     style={{
@@ -122,19 +122,24 @@ const Language = ({ name, showName, image, variants }) => (
     }}
   >
     <img src={image} style={{ width: "50px", padding: "0 20px" }} />
-    <div>
-      {showName && <p className="accented">{name}</p>}
+    <ul>
+      {showName && (
+        <li className="accented">
+          {nameLink ? <a href={nameLink}>{name}</a> : name}
+        </li>
+      )}
       {variants.map(variant => (
-        <div key={variant}>
+        <li key={variant}>
           <MarkdownBlock>{variant}</MarkdownBlock>
-        </div>
+        </li>
       ))}
-    </div>
+    </ul>
   </div>
 );
 
 Language.propTypes = {
   name: PropTypes.string,
+  nameLink: PropTypes.string,
   showName: PropTypes.bool,
   image: PropTypes.string,
   variants: PropTypes.array
@@ -149,7 +154,7 @@ const LanguagesSection = () => {
         last.length < 2 &&
         last.reduce((sum, lang) => sum + lang.variants.length, 0) +
           language.variants.length <
-          5
+          9
       ) {
         last.push(language);
       } else {
