84e2b4da684f267233adbb8d2a8f696da16cd659
Christopher J. Brody <chris@brody.consulting>
2019-02-27 13:58:03 -0500

    [prettierx] cover parenSpacing in tests/ternaries

--- tests/ternaries/jsfmt.spec.js
+++ tests/ternaries/jsfmt.spec.js
@@ -19,3 +19,8 @@ run_spec(__dirname, ["flow", "typescript"], {
   alignTernaryLines: false,
   tabWidth: 4
 });
+
+run_spec(__dirname, ["babel", "flow", "typescript"], {
+  alignTernaryLines: true,
+  parenSpacing: true
+});
