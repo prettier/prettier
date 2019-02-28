de3e798f2130c7b251d474e91d5d075d38ec49c6
Warren Seine <w.seine@criteo.com>
2019-02-28 23:17:05 +0100

    Fix trailing comment output when the printer returns a doc instead of a string.
    Previously, Prettier printed `[Object object]` in that situation.

--- src/main/comments.js
+++ src/main/comments.js
@@ -443,7 +443,10 @@ function printTrailingComment(commentPath, print, options) {
     return concat([" ", contents]);
   }
 
-  return concat([lineSuffix(" " + contents), !isBlock ? breakParent : ""]);
+  return concat([
+    lineSuffix(concat([" ", contents])),
+    !isBlock ? breakParent : ""
+  ]);
 }
 
 function printDanglingComments(path, options, sameIndent, filter) {
