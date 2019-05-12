394d963b835e4152f8c728d6c3644c33030636df
Remco Haszing <remcohaszing@gmail.com>
2019-05-12 14:31:17 +0200

    Remove languages from external plugin override test
    It doesn’t have any effect on the printer.

--- tests_integration/plugins/plugin-external-precedence/plugin.js
+++ tests_integration/plugins/plugin-external-precedence/plugin.js
@@ -1,13 +1,6 @@
 "use strict";
 
 module.exports = {
-  languages: [
-    {
-      name: "package.json",
-      parsers: ["json-stringify"],
-      filenames: ["package.json"]
-    }
-  ],
   printers: {
     "estree-json": {
       print: () => "Overridden"
