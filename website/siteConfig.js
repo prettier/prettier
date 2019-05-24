28727c3bb6310c629cc2db2d80e5c4efd2ac568d
Lucas Duailibe <lucasds@gmail.com>
2019-05-24 18:45:04 -0300

    Fix: move playground link back

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -34,9 +34,9 @@ const siteConfig = {
   /* base url for editing docs, usage example: editUrl + 'en/doc1.md' */
   editUrl: `${GITHUB_URL}/edit/master/docs/`,
   headerLinks: [
+    { href: "/playground/", label: "Playground" },
     { doc: "index", label: "About" },
     { doc: "install", label: "Usage" },
-    { href: "/playground/", label: "Playground" },
     { blog: true, label: "Blog" },
     { search: true },
     { href: "https://opencollective.com/prettier", label: "Donate" },
