1ed697566b9ec796ca0f9a940e986812d5e12d24
fisker <lionkay@gmail.com>
2019-06-09 03:52:22 +0800

    chore: minify parser-postcss bundle

--- scripts/build/bundler.js
+++ scripts/build/bundler.js
@@ -176,7 +176,9 @@ function getWebpackConfig(bundle) {
       path: path.resolve(root, "dist"),
       filename: bundle.output,
       library: ["prettierPlugins", bundle.name],
-      libraryTarget: "umd"
+      libraryTarget: "umd",
+      // see https://github.com/webpack/webpack/issues/6784
+      globalObject: 'new Function("return this")()'
     },
     plugins: [
       new webpack.DefinePlugin({
