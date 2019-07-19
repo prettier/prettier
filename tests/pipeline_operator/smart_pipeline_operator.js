4930602f61f897f2361c6b36a2be669e9a7de6a5
Sosuke Suzuki <aosukeke@gmail.com>
2019-07-20 04:32:43 +0900

    Add tests

---
+++ tests/pipeline_operator/smart_pipeline_operator.js
@@ -0,0 +1,23 @@
+a |> await # |> # * 3;
+
+foo
+  |> await #
+  |> # || throw new Error(`foo ${bar1}`)
+  |> bar2(#, ", ")
+  |> bar3
+  |> # + "!"
+  |> new Bar.Foo(#)
+  |> await bar.bar(#)
+  |> console.log;
+
+const result = "hello"
+  |> doubleSay
+  |> capitalize(#, "foo")
+  |> exclaim;
+
+function createPerson (attrs) {
+  attrs
+    |> foo
+    |> foo
+    |> Person.insertIntoDatabase;
+}
