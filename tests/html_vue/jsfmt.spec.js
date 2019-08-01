34f302348a135f4d801adddf48c05b62284d495f
Simon Lydell <simon.lydell@gmail.com>
2019-08-01 09:40:16 +0200

    Remove noisy snapshots

--- tests/html_vue/jsfmt.spec.js
+++ tests/html_vue/jsfmt.spec.js
@@ -1,5 +1,3 @@
 run_spec(__dirname, ["vue"]);
 run_spec(__dirname, ["vue"], { trailingComma: "es5" });
 run_spec(__dirname, ["vue"], { semi: false });
-run_spec(__dirname, ["vue"], { vueIndentScriptAndStyle: true });
-run_spec(__dirname, ["vue"], { vueIndentScriptAndStyle: false });
