6650f65ed435785f9a45bbeccb79d0ce28dcac48
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-02 20:50:43 +0900

    Fix tests

--- tests/export_extension/export.js
+++ tests/export_extension/export.js
@@ -3,4 +3,4 @@ export v from 'mod';
 export a, * as b from 'mod';
 export c, { foo } from 'mod';
 export * as d, { bar } from 'mod';
-export { fooooooooooooooooooooooooooooooooooooooooooooooooo } from 'foooooooooooooooooooooooooooooo'
+export { fooooooooooooooooooooooooooooooooooooooooooooooooo } from "fooooooooooooooooooooooooooooo";
