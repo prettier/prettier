3ae7d7dcc86fd49e08eeff7b7ff23f20956bf27b
Panayiotis Lipiridis <lipiridis@gmail.com>
2019-09-06 21:02:20 +0300

    website: Update dependencies on website

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -417,7 +417,7 @@ const UsersSection = ({ language }) => {
               <img src="/images/npm_grey.svg" style={{ height: "100px" }} />
             </a>
             <div style={{ marginLeft: ".7em", width: "260px" }}>
-              <p>More than 2000 tools and integrations on npm</p>
+              <p>More than <strong>3000</strong> tools and integrations on npm</p>
               <Button href="https://www.npmjs.com/browse/depended/prettier">
                 Install Them
               </Button>
@@ -432,7 +432,7 @@ const UsersSection = ({ language }) => {
               <img src="/images/github_grey.svg" style={{ height: "100px" }} />
             </a>
             <div style={{ marginLeft: ".7em", width: "260px" }}>
-              <p>More than 400,000 dependent repositories on GitHub</p>
+              <p>More than <stgrong>1M</stgrong> dependent repositories on GitHub</p>
               <Button href="https://github.com/prettier/prettier/network/dependents">
                 Check Them Out
               </Button>
