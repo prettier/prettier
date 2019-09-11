7c12577f349b02e46941afc52f5bf32bb8c7a4dc
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-12 01:24:08 +0900

    Modify to keep inline type parameter with variable declaration

---
+++ tests/typescript/custom/typeParameters/variables.ts
@@ -0,0 +1,4 @@
+const fooooooooooooooo: SomeThing<boolean> = looooooooooooooooooooooooooooooongNameFunc();
+const baaaaaaaaaaaaaaaaaaaaar: SomeThing<boolean, boolean> = looooooooooooooooooooooooooooooongNameFunc();
+const baaaaaaaaaaaaaaar: SomeThing<{ [P in "x" | "y"]: number }> = looooooooooooooooooooooooooooooongNameFunc();
+const baaaaaaaaaaaaaaaar: SomeThing<K extends T ? G : S> = looooooooooooooooooooooooooooooongNameFunc();
