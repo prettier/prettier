28a50f79ddf00c3ab403fb873a5876a0cb75cb97
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-12 03:15:55 +0900

    Add tests

--- tests/typescript/custom/typeParameters/variables.ts
+++ tests/typescript/custom/typeParameters/variables.ts
@@ -6,3 +6,9 @@ const fooooooooooooooo: SomeThing<boolean> = looooooooooooooooooooooooooooooongN
 const baaaaaaaaaaaaaaaaaaaaar: SomeThing<boolean, boolean> = looooooooooooooooooooooooooooooongNameFunc();
 const baaaaaaaaaaaaaaar: SomeThing<{ [P in "x" | "y"]: number }> = looooooooooooooooooooooooooooooongNameFunc();
 const baaaaaaaaaaaaaaaar: SomeThing<K extends T ? G : S> = looooooooooooooooooooooooooooooongNameFunc();
+const isAnySuccessfulAttempt$: Observable<boolean> = this._quizService.isAnySuccessfulAttempt$().pipe(
+  tap((isAnySuccessfulAttempt: boolean) => {
+    this.isAnySuccessfulAttempt = isAnySuccessfulAttempt;
+  }),
+);
+const isAnySuccessfulAttempt$: Observable<boolean> = this._someMethodWithLongName();
