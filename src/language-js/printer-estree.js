e7069854e22777ceed056d12f6b866fd6d209566
mmkal <15040698+mmkal@users.noreply.github.com>
2019-10-19 17:50:24 -0400

    initial non-regex isSimple implementation

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -5125,25 +5125,14 @@ function printMemberChain(path, options, print) {
 
   // We don't want to print in one line if the chain has:
   //  * A comment.
-  //  * Two or more calls, with some non-trivial arguments.
+  //  * Non-trivial arguments before the last item.
   //  * Any group but the last one has a hard line.
   // If the last group is a function it's okay to inline if it fits.
   if (
     hasComment ||
-    (callExpressions.length >= 2 &&
-      callExpressions.slice(0, -1).some(expr =>
-        expr.arguments.some(arg => {
-          if (isFunctionOrArrowExpression(arg)) {
-            return true;
-          }
-          const code = options.originalText.slice(
-            options.locStart(arg),
-            options.locEnd(arg)
-          );
-          const simple = code.match(/^[\w.[\]"'`, ]+$/);
-          return !simple;
-        })
-      )) ||
+    callExpressions
+      .slice(0, -1)
+      .some(expr => expr.arguments.some(arg => !isSimple(arg))) ||
     printedGroups.slice(0, -1).some(willBreak) ||
     /**
      *     scopes.filter(scope => scope.value !== '').map((scope, i) => {
@@ -5169,6 +5158,34 @@ function printMemberChain(path, options, print) {
     willBreak(oneLine) || shouldHaveEmptyLineBeforeIndent ? breakParent : "",
     conditionalGroup([oneLine, expanded])
   ]);
+}
+
+/**
+ * @param {import('estree').Node} node
+ * @returns {boolean}
+ */
+function isSimple(node) {
+  if (node.type === "Literal") {
+    return true;
+  }
+  if (node.type === "TemplateLiteral" && node.expressions.length === 0) {
+    return true;
+  }
+  if (node.type === "Identifier") {
+    return true;
+  }
+  if (node.type === "ArrayExpression") {
+    return node.elements.every(isSimple);
+  }
+  if (node.type === "CallExpression") {
+    return (
+      node.arguments.every(isSimple) && (!node.callee || isSimple(node.callee))
+    );
+  }
+  if (node.type === "MemberExpression") {
+    return isSimple(node.object) && isSimple(node.property);
+  }
+  return false;
 }
 
 function separatorNoWhitespace(
