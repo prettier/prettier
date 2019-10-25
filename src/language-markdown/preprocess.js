a8675ec0fe874248ac7f2aee53153df2fadf27b0
fisker <lionkay@gmail.com>
2019-10-25 18:36:28 +0800

    lower case definition.label to match ast

--- src/language-markdown/preprocess.js
+++ src/language-markdown/preprocess.js
@@ -15,6 +15,7 @@ function preprocess(ast, options) {
   ast = splitTextIntoSentences(ast, options);
   ast = transformImportExport(ast);
   ast = mergeContinuousImportExport(ast);
+  ast = transformDefinition(ast);
   return ast;
 }
 
@@ -35,6 +36,18 @@ function transformInlineCode(ast) {
     }
 
     return { ...node, value: node.value.replace(/\s+/g, " ") };
+  });
+}
+
+function transformDefinition(ast) {
+  return mapAst(ast, node => {
+    if (node.type !== "definition") {
+      return node;
+    }
+
+    return Object.assign({}, node, {
+      label: node.label.toLoweCase()
+    });
   });
 }
 
