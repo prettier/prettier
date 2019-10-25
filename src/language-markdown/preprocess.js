5dba5ca251f54bc0f0f94251d0df833c0c5de353
fisker <lionkay@gmail.com>
2019-10-25 18:37:37 +0800

    trim definition.label to match ast

--- src/language-markdown/preprocess.js
+++ src/language-markdown/preprocess.js
@@ -48,7 +48,7 @@ function transformDefinition(ast) {
     }
 
     return Object.assign({}, node, {
-      label: node.label.toLoweCase()
+      label: node.label.trim().toLowerCase()
     });
   });
 }
