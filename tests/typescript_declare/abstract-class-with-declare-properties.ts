2781be3287f3de76eefa13d9448f10e27f013d9b
fisker <lionkay@gmail.com>
2019-10-29 16:56:49 +0800

    Typescript: add support for declare class properties

---
+++ tests/typescript_declare/abstract-class-with-declare-properties.ts
@@ -0,0 +1,7 @@
+abstract class AbstractDeclProps {
+  declare prop1: string;
+  declare abstract prop2: string;
+  declare public abstract prop3: string;
+  declare readonly abstract prop4: string;
+  declare public readonly abstract prop5: string;
+}
---
+++ tests/typescript_declare/class-with-declare-properties.ts
@@ -0,0 +1,9 @@
+class DeclProps {
+  declare prop1: string;
+  declare public prop2: string;
+  declare static prop3: string;
+  declare readonly prop3: string;
+  declare public readonly prop4: string;
+  declare public static prop5: string;
+  declare public static readonly prop6: string;
+}
