ec22a7ab5937828eb25c50c76598451ccd61c523
fisker <lionkay@gmail.com>
2019-10-29 17:06:53 +0800

    remove test

--- tests/typescript_declare/class-with-declare-properties.ts
+++
@@ -1,9 +0,0 @@
-class DeclProps {
-  declare prop1: string;
-  declare public prop2: string;
-  declare static prop3: string;
-  declare readonly prop3: string;
-  declare public readonly prop4: string;
-  declare public static prop5: string;
-  declare public static readonly prop6: string;
-}
