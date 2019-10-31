226de4832093d5019fe952393a6d66346cb84d8e
fisker <lionkay@gmail.com>
2019-10-31 14:15:46 +0800

    expand directories test

--- tests_integration/__tests__/patterns.js
+++ tests_integration/__tests__/patterns.js
@@ -5,13 +5,19 @@ const runPrettier = require("../runPrettier");
 expect.addSnapshotSerializer(require("../path-serializer"));
 
 describe("ignores node_modules by default", () => {
-  runPrettier("cli/with-dot-pattern", [".", "-l"]).test({
+  runPrettier("cli/patterns", [".", "-l"]).test({
     status: 1
   });
 });
 
 describe("doesn't ignore node_modules with --with-node-modules flag", () => {
-  runPrettier("cli/with-dot-pattern", [".", "-l", "--with-node-modules"]).test({
+  runPrettier("cli/patterns", [".", "-l", "--with-node-modules"]).test({
+    status: 1
+  });
+});
+
+describe("should expand directories", () => {
+  runPrettier("cli/patterns", ["expand-dir", "-l"]).test({
     status: 1
   });
 });
