1a137fc29e707ed656f59d4335e505565130f630
fisker <lionkay@gmail.com>
2019-10-31 14:24:03 +0800

    restore import order

--- src/common/load-plugins.js
+++ src/common/load-plugins.js
@@ -2,8 +2,8 @@
 
 const uniqBy = require("lodash.uniqby");
 const fs = require("fs");
-const path = require("path");
 const globby = require("globby");
+const path = require("path");
 const resolve = require("resolve");
 const thirdParty = require("./third-party");
 const internalPlugins = require("./internal-plugins");
