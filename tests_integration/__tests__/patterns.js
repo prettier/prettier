f3838cdac8c5a21f246c8a14d6d370919bd8a79d
fisker <lionkay@gmail.com>
2019-11-01 09:37:49 +0800

    reuse multiple-patterns tests

--- tests_integration/__tests__/patterns.js
+++ tests_integration/__tests__/patterns.js
@@ -4,14 +4,20 @@ const runPrettier = require("../runPrettier");
 
 expect.addSnapshotSerializer(require("../path-serializer"));
 
+// TODO: move `multiple-patterns` tests into this one
+
 describe("should support dot pattern", () => {
-  runPrettier("cli/patterns", [".", "-l"]).test({
+  runPrettier("cli/multiple-patterns", [".", "-l"]).test({
     status: 1
   });
 });
 
 describe("should expand directories", () => {
-  runPrettier("cli/patterns", ["subdir", "-l"]).test({
+  runPrettier("cli/multiple-patterns", [
+    "directory",
+    "other-directory",
+    "-l"
+  ]).test({
     status: 1
   });
 });
