ea6ae31ba07e279152fdf1356c4b42b2982a8052
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2019-11-01 18:43:03 +0200

    test: fix a FIXME, add typescript (#6744)
    * test: fix a FIXME, add typescript
    
    * tests/es6modules
    
    * tests/no-semi
    
    * tests/array_spread

--- tests/jsx_escape/jsfmt.spec.js
+++ tests/jsx_escape/jsfmt.spec.js
@@ -1,3 +1,1 @@
-run_spec(__dirname, ["flow"]);
-// FIXME nbsp.js flow != babel output, waiting for: https://github.com/babel/babylon/pull/344
-run_spec(__dirname, ["babel"]);
+run_spec(__dirname, ["babel", "flow", "typescript"]);
