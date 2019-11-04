f25b27698d2ef539c53461798114a29628a13c5e
fisker <lionkay@gmail.com>
2019-11-04 14:43:57 +0800

    add many cases

--- tests/template_literals/styled-jsx-with-expressions.js
+++ tests/template_literals/styled-jsx-with-expressions.js
@@ -1,3 +1,200 @@
+// current logic breaks on any expression fail,
+// so test one by one
+
+// expression in tag selector(middle)
+<style jsx>{`
+         foo${exp}bar {}
+`}</style>;
+// expression in tag selector(beginning)
+<style jsx>{`
+         ${exp}bar {}
+`}</style>;
+// expression in tag selector(end)
+<style jsx>{`
+         foo${exp} {}
+`}</style>;
+
+
+// expression in id selector(middle)
+<style jsx>{`
+         #foo${exp}bar {}
+`}</style>;
+// expression in id selector(beginning)
+<style jsx>{`
+         #${exp}bar {}
+`}</style>;
+// expression in id selector(end)
+<style jsx>{`
+         #${exp}bar {}
+`}</style>;
+
+
+// expression in class selector(middle)
+<style jsx>{`
+         .foo${exp}bar {}
+`}</style>;
+// expression in class selector(beginning)
+<style jsx>{`
+         .${exp}bar {}
+`}</style>;
+// expression in class selector(end)
+<style jsx>{`
+         .${exp}bar {}
+`}</style>;
+
+
+// expression in attribute name(middle)
+<style jsx>{`
+         [foo${exp}bar] {}
+`}</style>;
+// expression in attribute name(beginning)
+<style jsx>{`
+         [${exp}bar] {}
+`}</style>;
+// expression in attribute name(end)
+<style jsx>{`
+         [foo${exp}] {}
+`}</style>;
+
+
+// expression in attribute value(middle)
+<style jsx>{`
+         [a^=foo${exp}bar] {}
+`}</style>;
+// expression in attribute value(beginning)
+<style jsx>{`
+         [a^=${exp}bar] {}
+`}</style>;
+// expression in attribute value(end)
+<style jsx>{`
+         [a^=foo${exp}] {}
+`}</style>;
+
+
+// expression in decl prop(middle)
+<style jsx>{`
+         a {foo${exp}bar: 1;}
+`}</style>;
+// expression in decl prop(beginning)
+<style jsx>{`
+         a {${exp}bar: 1;}
+`}</style>;
+// expression in decl prop(end)
+<style jsx>{`
+         a {foo${exp}: 1;}
+`}</style>;
+
+
+// expression in decl value(middle)
+<style jsx>{`
+         a {a: foo${exp}bar;}
+`}</style>;
+// expression in decl value(beginning)
+<style jsx>{`
+         a {a: ${exp}bar;}
+`}</style>;
+// expression in decl value(end)
+<style jsx>{`
+         a {a: foo${exp};}
+`}</style>;
+
+// expression in at-rule(middle)
+<style jsx>{`
+         @foo${exp}bar (min-width: 100px) {div {}}
+`}</style>;
+// expression in at-rule(beginning)
+<style jsx>{`
+         @${exp}bar (min-width: 100px) {div {}}
+`}</style>;
+// expression in at-rule(end)
+<style jsx>{`
+         @foo${exp} (min-width: 100px) {div {}}
+`}</style>;
+
+// expression in at-rule prop(middle)
+<style jsx>{`
+         @media (foo${exp}bar: 100px) {div {}}
+`}</style>;
+// expression in at-rule prop(beginning)
+<style jsx>{`
+         @media (${exp}bar: 100px) {div {}}
+`}</style>;
+// expression in at-rule prop(end)
+<style jsx>{`
+         @media (foo${exp}: 100px) {div {}}
+`}</style>;
+
+// expression in at-rule value(middle)
+<style jsx>{`
+         @media (min-width: foo${exp}bar) {div {}}
+`}</style>;
+// expression in at-rule value(beginning)
+<style jsx>{`
+         @media (min-width: ${exp}bar) {div {}}
+`}</style>;
+// expression in at-rule value(end)
+<style jsx>{`
+         @media (min-width: foo${exp}) {div {}}
+`}</style>;
+
+// expression in at-rule condition(middle)
+<style jsx>{`
+         @media (foo${exp}bar) {div {}}
+`}</style>;
+// expression in at-rule condition(beginning)
+<style jsx>{`
+         @media (${exp}bar) {div {}}
+`}</style>;
+// expression in at-rule condition(end)
+<style jsx>{`
+         @media (foo${exp}) {div {}}
+`}</style>;
+
+// expression as decl
+<style jsx>{`
+         a {
+${exp};
+	   }
+`}</style>;
+
+// expression as decl(many)
+<style jsx>{`
+         a {
+${exp};
+${exp};
+	   }
+`}</style>;
+// expression as decl(no breaks)
+<style jsx>{`
+         a {
+${exp}${exp};
+	   }
+`}</style>;
+<style jsx>{`
+         a {
+${exp};${exp};
+	   }
+`}</style>;
+// expression as decl(middle), FIXME
+<style jsx>{`
+         a {
+foo${exp}bar;
+	   }
+`}</style>;
+// expression as decl(beginning), FIXME
+<style jsx>{`
+         a {
+${exp}bar;
+	   }
+`}</style>;
+// expression as decl(end), FIXME
+<style jsx>{`
+         a {
+foo${exp};
+	   }
+`}</style>;
+
+// real world cases
 <style jsx>{`
   div {
   display: ${expr};
@@ -42,51 +239,4 @@
   div {
   animation: 3s ease-in 1s ${foo => foo.getIterations()} reverse both paused slidein;
   }
-`}</style>;
-
-// need fix bellow
-
-// expression in tag
-<style jsx>{`
-foo${exp}bar {}
-`}</style>;
-
-// expression in id
-<style jsx>{`
- foo${exp}bar {}
-`}</style>;
-
-// expression in attribute name
-<style jsx>{`
-[foo${exp}bar] {}
-`}</style>;
-
-// expression in attribute value
-<style jsx>{`
-[a^=foo${exp}bar] {}
-`}</style>;
-
-// expression in class selector
-<style jsx>{`
-.foo${exp}bar {}
-`}</style>;
-
-// FIXME:
-// expression in expression in decl prop
-<style jsx>{`
-div {
-foo${exp}bar: 1;
-foo${exp}: 1;
-${exp}bar: 1;
-}
-`}</style>;
-
-// FIXME: only working for prefix
-// expression in expression in decl value
-<style jsx>{`
-div {
-a: foo${exp}bar;
-b: ${exp}bar;
-b: foo${exp};
-}
 `}</style>;
