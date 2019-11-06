4fb705b4ef859905e848d51cc08f030c2fb7ed06
Christopher J. Brody <chris@brody.consulting>
2019-11-05 19:42:14 -0500

    add pipeline function call test
    which was correct according to prettier@1.18.2
    
    Co-authored-by: Simon Lydell <simon.lydell@gmail.com>
    Co-authored-by: Christopher J. Brody <chris@brody.consulting>

---
+++ tests/functional_composition/pipe-function-calls.js
@@ -0,0 +1,43 @@
+// input is formatted correctly according to prettier@1.18.2
+
+(() => {
+  pipe(
+    timelines,
+    everyCommitTimestamps,
+    A.sort(ordDate),
+    A.head
+  );
+
+  pipe(
+    serviceEventFromMessage(msg),
+    TE.chain(
+      flow(
+        publishServiceEvent(analytics),
+        TE.mapLeft(nackFromError)
+      )
+    )
+  )()
+    .then(messageResponse(logger, msg))
+    .catch((err: Error) => {
+      logger.error(
+        pipe(
+          O.fromNullable(err.stack),
+          O.getOrElse(constant(err.message))
+        )
+      );
+      process.exit(1);
+    });
+
+  pipe(
+    Changelog.timestampOfFirstCommit([[commit]]),
+    O.toUndefined
+  );
+
+  chain(
+    flow(
+      getUploadUrl,
+      E.mapLeft(Errors.unknownError),
+      TE.fromEither
+    )
+  );
+})();
