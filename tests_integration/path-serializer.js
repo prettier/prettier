1cc03f750d10dda58133afa1906d6d8cc4890184
fisker <lionkay@gmail.com>
2019-11-07 13:06:45 +0800

    Show invalid config filename in error message

--- tests_integration/path-serializer.js
+++ tests_integration/path-serializer.js
@@ -1,9 +1,18 @@
 "use strict";
 
+const replaceCWD = path => {
+  const cwd = process.cwd();
+  while (path.indexOf(cwd) !== -1) {
+    path = path.replace(cwd, "<cwd>");
+  }
+
+  return path;
+};
+
 module.exports = {
   test: value =>
     typeof value === "string" &&
     (value.indexOf("\\") > -1 || value.indexOf(process.cwd()) > -1),
   print: (value, serializer) =>
-    serializer(value.replace(process.cwd(), "<cwd>").replace(/\\/g, "/"))
+    serializer(replaceCWD(value).replace(/\\/g, "/"))
 };
