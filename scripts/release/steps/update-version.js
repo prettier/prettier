b790c4bc1986b04fb6b6fe569471c69abfdb772b
fisker <lionkay@gmail.com>
2019-11-08 13:01:35 +0800

    Fix unpkg link in docs

--- scripts/release/steps/update-version.js
+++ scripts/release/steps/update-version.js
@@ -3,7 +3,7 @@
 const execa = require("execa");
 const { logPromise, readJson, writeJson, processFile } = require("../utils");
 
-async function bump({ version }) {
+async function bump({ version, previousVersion }) {
   const pkg = await readJson("package.json");
   pkg.version = version;
   await writeJson("package.json", pkg, { spaces: 2 });
@@ -15,6 +15,17 @@ async function bump({ version }) {
   processFile(".github/ISSUE_TEMPLATE/integration.md", content =>
     content.replace(/^(- Prettier Version: ).*?$/m, `$1${version}`)
   );
+
+  // Update unpkg link in docs
+  processFile("docs/browser.md", content => {
+    const previousUnpkgLink = `https://unpkg.com/prettier@${previousVersion}/`;
+    const unpkgLink = `https://unpkg.com/prettier@${version}/`;
+    while (content.includes(previousUnpkgLink)) {
+      content = content.replace(previousUnpkgLink, unpkgLink);
+    }
+
+    return content;
+  });
 
   await execa("yarn", ["update-stable-docs"], {
     cwd: "./website"
