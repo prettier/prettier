776303a339c21c13cd165db4553feff9f1d27ad0
fisker <lionkay@gmail.com>
2019-11-09 23:51:52 +0800

    Fix bin mod

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -99,7 +99,7 @@ async function run(params) {
     await execa("rm", ["-rf", ".cache"]);
   }
 
-  const bundleCache = new Cache(".cache/", "v18");
+  const bundleCache = new Cache(".cache/", "v19");
   await bundleCache.load();
 
   console.log(chalk.inverse(" Building packages "));
