1186a0bfbfd29267bfb782abdd8e660c18d2496d
fisker <lionkay@gmail.com>
2019-11-10 15:43:10 +0800

    Update cache version

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -98,7 +98,7 @@ async function run(params) {
     await execa("rm", ["-rf", ".cache"]);
   }
 
-  const bundleCache = new Cache(".cache/", "v19");
+  const bundleCache = new Cache(".cache/", "v20");
   await bundleCache.load();
 
   console.log(chalk.inverse(" Building packages "));
