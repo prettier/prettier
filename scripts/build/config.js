37846a70b192aa091ca925c5c45b9d3638384da7
Nick McCurdy <nick@nickmccurdy.com>
2019-11-12 11:46:28 -0500

    Remove typescript-etw from build (#6919)
    Co-authored-by: Jed Fox <git@twopointzero.us>

--- scripts/build/config.js
+++ scripts/build/config.js
@@ -41,11 +41,8 @@ const parsers = [
     input: "src/language-js/parser-typescript.js",
     target: "universal",
     babelPlugins: [babelReplaceArrayIncludesWithIndexof],
-    commonjs: {
-      ignore: [
-        // Optional package for TypeScript that logs ETW events (a Windows-only technology).
-        "@microsoft/typescript-etw"
-      ]
+    replace: {
+      'require("@microsoft/typescript-etw")': "undefined"
     }
   },
   {
