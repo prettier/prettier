dffcc2d7a9f07deaf082f3d18312cd924c91aef2
Christopher J. Brody <chris@brody.consulting>
2019-11-13 01:23:05 +0000

    [TBD ...] update mutation in stryker.conf.js for language-js processing

--- stryker.conf.js
+++ stryker.conf.js
@@ -3,6 +3,15 @@
 module.exports = function(config) {
   config.set({
     mutator: "javascript",
+    mutate: [
+      // [FUTURE TBD] this seems to take about 100 hours on a 40-CPU server in the cloud:
+      // "src/**/*.js"
+      // [TBD] limited testing related to language-js processing for now:
+      "src/common/*.js",
+      "src/language-js/*.js",
+      "src/utils/*.js"
+      // ...
+    ],
     packageManager: "yarn",
     reporters: ["html", "clear-text", "progress"],
     testRunner: "command",
