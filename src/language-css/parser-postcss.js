1160388eb29eb24a7c5327d9a7344d2a9fadb07c
fisker <lionkay@gmail.com>
2019-11-17 01:01:22 +0800

    remove hack

--- src/language-css/parser-postcss.js
+++ src/language-css/parser-postcss.js
@@ -513,13 +513,6 @@ function requireParser(isSCSSParser) {
     return require("postcss-scss");
   }
 
-  // TODO: Remove this hack when this issue is fixed:
-  // https://github.com/shellscape/postcss-less/issues/88
-  const LessParser = require("postcss-less/dist/less-parser");
-  LessParser.prototype.atrule = function(...args) {
-    return Object.getPrototypeOf(LessParser.prototype).atrule.apply(this, args);
-  };
-
   return require("postcss-less");
 }
 
