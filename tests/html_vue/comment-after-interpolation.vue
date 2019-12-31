685e51e023f3dcc07935d70a232a022967e1fc8c
Daiki Ihara <sasurau4@gmail.com>
2019-12-31 23:40:07 +0900

    add test for comment-after-interpolation

---
+++ tests/html_vue/comment-after-interpolation.vue
@@ -0,0 +1,5 @@
+<template>
+  <legend>{{ $t('package') }}<!-- comment after interpolation --></legend><!-- comment after tag-->
+  
+  <legend>{{ $t('package') }}</legend><!-- comment after tag -->
+</template>
