85eefb918e7e5bf878b6007c07d446e172337f15
fisker Cheung <lionkay@gmail.com>
2020-01-14 19:25:24 +0800

    Style: enable `object-shorthand` (#7322)

--- src/standalone.js
+++ src/standalone.js
@@ -42,7 +42,7 @@ function withPlugins(fn) {
 const formatWithCursor = withPlugins(core.formatWithCursor);
 
 module.exports = {
-  formatWithCursor: formatWithCursor,
+  formatWithCursor,
 
   format(text, opts) {
     return formatWithCursor(text, opts).formatted;
