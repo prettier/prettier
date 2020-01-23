6f1ed54206b0876e1ec08bb2c64edfb778cd0b86
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2020-01-23 10:34:53 +0200

    fix tests on Windows: drive letter case returned by process.cwd() depends on environment (#7409)

--- tests_integration/path-serializer.js
+++ tests_integration/path-serializer.js
@@ -2,8 +2,18 @@
 
 const replaceCWD = text => {
   const cwd = process.cwd();
-  while (text.includes(cwd)) {
-    text = text.replace(cwd, "<cwd>");
+
+  const variants = /^[a-z]:\\/i.test(cwd)
+    ? [
+        cwd.charAt(0).toLowerCase() + cwd.slice(1),
+        cwd.charAt(0).toUpperCase() + cwd.slice(1)
+      ]
+    : [cwd];
+
+  for (const variant of variants) {
+    while (text.includes(variant)) {
+      text = text.replace(variant, "<cwd>");
+    }
   }
 
   return text;
