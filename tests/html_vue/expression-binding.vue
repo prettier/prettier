141305872041baefecd7cd9c0fdbad4fe66c45cb
fisker <lionkay@gmail.com>
2020-01-30 15:24:21 +0800

    Add failed test

---
+++ tests/html_vue/expression-binding.vue
@@ -0,0 +1,13 @@
+<!-- #7396 -->
+<template>
+	<Element :src="`loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong${foo} template literal value`"/>
+</template>
+<template>
+	<Element :src="`loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong${foo +               bar} template literal value`"/>
+</template>
+<template>
+	<img :src="'loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong literal string value'"/>
+</template>
+<template>
+	<img :src="100000000000000000000000000000000000000000000000000000000000000000000000000000"/>
+</template>
