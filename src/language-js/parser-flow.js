e27039cbf025a8c1184b6ed25a53156e934cdb8e
fisker <lionkay@gmail.com>
2020-01-30 19:28:48 +0800

    Update flow parse options

--- src/language-js/parser-flow.js
+++ src/language-js/parser-flow.js
@@ -11,12 +11,13 @@ function parse(text, parsers, opts) {
   const flowParser = require("flow-parser");
 
   const ast = flowParser.parse(text, {
-    enums: true,
+    esproposal_decorators: true,
     esproposal_class_instance_fields: true,
     esproposal_class_static_fields: true,
     esproposal_export_star_as: true,
     esproposal_optional_chaining: true,
-    esproposal_nullish_coalescing: true
+    esproposal_nullish_coalescing: true,
+    types: true
   });
 
   if (ast.errors.length > 0) {
