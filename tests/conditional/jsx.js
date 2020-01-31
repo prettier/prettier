01611382e365c70e4b8f91cea1de66535556e9ec
fisker <lionkay@gmail.com>
2020-01-31 21:15:20 +0800

    Add failed test

---
+++ tests/conditional/jsx.js
@@ -0,0 +1,24 @@
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+null
+
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+undefined
+
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+0
+
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+function() {}
+
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+() => {}
+
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+''
+
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+<span>a short jsx</span>
+
+foo ? <span>loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span> :
+<span>another loooooooooooooooooooooooooooooooooooooooooooooooooooooong jsx</span>
+
