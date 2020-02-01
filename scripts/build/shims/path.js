fe3323d12268d608c79f9b87b4958b88c9180345
fisker <lionkay@gmail.com>
2020-02-02 03:37:13 +0800

    Fix issue with shim

--- scripts/build/shims/path.js
+++ scripts/build/shims/path.js
@@ -1,7 +1,14 @@
+const sep = /[\\/]/;
+
 export function extname(path) {
+  path = path.split(sep).pop();
   const dotIndex = path.lastIndexOf(".");
   if (dotIndex === -1) return "";
   return path.slice(dotIndex);
+}
+
+export function basename(path) {
+  return path.split(sep).pop();
 }
 
 export function isAbsolute() {
