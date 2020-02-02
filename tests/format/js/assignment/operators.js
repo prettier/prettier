f41990d9a77a530283b1f3e7afb3883e01a597c2
Brandon Mills <mills.brandont@gmail.com>
2020-02-02 18:02:40 -0500

    Add additional tests around operators
    Before changing operator positions, these tests demonstrate the current
    state.

---
+++ tests/format/js/assignment/operators.js
@@ -0,0 +1,16 @@
+var varDeclaration = Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+let letDeclaration = Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+const constDeclaration = Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+assignment = Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+additionAssignment += Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+subtractionAssignment -= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+multiplicationAssignment *= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+divisionAssignment /= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+remainderAssignment %= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+exponentiationAssignment **= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+leftShiftAssignment <<= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+rightShiftAssignment >>= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+unsignedRightShiftAssignment >>>= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+bitwiseAndAssignment &= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+bitwiseXorAssignment ^= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
+bitwiseOrAssignment |= Math.random() * (yRange * (1 - minVerticalFraction)) + minVerticalFraction * yRange - offset;
