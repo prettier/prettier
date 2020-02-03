fb0503588200fb07587321eec4f6157fb6cb587c
fisker <lionkay@gmail.com>
2020-02-03 18:20:13 +0800

    Try usePrism for all

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -59,7 +59,7 @@ const siteConfig = {
   stylesheets: [
     "//unpkg.com/@sandhose/prettier-animated-logo@1.0.3/dist/wide.css"
   ],
-  usePrism: ["jsx"],
+  usePrism: true,
   algolia: {
     apiKey: process.env.ALGOLIA_PRETTIER_API_KEY,
     indexName: "prettier"
