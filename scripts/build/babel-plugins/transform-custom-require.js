d241eb103bd04a0ee70f0fb822dd91064f2f5e1a
Christopher J. Brody <chris.brody@gmail.com>
2020-02-04 12:56:01 -0500

    update transform-custom-require Babel plugin

--- scripts/build/babel-plugins/transform-custom-require.js
+++ scripts/build/babel-plugins/transform-custom-require.js
@@ -19,26 +19,13 @@ module.exports = function(babel) {
     visitor: {
       CallExpression(path) {
         const { node } = path;
-        if (isEvalRequire(node.callee) && node.arguments.length === 1) {
+        if (isEvalRequire(node) && node.arguments.length === 1) {
           let arg = node.arguments[0];
           if (t.isLiteral(arg) && arg.value.startsWith(".")) {
             const value = "." + arg.value.slice(arg.value.lastIndexOf("/"));
             arg = t.stringLiteral(value);
           }
           path.replaceWith(t.callExpression(t.identifier("require"), [arg]));
-        }
-      },
-      MemberExpression(path) {
-        const { node } = path;
-        if (isEvalRequire(node.object)) {
-          path.replaceWith(
-            t.memberExpression(
-              t.identifier("require"),
-              node.property,
-              node.compute,
-              node.optional
-            )
-          );
         }
       }
     }
@@ -47,9 +34,7 @@ module.exports = function(babel) {
   function isEvalRequire(node) {
     return (
       t.isCallExpression(node) &&
-      t.isIdentifier(node.callee, { name: "eval" }) &&
-      node.arguments.length === 1 &&
-      t.isLiteral(node.arguments[0], { value: "require" })
+      t.isIdentifier(node.callee, { name: "requireModule" })
     );
   }
 };
