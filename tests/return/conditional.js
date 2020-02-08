0e5e3295d3206f5e6cc78509d492079faed5f022
fisker <lionkay@gmail.com>
2020-02-09 03:09:58 +0800

    Add fail tests

---
+++ tests/return/conditional.js
@@ -0,0 +1,23 @@
+function foo (){
+      return (
+          a
+? looooooooooooooooooooooooooooooooooooooooooooooooooong1
+: looooooooooooooooooooooooooooooooooooooooooooooooooong2
+      );
+}
+
+function foo (){
+      return (
+          a
+? looooooooooooooooooooooooooooooooooooooooooooooooooong1
+: looooooooooooooooooooooooooooooooooooooooooooooooooong2
+      ).foo;
+}
+
+function foo (){
+      return (
+          a
+? looooooooooooooooooooooooooooooooooooooooooooooooooong1
+: looooooooooooooooooooooooooooooooooooooooooooooooooong2
+      ).foo();
+}
