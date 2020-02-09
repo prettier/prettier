0909039f56bb1ba3f1eff1ed057e58870503cf9c
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2020-02-08 22:55:33 -0500

    Test for new printing style

---
+++ tests/typescript_typed_arrow_fn_declr/declared-arrow-fn.ts
@@ -0,0 +1,2 @@
+const arrFn: (x: string) => (y: number) => (z: boolean) => [string, number, boolean]
+  = (x) => y => z => [x, y, z]
---
+++ tests/typescript_typed_arrow_fn_declr/long-type-defn.ts
@@ -0,0 +1,9 @@
+const arrFn: (x: SuperLongTypeNameDefinedElseWhere) => (y: AnotherReallyLongTypeName) => (z: ThirdVeryLongAndBoringInterfaceName) => VeryLongReturnTypeValue;
+
+const arrFnWithImpl: (x: SuperLongTypeNameDefinedElseWhere) => (y: AnotherReallyLongTypeName) => (z: ThirdVeryLongAndBoringInterfaceName) => VeryLongReturnTypeValue
+  = (argumentNumberOne) => (argumentNumberTwo) => (argumentNumberThree) =>
+    someVeryVeryVeryLoooooooooongFunctionInvocation(argumentNumberOne, argumentNumberTwo, argumentNumberThree);
+
+const arrFnWithLongerImpl: (x: SuperLongTypeNameDefinedElseWhere) => (y: AnotherReallyLongTypeName) => (z: ThirdVeryLongAndBoringInterfaceName) => VeryLongReturnTypeValue
+  = (argumentNumberOne) => (argumentNumberTwo) => (argumentNumberThreeIsLoooooooooooooooooooooooooooooong) =>
+    someVeryVeryVeryLoooooooooongFunctionInvocation(argumentNumberOne, argumentNumberTwo, argumentNumberThreeIsLoooooooooooooooooooooooooooooong);
