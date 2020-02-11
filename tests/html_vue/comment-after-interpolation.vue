ea7fc29a79a2fe6d2bb22c7bcb1bebd9768b71f1
Daiki Ihara <sasurau4@gmail.com>
2020-02-11 23:34:35 +0900

    fix test of long long comment after interpolation in vue file

--- tests/html_vue/comment-after-interpolation.vue
+++ tests/html_vue/comment-after-interpolation.vue
@@ -2,4 +2,6 @@
   <legend>{{ $t('package') }}<!-- comment after interpolation --></legend><!-- comment after tag-->
   
   <legend>{{ $t('package') }}</legend><!-- comment after tag -->
+
+  <legend>{{ $t('package') }}<!-- long long long long comment after interpolation --></legend>
 </template>
