0da9bd1eafd8fd2eca407bd8131c7644d6d441d4
fisker <lionkay@gmail.com>
2020-02-15 17:04:31 +0800

    `node_modules` and `.svn` tests

--- tests_integration/__tests__/patterns-glob.js
+++ tests_integration/__tests__/patterns-glob.js
@@ -8,7 +8,7 @@ expect.addSnapshotSerializer(require("../path-serializer"));
 fixtures-1/
 ├─ !file.js
 ├─ a.js
-└─ b.mjs
+└─ b.js
 */
 
 describe("fixtures-1: Should match all files", () => {
@@ -33,19 +33,53 @@ fixtures-2/
 */
 
 describe("fixtures-2: Should match all files", () => {
-  runPrettier("cli/patterns-glob/fixtures-2", ["**.js", "!dir.js", "-l"]).test({
+  runPrettier("cli/patterns-glob/fixtures-2", ["*.js", "!dir.js", "-l"]).test({
     status: 1
   });
 });
 
 describe("fixtures-2: Should not match any css file", () => {
-  runPrettier("cli/patterns-glob/fixtures-2", ["**.js", "!b.js", "-l"]).test({
+  runPrettier("cli/patterns-glob/fixtures-2", ["*.js", "!b.js", "-l"]).test({
     status: 1
   });
 });
 
 describe("fixtures-2: Should only match `!b.js`", () => {
-  runPrettier("cli/patterns-glob/fixtures-2", ["**.js", "!a.js", "-l"]).test({
+  runPrettier("cli/patterns-glob/fixtures-2", ["*.js", "!a.js", "-l"]).test({
+    status: 1
+  });
+});
+
+/*
+fixtures-3/
+├─ outside.js
+└─ !dir.js/
+  ├─ inside.js
+  ├─ node_modules
+  │ └─in-node_modules.js
+  └─ .svn
+    └─in-svn.js
+*/
+
+describe("fixtures-3: Should match all files except `.svn/in-svn.js`", () => {
+  runPrettier("cli/patterns-glob/fixtures-3", [
+    "*.js",
+    "!dir.js",
+    "-l",
+    "--with-node-modules"
+  ]).test({
+    status: 1
+  });
+});
+
+describe("fixtures-3: Should exclude `node_modules/in-node_modules.js`", () => {
+  runPrettier("cli/patterns-glob/fixtures-3", ["*.js", "!dir.js", "-l"]).test({
+    status: 1
+  });
+});
+
+describe("fixtures-3: Should exclude `.svn`", () => {
+  runPrettier("cli/patterns-glob/fixtures-3", ["*.js", ".svn", "-l"]).test({
     status: 1
   });
 });
