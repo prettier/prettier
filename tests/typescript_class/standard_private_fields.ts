c6607e5ed9098703f9fdd390d0ad29da16e5ebfa
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2020-02-24 16:25:22 +0200

    Update standard_private_fields.ts

--- tests/typescript_class/standard_private_fields.ts
+++ tests/typescript_class/standard_private_fields.ts
@@ -1,8 +1,10 @@
 class Square {
     #sideLength: number;
+    readonly #area: number;
 
     constructor(sideLength: number) {
         this.#sideLength = sideLength;
+        this.#area = this.#sideLength ** 2;
     }
 
     equals(other: any) {
