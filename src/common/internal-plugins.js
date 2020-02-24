b3f0102a872c8cae81b207a64fdce4cab6563531
fisker Cheung <lionkay@gmail.com>
2020-02-25 00:19:03 +0800

    Remove `babylon` parser (#7533)
    * Remove `babylon`
    
    * Add changelog
    
    * Fix website files

--- src/common/internal-plugins.js
+++ src/common/internal-plugins.js
@@ -12,42 +12,39 @@ module.exports = [
     parsers: {
       // JS - Babel
       get babel() {
-        return eval("require")("../language-js/parser-babylon").parsers.babel;
+        return eval("require")("../language-js/parser-babel").parsers.babel;
       },
       get "babel-flow"() {
-        return eval("require")("../language-js/parser-babylon").parsers[
+        return eval("require")("../language-js/parser-babel").parsers[
           "babel-flow"
         ];
       },
       get "babel-ts"() {
-        return eval("require")("../language-js/parser-babylon").parsers[
+        return eval("require")("../language-js/parser-babel").parsers[
           "babel-ts"
         ];
       },
-      get babylon() {
-        return eval("require")("../language-js/parser-babylon").parsers.babel;
-      },
       get json() {
-        return eval("require")("../language-js/parser-babylon").parsers.json;
+        return eval("require")("../language-js/parser-babel").parsers.json;
       },
       get json5() {
-        return eval("require")("../language-js/parser-babylon").parsers.json5;
+        return eval("require")("../language-js/parser-babel").parsers.json5;
       },
       get "json-stringify"() {
-        return eval("require")("../language-js/parser-babylon").parsers[
+        return eval("require")("../language-js/parser-babel").parsers[
           "json-stringify"
         ];
       },
       get __js_expression() {
-        return eval("require")("../language-js/parser-babylon").parsers
+        return eval("require")("../language-js/parser-babel").parsers
           .__js_expression;
       },
       get __vue_expression() {
-        return eval("require")("../language-js/parser-babylon").parsers
+        return eval("require")("../language-js/parser-babel").parsers
           .__vue_expression;
       },
       get __vue_event_binding() {
-        return eval("require")("../language-js/parser-babylon").parsers
+        return eval("require")("../language-js/parser-babel").parsers
           .__vue_event_binding;
       },
       // JS - Flow
