17b7c58a00692a692b0a2a1cba2c4bfbcb27f58b
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2020-03-09 23:31:42 +0200

    improve tests for import/export type modifier

--- tests/typescript_import_export/type-modifier.ts
+++ tests/typescript_import_export/type-modifier.ts
@@ -1,5 +1,16 @@
-import type { SomeThing } from "./some-module.js";
 export type { SomeThing };
+export type { A as B };
+export type { B as C } from './a';
+export type { foo } from 'bar';
+export type * from 'bar';
+export type { foo };
 
-import type Foo from "./foo.js";
-export type Foo;
+// this should be treated as a normal import statement
+import type from './foo';
+
+import type { SomeThing } from "./some-module.js";
+import type { foo, bar } from 'baz';
+import type { foo as bar } from 'baz';
+import type * as foo from './bar';
+import type foo from 'bar';
+import type foo, { bar } from 'bar';
