6566c19b723be632a0e80545963210d5bb146c8c
fisker <lionkay@gmail.com>
2020-03-18 14:27:11 +0800

    jest config

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,6 @@ module.exports = {
     "jest-snapshot-serializer-ansi"
   ],
   testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
-  testPathIgnorePatterns: ["tests/new_react", "tests/more_react"],
   collectCoverage: ENABLE_CODE_COVERAGE,
   collectCoverageFrom: ["src/**/*.js", "!<rootDir>/node_modules/"],
   coveragePathIgnorePatterns: [
