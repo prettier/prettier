875dc1d447eabf39fca36322444dadadb962ab3d
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2020-03-19 11:58:47 +0200

    refine tests

--- tests/comments_closure_typecast/non-casts.js
+++ tests/comments_closure_typecast/non-casts.js
@@ -4,7 +4,15 @@ z(x => {
   return (1);
 })
 
+/** @type { } */
+z(x => {
+  (foo)((bar)(2+(3)))
+  return (1);
+})
+
 /** @type {number} */
 let q = z(x => {
   return (1);
 })
+
+const w1 = /** @typefoo Foo */ (value);
