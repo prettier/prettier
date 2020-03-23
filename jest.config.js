54f907dba391946ced6f8a56f450e88319957ead
fisker Cheung <lionkay@gmail.com>
2020-03-23 09:36:44 +0800

    Followup work on setup github actions (#7431)
    * Followup work on setup github actions
    
    * Coverage report & split `prod_lint`
    
    * Add env
    
    * Remove coverage report on azure pipelines
    
    * Remove build step on lint
    
    * remove coverage report on prod_test
    
    * Use `ci-info` instead of `is-ci`
    
    * Always enable `ENABLE_TEST_RESULTS` and `AST_COMPARE` on CI
    
    * Standalone test and remove some env
    
    * coverage report
    
    * reduce matrix
    
    * standalone test
    
    * `ci-info`
    
    * Remove token
    
    * Disable `coverage` on azure pipelines
    
    * use master version of codecov-action
    
    * Link to master status
    
    * fix prod lint
    
    * Use boolean env
    
    * Remove locv report
    
    * Try lcov report
    
    * pin `codecov-action` version, enable `verbose`
    
    * azure coverage
    
    * Name
    
    * Trigger CI
    
    * Use `basename`
    
    * Trigger CI
    
    * Try `runInBand`
    
    * try `maxWorkers`
    
    Co-authored-by: Georgii Dolzhykov <thorn.mailbox@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
     "<rootDir>/src/doc/doc-debug.js",
     "<rootDir>/src/main/massage-ast.js",
   ],
-  coverageReporters: ["text", "html", "cobertura"],
+  coverageReporters: ["text", "html", "cobertura", "lcov"],
   moduleNameMapper: {
     // Jest wires `fs` to `graceful-fs`, which causes a memory leak when
     // `graceful-fs` does `require('fs')`.
@@ -37,4 +37,5 @@ module.exports = {
     "jest-watch-typeahead/testname",
   ],
   reporters: ["default"].concat(ENABLE_TEST_RESULTS ? "jest-junit" : []),
+  verbose: true,
 };
