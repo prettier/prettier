56179f196f554ba0162ac5b974ff89c83cc66fee
sidv <sidharthv96@gmail.com>
2020-03-28 17:19:30 +0530

    Add tests #6501

---
+++ tests/end_of_line/multiline.js
@@ -0,0 +1,16 @@
+const foo = bar(
+  (_0: baz) => {
+    setTimeout(() => {
+      /*
+          Multiline comment
+          Multiline comment
+          Multiline comment
+      */
+      console.log(
+        'Multiline string\
+         Multiline string\
+         Multiline string'
+      );
+    });
+  }
+);
