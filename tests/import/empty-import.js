e500fd7a1d006a375662fe94cbb830447192da8f
fisker <lionkay@gmail.com>
2020-04-11 08:15:32 +0800

    Add failed tests

--- tests/import/empty-import.js
+++ tests/import/empty-import.js
@@ -1,1 +1,38 @@
 import { } from '@types/googlemaps';
+import 'a';
+import /* comment */ 'a';
+import // comment
+'a';
+import { /* comment */ } from 'a';
+import /* comment */ {} from 'a';
+import {} /* comment */ from 'a';
+import /* comment */ {/* comment */} /* comment */ from 'a';
+import {
+  // comment
+} from 'a';
+import // comment
+{} from 'a';
+import {}
+// comment
+from 'a';
+import
+// comment
+{
+// comment
+}
+// comment
+from 'a';
+
+import {} from // comment ends with from
+'a';
+import {} from /* comment ends with from */ 'a';
+import {} from // comment not ends with from ___
+'a';
+import {} from /* comment not ends with from ___ */ 'a';
+
+import // comment ends with from
+'a';
+import /* comment ends with from */ 'a';
+import // comment not ends with from ___
+'a';
+import /* comment not ends with from ___ */ 'a';
