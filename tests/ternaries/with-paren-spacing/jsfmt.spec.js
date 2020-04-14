c79ae9729d518da083b6882931d038798d7764ad
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-04-13 21:46:58 -0400

    [prettierx] fix comments in tests/ternaries/with-*
    (tests/ternaries/with-*/jsfmt.spec.js)

--- tests/ternaries/with-paren-spacing/jsfmt.spec.js
+++ tests/ternaries/with-paren-spacing/jsfmt.spec.js
@@ -1,5 +1,6 @@
-// NOTICE: This test script runs for test files in ..,
-// NOT on any files in the same subdirectory.
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
 
 const dirpath = `${__dirname}/..`;
 
