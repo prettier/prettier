d06c42ec57a5dc60b704ed18be181797ec1e2d40
fisker <lionkay@gmail.com>
2020-04-17 18:06:19 +0800

    Lazy load

--- src/common/resolve.js
+++ src/common/resolve.js
@@ -1,19 +1,17 @@
 "use strict";
 
-const resolve = require("resolve");
-
-const { resolve: nativeResolve } = eval("require");
+let { resolve } = eval("require");
 
 // In the VS Code extension `require` is overridden and `require.resolve` doesn't support the 2nd argument.
-if (nativeResolve.length === 1) {
-  module.exports = (id, options) => {
+if (resolve.length === 1) {
+  resolve = (id, options) => {
     let baseDir;
     if (options && options.paths && options.paths.length === 1) {
       baseDir = options.paths[0];
     }
 
-    return resolve.sync(id, { baseDir });
+    return require("resolve").sync(id, { baseDir });
   };
-} else {
-  module.exports = nativeResolve;
 }
+
+module.exports = resolve;
