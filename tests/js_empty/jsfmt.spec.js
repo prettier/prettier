3a373074bcce194f5a9c6470ac545f366f0dd5f0
fisker Cheung <lionkay@gmail.com>
2020-04-21 15:57:58 +0800

    Run tests on snippets instead of files (#8088)
    Co-authored-by: Georgii Dolzhykov <thorn.mailbox@gmail.com>

--- tests/js_empty/jsfmt.spec.js
+++ tests/js_empty/jsfmt.spec.js
@@ -1,1 +1,18 @@
-run_spec(__dirname, ["babel", "flow", "typescript"]);
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      // empty
+      "",
+      // empty lines
+      "\n",
+      "\n\n\n\n",
+      // semicolons
+      ";",
+      ";;;;",
+      ";\n",
+      ";\n\n;;;\n",
+    ],
+  },
+  ["babel", "flow", "typescript"]
+);
