6cfe1f1f02bad0fd6260061d73513c1024b00a7c
fisker <lionkay@gmail.com>
2020-04-22 10:59:32 +0800

    Fix trailing commas when last property is ignored

--- tests/object_property_ignore/ignore.js
+++ tests/object_property_ignore/ignore.js
@@ -1,4 +1,36 @@
-const foo = {
+foo = {
   // prettier-ignore
-  bar: 1,
+  bar:            1,
+}
+
+foo = {
+  _: '',
+  // prettier-ignore
+  bar:            1,
+}
+
+/* comments */
+foo = {
+  _: '',
+  // prettier-ignore
+  bar:            1,         // comment
+}
+
+foo = {
+  _: '',
+  // prettier-ignore
+  bar:            1,         /* comment */
+}
+
+foo = {
+  _: '',
+  // prettier-ignore
+  bar:            /* comment */          1,
+}
+
+/* RestElement */
+foo = {
+  _: '',
+  // prettier-ignore
+  ...bar,
 }
--- tests/object_property_ignore/jsfmt.spec.js
+++ tests/object_property_ignore/jsfmt.spec.js
@@ -1,3 +1,5 @@
-run_spec(__dirname, ["babel"] /*, { trailingComma: "es5" }*/);
-run_spec(__dirname, ["babel"], { trailingComma: "none" });
-run_spec(__dirname, ["babel"], { trailingComma: "all" });
+const parser = ["babel", "flow", "typescript"];
+
+run_spec(__dirname, parser /*, { trailingComma: "es5" }*/);
+run_spec(__dirname, parser, { trailingComma: "none" });
+run_spec(__dirname, parser, { trailingComma: "all" });
