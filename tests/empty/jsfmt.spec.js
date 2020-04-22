47af6ceff48def73aeb23d9964ec999e9870f2b6
fisker <lionkay@gmail.com>
2020-04-22 14:09:57 +0800

    Test empty files with `snippets`

--- tests/empty/jsfmt.spec.js
+++ tests/empty/jsfmt.spec.js
@@ -1,11 +1,43 @@
-const prettier = require("prettier/local");
-const parsers = prettier
+const parsers = require("prettier/local")
   .getSupportInfo()
-  .options.find((option) => option.name === "parser");
+  .options.find((option) => option.name === "parser")
+  .choices.filter((choice) => !choice.deprecated)
+  .map((choice) => choice.value);
 
 run_spec(
-  __dirname,
-  parsers.choices
-    .filter((choice) => !choice.deprecated)
-    .map((choice) => choice.value)
+  {
+    dirname: __dirname,
+    snippets: [
+      // empty
+      "",
+      // space
+      " ",
+      "     ",
+      // `\n`
+      "\n",
+      " \n",
+      " \n ",
+      "\n\n\n\n",
+      " \n  \n \n\n",
+      " \n  \n \n\n ",
+      // TODO: fix following tests
+      // // `\r`
+      // "\r",
+      // " \r",
+      // " \r ",
+      // "\r\r\r\r",
+      // " \r  \r \r\r",
+      // " \r  \r \r\r ",
+      // // `\r\n`
+      // "\r\n",
+      // " \r\n",
+      // " \r\n ",
+      // "\r\n\r\n\r\n\r\n",
+      // " \r\n  \r\n \r\n\r\n",
+      // " \r\n  \r\n \r\n\r\n ",
+      // // mixed `\r` `\r\n` '\n'
+      // " \r \r\n \n",
+    ],
+  },
+  parsers
 );
