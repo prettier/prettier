9db1d9fae6d35bbd6bdfd68c93b9902b0f5a9b82
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2020-04-23 19:05:01 +0300

    add tests from #6259 and #7883

--- tests/multiparser_js_css/var.js
+++ tests/multiparser_js_css/var.js
@@ -2,3 +2,9 @@ const Something = styled.div`
   background: var(--${one}); /* ... */
   border: 1px solid var(--${two}); /* ... */
 `;
+
+const StyledPurchaseCard = styled(Card)`
+  min-width: 200px;
+  background-color: var(--${props => props.color});
+  color: #fff;
+`;
