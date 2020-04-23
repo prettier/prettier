b64d794f22f721b3d4409d6cd57f4b5a92d0e16e
fisker <lionkay@gmail.com>
2020-04-23 13:44:44 +0800

    Test codebase install on `yarn 2`

--- scripts/install-prettier.js
+++ scripts/install-prettier.js
@@ -7,7 +7,7 @@ const tempy = require("tempy");
 shell.config.fatal = true;
 
 const rootDir = path.join(__dirname, "..");
-const client = process.env.NPM_CLIENT || "yarn";
+const client = (process.env.NPM_CLIENT || "yarn").trim();
 const packageDir =
   process.env.NODE_ENV === "production" ? path.join(rootDir, "dist") : rootDir;
 
@@ -15,22 +15,24 @@ module.exports = () => {
   const tmpDir = tempy.directory();
   const file = shell.exec("npm pack", { cwd: packageDir }).stdout.trim();
   shell.mv(path.join(packageDir, file), tmpDir);
-  const tarPath = path.join(tmpDir, file);
-
-  shell.exec(`${client} init -y`, { cwd: tmpDir, silent: true });
+  shell.exec("npm init -y", { cwd: tmpDir, silent: true });
   let installCommand = "";
   switch (client) {
     case "npm":
       // npm fails when engine requirement only with `--engine-strict`
-      installCommand = `npm install "${tarPath}" --engine-strict`;
+      installCommand = `npm install "file:./${file}" --engine-strict`;
       break;
     case "pnpm":
       // Note: current pnpm can't work with `--engine-strict` and engineStrict setting in `.npmrc`
-      installCommand = `pnpm add "${tarPath}"`;
+      installCommand = `pnpm add "file:./${file}"`;
       break;
+    case "berry":
+      shell.exec("yarn set version berry", { cwd: tmpDir });
+    // Fall through
+    case "yarn":
     default:
       // yarn fails when engine requirement not compatible by default
-      installCommand = `yarn add "${tarPath}"`;
+      installCommand = `yarn add "file:./${file}"`;
   }
 
   shell.exec(installCommand, { cwd: tmpDir });
