960ecb469c0cf6342226661b606fb1028c7e3e2f
shisama <shisama07@gmail.com>
2020-04-24 22:44:45 +0900

    test: Add trailing-extra-space.js

---
+++ tests/comments/trailing-extra-space.js
@@ -0,0 +1,4 @@
+var a = { /* extra whitespace --> */  
+  b };
+var a = { /* no whitespace --> */
+  b };
--- tests/comments/trailing_space.js
+++ tests/comments/trailing_space.js
@@ -3,8 +3,3 @@
 // Do not trim trailing whitespace from this source file!
 
 // There is some space here ->                        
-
-var a = { /* extra whitespace --> */  
-  b };
-var a = { /* no whitespace --> */
-  b };
