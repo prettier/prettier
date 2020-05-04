208eaad44eefb4585160e6597d11048a9d64a8fd
fisker Cheung <lionkay@gmail.com>
2020-05-04 18:27:15 +0800

    Test empty files with `snippets` (#8116)
    * Test empty files with `snippets`
    * Assert output

--- tests/empty/jsfmt.spec.js
+++ tests/empty/jsfmt.spec.js
@@ -1,11 +1,42 @@
-const prettier = require("prettier/local");
-const parsers = prettier
+const parsers = require("prettier/local")
   .getSupportInfo()
-  .options.find((option) => option.name === "parser");
+  .options.find((option) => option.name === "parser")
+  .choices.filter((choice) => !choice.deprecated)
+  .map((choice) => choice.value);
 
 run_spec(
-  __dirname,
-  parsers.choices
-    .filter((choice) => !choice.deprecated)
-    .map((choice) => choice.value)
+  {
+    dirname: __dirname,
+    snippets: [
+      // empty
+      "",
+      // space
+      " ",
+      "     ",
+      // `\n`
+      "\n",
+      " \n",
+      " \n ",
+      "\n\n\n\n",
+      " \n  \n \n\n",
+      " \n  \n \n\n ",
+      // `\r`
+      "\r",
+      " \r",
+      " \r ",
+      "\r\r\r\r",
+      " \r  \r \r\r",
+      " \r  \r \r\r ",
+      // `\r\n`
+      "\r\n",
+      " \r\n",
+      " \r\n ",
+      "\r\n\r\n\r\n\r\n",
+      " \r\n  \r\n \r\n\r\n",
+      " \r\n  \r\n \r\n\r\n ",
+      // mixed `\r` `\r\n` '\n'
+      " \r \r\n \n ",
+    ].map((code) => ({ code, output: "" })),
+  },
+  parsers
 );
