5f238c932f9c0b941493276cf7ddcd26f40a61db
fisker <lionkay@gmail.com>
2020-05-09 00:24:06 +0800

    Restore

--- scripts/build/bundler.js
+++ scripts/build/bundler.js
@@ -147,7 +147,8 @@ function getRollupConfig(bundle) {
 
   const babelConfig = getBabelConfig(bundle);
 
-  const alias = { ...bundle.alias, entries: [...entries, ...(bundle.alias.entries || [])] };
+  const alias = { ...bundle.alias };
+  alias.entries = [...entries, ...(alias.entries || [])];
 
   config.plugins = [
     replace({
