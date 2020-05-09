af6f9d8be58e2ef70f04fb8aba84b11621198508
fisker <lionkay@gmail.com>
2020-05-10 03:32:37 +0800

    Workaround

--- src/language-js/parser-angular.js
+++ src/language-js/parser-angular.js
@@ -1,9 +1,30 @@
 "use strict";
 
 const locFns = require("./loc");
+let angularEstreeParserFixed = false;
 
 function createParser(_parse) {
   const parse = (text, parsers, options) => {
+    if (!angularEstreeParserFixed) {
+      const transformModule = require("angular-estree-parser/lib/transform");
+      const utils = require("angular-estree-parser/lib/utils");
+      const originalTransform = transformModule.transform;
+      transformModule.transform = (node, context, isInParentParens) => {
+        const type = utils.getNgType(node);
+        if (type === "MethodCall" && node.args && node.args.length === 1) {
+          const [firstArgument] = node.args;
+          node.args = [];
+          const result = originalTransform(node, context, isInParentParens);
+          result.arguments = [
+            originalTransform(firstArgument, context, isInParentParens),
+          ];
+          return result;
+        }
+        return originalTransform(node, context, isInParentParens);
+      };
+      angularEstreeParserFixed = true;
+    }
+
     const ngEstreeParser = require("angular-estree-parser");
     const node = _parse(text, ngEstreeParser);
     return {
