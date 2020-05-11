e618ba6dd4ed2ec7e30aed6b6fbc1dfc48035c6a
fisker <lionkay@gmail.com>
2020-05-11 16:13:52 +0800

    Update cache version

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -21,7 +21,7 @@ process.on("unhandledRejection", (err) => {
   process.exit(1);
 });
 
-const CACHE_VERSION = "v23"; // This need update when updating build scripts
+const CACHE_VERSION = "v24"; // This need update when updating build scripts
 const CACHED = chalk.bgYellow.black(" CACHED ");
 const OK = chalk.bgGreen.black("  DONE  ");
 const FAIL = chalk.bgRed.black("  FAIL  ");
