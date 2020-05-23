f2c1d70918d701b5ed977cff74a86fb77f503a90
fisker Cheung <lionkay@gmail.com>
2020-05-23 19:28:05 +0800

    JS: `pragma` support files with shebang (#8376)

--- src/language-js/pragma.js
+++ src/language-js/pragma.js
@@ -2,12 +2,32 @@
 
 const docblock = require("jest-docblock");
 
+// Logic from `/src/common/parser-include-shebang.js`
+function getShebang(text) {
+  if (!text.startsWith("#!")) {
+    return "";
+  }
+  const index = text.indexOf("\n");
+  if (index === -1) {
+    return "";
+  }
+  return text.slice(0, index);
+}
+
 function hasPragma(text) {
+  const shebang = getShebang(text);
+  if (shebang) {
+    text = text.slice(shebang.length + 1);
+  }
   const pragmas = Object.keys(docblock.parse(docblock.extract(text)));
   return pragmas.includes("prettier") || pragmas.includes("format");
 }
 
 function insertPragma(text) {
+  const shebang = getShebang(text);
+  if (shebang) {
+    text = text.slice(shebang.length + 1);
+  }
   const parsedDocblock = docblock.parseWithComments(docblock.extract(text));
   const pragmas = { format: "", ...parsedDocblock.pragmas };
   const newDocblock = docblock
@@ -18,7 +38,12 @@ function insertPragma(text) {
     .replace(/(\r\n|\r)/g, "\n"); // normalise newlines (mitigate use of os.EOL by jest-docblock)
   const strippedText = docblock.strip(text);
   const separatingNewlines = strippedText.startsWith("\n") ? "\n" : "\n\n";
-  return newDocblock + separatingNewlines + strippedText;
+  return (
+    (shebang ? `${shebang}\n` : "") +
+    newDocblock +
+    separatingNewlines +
+    strippedText
+  );
 }
 
 module.exports = {
