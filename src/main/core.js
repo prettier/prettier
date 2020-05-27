7f2ba08fb043b6927c94db817c60dbc8707a01ff
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2020-05-27 11:48:02 +0300

    refactor(range): minor cleanup (#8421)

--- src/main/core.js
+++ src/main/core.js
@@ -175,8 +175,7 @@ function formatRange(text, opts) {
   const { ast } = parsed;
   text = parsed.text;
 
-  const range = rangeUtil.calculateRange(text, opts, ast);
-  const { rangeStart, rangeEnd } = range;
+  const { rangeStart, rangeEnd } = rangeUtil.calculateRange(text, opts, ast);
   const rangeString = text.slice(rangeStart, rangeEnd);
 
   // Try to extend the range backwards to the beginning of the line.
