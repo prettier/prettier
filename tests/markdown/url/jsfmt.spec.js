0e25460ab2b7599bb3207b3f3f86567a0cf4cb9b
fisker <lionkay@gmail.com>
2020-05-30 02:33:13 +0800

    Test markdown auto link

---
+++ tests/markdown/url/jsfmt.spec.js
@@ -0,0 +1,45 @@
+const { flatten } = require("lodash");
+
+const urls = [
+  "http://www.example.com:80/_a",
+  "http://www.example.com:80/_a_",
+  "http://www.example.com:80/_a__",
+  "http://www.example.com:80/_a_b",
+  "http://www.example.com:80/_a_/",
+  "http://www.example.com:80/_a_/_",
+  "http://www.example.com:80/*a",
+  "http://www.example.com:80/*a*",
+  "http://www.example.com:80/*a**",
+  "http://www.example.com:80/*a*b",
+  "http://www.example.com:80/*a*/",
+  "http://www.example.com:80/*a*/*",
+  "http://www.example.com:80/_a*",
+  "http://www.example.com:80/_a_*",
+  "http://www.example.com:80/_a*b",
+  "http://www.example.com:80/_a*/",
+  "http://www.example.com:80/_a_/*",
+];
+
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: flatten(
+      urls.map((url) => [
+        url,
+        `_${url}_`,
+        `*${url}*`,
+        `_${url}*`,
+        `_ ${url}_`,
+        `* ${url}*`,
+        `_ ${url}*`,
+        `_${url} _`,
+        `*${url} *`,
+        `_${url} *`,
+        `_ ${url} _`,
+        `* ${url} *`,
+        `_ ${url} *`,
+      ])
+    ),
+  },
+  ["markdown"]
+);
