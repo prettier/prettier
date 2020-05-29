43da0adde7b50c97b9c1997534bdcbbeccb6bfc9
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2020-05-29 23:23:26 +0300

    Linting fix

--- tests/markdown/url/jsfmt.spec.js
+++ tests/markdown/url/jsfmt.spec.js
@@ -1,23 +1,23 @@
-const { flatten } = require('lodash');
+const { flatten } = require("lodash");
 
 const urls = [
-  'http://www.example.com:80/_a',
-  'http://www.example.com:80/_a_',
-  'http://www.example.com:80/_a__',
-  'http://www.example.com:80/_a_b',
-  'http://www.example.com:80/_a_/',
-  'http://www.example.com:80/_a_/_',
-  'http://www.example.com:80/*a',
-  'http://www.example.com:80/*a*',
-  'http://www.example.com:80/*a**',
-  'http://www.example.com:80/*a*b',
-  'http://www.example.com:80/*a*/',
-  'http://www.example.com:80/*a*/*',
-  'http://www.example.com:80/_a*',
-  'http://www.example.com:80/_a_*',
-  'http://www.example.com:80/_a*b',
-  'http://www.example.com:80/_a*/',
-  'http://www.example.com:80/_a_/*',
+  "http://www.example.com:80/_a",
+  "http://www.example.com:80/_a_",
+  "http://www.example.com:80/_a__",
+  "http://www.example.com:80/_a_b",
+  "http://www.example.com:80/_a_/",
+  "http://www.example.com:80/_a_/_",
+  "http://www.example.com:80/*a",
+  "http://www.example.com:80/*a*",
+  "http://www.example.com:80/*a**",
+  "http://www.example.com:80/*a*b",
+  "http://www.example.com:80/*a*/",
+  "http://www.example.com:80/*a*/*",
+  "http://www.example.com:80/_a*",
+  "http://www.example.com:80/_a_*",
+  "http://www.example.com:80/_a*b",
+  "http://www.example.com:80/_a*/",
+  "http://www.example.com:80/_a_/*",
 ];
 
 run_spec(
@@ -41,5 +41,5 @@ run_spec(
       ])
     ).map((code) => ({ code, name: code })),
   },
-  ['markdown']
+  ["markdown"]
 );
