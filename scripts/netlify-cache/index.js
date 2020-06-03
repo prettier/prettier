db9e728c92c54a47c3ad23f9a3a3020c2ba772bf
fisker <lionkay@gmail.com>
2020-06-03 21:16:02 +0800

    Add message

--- scripts/netlify-cache/index.js
+++ scripts/netlify-cache/index.js
@@ -4,9 +4,13 @@ const CACHE_DIR = "./.cache";
 
 module.exports = {
   async onPreBuild({ utils }) {
+    console.log(`Restoring cache ${CACHE_DIR}`);
     await utils.cache.restore(CACHE_DIR);
+    console.log(`Cache restored ${CACHE_DIR}`);
   },
   async onPostBuild({ utils }) {
+    console.log(`Saving  Cache ${CACHE_DIR}`);
     await utils.cache.save(CACHE_DIR);
+    console.log(`Cache Saved  ${CACHE_DIR}`);
   },
 };
