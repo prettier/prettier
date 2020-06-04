807f7fc1578b272c2ab817e29e7dcec940499e30
fisker <lionkay@gmail.com>
2020-06-04 21:04:10 +0800

    Fix crash on `tabWidth: 0`

---
+++ tests/misc/tab-width/jsfmt.spec.js
@@ -0,0 +1,3 @@
+run_spec(__dirname, ["babel"], { useTabs: true, tabWidth: 0 });
+run_spec(__dirname, ["babel"], { useTabs: true, tabWidth: Infinity });
+run_spec(__dirname, ["babel"], { useTabs: true, tabWidth: -2 });
