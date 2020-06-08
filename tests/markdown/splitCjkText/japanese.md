ed760cc00432ce70ee2e7a17fe1b4c3de51abd4f
remin <rdrgn@users.noreply.github.com>
2020-06-08 20:38:26 +0900

    test(markdown): add tests on cjkSpacing

---
+++ tests/markdown/splitCjkText/japanese.md
@@ -0,0 +1,5 @@
+私はJavaScriptが好きです。
+
+私は JavaScript が好きです。
+
+作る means MAKE in English.
--- tests/markdown/splitCjkText/jsfmt.spec.js
+++ tests/markdown/splitCjkText/jsfmt.spec.js
@@ -1,1 +1,7 @@
+run_spec(__dirname, ["markdown"]);
 run_spec(__dirname, ["markdown"], { proseWrap: "always" });
+run_spec(__dirname, ["markdown"], { cjkSpacing: "always" });
+run_spec(__dirname, ["markdown"], {
+  proseWrap: "always",
+  cjkSpacing: "always",
+});
