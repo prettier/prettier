f051170f9838f084b4bb51be0cdd07625e4d52e6
boyenn <boyenvaesen@hotmail.com>
2020-06-09 06:55:24 +0000

    fix: Update scss comment indentation (#7844)
    Co-authored-by: Boyen Vaesen <boyen.vaesen@kbc.be>

--- src/language-css/printer-postcss.js
+++ src/language-css/printer-postcss.js
@@ -709,6 +709,10 @@ function genericPrint(path, options, print) {
 
         // Add `hardline` after inline comment (i.e. `// comment\n foo: bar;`)
         if (isInlineValueCommentNode(iNode)) {
+          if (parentNode.type === "value-paren_group") {
+            parts.push(dedent(hardline));
+            continue;
+          }
           parts.push(hardline);
           continue;
         }
