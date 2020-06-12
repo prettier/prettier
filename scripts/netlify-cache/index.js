a5ca2b6161cb4f2b070a409c4ce0a8d15adceb92
fisker <lionkay@gmail.com>
2020-06-12 11:31:59 +0800

    Try absolute path

--- scripts/netlify-cache/index.js
+++ scripts/netlify-cache/index.js
@@ -1,9 +1,13 @@
 "use strict";
+const path = require('path');
 
 const CACHE_DIR = "./.cache";
 
+
 module.exports = {
   async onPreBuild({ utils }) {
+    const CACHE_DIR = path.join(__dirname, '../../.cache')
+
     console.log(`Restoring cache ${CACHE_DIR}`);
     try {
       await utils.cache.restore([CACHE_DIR]);
@@ -13,6 +17,7 @@ module.exports = {
     }
   },
   async onPostBuild({ utils }) {
+    const CACHE_DIR = path.join(__dirname, '../../.cache')
     console.log(`Saving  Cache ${CACHE_DIR}`);
     try {
       await utils.cache.save([CACHE_DIR]);
