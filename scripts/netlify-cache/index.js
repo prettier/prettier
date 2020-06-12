6d3fbc630477bdb03c5b470cc3af197d83aa8fc9
fisker <lionkay@gmail.com>
2020-06-12 10:36:28 +0800

    try/catch

--- scripts/netlify-cache/index.js
+++ scripts/netlify-cache/index.js
@@ -5,12 +5,20 @@ const CACHE_DIR = "./.cache";
 module.exports = {
   async onPreBuild({ utils }) {
     console.log(`Restoring cache ${CACHE_DIR}`);
-    await utils.cache.restore(CACHE_DIR);
-    console.log(`Cache restored ${CACHE_DIR}`);
+    try {
+      await utils.cache.restore(CACHE_DIR);
+      console.log(`Cache restored ${CACHE_DIR}`);
+    } catch (error) {
+      console.error(error);
+    }
   },
   async onPostBuild({ utils }) {
     console.log(`Saving  Cache ${CACHE_DIR}`);
-    await utils.cache.save(CACHE_DIR);
-    console.log(`Cache Saved  ${CACHE_DIR}`);
+    try {
+      await utils.cache.save(CACHE_DIR);
+      console.log(`Cache Saved  ${CACHE_DIR}`);
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
