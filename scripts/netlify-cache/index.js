af6548be6a01127f1595fea8dac1f2f7d5d39204
fisker <lionkay@gmail.com>
2020-06-12 10:22:20 +0800

    update plugin

--- scripts/netlify-cache/index.js
+++ scripts/netlify-cache/index.js
@@ -1,37 +1,16 @@
 "use strict";
 
-// const CACHE_DIR = "./.cache";
+const CACHE_DIR = "./.cache";
 
 module.exports = {
-  onPreBuild: () => {
-    console.log("Hello world from onPreBuild event!");
+  async onPreBuild({ utils }) {
+    console.log(`Restoring cache ${CACHE_DIR}`);
+    await utils.cache.restore(CACHE_DIR);
+    console.log(`Cache restored ${CACHE_DIR}`);
   },
-  onBuild: () => {
-    console.log("Hello world from onBuild  event!");
-  },
-  onPostBuild: () => {
-    console.log("Hello world from onPostBuild  event!");
-  },
-  onSuccess: () => {
-    console.log("Hello world from onSuccess  event!");
-  },
-  onError: () => {
-    console.log("Hello world from onError   event!");
-  },
-  onEnd: () => {
-    console.log("Hello world from onEnd   event!");
+  async onPostBuild({ utils }) {
+    console.log(`Saving  Cache ${CACHE_DIR}`);
+    await utils.cache.save(CACHE_DIR);
+    console.log(`Cache Saved  ${CACHE_DIR}`);
   },
 };
-
-// module.exports = {
-//   async onPreBuild({ utils }) {
-//     console.log(`Restoring cache ${CACHE_DIR}`);
-//     await utils.cache.restore(CACHE_DIR);
-//     console.log(`Cache restored ${CACHE_DIR}`);
-//   },
-//   async onPostBuild({ utils }) {
-//     console.log(`Saving  Cache ${CACHE_DIR}`);
-//     await utils.cache.save(CACHE_DIR);
-//     console.log(`Cache Saved  ${CACHE_DIR}`);
-//   },
-// };
