a4bdc69715e0ab8cede36099c05229f3e0f9d23b
fisker <lionkay@gmail.com>
2020-06-24 15:14:33 +0800

    Types

--- tests_integration/__tests__/cli.js
+++ tests_integration/__tests__/cli.js
@@ -8,30 +8,57 @@ describe("CLI", () => {
   test("CLI should be executable.", () => {
     expect(() => fs.accessSync(prettierCli, fs.constants.X_OK)).not.toThrow();
   });
-});
 
-if (isProduction) {
-  test("prefer local installation", async () => {
-    const packageJson = require(path.join(prettierRootDir, "package.json"));
-    const workingDirectory = path.join(prettierRootDir, "../..");
+  const packageJson = require(path.join(prettierRootDir, "package.json"));
+  const version = packageJson.version;
 
-    // copy bin file
-    const binFilename = path.basename(prettierCli);
-    const binFile = path.join(workingDirectory, binFilename);
-    fs.writeFileSync(fs.readFileSync(prettierCli));
+  test("CLI version should read from `package.json`.", () => {
+    const randomVersion = version + Math.random();
 
-    // write package.json
-    const packageJsonFile = path.join(workingDirectory, "package.json");
     fs.writeFileSync(
       packageJsonFile,
       JSON.stringify({
         ...packageJson,
-        version: "global",
+        version: randomVersion,
       })
     );
 
-    expect(
-      (await execa(`node ${binFilename} --version`).stdout, { cwd: workingDirectory })
-    ).toBe(packageJson.version);
+    const actualVersion = (await execa(`node ${prettierCli} --version`, { cwd: prettierRootDir })).stdout
+
+    fs.writeFileSync(
+      packageJsonFile,
+      JSON.stringify({
+        ...packageJson,
+        version,
+      })
+    );
+
+    expect(actualVersion).toBe(randomVersion);
   });
-}
+
+  if (isProduction) {
+    test("prefer local installation", async () => {
+      const workingDirectory = path.join(prettierRootDir, "../..");
+
+      // copy bin file
+      const binFilename = path.basename(prettierCli);
+      const binFile = path.join(workingDirectory, binFilename);
+      fs.writeFileSync(fs.readFileSync(prettierCli));
+
+      // write package.json
+      const packageJsonFile = path.join(workingDirectory, "package.json");
+      fs.writeFileSync(
+        packageJsonFile,
+        JSON.stringify({
+          ...packageJson,
+          version: "global",
+        })
+      );
+
+      expect(
+        (await execa.sync(`node ${binFile} --version`, { cwd: prettierRootDir })).stdout
+      ).toBe(version);
+    });
+  }
+
+});
