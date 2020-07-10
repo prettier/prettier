11e25b1b50a844ed035fef962df34006dee0e338
fisker <lionkay@gmail.com>
2020-07-10 17:04:38 +0800

    Fix issue

--- src/main/comments.js
+++ src/main/comments.js
@@ -342,6 +342,14 @@ function breakTies(tiesToBreak, text, options) {
     }
   });
 
+  for (const node of [precedingNode, followingNode]) {
+    if (node.comments && node.comments.length > 1) {
+      node.comments = node.comments.sort(
+        (a, b) => options.locStart(a) - options.locStart(b)
+      );
+    }
+  }
+
   tiesToBreak.length = 0;
 }
 
