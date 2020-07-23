291d730d59a31abb47c361810896ab4ee413a1ec
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-07-23 10:47:12 -0400

    prettierx: include typescript in standard/flow tests
    (add typescript parser to tests/standard/flow/jsfmt.spec.js)
    
    as well as babel, babel-flow, and flow
    
    using prettierx with typescript parser seems to omit a semicolon
    which is added when using babel, babel-flow, or flow parser
    in a case like this:
    
     interface Foo<T> {
    -  getter(value: T): T;
    +  getter(value: T): T
     }

--- tests/standard/flow/jsfmt.spec.js
+++ tests/standard/flow/jsfmt.spec.js
@@ -6,3 +6,20 @@ run_spec(__dirname, ["babel", "babel-flow", "flow"], {
   jsxSingleQuote: true,
   semi: false
 });
+
+/* using prettierx with typescript parser seems to omit a semicolon
+ * which is added when using babel, babel-flow, or flow parser
+ * in a case like this:
+ interface Foo<T> {
+-  getter(value: T): T;
++  getter(value: T): T
+ }
+ * */
+run_spec(__dirname, ["typescript"], {
+  yieldStarSpacing: true,
+  generatorStarSpacing: true,
+  spaceBeforeFunctionParen: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false
+});
