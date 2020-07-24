7d8778c896d89b2b5e3499760480e80099cf64d5
fisker <lionkay@gmail.com>
2020-07-24 17:43:54 +0800

    Add tests

---
+++ tests_integration/__tests__/ignore-unknown.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const runPrettier = require("../runPrettier");
+
+describe("ignore-unknown dir", () => {
+  runPrettier("cli/ignore-unknown", [
+    ".",
+    "--ignore-unknown",
+    "--list-different"
+  ]).test({
+    status: "non-zero",
+    stderr: "",
+    write: []
+  });
+});
+
+describe("ignore-unknown pattern", () => {
+  runPrettier("cli/ignore-unknown", [
+    "*",
+    "--ignore-unknown",
+    "--list-different"
+  ]).test({
+    status: "non-zero",
+    stderr: "",
+    write: []
+  });
+});
+
+describe("ignore-unknown write", () => {
+  runPrettier("cli/ignore-unknown", [
+    ".",
+    "--ignore-unknown",
+    "--write",
+    "--list-different"
+  ]).test({
+    status: 0,
+    stderr: ""
+  });
+});
+
+describe("ignore-unknown check", () => {
+  runPrettier("cli/ignore-unknown", [
+    ".",
+    "--ignore-unknown",
+    "--check",
+  ]).test({
+    status: 1
+  });
+});
