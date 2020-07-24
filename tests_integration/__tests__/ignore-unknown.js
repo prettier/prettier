cc4b0ad0f0eaa265faebc7106f033528dd3f8900
fisker <lionkay@gmail.com>
2020-07-25 00:12:19 +0800

    More tests

--- tests_integration/__tests__/ignore-unknown.js
+++ tests_integration/__tests__/ignore-unknown.js
@@ -43,3 +43,27 @@ describe("ignore-unknown check", () => {
     status: 1,
   });
 });
+
+describe("None exist file", () => {
+  runPrettier("cli/ignore-unknown", [
+    "non-exist-file",
+    "--ignore-unknown",
+  ]).test({
+    status: 2,
+  });
+});
+
+describe("Not matching pattern", () => {
+  runPrettier("cli/ignore-unknown", [
+    "*.non-exist-pattern",
+    "--ignore-unknown",
+  ]).test({
+    status: 2,
+  });
+});
+
+describe("Ignored file", () => {
+  runPrettier("cli/ignore-unknown", ["ignored.js", "--ignore-unknown"]).test({
+    status: 0,
+  });
+});
