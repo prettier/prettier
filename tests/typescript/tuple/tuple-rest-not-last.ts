2a400b6a9c7bbc51882fc80dfe87d40bc9ae1c7e
sosukesuzuki <aosukeke@gmail.com>
2020-08-04 15:26:18 +0900

    Fix for default TS parser

--- tests/typescript/tuple/tuple-rest-not-last.ts
+++ tests/typescript/tuple/tuple-rest-not-last.ts
@@ -1,3 +1,21 @@
 // https://github.com/babel/babel/pull/11753
 
 let x: [...[number, string], string]
+
+type ValidateArgs = [
+	{
+		[key: string]: any;
+	},
+	string,
+	string,
+	...string[],
+];
+
+type ValidateArgs = [
+	{
+		[key: string]: any;
+	},
+	string,
+	...string[],
+	string,
+];
