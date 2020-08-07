8b8bd3bc0781f6b22859badf3c87b70163acce16
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-08-07 20:45:47 +0530

    Basic range ignore test for typescript

--- tests/typescript/union/prettier-ignore.ts
+++ tests/typescript/union/prettier-ignore.ts
@@ -23,3 +23,13 @@ export type a =
   | bar1&bar2
   // qux
   | qux1&qux2;
+
+// prettier-ignore-start
+export type a =
+  // foo
+  | foo1&foo2
+  // bar
+  | bar1&bar2
+  // qux
+  | qux1&qux2;
+// prettier-ignore-end
