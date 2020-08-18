52153a72152ca90f8adca04dcbcd5b54d78aa3dd
fisker <lionkay@gmail.com>
2020-08-18 10:41:13 +0800

    Add failed test

--- tests_config/require_standalone.js
+++ tests_config/require_standalone.js
@@ -16,6 +16,13 @@ const source = globby
 
 vm.runInContext(source, sandbox);
 
+const allowedGlobalObjects = new Set(["prettier", "prettierPlugins"])
+for (const property of Object.keys(sandbox)) {
+  if (!allowedGlobalObjects.has(property)) {
+    throw new Error(`Global "${property}" object should not be exposed.`)
+  }
+}
+
 // TODO: maybe expose (and write tests) for `format`, `utils`, and
 // `__debug` methods
 module.exports = {
