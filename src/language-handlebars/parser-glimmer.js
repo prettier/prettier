3c645706a24e84e62e8a2c145c23e3a9b361027f
Cyrille David <cyrille@qonto.eu>
2020-08-21 19:00:21 +0200

    Inline postprocess file in parser-glimmer

--- src/language-handlebars/parser-glimmer.js
+++ src/language-handlebars/parser-glimmer.js
@@ -1,13 +1,29 @@
 "use strict";
 
 const createError = require("../common/parser-create-error");
-const postprocess = require("./postprocess");
+
+/* from the following template: `non-escaped mustache \\{{helper}}`
+ * glimmer parser will produce an AST missing a backslash
+ * so here we add it back
+ * */
+function addBackslash(/* options*/) {
+  return {
+    name: "addBackslash",
+    visitor: {
+      TextNode(node) {
+        if (node.chars.includes("\\")) {
+          node.chars = node.chars.replace(/\\/, "\\\\");
+        }
+      },
+    }
+  };
+}
 
 function parse(text) {
   const { preprocess: glimmer } = require("@glimmer/syntax");
   let ast;
   try {
-    ast = glimmer(text, { mode: "codemod", plugins: { ast: postprocess } });
+    ast = glimmer(text, { mode: "codemod", plugins: { ast: [addBackslash] } });
   } catch (error) {
     const matches = error.message.match(/on line (\d+)/);
     /* istanbul ignore else */
