76f8455f05a4e61a376f28f7f265c69627eb50ef
fisker Cheung <lionkay@gmail.com>
2020-08-22 01:00:33 +0800

    Prevent jest warning on production tests (#9022)

--- jest.config.js
+++ jest.config.js
@@ -26,6 +26,7 @@ module.exports = {
     "prettier/local": "<rootDir>/tests_config/require_prettier.js",
     "prettier/standalone": "<rootDir>/tests_config/require_standalone.js",
   },
+  modulePathIgnorePatterns: ["<rootDir>/dist"],
   testEnvironment: "node",
   transform: {},
   watchPlugins: [
