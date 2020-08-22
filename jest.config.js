097c0542fdac9df91e8a20ca9d81cf1d1d4371bd
Yuku Kotani <yukukotani@gmail.com>
2020-08-23 00:45:55 +0900

    Only test ESM bundle for prod

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,23 @@
 
 const installPrettier = require("./scripts/install-prettier");
 
+const isProduction = process.env.NODE_ENV === "production";
 const ENABLE_CODE_COVERAGE = !!process.env.ENABLE_CODE_COVERAGE;
-if (process.env.NODE_ENV === "production" || process.env.INSTALL_PACKAGE) {
+if (isProduction || process.env.INSTALL_PACKAGE) {
   process.env.PRETTIER_DIR = installPrettier();
 }
 const { TEST_STANDALONE } = process.env;
+
+const testPathIgnorePatterns = [];
+if (TEST_STANDALONE) {
+  testPathIgnorePatterns.push("<rootDir>/tests_integration/");
+}
+if (!isProduction) {
+  // Only test ESM bundle for production
+  testPathIgnorePatterns.push(
+    "<rootDir>/tests_integration/__tests__/esm-bundle.js"
+  );
+}
 
 module.exports = {
   setupFiles: ["<rootDir>/tests_config/run_spec.js"],
@@ -15,9 +27,7 @@ module.exports = {
     "jest-snapshot-serializer-ansi",
   ],
   testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
-  testPathIgnorePatterns: TEST_STANDALONE
-    ? ["<rootDir>/tests_integration/"]
-    : [],
+  testPathIgnorePatterns,
   collectCoverage: ENABLE_CODE_COVERAGE,
   collectCoverageFrom: ["<rootDir>/src/**/*.js", "<rootDir>/bin/**/*.js"],
   coveragePathIgnorePatterns: ["<rootDir>/src/document/doc-debug.js"],
