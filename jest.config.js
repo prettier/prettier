4f8d3ed05482c2e16e7ac9d9fc9648d6b2c34194
fisker <lionkay@gmail.com>
2020-08-23 08:56:09 +0800

    Refactor test

--- jest.config.js
+++ jest.config.js
@@ -2,23 +2,11 @@
 
 const installPrettier = require("./scripts/install-prettier");
 
-const isProduction = process.env.NODE_ENV === "production";
 const ENABLE_CODE_COVERAGE = !!process.env.ENABLE_CODE_COVERAGE;
-if (isProduction || process.env.INSTALL_PACKAGE) {
+if (process.env.NODE_ENV === "production" || process.env.INSTALL_PACKAGE) {
   process.env.PRETTIER_DIR = installPrettier();
 }
 const { TEST_STANDALONE } = process.env;
-
-const testPathIgnorePatterns = [];
-if (TEST_STANDALONE) {
-  testPathIgnorePatterns.push("<rootDir>/tests_integration/");
-}
-if (!isProduction) {
-  // Only test ESM bundle for production
-  testPathIgnorePatterns.push(
-    "<rootDir>/tests_integration/__tests__/esm-bundle.js"
-  );
-}
 
 module.exports = {
   setupFiles: ["<rootDir>/tests_config/run_spec.js"],
@@ -27,7 +15,9 @@ module.exports = {
     "jest-snapshot-serializer-ansi",
   ],
   testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
-  testPathIgnorePatterns,
+  testPathIgnorePatterns: TEST_STANDALONE
+    ? ["<rootDir>/tests_integration/"]
+    : [],
   collectCoverage: ENABLE_CODE_COVERAGE,
   collectCoverageFrom: ["<rootDir>/src/**/*.js", "<rootDir>/bin/**/*.js"],
   coveragePathIgnorePatterns: ["<rootDir>/src/document/doc-debug.js"],
---
+++ tests_integration/__tests__/bundle.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const path = require("path");
+const globby = require("globby");
+const { isProduction, projectRoot } = require("../env");
+const coreOptions = require("../../src/main/core-options");
+const codeSamples = require("../../website/playground/codeSamples").default;
+
+const parserNames = coreOptions.options.parser.choices.map(
+  ({ value }) => value
+);
+const distDirectory = path.join(projectRoot, "dist");
+
+describe("standalone", () => {
+  const standalone = require(path.join(distDirectory, "standalone.js"));
+  const plugins = globby
+    .sync(["parser-*.js"], { cwd: distDirectory, absolute: true })
+    .map((file) => require(file));
+
+  let esmStandalone;
+  let esmPlugins;
+
+  if (isProduction) {
+    esmStandalone = require(path.join(distDirectory, "standalone.mjs")).default;
+    esmPlugins = globby
+      .sync(["esm/parser-*.mjs"], { cwd: distDirectory, absolute: true })
+      .map((file) => require(file).default);
+  }
+
+  for (const parser of parserNames) {
+    test(parser, () => {
+      const input = codeSamples(parser);
+      const umdOutput = standalone.format(input, {
+        parser,
+        plugins,
+      });
+
+      expect(typeof input).toBe("string");
+      expect(typeof umdOutput).toBe("string");
+      expect(umdOutput).not.toBe(input);
+
+      if (isProduction) {
+        const esmOutput = esmStandalone.format(input, {
+          parser,
+          plugins: esmPlugins,
+        });
+
+        expect(esmOutput).toBe(umdOutput);
+      }
+    });
+  }
+});
