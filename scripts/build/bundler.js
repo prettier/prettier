dba8d3bf1f6e87ffcd4cc90a68615ac608a4dfe7
fisker <lionkay@gmail.com>
2020-08-24 16:35:29 +0800

    Quick fix for #8639

--- scripts/build/bundler.js
+++ scripts/build/bundler.js
@@ -19,9 +19,6 @@ const externals = require("./rollup-plugins/externals");
 const bundles = require("./config");
 
 const PROJECT_ROOT = path.resolve(__dirname, "../..");
-const plugins = bundles
-  .filter(({ type }) => type === "plugin")
-  .map(({ input }) => path.join(PROJECT_ROOT, input));
 
 const EXTERNALS = [
   "assert",
@@ -178,11 +175,9 @@ function getRollupConfig(bundle) {
     commonjs({
       ignoreGlobal: bundle.target === "node",
       ...bundle.commonjs,
+      ignore: bundle.type === "plugin" ? undefined: (id) =>/\.\/parser-.*?/.test(id)
     }),
-    externals([
-      ...(bundle.externals || []),
-      ...(bundle.type !== "plugin" ? plugins : []),
-    ]),
+    externals(bundle.externals),
     bundle.target === "universal" && nodeGlobals(),
     babel(babelConfig),
     bundle.minify !== false && bundle.target === "universal" && terser(),
