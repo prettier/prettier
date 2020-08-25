481ea90b8fb17aefba0d5647ee0b3f5dc7f4b30e
fisker <lionkay@gmail.com>
2020-08-25 18:05:17 +0800

    HTML: Fix format on `style[lang="sass"]`

---
+++ tests/vue/multiparser/unknow.vue
@@ -0,0 +1,24 @@
+<style lang="unknown">
+    foo
+          bar
+</style>
+
+<script lang="unknown">
+      foo
+            bar
+</script>
+
+<script type="unknown">
+      foo
+            bar
+</script>
+
+<script type="unknown" lang="unknown">
+    foo
+          bar
+</script>
+
+<template lang="unknown">
+    foo
+          bar
+</template>
