f31b9397e156692500a4107b2989433c3975f215
fisker <lionkay@gmail.com>
2020-08-25 19:18:50 +0800

    Add failed tests

---
+++ tests/vue/with-plugins/with-src.vue
@@ -0,0 +1,9 @@
+<template lang="uppercase-rocks" src="foo.uppercase-rocks"></template>
+<style lang="uppercase-rocks" src="foo.uppercase-rocks"></style>
+<script lang="uppercase-rocks" src="foo.uppercase-rocks"></script>
+<custom lang="uppercase-rocks" src="foo.uppercase-rocks"></custom>
+
+<template lang="uppercase-rocks" src="foo.uppercase-rocks"/>
+<style lang="uppercase-rocks" src="foo.uppercase-rocks"/>
+<script lang="uppercase-rocks" src="foo.uppercase-rocks"/>
+<custom lang="uppercase-rocks" src="foo.uppercase-rocks"/>
