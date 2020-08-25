ac1c5e5ea0e98b8fcabbb51d4609a97fd0d51b0e
fisker <lionkay@gmail.com>
2020-08-25 23:20:02 +0800

    Code style

--- tests/vue/multiparser/unknown/jsfmt.spec.js
+++ tests/vue/multiparser/unknown/jsfmt.spec.js
@@ -1,3 +1,3 @@
 run_spec(__dirname, ["vue"]);
-run_spec(__dirname, ["vue"], {vueIndentScriptAndStyle: true});
-run_spec(__dirname, ["vue"], {embeddedLanguageFormatting: "off"});
+run_spec(__dirname, ["vue"], { vueIndentScriptAndStyle: true });
+run_spec(__dirname, ["vue"], { embeddedLanguageFormatting: "off" });
