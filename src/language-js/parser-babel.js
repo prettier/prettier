caccbdbeb9130701888c042c52fd6ff9ead26d85
fisker <lionkay@gmail.com>
2020-08-27 09:36:05 +0800

    Allow trailing space

--- src/language-js/parser-babel.js
+++ src/language-js/parser-babel.js
@@ -64,7 +64,7 @@ function isFlowFile(text, options) {
   }
 
   //`// @flow` `// @noflow` `/* @flow */` `/* @noflow */`
-  if (/^\/(?:\/ *@(?:no)?flow\n|\*\s*@(?:no)?flow\s*\*\/)/.test(text)) {
+  if (/^\/(?:\/ *@(?:no)?flow *\n|\*\s*@(?:no)?flow\s*\*\/)/.test(text)) {
     return true;
   }
 
