f2e3cb7f3b4858db6330331b4137f2c5f030b468
fisker Cheung <lionkay@gmail.com>
2020-08-28 01:56:53 +0800

    Support `.wxs` &  `.wxss` (#9081)
    * Support `.wxs` &  `.wxss`
    
    * Spell check
    
    * Add comments
    
    * Spellcheck

--- src/language-js/index.js
+++ src/language-js/index.js
@@ -13,6 +13,12 @@ const languages = [
       parsers: ["babel", "flow"],
       vscodeLanguageIds: ["javascript", "mongo"],
       interpreters: data.interpreters.concat(["nodejs"]),
+      extensions: [
+        ...data.extensions,
+        // WeiXin Script (Weixin Mini Programs)
+        // https://developers.weixin.qq.com/miniprogram/en/dev/framework/view/wxs/
+        ".wxs",
+      ],
     })
   ),
   createLanguage(require("linguist-languages/data/JavaScript.json"), () => ({
