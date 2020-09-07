ec1b4193079a625633dd0dc86f9a9217a3ad9d7e
Sosuke Suzuki <aosukeke@gmail.com>
2020-09-07 20:10:05 +0900

    YAML: Fix printing doubles a blank line before a comment (#9143)
    * Print hardline for endComments only when under mappingValue
    
    * Add changelog
    
    * Fix from linter
    
    * Update tests
    
    * Use shouldPrintEndCommentsWithAlign
    
    * Add tests
    
    * Add test
    
    * Fix format
    
    * Update test
    
    * Test map too
    
    * Update tests
    
    * `shouldPrintEndCommentsWithAlign` -> `shouldPrintEndComments`
    
    Co-authored-by: fisker <lionkay@gmail.com>

--- src/language-yaml/printer-yaml.js
+++ src/language-yaml/printer-yaml.js
@@ -127,8 +127,7 @@ function genericPrint(path, options, print) {
           ])
         )
       : "",
-    nextEmptyLine,
-    hasEndComments(node) && !isNode(node, ["documentHead", "documentBody"])
+    shouldPrintEndComments(node)
       ? align(
           node.type === "sequenceItem" ? 2 : 0,
           concat([
@@ -153,6 +152,7 @@ function genericPrint(path, options, print) {
           ])
         )
       : "",
+    nextEmptyLine,
   ]);
 }
 
@@ -703,6 +703,12 @@ function needsSpaceInFrontOfMappingValue(node) {
   return node.key.content && node.key.content.type === "alias";
 }
 
+function shouldPrintEndComments(node) {
+  return (
+    hasEndComments(node) && !isNode(node, ["documentHead", "documentBody"])
+  );
+}
+
 function printNextEmptyLine(path, originalText) {
   const node = path.getValue();
   const root = path.stack[0];
@@ -713,7 +719,9 @@ function printNextEmptyLine(path, originalText) {
   if (!root.isNextEmptyLinePrintedChecklist[node.position.end.line]) {
     if (isNextLineEmpty(node, originalText)) {
       root.isNextEmptyLinePrintedChecklist[node.position.end.line] = true;
-      return softline;
+      if (!shouldPrintEndComments(path.getParentNode())) {
+        return softline;
+      }
     }
   }
 
