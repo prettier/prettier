e8d88ea96ced59b1cc4d3ccec99dc7268c81139b
agamkrbit <agamkrbit123@gmail.com>
2020-09-11 00:34:53 +0530

    fixed-SCSS-function-arguments-on-different-lines-in-maps-#9128

--- src/language-css/parser-postcss.js
+++ src/language-css/parser-postcss.js
@@ -120,6 +120,19 @@ function parseValueNode(valueNode, options) {
       parenGroupStack.pop();
       parenGroup = parenGroupStack[parenGroupStack.length - 1];
     } else if (node.type === "comma") {
+      if (commaGroup.groups.length > 1) {
+        for (const group of commaGroup.groups) {
+          // if css interpolation
+          if (group.value && 
+              typeof group.value === "string" && 
+              group.value.includes("#{")) {
+            commaGroup.groups = [stringifyNode({
+              groups: commaGroup.groups,
+            }).trim()];
+            break;
+          }
+        }
+      }
       parenGroup.groups.push(commaGroup);
       commaGroup = {
         groups: [],
