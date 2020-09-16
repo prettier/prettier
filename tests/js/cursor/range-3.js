94ef5f56832895f58b9c519fe979f39aeb32bcfb
fisker Cheung <lionkay@gmail.com>
2020-09-17 07:31:29 +0800

    Test: Fix indexes calculation (#9152)
    * Test: Fix indexes calculation
    
    * Simplify logic
    
    * Update snapshots

--- tests/js/cursor/range-3.js
+++ tests/js/cursor/range-3.js
@@ -1,5 +1,5 @@
-thisWontBeFormatted <|> ( 1  ,3<<<PRETTIER_RANGE_START>>>)
-
+thisWontBeFormatted <|> ( 1  ,3)
+<<<PRETTIER_RANGE_START>>>
     thisWillBeFormatted    (2  ,3,   )
 
  <<<PRETTIER_RANGE_END>>>   thisWontBeFormatted  (2, 90  ,)
