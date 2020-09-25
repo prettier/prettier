04e7bab4035e5d6fb811841b4e0fadec98cba8e5
fisker Cheung <lionkay@gmail.com>
2020-09-26 00:27:47 +0800

    Fix logic

--- src/language-css/parser-postcss.js
+++ src/language-css/parser-postcss.js
@@ -325,7 +325,7 @@ function parseNestedCSS(node, options) {
         let parse;
         if (options.parser === "scss") {
           parse = parseScss;
-        } else if (options.parser === "scss") {
+        } else if (options.parser === "css") {
           parse = parseCss;
         }
         let ast;
