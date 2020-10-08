bba196abc80f055b801ae6b2f55e0041237c1008
Sosuke Suzuki <aosukeke@gmail.com>
2020-10-08 20:11:15 +0900

    Improve formatting for AssignmentExpression with ClassExpression that has long superClass (#9341)
    * Improve formatting for AssignmentExpression with ClassExpression that has long superClass
    
    Fix by prettier
    
    * Add changelog
    
    * Remove unnecesary concat call
    
    * Mention abount Google Closure Library in changelog

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -4322,9 +4322,22 @@ function printClass(path, options, print) {
   }
 
   if (n.superClass) {
+    const printSuperClass = (path) => {
+      const printed = path.call(print, "superClass");
+      const parent = path.getParentNode();
+      if (parent && parent.type === "AssignmentExpression") {
+        return concat([
+          ifBreak("("),
+          indent(concat([softline, printed])),
+          softline,
+          ifBreak(")"),
+        ]);
+      }
+      return printed;
+    };
     const printed = concat([
       "extends ",
-      path.call(print, "superClass"),
+      printSuperClass(path),
       path.call(print, "superTypeParameters"),
     ]);
     const printedWithComments = path.call(
