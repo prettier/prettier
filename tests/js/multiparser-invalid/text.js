c7df2682bf9a4a1e61b370a3dfdd863494d88431
fisker <lionkay@gmail.com>
2020-10-16 18:39:56 +0800

    Add changelog, update tests

--- tests/js/multiparser-invalid/text.js
+++ tests/js/multiparser-invalid/text.js
@@ -1,23 +1,23 @@
-foo`\u{prettier}\u{0065}`;
-html`\u{prettier}\u{0065}`;
-graphql`\u{prettier}\u{0065}`;
-markdown`\u{prettier}\u{0065}`;
-css`\u{prettier}\u{0065}`;
+foo = foo`\u{prettier}\u{0065}`;
+foo = html`\u{prettier}\u{0065}`;
+foo = graphql`\u{prettier}\u{0065}`;
+foo = markdown`\u{prettier}\u{0065}`;
+foo = css`\u{prettier}\u{0065}`;
 foo = /* HTML */`\u{prettier}\u{0065}`;
 foo = /* GraphQL */`\u{prettier}\u{0065}`;
 
-foo`\u{prettier}${foo}pr\u{0065}ttier`;
-html`\u{prettier}${foo}pr\u{0065}ttier`;
-graphql`\u{prettier}${foo}pr\u{0065}ttier`;
-markdown`\u{prettier}${foo}pr\u{0065}ttier`;
-css`\u{prettier}${foo}pr\u{0065}ttier`;
+foo = foo`\u{prettier}${foo}pr\u{0065}ttier`;
+foo = html`\u{prettier}${foo}pr\u{0065}ttier`;
+foo = graphql`\u{prettier}${foo}pr\u{0065}ttier`;
+foo = markdown`\u{prettier}${foo}pr\u{0065}ttier`;
+foo = css`\u{prettier}${foo}pr\u{0065}ttier`;
 foo = /* HTML */`\u{prettier}${foo}pr\u{0065}ttier`;
 foo = /* GraphQL */`\u{prettier}${foo}pr\u{0065}ttier`;
 
-foo`\u{0065}${foo}pr\u{0065}ttier`;
-html`\u{0065}${foo}pr\u{0065}ttier`;
-graphql`\u{0065}${foo}pr\u{0065}ttier`;
-markdown`\u{0065}${foo}pr\u{0065}ttier`;
-css`\u{prettier}${foo}pr\u{0065}ttier`;
+foo = foo`\u{0065}${foo}pr\u{0065}ttier`;
+foo = html`\u{0065}${foo}pr\u{0065}ttier`;
+foo = graphql`\u{0065}${foo}pr\u{0065}ttier`;
+foo = markdown`\u{0065}${foo}pr\u{0065}ttier`;
+foo = css`\u{prettier}${foo}pr\u{0065}ttier`;
 foo = /* HTML */`\u{prettier}${foo}pr\u{0065}ttier`;
 foo = /* GraphQL */`\u{prettier}${foo}pr\u{0065}ttier`;
