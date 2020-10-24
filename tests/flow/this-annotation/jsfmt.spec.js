d9271d6ec7f848fd9dce7cb614f8669c723b923d
fisker <lionkay@gmail.com>
2020-10-24 13:52:03 +0800

    Fix tests

--- tests/flow/this-annotation/jsfmt.spec.js
+++ tests/flow/this-annotation/jsfmt.spec.js
@@ -1,11 +1,6 @@
 run_spec(__dirname, ["flow", "babel-flow"], {
   trailingComma: "all",
   errors: {
-    "babel-flow": [
-      "function_type.js",
-      "line_break.js",
-      "union_type.js",
-      "only-parameter.js",
-    ],
+    "babel-flow": ["function_type.js", "line_break.js", "union_type.js"],
   },
 });
