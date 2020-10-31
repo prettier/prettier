382f752275b14559ef6e2afbea6ae0597d17ff6b
Alex Rattray <rattrayalex@stripe.com>
2020-10-31 11:02:43 -0400

    Unrelated: remove trailing whitespace from a test

--- tests/js/ternaries/func-call.js
+++ tests/js/ternaries/func-call.js
@@ -2,7 +2,7 @@ fn(
   bifornCringerMoshedPerplexSawder,
   askTrovenaBeenaDependsRowans,
   glimseGlyphsHazardNoopsTieTie === averredBathersBoxroomBuggyNurl &&
-    anodyneCondosMalateOverateRetinol 
-      ? annularCooeedSplicesWalksWayWay 
-      : kochabCooieGameOnOboleUnweave 
+    anodyneCondosMalateOverateRetinol
+      ? annularCooeedSplicesWalksWayWay
+      : kochabCooieGameOnOboleUnweave
 );
