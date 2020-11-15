35a149702472dbb524e13af22beb074e816ec8d7
fisker Cheung <lionkay@gmail.com>
2020-11-16 01:03:35 +0800

    Update `remark-math` to v3 (#7938)
    * bump remark-math to 3.0.1
    
    * escape $ only it was escaped in the input
    
    * remove unsupported syntax from tests
    
    remark-math@3 doesn't support text after the closing fence ($$). Such $$ aren't parsed as a closing fence anymore.
    
    * refactor
    
    * add a test
    
    Co-authored-by: Georgii Dolzhykov <thorn.mailbox@gmail.com>

--- src/language-markdown/printer-markdown.js
+++ src/language-markdown/printer-markdown.js
@@ -94,7 +94,7 @@ function genericPrint(path, options, print) {
       return printChildren(path, options, print);
     case "word": {
       let escapedValue = node.value
-        .replace(/[$*]/g, "\\$&") // escape all `*` and `$` (math)
+        .replace(/\*/g, "\\$&") // escape all `*`
         .replace(
           new RegExp(
             [
