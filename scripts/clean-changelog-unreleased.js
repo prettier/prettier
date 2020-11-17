37928c4e30c86387b349a98dc8907ed672bcbcee
sosukesuzuki <aosukeke@gmail.com>
2020-11-17 20:10:56 +0900

    Remove blog-post-intro.md via globby

--- scripts/clean-changelog-unreleased.js
+++ scripts/clean-changelog-unreleased.js
@@ -8,12 +8,9 @@ const globby = require("globby");
 
 const changelogUnreleasedDir = path.join(__dirname, "../changelog_unreleased");
 
-// Remove blog-post-intro.md
-const introFile = path.join(changelogUnreleasedDir, "blog-post-intro.md");
-fs.unlinkSync(introFile);
-
-// Remove change files
-const files = globby.sync("*/*.md", { cwd: changelogUnreleasedDir });
+const files = globby.sync(["blog-post-intro.md", "*/*.md"], {
+  cwd: changelogUnreleasedDir,
+});
 for (const file of files) {
   fs.unlinkSync(path.join(changelogUnreleasedDir, file));
 }
