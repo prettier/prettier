2527a60d1583bf2ae0c1fa2867ea909bb3226ec0
sosukesuzuki <aosukeke@gmail.com>
2020-11-17 12:47:33 +0900

    Modify to remove intro file in clean changelog script

--- scripts/clean-changelog-unreleased.js
+++ scripts/clean-changelog-unreleased.js
@@ -4,26 +4,13 @@
 
 const fs = require("fs");
 const path = require("path");
-const { outdent } = require("outdent");
 const globby = require("globby");
 
 const changelogUnreleasedDir = path.join(__dirname, "../changelog_unreleased");
 
-// Revert blog-post-intro.md
+// Remove blog-post-intro.md
 const introFile = path.join(changelogUnreleasedDir, "blog-post-intro.md");
-fs.writeFileSync(
-  introFile,
-  outdent`
-  ---
-  author: "🚧"
-  authorURL: "https://github.com/🚧"
-  title: "Prettier 🚧"
-  ---
-
-  🚧 Write an introduction here.
-
-`
-);
+fs.unlinkSync(introFile);
 
 // Remove change files
 const files = await globby("*/*.md", { cwd: changelogUnreleasedDir });
