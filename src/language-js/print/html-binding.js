cc83a3bba2e8e6f2ffb934724b1380eea7c9068a
fisker <lionkay@gmail.com>
2020-11-20 12:50:48 +0800

    Move html-binding.js to `print/`

--- src/language-js/print/html-binding.js
+++ src/language-js/print/html-binding.js
@@ -2,7 +2,7 @@
 
 const {
   builders: { concat, join, line, group, softline, indent },
-} = require("../document");
+} = require("../../document");
 
 function printHtmlBinding(path, options, print) {
   const node = path.getValue();
