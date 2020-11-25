86d7104699f2cab5e47ef0c01d10605d4a01edcf
fisker <lionkay@gmail.com>
2020-11-25 18:46:18 +0800

    Add eslint rule to forbind `concat`

--- scripts/tools/eslint-plugin-prettier-internal-rules/index.js
+++ scripts/tools/eslint-plugin-prettier-internal-rules/index.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     "better-parent-property-check-in-needs-parens": require("./better-parent-property-check-in-needs-parens"),
     "directly-loc-start-end": require("./directly-loc-start-end"),
+    "no-doc-builder-concat": require("./no-doc-builder-concat"),
     "prefer-fast-path-each": require("./prefer-fast-path-each"),
     "require-json-extensions": require("./require-json-extensions"),
   },
