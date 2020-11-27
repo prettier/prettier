fcf99c7273cb046033334a630202295c236050ec
fisker <lionkay@gmail.com>
2020-11-28 00:58:05 +0800

    Try remove fill if only one doc in parts

--- src/document/doc-utils.js
+++ src/document/doc-utils.js
@@ -276,17 +276,10 @@ const isConcat = (doc) => doc && doc.type === "concat";
 function cleanDocFn(doc) {
   switch (doc.type) {
     case "fill":
-      {
-        // Empty string should be able to remove;
-        // doc.parts = doc.parts.filter(Boolean);
-
-        // If only one string, should be able to turn into string
-        if (doc.parts.length === 1 && typeof doc.parts[0] === "string") {
-          return doc.parts[0];
-        }
+      if (doc.parts.length === 1) {
+        return doc.parts[0];
       }
       break;
-
     case "group":
       {
         // if (!doc.contents) {
