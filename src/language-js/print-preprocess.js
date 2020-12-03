471874965ed1e7e81ac8f10088f3e924a54ff690
Salem Hilal <salemhilal@gmail.com>
2020-12-03 08:43:06 -0500

    First pass, doesn't work in all cases

--- src/language-js/print-preprocess.js
+++ src/language-js/print-preprocess.js
@@ -1,6 +1,8 @@
 "use strict";
 
 function preprocess(ast, options) {
+  markIgnoredNodes(ast, options);
+
   switch (options.parser) {
     case "json":
     case "json5":
@@ -17,6 +19,91 @@ function preprocess(ast, options) {
     default:
       return ast;
   }
+}
+
+function markIgnoredNodes(node, options, key = "root", isIgnoring = false) {
+  if (!node) {
+    return;
+  }
+
+  for (const [key, value] of Object.entries(node)) {
+    if (value && typeof value.type === "string") {
+      markIgnoredNodes(value, options, key, isIgnoring);
+    } else if (Array.isArray(value)) {
+      let ignoring = false;
+      value.forEach(elem => {
+        if (hasLeadingStartComment(elem)) {
+          ignoring = true;
+          console.log("ignoring start", elem);
+        }
+        if (hasLeadingEndComment(elem)) {
+          ignoring = false;
+          console.log("ignoring ended", elem);
+        }
+
+        if (ignoring) {
+          elem.prettierIgnore = ignoring;
+          console.log("ignoring contd", elem);
+        } else {
+          markIgnoredNodes(elem, options, key);
+        }
+
+        if (hasTrailingStartComment(elem)) {
+          ignoring = true
+          console.log("ignoring start", elem);
+        }
+        if (hasTrailingEndComment(elem)) {
+          if (!ignoring) { 
+            console.log("Found prettier-ignore-end without a matching prettier-ignore-start", elem);
+          }
+          ignoring = false;
+          console.log("ignoring ended", elem);
+        }
+
+      })
+    }
+  }
+  return node;
+}
+
+function hasLeadingStartComment(node) {
+  // TODO: use util#getComment instead of node.comments
+  if (Array.isArray(node.comments)) {
+    return node.comments
+    .filter(comment => comment.leading)
+    .filter(comment=>comment.value.includes("prettier-ignore-start")).length > 0;
+  }
+  return false;
+}
+function hasLeadingEndComment(node) {
+  // TODO: use util#getComment instead of node.comments
+  if (Array.isArray(node.comments)) {
+    return node.comments
+    .filter(comment => comment.leading)
+    .filter(comment=>comment.value.includes("prettier-ignore-end")).length > 0;
+  }
+  return false;
+}
+
+
+function hasTrailingStartComment(node) {
+  // TODO: use util#getComment instead of node.comments
+  if (Array.isArray(node.comments)) {
+    return node.comments
+    .filter(comment => comment.trailing)
+    .filter(comment=>comment.value.includes("prettier-ignore-start")).length > 0;
+  }
+  return false;
+}
+
+function hasTrailingEndComment(node) {
+  // TODO: use util#getComment instead of node.comments
+  if (Array.isArray(node.comments)) {
+    return node.comments
+    .filter(comment => comment.trailing)
+    .filter(comment=>comment.value.includes("prettier-ignore-end")).length > 0;
+  }
+  return false;
 }
 
 module.exports = preprocess;
