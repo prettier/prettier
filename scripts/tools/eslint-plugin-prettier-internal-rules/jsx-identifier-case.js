b20f9a114487231d923e1b63ec2682c5fa79244e
fisker <lionkay@gmail.com>
2020-12-11 14:51:03 +0800

    Fix code style

--- scripts/tools/eslint-plugin-prettier-internal-rules/jsx-identifier-case.js
+++ scripts/tools/eslint-plugin-prettier-internal-rules/jsx-identifier-case.js
@@ -13,34 +13,34 @@ module.exports = {
         "https://github.com/prettier/prettier/blob/master/scripts/eslint-plugin-prettier-internal-rules/jsx-identifier-case.js",
     },
     messages: {
-      [MESSAGE_ID]:
-        "Please rename '{{name}}' to '{{fixed}}'.",
+      [MESSAGE_ID]: "Please rename '{{name}}' to '{{fixed}}'.",
     },
     fixable: "code",
   },
   create(context) {
     const ignored = new Set(context.options);
     return {
-      'Identifier[name=/JSX/]:not(ObjectExpression > Property.properties > .key)'(node) {
-        const {name} = node;
+      "Identifier[name=/JSX/]:not(ObjectExpression > Property.properties > .key)"(
+        node
+      ) {
+        const { name } = node;
 
         if (ignored.has(name)) {
           return;
         }
 
-        const fixed = name.replace(/JSX/g, 'Jsx');
+        const fixed = name.replace(/JSX/g, "Jsx");
         context.report({
           node,
           messageId: MESSAGE_ID,
           data: { name, fixed },
-          fix: (fixer) =>
-            fixer.replaceText(node, fixed),
+          fix: (fixer) => fixer.replaceText(node, fixed),
         });
       },
     };
   },
   schema: {
-    type: 'array',
-    uniqueItems: true
-  }
+    type: "array",
+    uniqueItems: true,
+  },
 };
