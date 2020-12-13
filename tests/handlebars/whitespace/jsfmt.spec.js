2209564dd4343790762b06d7882ed879fb3944c2
Cyrille David <cyrille@qonto.eu>
2020-12-13 18:08:20 +0100

    Resolve linting

--- tests/handlebars/whitespace/jsfmt.spec.js
+++ tests/handlebars/whitespace/jsfmt.spec.js
@@ -1,2 +1,5 @@
 run_spec(__dirname, ["glimmer"], { printWidth: 40 });
-run_spec(__dirname, ["glimmer"], { printWidth: 40, htmlWhitespaceSensitivity: "ignore" });
+run_spec(__dirname, ["glimmer"], {
+  printWidth: 40,
+  htmlWhitespaceSensitivity: "ignore",
+});
