000288574aa3d9f7f4cd6fbc823ab6cd087b187d
fisker <lionkay@gmail.com>
2020-12-15 18:16:04 +0800

    Simplify logic

--- src/language-js/print/block.js
+++ src/language-js/print/block.js
@@ -14,48 +14,44 @@ const { printDirectives, hasDirectives } = require("./directives");
 /** @typedef {import("../../document").Doc} Doc */
 
 function printBlock(path, options, print) {
-  const n = path.getValue();
+  const node = path.getValue();
   const parts = [];
 
-  if (n.type === "StaticBlock") {
+  if (node.type === "StaticBlock") {
     parts.push("static ");
-  }
-
-  const nodeHasDirectives = hasDirectives(n);
-  const nodeHasBody = n.body.some((node) => node.type !== "EmptyStatement");
-  const nodeHasComment = hasComment(n, CommentCheckFlags.Dangling);
-
-  const parent = path.getParentNode();
-  const parentParent = path.getParentNode(1);
-  if (
-    !nodeHasBody &&
-    !nodeHasDirectives &&
-    !nodeHasComment &&
-    (parent.type === "ArrowFunctionExpression" ||
-      parent.type === "FunctionExpression" ||
-      parent.type === "FunctionDeclaration" ||
-      parent.type === "ObjectMethod" ||
-      parent.type === "ClassMethod" ||
-      parent.type === "ClassPrivateMethod" ||
-      parent.type === "ForStatement" ||
-      parent.type === "WhileStatement" ||
-      parent.type === "DoWhileStatement" ||
-      parent.type === "DoExpression" ||
-      (parent.type === "CatchClause" && !parentParent.finalizer) ||
-      parent.type === "TSModuleDeclaration" ||
-      parent.type === "TSDeclareFunction" ||
-      n.type === "StaticBlock")
-  ) {
-    return concat([...parts, "{}"]);
   }
 
   parts.push("{");
 
   const printed = printBlockBody(path, options, print);
   if (printed) {
-    parts.push(indent(concat([hardline, printed])));
+    parts.push(indent(concat([hardline, printed])), hardline);
+  } else {
+    const parent = path.getParentNode();
+    const parentParent = path.getParentNode(1);
+    if (
+      !(
+        parent.type === "ArrowFunctionExpression" ||
+        parent.type === "FunctionExpression" ||
+        parent.type === "FunctionDeclaration" ||
+        parent.type === "ObjectMethod" ||
+        parent.type === "ClassMethod" ||
+        parent.type === "ClassPrivateMethod" ||
+        parent.type === "ForStatement" ||
+        parent.type === "WhileStatement" ||
+        parent.type === "DoWhileStatement" ||
+        parent.type === "DoExpression" ||
+        (parent.type === "CatchClause" && !parentParent.finalizer) ||
+        parent.type === "TSModuleDeclaration" ||
+        parent.type === "TSDeclareFunction" ||
+        node.type === "StaticBlock"
+      )
+    ) {
+      parts.push(hardline);
+    }
   }
-  parts.push(hardline, "}");
+
+  parts.push("}");
 
   return concat(parts);
 }
