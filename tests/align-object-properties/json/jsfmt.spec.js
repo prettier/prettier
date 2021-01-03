a618748f8d22a0863e881bf06d4bb27b2e01bc2f
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-03 15:28:23 -0500

    prettierx: remove tests/align-object-properties/json
    due to lack of relevance

--- tests/align-object-properties/json/jsfmt.spec.js
+++
@@ -1,8 +0,0 @@
-run_spec(__dirname, ["json", "json-stringify"], {
-  alignObjectProperties: true,
-  trailingComma: "none", // ("Standard JS")
-});
-run_spec(__dirname, ["json5"], {
-  alignObjectProperties: true,
-  trailingComma: "none", // ("Standard JS")
-});
