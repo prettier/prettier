a6bc9698fe8fd1c7b6bcb4ea8b6c79678c0413b2
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-03 16:01:12 -0500

    prettierx: add tests/method-chain/with-paren-spacing

---
+++ tests/method-chain/with-paren-spacing/jsfmt.spec.js
@@ -0,0 +1,12 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/..`;
+
+run_spec(dirpath, ["babel", "babel-flow", "flow", "typescript"], {
+  // [prettierx] recommended setting with --paren-spacing
+  arrowParens: "avoid",
+  // [prettierx] test with --paren-spacing
+  parenSpacing: true,
+});
