4c2fcde675cec06e2a33556fc643fbbf650085bc
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-03 19:25:36 -0500

    prettierx: update tests/empty_paren_comment
    with separate snapshot for paren spacing

---
+++ tests/empty_paren_comment/with-paren-spacing/jsfmt.spec.js
@@ -0,0 +1,9 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/..`;
+
+run_spec(dirpath, ["babel", "babel-flow", "flow", "typescript"], {
+  parenSpacing: true,
+});
