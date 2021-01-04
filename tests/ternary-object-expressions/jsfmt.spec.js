2f12f477767efa468ac59214e1aea784615d295a
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-03 19:29:40 -0500

    prettierx: break out tests/ternaries/crewdress.js
    into: tests/ternary-object-expressions
    
    (not tested with paren-spacing as this would be meaningless)

---
+++ tests/ternary-object-expressions/jsfmt.spec.js
@@ -0,0 +1,16 @@
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
+  alignTernaryLines: true,
+});
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
+  alignTernaryLines: true,
+  tabWidth: 4,
+});
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
+  alignTernaryLines: true,
+  useTabs: true,
+});
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
+  alignTernaryLines: true,
+  useTabs: true,
+  tabWidth: 4,
+});
---
+++ tests/ternary-object-expressions/with-balanced-formatting/jsfmt.spec.js
@@ -0,0 +1,25 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/..`;
+
+run_spec(dirpath, ["babel", "babel-flow", "flow", "typescript"], {
+  // [prettierx] balanced ternary formatting option
+  // to improve consistency with "Standard JS" in certain cases
+  // (may lead to inconsistencies in some other cases):
+  alignTernaryLines: false,
+  // [prettierx] more options needed for consistency with "Standard JS":
+  arrowParens: "avoid",
+  trailingComma: "none",
+});
+
+run_spec(dirpath, ["babel", "babel-flow", "flow", "typescript"], {
+  // variation from ../jsfmt.spec.js:
+  tabWidth: 4,
+  // [prettierx] balanced ternary formatting option:
+  alignTernaryLines: false,
+  // [prettierx] more options needed for consistency with "Standard JS":
+  arrowParens: "avoid",
+  trailingComma: "none",
+});
