8055f005f4c28bec8b83b610b5103ba015fb6ec7
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2021-01-16 00:49:54 +0200

    Split tests to make snapshots more readable

--- tests/js/comments/dangling_for.js
+++ tests/js/comments/dangling_for.js
@@ -3,60 +3,6 @@ for // comment
 
 for /* comment */(;;);
 
-for(/*a*/;/*b*/;/*c*/);
-for(/*a*/a;/*b*/;/*c*/);
-for(/*a*/;a/*b*/;/*c*/);
-for(/*a*/;/*b*/;a/*c*/);
-
-for(
-  ; // no init!
-  i > 0;
-  i++
-) {}
-
-for(
-  /* j = 10 */; // no init
-  j > 0; // test
-  // no update
-) {}
-
-for(
-  // a
-  /* k = 10 */; // no init
-  // b
-  k > 0; // test
-  /* k++ */ // no update
-) {}
-
-for(
-  // a
-  /* m = 10 */; // no init
-  // b
-  m > 0 /* (!) */; // test
-  // c
-  /* m++ */ // no update
-) {}
-
-for(
-  // a
-  /* m = 10 */; // no init
-  // b
-  m > 0 /* (!) */; // test
-  // c
-  /* 1 */ /* 2 */
-  /* m++ */ // no update
-  /* n++ */ // no update2
-
-  // d
-) {}
-
-for // a
-// b
-( // c
-;
-;
-);
-
 for (;;
 // ...
 );
@@ -64,10 +10,3 @@ for (;;
 for (
   // comment
 ;;);
-
-for /* 0 */ // 1
-( // 2
-// 3
-; // 4
-i<j;
-k++);
