1260b1fc0e5fb98ed67c9be4491c872b5c631fd9
sosukesuzuki <aosukeke@gmail.com>
2021-01-20 00:27:06 +0900

    Add tests for complex extends

---
+++ tests/js/class-extends/complex.js
@@ -0,0 +1,17 @@
+class loooooooooooooooooooong1 extends foooooooo(foooooooo(foooooooo(foooooooo(foooooooo(foooooooo(foooooooo(foooooooo()))))))) {}
+
+class loooooooooooooooooooong2 extends function (make, model, year, owner) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.owner = owner;
+} {}
+
+class loooooooooooooooooooong3 extends class {
+  cconstructor(make, model, year, owner) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.owner = owner;
+  }
+} {}
