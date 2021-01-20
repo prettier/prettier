79a80fcbf140d0073b90ae144c051f038b0b6387
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-20 16:32:35 -0500

    add XXX TODO comment for test in catch clause

--- tests/js/destructuring/more-destructuring.js
+++ tests/js/destructuring/more-destructuring.js
@@ -26,7 +26,7 @@ class A2 {
   func({ a: { info1, info2 }, b: { info3, info4 } }) {}
 }
 
-// these catch arguments should be destructured on multiple lines:
+// XXX TODO: these catch arguments should be destructured on multiple lines:
 try {
   // code
 } catch ({ first: { info1, info2 }, second: { info3, info4 } }) {
