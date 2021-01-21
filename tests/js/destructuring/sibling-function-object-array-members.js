d6c5e7e94f8b714fc54efe019033ac6882b8203f
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-21 18:02:22 -0500

    fix multi-line formatting of this pattern: f({ first: [inner1, inner2], second: [inner3, inner4] }) {}

--- tests/js/destructuring/sibling-function-object-array-members.js
+++ tests/js/destructuring/sibling-function-object-array-members.js
@@ -1,8 +1,8 @@
 // small enough for one line:
 function f1({ first: [inner1, inner2], second }) {}
 
-// XXX TODO: these arguments should be destructured on multiple lines:
+// these arguments should be destructured on multiple lines:
 function f2({ first: [inner1, inner2], second: [inner3, inner4] }) {}
 
-// XXX TODO: these arguments should be destructured on multiple lines:
+// these arguments should be destructured on multiple lines:
 function f3({ first: [inner1, inner2], second: { inner3, inner4 } }) {}
