468d0426b4b20c01ae0c3a841279c9ae610fe517
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-21 16:28:09 -0500

    update tests/js/destructuring/sibling-function-object-members.js (with some TODO items)

--- tests/js/destructuring/sibling-function-object-members.js
+++ tests/js/destructuring/sibling-function-object-members.js
@@ -1,12 +1,21 @@
+// small enough for one line:
+function f1({ first: { inner1, inner2 }, second }) {}
+
 // these arguments should be destructured on multiple lines:
 function f2({ first: { inner1, inner2 }, second: { inner3, inner4 } }) {}
 
-const obj2 = {
+// small enough for one line:
+function f3({ first: { inner1, inner2 }, second } = {}) {}
+
+// XXX TODO: these arguments should be destructured on multiple lines:
+function f4({ a: { inner1, inner2 }, b: { inner3, inner4 } } = {}) {}
+
+const obj = {
   // these arguments should be destructured on multiple lines:
   func({ a: { info1, info2 }, b: { info3, info4 } }) {}
 };
 
-class A2 {
+class A {
   // these arguments should be destructured on multiple lines:
   func({ a: { info1, info2 }, b: { info3, info4 } }) {}
 }
