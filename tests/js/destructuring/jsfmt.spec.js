f6d2ea29e833e6b24cd8ee31d221377fbfccfe72
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-21 23:36:15 -0500

    Revert "[XXX TODO...] SKIP `babel` & `typescript` parsers for now in tests/js/destructuring"
    This reverts commit 967c61ded80114362002e3694ee4eaac6adb355d.

--- tests/js/destructuring/jsfmt.spec.js
+++ tests/js/destructuring/jsfmt.spec.js
@@ -1,6 +1,1 @@
-// NOTE that `babel` & `typescript` parsers are left out for now
-// to avoid a conflict with testing private class functions.
-// XXX TODO add `babel` & `typescript` parsers back
-// once the issue with private functions & destructured objects
-// is resolved
-run_spec(__dirname, ["babel-flow", "flow", "babel-ts"]);
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"]);
