cd507d2a1e092644e923568a21c7f6afd1b29e0b
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-22 01:56:01 -0500

    include all ES parsers again in tests/js/destructuring

--- tests/js/destructuring/jsfmt.spec.js
+++ tests/js/destructuring/jsfmt.spec.js
@@ -3,10 +3,4 @@ const errors = {
   flow: ["sibling-private-function-object-members.js"],
 };
 
-// NOTE that multiple parsers are omitted for now to avoid some conflicts when
-// testing a private class function with a destructured object argument.
-// Keeping the following parsers explicit for now:
-// - `espree` (implicitly included when testing `babel` parser)
-// - `babel-ts` (implicitly included when testing `typescript` parser)
-// XXX TODO add the parsers back once these conflicts are resolved.
-run_spec(__dirname, ["espree", "babel-flow", "flow", "babel-ts"], { errors });
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], { errors });
