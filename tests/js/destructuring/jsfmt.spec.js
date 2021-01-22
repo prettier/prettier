f39caa0c1c6f893f59ad9070d53eea62fe24c51b
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-22 00:40:01 -0500

    add tests/js/destructuring/sibling-private-function-object-members.js (with a TODO item)

--- tests/js/destructuring/jsfmt.spec.js
+++ tests/js/destructuring/jsfmt.spec.js
@@ -1,7 +1,12 @@
+const errors = {
+  espree: ["sibling-private-function-object-members.js"],
+  flow: ["sibling-private-function-object-members.js"],
+};
+
 // NOTE that multiple parsers are omitted for now to avoid some conflicts when
 // testing a private class function with a destructured object argument.
 // Keeping the following parsers explicit for now:
 // - `espree` (implicitly included when testing `babel` parser)
 // - `babel-ts` (implicitly included when testing `typescript` parser)
 // XXX TODO add the parsers back once these conflicts are resolved.
-run_spec(__dirname, ["espree", "babel-flow", "flow", "babel-ts"]);
+run_spec(__dirname, ["espree", "babel-flow", "flow", "babel-ts"], { errors });
---
+++ tests/js/destructuring/sibling-private-function-object-members.js
@@ -0,0 +1,8 @@
+class A {
+  // small enough for one line:
+  #f1({ a, b }) {}
+
+  // XXX TODO: these arguments should be destructured on multiple lines:
+  #f2({ a: { info1, info2 }, b: { info3, info4 } }) {}
+}
+
