87ec27cbf3e1c6a67d1075420445ba2ef4658fd1
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-22 14:55:05 -0500

    remove tests/js/destructuring-prop-types

--- tests/js/destructuring-prop-types/jsfmt.spec.js
+++
@@ -1,9 +0,0 @@
-const errors = {
-  espree: ["prop-types.js"],
-  meriyah: ["prop-types.js"],
-};
-
-run_spec(__dirname, ["babel", "babel-flow", "flow"], { errors });
-
-// different output when using TypeScript parsers
-run_spec(__dirname, ["babel-ts", "typescript"]);
