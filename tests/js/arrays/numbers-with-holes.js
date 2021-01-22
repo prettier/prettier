78b9bb328a97c66772d418e75b3bd6253bbc4bd3
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2021-01-22 22:20:51 +0200

    Add tests for args and assignments

--- tests/js/arrays/numbers-with-holes.js
+++ tests/js/arrays/numbers-with-holes.js
@@ -2,13 +2,13 @@ const numberWithHoles1 = [
   7234932941,
   7234932722,
   7234932312,
-  // comment before a hole
+  // comment before a hole 1
   ,
   7234932841,
   ,
   7234932843,
   ,
-  // comment after a hole
+  // comment after a hole 1
   7234932436,
 ];
 
@@ -17,13 +17,13 @@ const numberWithHoles2 = [
   0x234932722,
   0x234932312,
 
-  // comment before a hole
+  // comment before a hole 2
   ,
   0x234932841,
   ,
   0x234932843,
   ,
 
-  // comment after a hole
+  // comment after a hole 2
   0x234932436,
 ];
