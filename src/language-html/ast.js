daf900f94a4be342ad752275e2c020a0458e3407
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-23 23:48:43 -0500

    add some more @ts-ignore comments to src/language-html/ast.js

--- src/language-html/ast.js
+++ src/language-html/ast.js
@@ -57,6 +57,7 @@ class Node {
           newNode[key] = this[key];
         }
       }
+      // @ts-ignore
       const { index, siblings, prev, next, parent } = this;
       setNonEnumerableProperties(newNode, {
         index,
@@ -78,18 +79,22 @@ class Node {
   }
 
   get firstChild() {
+    // @ts-ignore
     return isNonEmptyArray(this.children) ? this.children[0] : null;
   }
 
   get lastChild() {
+    // @ts-ignore
     return last(this.children);
   }
 
   // for element and attribute
   get rawName() {
+    // @ts-ignore
     return this.hasExplicitNamespace ? this.fullName : this.name;
   }
   get fullName() {
+    // @ts-ignore
     return this.namespace ? this.namespace + ":" + this.name : this.name;
   }
 }
