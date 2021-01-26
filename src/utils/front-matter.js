ec0709f68c987d51b194b749f6c1516f475636ff
fisker <lionkay@gmail.com>
2021-01-26 10:58:25 +0800

    Simplify frontMatter parse

--- src/utils/front-matter.js
+++ src/utils/front-matter.js
@@ -1,55 +1,34 @@
 "use strict";
 
-const escape = require("escape-string-regexp");
 const {
   builders: { hardline, markAsRoot },
 } = require("../document");
 
-const YAML_DELIMITER = "---";
 // In some markdown processors such as pandoc,
 // "..." can be used as the end delimiter for YAML front-matter.
-const YAML_END_DELIMITER = "...";
-const DELIMITER_MAP = {
-  [YAML_DELIMITER]: "yaml",
-  "+++": "toml",
-};
+
+// trailing spaces after delimiters are allowed
+const frontMatterRegex = /^(?<startDelimiter>-{3}|\+{3})(?<language>[^\n]*)\n(?:|(?<value>[\S\s]*?)\n)(?<endDelimiter>\k<startDelimiter>|\.{3})[^\S\n]*(?:\n|$)/;
 
 function parse(text) {
-  const startDelimiterRegex = Object.keys(DELIMITER_MAP).map(escape).join("|");
-  const endDelimiterRegex = [...Object.keys(DELIMITER_MAP), YAML_END_DELIMITER]
-    .map(escape)
-    .join("|");
-
-  const match = text.match(
-    // trailing spaces after delimiters are allowed
-    new RegExp(
-      `^(${startDelimiterRegex})([^\\n]*)\\n(?:|([\\s\\S]*?)\\n)(${endDelimiterRegex})[^\\n\\S]*(\\n|$)`
-    )
-  );
-
-  if (match === null) {
-    return { frontMatter: null, content: text };
+  const match = text.match(frontMatterRegex);
+  if (!match) {
+    return { content: text };
   }
 
-  const [raw, startDelimiter, language, value = "", endDelimiter] = match;
+  const { startDelimiter, language, value = "", endDelimiter } = match.groups;
 
-  let lang = DELIMITER_MAP[startDelimiter];
-  if (lang !== "toml" && language && language.trim()) {
-    lang = language.trim();
+  let lang = language.trim() || "yaml";
+  if (startDelimiter === "+++") {
+    lang = "toml";
   }
 
   // Only allow yaml to parse with a different end delimiter
-  if (
-    startDelimiter !== endDelimiter &&
-    !(
-      lang === "yaml" &&
-      startDelimiter === YAML_DELIMITER &&
-      endDelimiter === YAML_END_DELIMITER
-    )
-  ) {
-    return { frontMatter: null, content: text };
+  if (lang !== "yaml" && startDelimiter !== endDelimiter) {
+    return { content: text };
   }
 
+  const [raw] = match;
   const frontMatter = {
     type: "front-matter",
     lang,
@@ -58,6 +37,7 @@ function parse(text) {
     endDelimiter,
     raw: raw.replace(/\n$/, ""),
   };
+
   return {
     frontMatter,
     content: raw.replace(/[^\n]/g, " ") + text.slice(raw.length),
