ab791bbe461ca92c457d9bc981bae8e64f1bad43
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-26 13:57:08 -0500

    prettierx: update comments in tests/standard/jsfmt.spec.js

--- tests/standard/jsfmt.spec.js
+++ tests/standard/jsfmt.spec.js
@@ -1,8 +1,10 @@
 run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
   // [TBD] issue with babel-ts parser:
   disableBabelTS: ["incorrect-ternaries.js"],
-  // "Standard JS":
+  // explicit endOfLine setting, same as default since Prettier 2.0
+  // (shows <LF> in test snapshots)
   endOfLine: "lf",
+  // "Standard JS":
   yieldStarSpacing: true,
   generatorStarSpacing: true,
   spaceBeforeFunctionParen: true,
