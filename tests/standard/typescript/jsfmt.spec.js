fc8d0604c2fdc3b09fc76bbc97a06b6434161365
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-01-26 14:23:13 -0500

    prettierx: update tests/standard/typescript
    with separate snapshot for --paren-spacing

--- tests/standard/typescript/jsfmt.spec.js
+++ tests/standard/typescript/jsfmt.spec.js
@@ -6,13 +6,3 @@ run_spec(__dirname, ["typescript"], {
   jsxSingleQuote: true,
   semi: false,
 });
-
-run_spec(__dirname, ["typescript"], {
-  parenSpacing: true,
-  yieldStarSpacing: true,
-  generatorStarSpacing: true,
-  spaceBeforeFunctionParen: true,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  semi: false,
-});
---
+++ tests/standard/typescript/with-paren-spacing/jsfmt.spec.js
@@ -0,0 +1,15 @@
+// [prettierx] test script notice:
+// This test script runs for test files in parent directory,
+// **not** on any files in *this* directory.
+
+const dirpath = `${__dirname}/..`;
+
+run_spec(dirpath, ["typescript"], {
+  parenSpacing: true,
+  yieldStarSpacing: true,
+  generatorStarSpacing: true,
+  spaceBeforeFunctionParen: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+});
