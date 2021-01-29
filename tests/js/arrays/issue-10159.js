a7ea77e0eff76c31e992839576e99ef1f057ad47
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2021-01-29 19:58:24 +0200

    Fix regression after pr-10106 (#10160)

---
+++ tests/js/arrays/issue-10159.js
@@ -0,0 +1,4 @@
+{for (const srcPath of [src, `${src}.js`, `${src}/index`, `${src}/index.js`]) {}}
+{for (const srcPath of [123, 123_123_123, 123_123_123_1, 13_123_3123_31_43]) {}}
+{for (const srcPath of [123, 123_123_123, 123_123_123_1, 13_123_3123_31_432]) {}}
+{for (const srcPath of [123, 123_123_123, 123_123_123_1, 13_123_3123_31_4321]) {}}
