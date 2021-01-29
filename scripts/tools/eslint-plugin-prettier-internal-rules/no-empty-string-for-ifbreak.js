979938b468246365b717ae57ff192b8ec3625c6f
sosukesuzuki <aosukeke@gmail.com>
2021-01-29 16:16:50 +0900

    Add new ESLint rule for ifBreak

---
+++ scripts/tools/eslint-plugin-prettier-internal-rules/no-empty-string-for-ifbreak.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const selector = [
+  "CallExpression",
+  '[callee.name="ifBreak"]',
+  "[arguments]",
+  ">",
+  ":nth-child(2)",
+  '[value=""]',
+].join("");
+
+const messageId = "no-empty-string-for-ifbreak";
+
+module.exports = {
+  meta: {
+    type: "suggestion",
+    docs: {
+      url:
+        "https://github.com/prettier/prettier/blob/main/scripts/eslint-plugin-prettier-internal-rules/no-empty-string-for-ifbreak.js",
+    },
+    messages: {
+      [messageId]:
+        "Please don't pass an empty string to second parameter of ifBreak.",
+    },
+    fixable: "code",
+  },
+  create(context) {
+    return {
+      [selector](node) {
+        const { parent } = node;
+        const firstArg = parent.arguments[0];
+        context.report({
+          node,
+          messageId,
+          fix: (fixer) =>
+            fixer.removeRange([firstArg.range[1], parent.range[1] - 1]),
+        });
+      },
+    };
+  },
+};
--- scripts/tools/eslint-plugin-prettier-internal-rules/test.js
+++ scripts/tools/eslint-plugin-prettier-internal-rules/test.js
@@ -204,3 +204,44 @@ test("require-json-extensions", {
     },
   ],
 });
+
+test("no-empty-string-for-ifbreak", {
+  valid: [
+    "ifBreak('foo', 'bar')",
+    "ifBreak(doc1, doc2)",
+    "ifBreak(',')",
+    "ifBreak(doc)",
+  ],
+  invalid: [
+    {
+      code: "ifBreak('foo', '')",
+      output: "ifBreak('foo')",
+      errors: [
+        {
+          message:
+            "Please don't pass an empty string to second parameter of ifBreak.",
+        },
+      ],
+    },
+    {
+      code: "ifBreak('foo'    ,     ''   )",
+      output: "ifBreak('foo')",
+      errors: [
+        {
+          message:
+            "Please don't pass an empty string to second parameter of ifBreak.",
+        },
+      ],
+    },
+    {
+      code: "ifBreak(doc, '')",
+      output: "ifBreak(doc)",
+      errors: [
+        {
+          message:
+            "Please don't pass an empty string to second parameter of ifBreak.",
+        },
+      ],
+    },
+  ],
+});
