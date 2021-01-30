877182d7cf36411865dea77c95a4114e1527a1f5
Cyrille David <dcyriller@users.noreply.github.com>
2021-01-30 18:57:00 +0100

    Update src/language-handlebars/clean.js
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- src/language-handlebars/clean.js
+++ src/language-handlebars/clean.js
@@ -3,7 +3,11 @@
 function clean(ast, newNode /*, parent*/) {
   // (Glimmer/HTML) ignore TextNode
   if (ast.type === "TextNode") {
-    return null;
+    const trimmed = ast.chars.trim();
+    if (!trimmed) {
+      return null;
+    }
+    newNode.chars = trimmed.replace(/[\t\n\f\r ]+/g, " ");
   }
 
   // `class` is reformatted
