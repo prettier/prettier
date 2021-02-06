70c5574cea43463835246efa9e695a92f215a77e
fisker <lionkay@gmail.com>
2021-02-06 17:06:57 +0800

    Fix test

--- tests_integration/__tests__/reserved-words-in-path.js
+++ tests_integration/__tests__/reserved-words-in-path.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const runPrettier = require("../runPrettier");
+expect.addSnapshotSerializer(require("../path-serializer"));
 
 describe("don't skip file paths containing reserved words like `constructor`", () => {
   runPrettier("cli/reserved-words-in-path", [
