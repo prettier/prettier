6edc4511c26fdee3df294323d7e8b17a915353e9
fisker Cheung <lionkay@gmail.com>
2021-02-07 10:02:09 +0800

    Refactor: Use `fromPairs` instead of `Array#reduce`

--- src/main/parser.js
+++ src/main/parser.js
@@ -69,12 +69,12 @@ function parse(text, opts) {
 
   // Create a new object {parserName: parseFn}. Uses defineProperty() to only call
   // the parsers getters when actually calling the parser `parse` function.
-  const parsersForCustomParserApi = Object.keys(parsers).reduce(
-    (object, parserName) =>
+  const parsersForCustomParserApi = Object.entries(parsers).reduce(
+    (object, [parserName, parser]) =>
       Object.defineProperty(object, parserName, {
         enumerable: true,
         get() {
-          return parsers[parserName].parse;
+          return parser.parse;
         },
       }),
     {}
