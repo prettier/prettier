61d65944973fa283d30583d94f1ccc97843b2581
fisker Cheung <lionkay@gmail.com>
2021-02-10 23:44:41 +0800

    Revert "Try  move install to `setup.js`"
    This reverts commit eaa51aa6050c033e468b2d33b6ac7fdb32afc59b.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,21 @@
 "use strict";
 
+const path = require("path");
+const installPrettier = require("./scripts/install-prettier");
+
+const PROJECT_ROOT = __dirname;
 const isProduction = process.env.NODE_ENV === "production";
-const TEST_STANDALONE = Boolean(process.env.TEST_STANDALONE);
 const ENABLE_CODE_COVERAGE = Boolean(process.env.ENABLE_CODE_COVERAGE);
+const TEST_STANDALONE = Boolean(process.env.TEST_STANDALONE);
+const INSTALL_PACKAGE = Boolean(process.env.INSTALL_PACKAGE);
+
+let PRETTIER_DIR = isProduction
+  ? path.join(PROJECT_ROOT, "dist")
+  : PROJECT_ROOT;
+if (INSTALL_PACKAGE || (isProduction && !TEST_STANDALONE)) {
+  PRETTIER_DIR = installPrettier(PRETTIER_DIR);
+}
+process.env.PRETTIER_DIR = PRETTIER_DIR;
 
 const testPathIgnorePatterns = [];
 let transform;
