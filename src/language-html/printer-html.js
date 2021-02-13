dca2c55ef67de5ea295661776ee3897cccce9f81
fisker Cheung <lionkay@gmail.com>
2021-02-13 13:34:31 +0800

    Move `embeddedInHtml` to `language-html/` (#9739)
    * Move `embeddedInHtml` to `language-html/`
    
    * Fix
    
    * Rename to `__embeddedInHtml`

--- src/language-html/printer-html.js
+++ src/language-html/printer-html.js
@@ -78,7 +78,7 @@ function embed(path, print, textToDoc, options) {
         if (!isEmpty) {
           doc = textToDoc(
             htmlTrimPreserveIndentation(content),
-            { parser },
+            { parser, __embeddedInHtml: true },
             { stripTrailingHardline: true }
           );
           isEmpty = doc === "";
@@ -104,7 +104,7 @@ function embed(path, print, textToDoc, options) {
             parser === "markdown"
               ? dedentString(node.value.replace(/^[^\S\n]*?\n/, ""))
               : node.value;
-          const textToDocOptions = { parser };
+          const textToDocOptions = { parser, __embeddedInHtml: true };
           if (options.parser === "html" && parser === "babel") {
             let sourceType = "script";
             const { attrMap } = node.parent;
@@ -128,21 +128,24 @@ function embed(path, print, textToDoc, options) {
           ];
         }
       } else if (node.parent.type === "interpolation") {
+        const textToDocOptions = {
+          __isInHtmlInterpolation: true, // to avoid unexpected `}}`
+          __embeddedInHtml: true,
+        };
+        if (options.parser === "angular") {
+          textToDocOptions.parser = "__ng_interpolation";
+          textToDocOptions.trailingComma = "none";
+        } else if (options.parser === "vue") {
+          textToDocOptions.parser = "__vue_expression";
+        } else {
+          textToDocOptions.parser = "__js_expression";
+        }
         return [
           indent([
             line,
-            textToDoc(
-              node.value,
-              {
-                __isInHtmlInterpolation: true, // to avoid unexpected `}}`
-                ...(options.parser === "angular"
-                  ? { parser: "__ng_interpolation", trailingComma: "none" }
-                  : options.parser === "vue"
-                  ? { parser: "__vue_expression" }
-                  : { parser: "__js_expression" }),
-              },
-              { stripTrailingHardline: true }
-            ),
+            textToDoc(node.value, textToDocOptions, {
+              stripTrailingHardline: true,
+            }),
           ]),
           node.parent.next &&
           needsToBorrowPrevClosingTagEndMarker(node.parent.next)
@@ -190,7 +193,7 @@ function embed(path, print, textToDoc, options) {
           // strictly prefer single quote to avoid unnecessary html entity escape
           textToDoc(
             code,
-            { __isInHtmlAttribute: true, ...opts },
+            { __isInHtmlAttribute: true, __embeddedInHtml: true, ...opts },
             { stripTrailingHardline: true }
           ),
         options
@@ -992,7 +995,7 @@ function printEmbeddedAttributeValue(node, originalTextToDoc, options) {
   const textToDoc = (code, opts) =>
     originalTextToDoc(
       code,
-      { __onHtmlBindingRoot, ...opts },
+      { __onHtmlBindingRoot, __embeddedInHtml: true, ...opts },
       { stripTrailingHardline: true }
     );
 
