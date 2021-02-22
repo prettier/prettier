044ee2ddd7b5f70b392f221dd66a7f75b160e434
fisker Cheung <lionkay@gmail.com>
2021-02-22 10:46:12 +0800

    Tweak order to align with `Literal`

--- src/language-js/print/literal.js
+++ src/language-js/print/literal.js
@@ -5,21 +5,20 @@ function printLiteral(path, options /*, print*/) {
   const node = path.getNode();
 
   switch (node.type) {
-    case "NullLiteral": // Babel 6 Literal split
-      return "null";
     case "RegExpLiteral": // Babel 6 Literal split
       return printRegex(node);
-    case "NumericLiteral": // Babel 6 Literal split
-      return printNumber(node.extra.raw);
-    case "DecimalLiteral":
-      return printNumber(node.value) + "m";
     case "BigIntLiteral":
       // babel: node.extra.raw, flow: node.bigint
       return (node.bigint || node.extra.raw).toLowerCase();
-    case "BooleanLiteral": // Babel 6 Literal split
-      return String(node.value);
+    case "NumericLiteral": // Babel 6 Literal split
+      return printNumber(node.extra.raw);
     case "StringLiteral": // Babel 6 Literal split
       return printString(node.extra.raw, options);
+    case "NullLiteral": // Babel 6 Literal split
+    case "BooleanLiteral": // Babel 6 Literal split
+      return String(node.value);
+    case "DecimalLiteral":
+      return printNumber(node.value) + "m";
     case "Literal": {
       if (node.regex) {
         return printRegex(node.regex);
