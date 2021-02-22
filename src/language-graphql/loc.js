71b10a0a32ce81bad1175333c991361ec16c36a1
fisker Cheung <lionkay@gmail.com>
2021-02-22 11:26:21 +0800

    `graphql`: Fix loc

--- src/language-graphql/loc.js
+++ src/language-graphql/loc.js
@@ -1,17 +1,11 @@
 "use strict";
 
 function locStart(node) {
-  if (typeof node.start === "number") {
-    return node.start;
-  }
-  return node.loc && node.loc.start;
+  return node.kind === "Comment" ? node.start : node.loc.start;
 }
 
 function locEnd(node) {
-  if (typeof node.end === "number") {
-    return node.end;
-  }
-  return node.loc && node.loc.end;
+  return node.kind === "Comment" ? node.end : node.loc.end;
 }
 
 module.exports = { locStart, locEnd };
