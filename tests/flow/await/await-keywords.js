15c49083cbda33be146aeb24c98331cbdf783144
sosukesuzuki <aosukeke@gmail.com>
2021-02-25 13:55:38 +0900

    Add await tests for flow

---
+++ tests/flow/await/await-keywords.js
@@ -0,0 +1,15 @@
+export class C1 {
+  p = await (0);
+}
+
+await (0);
+
+async function foo() {
+  function bar(x = await (2)) {}
+}
+
+async (x = await (2)) => {};
+
+export class C2 {
+  p = await 0;
+}
