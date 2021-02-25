8ebb11b8593aaa00c119e1d8f76d75283baf7dd7
sosukesuzuki <aosukeke@gmail.com>
2021-02-25 14:00:52 +0900

    Add with-paren tests for Flow

--- tests/flow/await/await-keywords.js
+++ tests/flow/await/await-keywords.js
@@ -13,3 +13,22 @@ async (x = await (2)) => {};
 export class C2 {
   p = await 0;
 }
+
+function foo(promise) { await (promise); }
+
+function a() {
+  return await (1)
+}
+
+() => { await (x) };
+
+function foo() {
+  await
+  (foo);
+}
+
+export class C {
+  p = await (0);
+}
+
+await (0);
