2edfec5edae5a7a4d6ed7ba086b42f0e87548c17
fisker Cheung <lionkay@gmail.com>
2021-03-02 13:42:49 +0800

    JSON: Better error message (#10452)

--- tests/misc/errors/json/jsfmt.spec.js
+++ tests/misc/errors/json/jsfmt.spec.js
@@ -8,22 +8,29 @@ run_spec(
       '{"foo": false || "bar"}',
       '{"foo": undefined}',
       '{"foo": () => {}}',
-      '/* comment */{"foo": 1}',
-      '// comment\n{"foo": 1}',
-    ],
-  },
-  ["json-stringify"]
-);
-
-run_spec(
-  {
-    dirname: __dirname,
-    snippets: [
       "packages\\the-hub\\cypress\\fixtures\\gridConfiguration.json",
       "1+2",
       "[,]",
       "[1,,2]",
+      "{Infinity}",
+      "{[key]: 1}",
+      "{[key()]: 1}",
+      "{['CallExpression']: 1}",
+      "{['StringLiteral']: 1}",
+      "{['string']: 1}",
+      "{[1]: 1}",
+      "{[Infinity]: 1}",
+      "{[-Infinity]: 1}",
+      "{[{key: 'value'}]: 1}",
+      "{[[]]: 1}",
+      "{[null]: 1}",
+      "{key: +foo()}",
+      "{key: void foo()}",
+      "#!/usr/bin/env node\n{}",
+      '"use strict"\n{}',
+      "/* comment */",
+      "// comment",
     ],
   },
-  ["json"]
+  ["json", "json5", "json-stringify"]
 );
