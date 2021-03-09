3f4c3295c16274aadf642a493d6f15977624d688
fisker Cheung <lionkay@gmail.com>
2021-03-09 12:56:41 +0800

    Test json superset

---
+++ tests/json/json-superset/jsfmt.spec.js
@@ -0,0 +1,24 @@
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      {
+        code: '"\u2028\u2029\u0041\\u2028\\u2029\\u0041"',
+        output: '"\u2028\u2029\u0041\\u2028\\u2029\\u0041"\n',
+      },
+    ],
+  },
+  ["json", "json5"]
+);
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      {
+        code: '"\u2028\u2029\u0041\u2028\\u2029\\u0041"',
+        output: '"\u2028\u2029\u0041\u2028\u2029\u0041"\n',
+      },
+    ],
+  },
+  ["json-stringify"]
+);
