3995c2a1a1839753e08f0576c4161876bc5ad62e
fisker Cheung <lionkay@gmail.com>
2021-03-14 04:07:22 +0800

    Whitelist recovered babel parse errors (#10495)

--- tests/js/variable_declarator/jsfmt.spec.js
+++ tests/js/variable_declarator/jsfmt.spec.js
@@ -1,7 +1,1 @@
-run_spec(__dirname, ["babel", "flow", "typescript"], {
-  errors: {
-    espree: ["invalid-const.js"],
-    flow: ["invalid-const.js"],
-    meriyah: ["invalid-const.js"],
-  },
-});
+run_spec(__dirname, ["babel", "flow", "typescript"]);
---
+++ tests/misc/errors/js/object/setter-generator.js
@@ -0,0 +1,3 @@
+({
+    set *iterator(iter) { },
+});
