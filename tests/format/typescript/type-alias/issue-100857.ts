0d2ce2b8d3d8313eeda9171c3d84738c419da156
sosukesuzuki <aosukeke@gmail.com>
2021-05-17 00:03:12 +0900

    Add tests

---
+++ tests/format/typescript/type-alias/issue-100857.ts
@@ -0,0 +1,25 @@
+type FieldLayoutWith<
+  T extends string,
+  S extends unknown = { width: string }
+> = {
+  type: T;
+  code: string;
+  size: S;
+};
+
+type FieldLayoutWith<
+  T extends string,
+  S extends unknown,
+> = {
+  type: T;
+  code: string;
+  size: S;
+};
+
+type FieldLayoutWith<
+  S extends unknown = { width: string }
+> = {
+  type: T;
+  code: string;
+  size: S;
+};
