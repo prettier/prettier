0627e0f3dcf74f1a231c48585705eb64a4312205
fisker Cheung <lionkay@gmail.com>
2021-05-25 18:52:19 +0800

    Attempt to remove babel-jest hack

--- jest.config.js
+++ jest.config.js
@@ -25,25 +25,7 @@ if (TEST_STANDALONE) {
 if (isProduction) {
   // `esm` bundles need transform
   transform = {
-    "(?:\\.mjs|codeSamples\\.js)$": [
-      "babel-jest",
-      {
-        presets: [
-          [
-            "@babel/env",
-            {
-              targets: { node: "current" },
-              exclude: [
-                "transform-async-to-generator",
-                "transform-classes",
-                "proposal-async-generator-functions",
-                "transform-regenerator",
-              ],
-            },
-          ],
-        ],
-      },
-    ],
+    "(?:\\.mjs|codeSamples\\.js)$": "babel-jest",
   };
 } else {
   // Only test bundles for production
