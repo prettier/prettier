7ff2f28700cdf7639188322a994d018e55626719
sosukesuzuki <aosukeke@gmail.com>
2021-05-31 02:36:53 +0900

    Fix by prettier

--- scripts/changelog.mjs
+++ scripts/changelog.mjs
@@ -9,7 +9,7 @@ import { CHANGELOG_CATEGORIES } from "./utils/changelog-categories.mjs";
 const { __dirname } = createEsmUtils(import.meta);
 
 const [prNumberString, category] = process.argv.slice(2);
-if(!prNumberString || !category) {
+if (!prNumberString || !category) {
   throw new Error("Two args are required.");
 }
 const prNumber = convertToNumber(prNumberString);
@@ -32,9 +32,9 @@ async function getPr(prNumber) {
   const url = `https://api.github.com/repos/prettier/prettier/pulls/${prNumber}`;
   const response = await fetch(url, {
     Headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Accept: "application/vnd.github.v3+json",
-    }
+    },
   });
   if (!response.ok) {
     if (response.status === 404) {
@@ -45,36 +45,45 @@ async function getPr(prNumber) {
   const { title, user } = await response.json();
   return {
     title,
-    user: user.login
+    user: user.login,
   };
 }
 
 /**
- * @param {number} prNumber 
- * @param {string} category 
- * @param {string} newChangelog 
+ * @param {number} prNumber
+ * @param {string} category
+ * @param {string} newChangelog
  * @returns {Promise<void>}
  */
 async function addNewChangelog(prNumber, category, newChangelog) {
-  const newChangelogPath = path.resolve(__dirname, `../changelog_unreleased/${category}/${prNumber}.md`);
+  const newChangelogPath = path.resolve(
+    __dirname,
+    `../changelog_unreleased/${category}/${prNumber}.md`
+  );
   await fs.writeFile(newChangelogPath, newChangelog);
 }
 
 /**
- * @param {string} title 
- * @param {string} user 
- * @param {number} prNumber 
+ * @param {string} title
+ * @param {string} user
+ * @param {number} prNumber
  * @returns {string}
  */
 async function createChangelog(title, user, prNumber) {
-  const changelogTemplatePath = path.resolve(__dirname, "../changelog_unreleased/TEMPLATE.md");
+  const changelogTemplatePath = path.resolve(
+    __dirname,
+    "../changelog_unreleased/TEMPLATE.md"
+  );
   const changelogTemplate = await fs.readFile(changelogTemplatePath, "utf-8");
 
   const titlePart = "Title";
   const prNumberPart = "#XXXX";
-  const userPart = "@user"
+  const userPart = "@user";
 
-  return changelogTemplate.replace(titlePart, title).replace(prNumberPart, `#${prNumber}`).replace(userPart, `@${user}`);
+  return changelogTemplate
+    .replace(titlePart, title)
+    .replace(prNumberPart, `#${prNumber}`)
+    .replace(userPart, `@${user}`);
 }
 
 /**
@@ -94,7 +103,7 @@ function convertToNumber(value) {
  * @returns {void}
  */
 function assertCatogory(category) {
-  if(!CHANGELOG_CATEGORIES.includes(category)) {
+  if (!CHANGELOG_CATEGORIES.includes(category)) {
     throw new Error(`${category} is invalid category`);
   }
 }
