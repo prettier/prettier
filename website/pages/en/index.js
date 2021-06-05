79a9c1e77b1fffd241f77150f49ca301c3705933
sosukesuzuki <aosukeke@gmail.com>
2021-06-05 19:37:25 +0900

    Update dependents count

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -312,7 +312,7 @@ const UsersSection = ({ language }) => {
             <div style={{ marginLeft: ".7em", width: "260px" }}>
               <p>
                 More than{" "}
-                <strong data-placeholder="dependent-github">2.8 million</strong>{" "}
+                <strong data-placeholder="dependent-github">2.9 million</strong>{" "}
                 dependent repositories on GitHub
               </p>
               <Button href="https://github.com/prettier/prettier/network/dependents">
