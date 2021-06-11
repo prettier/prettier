c6482c2b5e38925bedf5a2064e08c09682122471
fisker Cheung <lionkay@gmail.com>
2021-06-11 09:47:58 +0800

    Rewrite vue tests

--- tests/format/vue/bracket-same-line/self-closing.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-     <div id="test-id-123" class="test-class-456" title="test-title-789" data-test="test-data-111213">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit.
-       <self-closing-component id="input-id" class="input-class" title="input-title" value="input-value" />
-     </div>
-  </div>
-</template>
---
+++ tests/format/vue/bracket-same-line/vue.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+<div :long_long_attribute="long_long_long_long_long_long_long_long_long_long_long_value">
+text
+</div>
+<div v-on:long_long_attribute="long_long_long_long_long_long_long_long_long_long_long_value"></div>
+<span long_long_attribute="long_long_long_long_long_long_long_long_long_long_long_value">
+text
+</span>
+<span long_long_attribute="long_long_long_long_long_long_long_long_long_long_long_value"></span>
+<img long_long_attribute="long_long_long_long_long_long_long_long_long_long_long_value"/>
+<div class="a">
+text
+</div>
+<span  class="a">
+text
+</span>
+<img class="a"/>
+  </div>
+</template>
+
+<script long_long_attribute="long_long_long_long_long_long_long_long_long_long_long_value">export default {}</script>
+<style  long_long_attribute="long_long_long_long_long_long_long_long_long_long_long_value">.a {color: #f00}</style>
