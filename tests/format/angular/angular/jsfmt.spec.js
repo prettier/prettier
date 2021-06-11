5f4a34b8abf5d5755a1eced395bd7a094beba6f0
fisker Cheung <lionkay@gmail.com>
2021-06-11 09:25:30 +0800

    Rewrite angular tests

--- tests/format/angular/angular/jsfmt.spec.js
+++ tests/format/angular/angular/jsfmt.spec.js
@@ -3,4 +3,3 @@ run_spec(__dirname, ["angular"]);
 run_spec(__dirname, ["angular"], { printWidth: 1 });
 run_spec(__dirname, ["angular"], { htmlWhitespaceSensitivity: "ignore" });
 run_spec(__dirname, ["angular"], { bracketSpacing: false });
-run_spec(__dirname, ["angular"], { bracketSameLine: true });
