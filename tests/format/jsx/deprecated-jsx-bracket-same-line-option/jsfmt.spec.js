98d4abb17b915cbc978390132daf0d81e3baba2f
fisker Cheung <lionkay@gmail.com>
2021-06-11 10:45:04 +0800

    Run tests on all js parsers

--- tests/format/jsx/deprecated-jsx-bracket-same-line-option/jsfmt.spec.js
+++ tests/format/jsx/deprecated-jsx-bracket-same-line-option/jsfmt.spec.js
@@ -1,10 +1,14 @@
-run_spec(__dirname, ["meriyah"], { jsxBracketSameLine: true });
-run_spec(__dirname, ["meriyah"], { jsxBracketSameLine: false });
-run_spec(__dirname, ["meriyah"], {
+run_spec(__dirname, ["babel", "flow", "typescript"], {
+  jsxBracketSameLine: true,
+});
+run_spec(__dirname, ["babel", "flow", "typescript"], {
+  jsxBracketSameLine: false,
+});
+run_spec(__dirname, ["babel", "flow", "typescript"], {
   jsxBracketSameLine: false,
   bracketSameLine: true,
 });
-run_spec(__dirname, ["meriyah"], {
+run_spec(__dirname, ["babel", "flow", "typescript"], {
   jsxBracketSameLine: true,
   bracketSameLine: false,
 });
