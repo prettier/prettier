fe9b28c2936c3991272bba99502efe6bbd5fd2e9
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-23 20:03:28 -0400

    prettierx test: add nested ternary object & array cases
    from the following:
    
    - https://github.com/brodybits/prettierx/issues/552
    - https://github.com/prettier/prettier/issues/4203#issuecomment-867213164

---
+++ tests/ternary-object-expressions/nesting-object-ternary-expression.js
@@ -0,0 +1,17 @@
+alice
+  ? {
+      beth: {
+          carol: 101
+        }
+        ? {
+            debbie: 202
+          }
+          ? {
+              ellen: 303
+            }
+          : 404
+        : 505
+    }
+    ? loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong0101
+    : loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong0102
+  : loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong0201
