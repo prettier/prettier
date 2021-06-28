e9dc2c3e59affeb07c7c155142225bf0a8afb3dd
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-28 19:56:19 -0400

    [prettierx] test function-parentheses with babel-flow
    (in prettierx-rebase-branch-001 branch)
    
    for improved consistency with prettierX 0.18.x

--- tests/format/flow/function-parentheses/jsfmt.spec.js
+++ tests/format/flow/function-parentheses/jsfmt.spec.js
@@ -1,3 +1,6 @@
-run_spec(__dirname, ["flow", "babel"]);
-run_spec(__dirname, ["flow", "babel"], { trailingComma: "all" });
-run_spec(__dirname, ["flow", "babel"], { arrowParens: "avoid" });
+// [prettierx] test with babel-flow
+// [TBD] skip TypeScript for now
+// (known parse error with babel-ts parser)
+run_spec(__dirname, ["flow", "babel", "babel-flow"]);
+run_spec(__dirname, ["flow", "babel", "babel-flow"], { trailingComma: "all" });
+run_spec(__dirname, ["flow", "babel", "babel-flow"], { arrowParens: "avoid" });
