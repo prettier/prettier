fda5ec114e11cea8665b80be4b2a99983f61d630
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-28 15:48:32 -0400

    prettierx test: fix early exit test to use fork name
    from package.json
    
    (in prettierx-update-branch-001)

--- tests/integration/__tests__/early-exit.js
+++ tests/integration/__tests__/early-exit.js
@@ -4,6 +4,9 @@ const prettier = require("prettier-local");
 const runPrettier = require("../runPrettier");
 const { isProduction } = require("../env");
 
+// [prettierx] fork name from package.json
+const { name } = require("../../../package.json");
+
 describe("show version with --version", () => {
   runPrettier("cli/with-shebang", ["--version"]).test({
     stdout: prettier.version + "\n",
@@ -102,7 +105,8 @@ test("node version error", async () => {
     const result = runPrettier("cli", ["--help"]);
     expect(await result.status).toBe(1);
     expect(await result.stderr).toBe(
-      `prettier requires at least version ${
+      // [prettierx] fork name from package.json
+      `${name} requires at least version ${
         isProduction ? "10.13.0" : "12.17.0"
       } of Node, please upgrade\n`
     );
