ee0bbf2f88e26272964e480a42d2c0df2d0b3f75
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-06-28 19:55:11 -0400

    prettierx test arrows with Flow & babel-flow
    (in prettierx-rebase-branch-001 version branch)
    
    for improved consistency with prettierX 0.18.x

--- tests/format/js/arrows/jsfmt.spec.js
+++ tests/format/js/arrows/jsfmt.spec.js
@@ -1,2 +1,8 @@
-run_spec(__dirname, ["babel", "typescript"], { arrowParens: "always" });
-run_spec(__dirname, ["babel", "typescript"], { arrowParens: "avoid" });
+// [prettierx] test with Flow & all Babel parsers
+// (babel-ts is normally included with typescript by default)
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
+  arrowParens: "always",
+});
+run_spec(__dirname, ["babel", "babel-flow", "flow", "typescript"], {
+  arrowParens: "avoid",
+});
