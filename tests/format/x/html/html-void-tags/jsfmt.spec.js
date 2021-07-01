daaa5f6181e69b59efa893306552f95e4fc8cd14
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-07-01 15:20:09 -0400

    [prettierx] test: update tests/format/x/html/html-void-tags
    skip vue parser
    
    use a bogus option to keep snapshot more consistent with
    prettierX 0.18.x
    
    update test snapshot

--- tests/format/x/html/html-void-tags/jsfmt.spec.js
+++ tests/format/x/html/html-void-tags/jsfmt.spec.js
@@ -1,3 +1,4 @@
-run_spec(__dirname, ["html", "angular", "vue"]);
+// using bogus option to keep snapshot more consistent with prettierX 0.18.x
+run_spec(__dirname, ["html", "angular"], { bogus: null });
 run_spec(__dirname, ["html"], { htmlVoidTags: true });
-run_spec(__dirname, ["angular", "vue"], { htmlVoidTags: true });
+run_spec(__dirname, ["angular"], { htmlVoidTags: true });
