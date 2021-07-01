fc3cb3b9d44ae3754548f1175a7b4837ff54d1e3
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-07-01 00:49:42 -0400

    [prettierx] test fix: tests/format/yaml/flow-mapping
    - bogus option to improve consistency of snapshot with Prettier 2.3.2
    - add comments
    - update test snapshot

--- tests/format/yaml/flow-mapping/jsfmt.spec.js
+++ tests/format/yaml/flow-mapping/jsfmt.spec.js
@@ -1,3 +1,5 @@
 run_spec(__dirname, ["yaml"]);
 run_spec(__dirname, ["yaml"], { tabWidth: 4 });
-run_spec(__dirname, ["yaml"], { yamlBracketSpacing: false });
+// [prettierx] support --no-yaml-bracket-spacing option
+// with bogus option to improve consistency of snapshot with Prettier 2.3.2
+run_spec(__dirname, ["yaml"], { bogus: null, yamlBracketSpacing: false });
