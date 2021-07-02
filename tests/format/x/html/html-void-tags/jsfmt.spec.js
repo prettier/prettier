a8f5ed6cdf71b65c846f74a7c6f5948b96eab15d
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-07-02 11:43:08 -0400

    [prettierx] test: improve testing of --html-void-tags
    - add testing with self-closing input tag
    - restore testing with `vue` parser
      (now reports error with input void element)
    - rename input test file
    - update test snapshot

--- tests/format/x/html/html-void-tags/jsfmt.spec.js
+++ tests/format/x/html/html-void-tags/jsfmt.spec.js
@@ -1,4 +1,15 @@
-// using bogus option to keep snapshot more consistent with prettierX 0.18.x
-run_spec(__dirname, ["html", "angular"], { bogus: null });
+run_spec(__dirname, ["html", "angular", "vue"], {
+  errors: {
+    vue: ["input-void-element.html"], // [TBD] no error with `vue` in prettierX 0.18.x
+  },
+  bogus1: true, // [improve consistency of snapshot with prettierX 0.18.x]
+});
+
 run_spec(__dirname, ["html"], { htmlVoidTags: true });
-run_spec(__dirname, ["angular"], { htmlVoidTags: true });
+
+run_spec(__dirname, ["angular", "vue"], {
+  errors: {
+    vue: ["input-void-element.html"], // [TBD] no error with `vue` in prettierX 0.18.x
+  },
+  htmlVoidTags: true,
+});
