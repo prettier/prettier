c10ae7ec6b113895228e9268fcbcc6f35cb4232b
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-07-05 16:07:10 -0400

    [prettierx] test: update html-tags-with-void-tags (...)
    * update tests/format/x/html/html-tags-with-void-tags/jsfmt.spec.js
      to use updated test paths
    
    * update snapshot in tests/format/x/html/html-tags-with-void-tags

--- tests/format/x/html/html-tags-with-void-tags/jsfmt.spec.js
+++ tests/format/x/html/html-tags-with-void-tags/jsfmt.spec.js
@@ -2,6 +2,6 @@
 // This test script runs for test files in another directory,
 // **not** on any files in *this* directory.
 
-const dirPath = `${__dirname}/../../../../html_tags`;
+const dirPath = `${__dirname}/../../../../format/html/tags`;
 
 run_spec(dirPath, ["html"], { htmlVoidTags: true });
