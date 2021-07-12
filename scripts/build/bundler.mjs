44012a0f1d7d62a97cfea465469a0c7fd401eb4a
fisker Cheung <lionkay@gmail.com>
2021-07-12 14:58:10 +0800

    Replace parsers with `undefined` in umd bundles

--- scripts/build/bundler.mjs
+++ scripts/build/bundler.mjs
@@ -175,22 +175,35 @@ function getRollupConfig(bundle) {
   const replaceModule = {};
   // Replace other bundled files
   if (bundle.target === "node") {
+    replaceModule[path.join(PROJECT_ROOT, "./package.json")] = "./package.json";
+
     for (const item of bundles) {
       if (item.input !== bundle.input) {
         replaceModule[path.join(PROJECT_ROOT, item.input)] = `./${item.output}`;
       }
     }
-  }
+  } else {
+    // Universal bundle only use version info from package.json
+    replaceModule[path.join(PROJECT_ROOT, "./package.json")] = {
+      code: `export default ${JSON.stringify({
+        version: require("../../package.json").version,
+      })}`,
+    };
 
-  replaceModule[path.join(PROJECT_ROOT, "./package.json")] =
-    bundle.target === "node"
-      ? "./package.json"
-      : // Universal bundle only use version info from package.json
-        {
-          code: `export default ${JSON.stringify({
-            version: require("../../package.json").version,
-          })}`,
-        };
+    for (const file of [
+      "./src/language-css/parsers.js",
+      "./src/language-graphql/parsers.js",
+      "./src/language-handlebars/parsers.js",
+      "./src/language-html/parsers.js",
+      "./src/language-js/parsers.js",
+      "./src/language-markdown/parsers.js",
+      "./src/language-yaml/parsers.js",
+    ]) {
+      replaceModule[path.join(PROJECT_ROOT, file)] = {
+        code: "export default undefined",
+      };
+    }
+  }
 
   Object.assign(replaceModule, bundle.replaceModule);
 
