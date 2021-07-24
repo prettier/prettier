282cae5ba430385f8f980baca4b2a840da7ec579
fisker <lionkay@gmail.com>
2021-07-24 10:43:56 +0800

    Minor tweak

--- scripts/release/release.js
+++ scripts/release/release.js
@@ -5,12 +5,12 @@ import { fileURLToPath } from "node:url";
 import path from "node:path";
 
 async function run() {
-  const { default: chalk } = await import("chalk");
-  const { default: minimist } = await import("minimist");
-  const { default: semver } = await import("semver");
-  const {
-    default: { string: outdentString },
-  } = await import("outdent");
+  const importDefault = async (module) => (await import(module)).default;
+
+  const chalk = await importDefault("chalk");
+  const minimist = await importDefault("minimist");
+  const semver = await importDefault("semver");
+  const { string: outdentString } = await importDefault("outdent");
   const { runGit, readJson } = await import("./utils.js");
 
   const params = minimist(process.argv.slice(2), {
@@ -34,24 +34,25 @@ async function run() {
     ).version;
   }
 
-  const steps = [
-    "./steps/validate-new-version.js",
-    "./steps/check-git-status.js",
-    "./steps/install-dependencies.js",
-    "./steps/run-tests.js",
-    "./steps/update-version.js",
-    "./steps/generate-bundles.js",
-    "./steps/update-changelog.js",
-    "./steps/push-to-git.js",
-    "./steps/publish-to-npm.js",
-    "./steps/bump-prettier.js",
-    "./steps/update-dependents-count.js",
-    "./steps/post-publish-steps.js",
-  ];
+  const steps = await Promise.all(
+    [
+      "./steps/validate-new-version.js",
+      "./steps/check-git-status.js",
+      "./steps/install-dependencies.js",
+      "./steps/run-tests.js",
+      "./steps/update-version.js",
+      "./steps/generate-bundles.js",
+      "./steps/update-changelog.js",
+      "./steps/push-to-git.js",
+      "./steps/publish-to-npm.js",
+      "./steps/bump-prettier.js",
+      "./steps/update-dependents-count.js",
+      "./steps/post-publish-steps.js",
+    ].map((step) => importDefault(step))
+  );
 
   try {
-    for (const file of steps) {
-      const { default: step } = await import(file);
+    for (const step of steps) {
       await step(params);
     }
   } catch (error) {
