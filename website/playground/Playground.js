5678b6176a4fe66e455af943368b07d2ac79c9ab
sosukesuzuki <aosukeke@gmail.com>
2021-09-02 17:23:12 +0900

    Fix

--- website/playground/Playground.js
+++ website/playground/Playground.js
@@ -23,6 +23,13 @@ const CATEGORIES_ORDER = [
   "HTML",
   "Special",
 ];
+const ISSUES_URL = "https://github.com/prettier/prettier/issues/new?body=";
+const MAX_LENGTH = 8000 - ISSUES_URL.length; // it seems that GitHub limit is 8195
+const COPY_MESSAGE =
+  "<!-- The issue body has been saved to the clipboard. Please paste it after this line! 👇 -->\n";
+
+const isProduction = location.host === "prettier.io";
+const isPreview = location.host.includes("netlify.app");
 const ENABLED_OPTIONS = [
   "parser",
   "printWidth",
@@ -32,7 +39,6 @@ const ENABLED_OPTIONS = [
   "singleQuote",
   "bracketSpacing",
   "jsxSingleQuote",
-  "jsxBracketSameLine",
   "quoteProps",
   "arrowParens",
   "trailingComma",
@@ -43,10 +49,14 @@ const ENABLED_OPTIONS = [
   "vueIndentScriptAndStyle",
   "embeddedLanguageFormatting",
 ];
-const ISSUES_URL = "https://github.com/prettier/prettier/issues/new?body=";
-const MAX_LENGTH = 8000 - ISSUES_URL.length; // it seems that GitHub limit is 8195
-const COPY_MESSAGE =
-  "<!-- The issue body has been saved to the clipboard. Please paste it after this line! 👇 -->\n";
+// "jsxBracketSameLine" will be deprected in 2.4
+if (isProduction) {
+  ENABLED_OPTIONS.push("jsxBracketSameLine");
+}
+// "bracketSameLine" will be landed in 2.4
+if (isPreview) {
+  ENABLED_OPTIONS.push("bracketSameLine");
+}
 
 class Playground extends React.Component {
   constructor(props) {
