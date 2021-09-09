71addc48682f008db380ed9205c9c3ab6e74267f
austaras <austaras@outlook.com>
2021-09-10 03:47:54 +0800

    Add alignColumns

--- src/language-markdown/options.js
+++ src/language-markdown/options.js
@@ -4,6 +4,7 @@ const commonOptions = require("../common/common-options.js");
 
 // format based on https://github.com/prettier/prettier/blob/main/src/main/core-options.js
 module.exports = {
+  alignColumns: commonOptions.alignColumns,
   proseWrap: commonOptions.proseWrap,
   singleQuote: commonOptions.singleQuote,
 };
---
+++ tests/format/markdown/table/cjk-long.md
@@ -0,0 +1,4 @@
+Pilot|Airport|Hours
+--|:--:|--:
+John Doe|SKG|1338ewrqewrewrewqrewrqwerewrweqrwerew
+Jane Roe|JFK|自分自身の状態を管理
