d334db6e3929914f5cc2991cd62305ec2593eb6b
Jack Bates <jack@nottheoilrig.com>
2021-10-06 14:55:22 -0700

    Discriminate placeholders to evade mapDoc() cache

--- src/language-js/embed/markdown.js
+++ src/language-js/embed/markdown.js
@@ -6,14 +6,17 @@ const {
 } = require("../../document/index.js");
 const { printTemplateExpressions } = require("../print/template-literal.js");
 
-// The counter is needed to distinguish nested embeds.
-let counter = 0;
 function format(path, print, textToDoc) {
   const node = path.getValue();
-  const placeholder = `PRETTIER_MARKDOWN_PLACEHOLDER_${counter}_IN_JS`;
-  counter = (counter + 1) >>> 0;
+  const placeholder = createPlaceholder();
   let text = unescapeBackticks(
-    node.quasis.map((quasi) => quasi.value.raw).join(placeholder)
+    node.quasis
+      .map((quasi, index, quasis) =>
+        index === quasis.length - 1
+          ? quasi.value.raw
+          : quasi.value.raw + placeholder(index)
+      )
+      .join("")
   );
   const indentation = getIndentation(text);
   const hasIndent = indentation !== "";
@@ -30,12 +33,14 @@ function format(path, print, textToDoc) {
     (currentDoc) =>
       typeof currentDoc !== "string"
         ? currentDoc
-        : currentDoc.split(placeholder).flatMap((unescaped, index, splits) => {
-            const escaped = escapeBackticks(unescaped);
-            return index === splits.length - 1
-              ? escaped
-              : [escaped, expressionDocs.shift()];
-          })
+        : currentDoc
+            .split(new RegExp(placeholder(".*?"), "g"))
+            .flatMap((unescaped, index, splits) => {
+              const escaped = escapeBackticks(unescaped);
+              return index === splits.length - 1
+                ? escaped
+                : [escaped, expressionDocs.shift()];
+            })
   );
   return [
     "`",
@@ -43,6 +48,15 @@ function format(path, print, textToDoc) {
     softline,
     "`",
   ];
+}
+
+// The counter is needed to distinguish nested embeds.
+let counter = 0;
+function createPlaceholder() {
+  const i = counter;
+  counter = (counter + 1) >>> 0;
+  // mapDoc() caches the placeholders so they have to all be distinct.
+  return (j) => `PRETTIER_MARKDOWN_PLACEHOLDER_${i}_${j}_IN_JS`;
 }
 
 // 1. Backticks are significant in Markdown, otherwise we wouldn't
