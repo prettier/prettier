5909f5b3f191a0a32f759e1f4378477d3b90e28e
Sosuke Suzuki <aosukeke@gmail.com>
2021-10-07 21:22:32 +0900

    Extract unified plugins (#11632)

---
+++ src/language-markdown/unified-plugins/html-to-jsx.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const mdx = require("../mdx.js");
+const { mapAst, INLINE_NODE_WRAPPER_TYPES } = require("../utils.js");
+
+function htmlToJsx() {
+  return (ast) =>
+    mapAst(ast, (node, _index, [parent]) => {
+      if (
+        node.type !== "html" ||
+        mdx.COMMENT_REGEX.test(node.value) ||
+        INLINE_NODE_WRAPPER_TYPES.includes(parent.type)
+      ) {
+        return node;
+      }
+      return { ...node, type: "jsx" };
+    });
+}
+
+module.exports = htmlToJsx;
