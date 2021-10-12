91af210e714ff8541201f22f5fa9f49e996e344c
sosukesuzuki <aosukeke@gmail.com>
2021-10-12 22:06:51 +0900

    Throw syntax error for inalid abstract property

--- src/language-js/parse/postprocess.js
+++ src/language-js/parse/postprocess.js
@@ -8,10 +8,69 @@ const createError = require("../../common/parser-create-error.js");
 const { locStart, locEnd } = require("../loc.js");
 const { isTypeCastComment } = require("../comments.js");
 
+function throwSyntaxError(node, message) {
+  const { start, end } = node.loc;
+  throw createError(message, {
+    start: { line: start.line, column: start.column + 1 },
+    end: { line: end.line, column: end.column + 1 },
+  });
+}
+
+// Invalid decorators are removed since `@typescript-eslint/typescript-estree` v4
+// https://github.com/typescript-eslint/typescript-eslint/pull/2375
+function throwErrorForInvalidDecorator(
+  tsNode,
+  esTreeNode,
+  tsNodeToESTreeNodeMap
+) {
+  const tsDecorators = tsNode.decorators;
+  if (!Array.isArray(tsDecorators)) {
+    return;
+  }
+  const esTreeDecorators = esTreeNode.decorators;
+  if (
+    !Array.isArray(esTreeDecorators) ||
+    esTreeDecorators.length !== tsDecorators.length ||
+    tsDecorators.some((tsDecorator) => {
+      const esTreeDecorator = tsNodeToESTreeNodeMap.get(tsDecorator);
+      return !esTreeDecorator || !esTreeDecorators.includes(esTreeDecorator);
+    })
+  ) {
+    throwSyntaxError(
+      esTreeNode,
+      "Leading decorators must be attached to a class declaration"
+    );
+  }
+}
+
+// Value of abstract property is removed since `@typescript-eslint/typescript-estree` v5
+// https://github.com/typescript-eslint/typescript-eslint/releases/tag/v5.0.0
+function throwErrorForInvalidAbstractProperty(tsNode, esTreeNode) {
+  const SYNTAX_KIND_PROPERTY_DEFINITION = 165;
+  const SYNTAX_KIND_ABSTRACT_KEYWORD = 126;
+  if (
+    tsNode.kind !== SYNTAX_KIND_PROPERTY_DEFINITION ||
+    (tsNode.modifiers &&
+      !tsNode.modifiers.some(
+        (modifier) => modifier.kind === SYNTAX_KIND_ABSTRACT_KEYWORD
+      ))
+  ) {
+    return;
+  }
+  if (tsNode.initializer && esTreeNode.value === null) {
+    throwSyntaxError(
+      esTreeNode,
+      "Abstract property cannot have an initializer"
+    );
+  }
+}
+
 function postprocess(ast, options) {
-  // Invalid decorators are removed since `@typescript-eslint/typescript-estree` v4
-  // https://github.com/typescript-eslint/typescript-eslint/pull/2375
-  if (options.parser === "typescript" && options.originalText.includes("@")) {
+  if (
+    options.parser === "typescript" &&
+    (options.originalText.includes("@") ||
+      options.originalText.includes("abstract"))
+  ) {
     const { esTreeNodeToTSNodeMap, tsNodeToESTreeNodeMap } =
       options.tsParseResult;
     ast = visitNode(ast, (node) => {
@@ -19,35 +78,12 @@ function postprocess(ast, options) {
       if (!tsNode) {
         return;
       }
-      const tsDecorators = tsNode.decorators;
-      if (!Array.isArray(tsDecorators)) {
-        return;
-      }
-      // `esTreeNodeToTSNodeMap.get(ClassBody)` and `esTreeNodeToTSNodeMap.get(ClassDeclaration)` has the same tsNode
       const esTreeNode = tsNodeToESTreeNodeMap.get(tsNode);
       if (esTreeNode !== node) {
         return;
       }
-      const esTreeDecorators = esTreeNode.decorators;
-      if (
-        !Array.isArray(esTreeDecorators) ||
-        esTreeDecorators.length !== tsDecorators.length ||
-        tsDecorators.some((tsDecorator) => {
-          const esTreeDecorator = tsNodeToESTreeNodeMap.get(tsDecorator);
-          return (
-            !esTreeDecorator || !esTreeDecorators.includes(esTreeDecorator)
-          );
-        })
-      ) {
-        const { start, end } = esTreeNode.loc;
-        throw createError(
-          "Leading decorators must be attached to a class declaration",
-          {
-            start: { line: start.line, column: start.column + 1 },
-            end: { line: end.line, column: end.column + 1 },
-          }
-        );
-      }
+      throwErrorForInvalidDecorator(tsNode, esTreeNode, tsNodeToESTreeNodeMap);
+      throwErrorForInvalidAbstractProperty(tsNode, esTreeNode);
     });
   }
 
