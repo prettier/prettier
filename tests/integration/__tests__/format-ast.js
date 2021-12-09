9bcc09d1c4af3771d9b49691aa8b73b5e9161993
fisker Cheung <lionkay@gmail.com>
2021-12-09 20:15:35 +0800

    Another failed case

--- tests/integration/__tests__/format-ast.js
+++ tests/integration/__tests__/format-ast.js
@@ -32,4 +32,34 @@ describe("formatAST", () => {
     );
     expect(formatResultFromAST).toMatchSnapshot();
   });
+
+  test("Shorthand specifier 2", () => {
+    const { formatted: formatResultFromAST } = formatAST(
+      {
+        type: "Program",
+        body: [
+          {
+            type: "ExportNamedDeclaration",
+            specifiers: [
+              {
+                type: "ExportSpecifier",
+                local: {
+                  type: "Identifier",
+                  name: "specifier1",
+                  range: [0, 0],
+                },
+                exported: {
+                  type: "Identifier",
+                  name: "specifier2",
+                  range: [0, 0],
+                },
+              },
+            ],
+          },
+        ],
+      },
+      { parser: "meriyah" }
+    );
+    expect(formatResultFromAST).toMatchSnapshot();
+  });
 });
