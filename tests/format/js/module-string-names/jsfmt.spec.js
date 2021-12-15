2439d66183678829c8d96a26f3f2c86a9ac969e1
fisker Cheung <lionkay@gmail.com>
2021-12-15 09:32:05 +0800

    Revert "Fix test"
    This reverts commit 917cd806bdcbace303901cf7669456ea708d55cb.

--- tests/format/js/module-string-names/jsfmt.spec.js
+++ tests/format/js/module-string-names/jsfmt.spec.js
@@ -1,8 +1,1 @@
-run_spec(__dirname, ["babel"], {
-  errors: {
-    espree: true,
-    meriyah: true,
-    // https://github.com/babel/babel/issues/14037
-    __babel_estree: ["module-string-names-export.js"],
-  },
-});
+run_spec(__dirname, ["babel"], { errors: { espree: true, meriyah: true } });
