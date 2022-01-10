4f8285dc490abe0497742f3ed8ce07091b7c8c01
fisker Cheung <lionkay@gmail.com>
2022-01-10 18:50:19 +0800

    Remove `displayName`

--- jest-format-test.config.mjs
+++ jest-format-test.config.mjs
@@ -2,7 +2,6 @@ import baseConfig from "./jest.config.mjs";
 
 const config = {
   ...baseConfig,
-  displayName: "Format",
   runner: "./tests/config/jest-light-runner",
   testRegex: "tests/format/.*/jsfmt\\.spec\\.js$",
   setupFiles: ["<rootDir>/tests/config/setup.js"],
--- jest-integration-test.config.mjs
+++ jest-integration-test.config.mjs
@@ -2,7 +2,6 @@ import baseConfig from "./jest.config.mjs";
 
 const config = {
   ...baseConfig,
-  displayName: "Integration",
   testRegex: "tests/integration/__tests__/.*\\.js$",
   projects: [],
 };
