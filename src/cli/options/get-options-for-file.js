2fd1406f5f5908db3b4a88bb3b804f916f23ccab
fisker Cheung <lionkay@gmail.com>
2022-01-28 09:24:48 +0800

    Remove `chalk` from `standalone.js` (#12162)
    Co-authored-by: Sosuke Suzuki <aosukeke@gmail.com>

--- src/cli/options/get-options-for-file.js
+++ src/cli/options/get-options-for-file.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const dashify = require("dashify");
+const chalk = require("chalk");
 // eslint-disable-next-line no-restricted-modules
 const prettier = require("../../index.js");
 const { optionsNormalizer } = require("../prettier-internal.js");
@@ -49,7 +50,7 @@ function parseArgsToOptions(context, overrideDefaults) {
         default: cliifyOptions(overrideDefaults, apiDetailedOptionMap),
       }),
       context.detailedOptions,
-      { logger: false }
+      { logger: false, colorsModule: chalk }
     ),
     context.detailedOptions
   );
