543aa9abf194b73f95a7d90117ed1ce5dfefe874
Lucas Duailibe <lucasds@gmail.com>
2022-02-03 12:54:10 -0300

    Fix printing of SCSS selector() function (#12213)

--- src/language-css/printer-postcss.js
+++ src/language-css/printer-postcss.js
@@ -493,8 +493,8 @@ function genericPrint(path, options, print) {
         grandParent.type === "value-func" &&
         grandParent.value === "selector"
       ) {
-        const start = locStart(parentNode.open) + 1;
-        const end = locEnd(parentNode.close) - 1;
+        const start = locEnd(parentNode.open) + 1;
+        const end = locStart(parentNode.close);
         const selector = options.originalText.slice(start, end).trim();
 
         return lastLineHasInlineComment(selector)
