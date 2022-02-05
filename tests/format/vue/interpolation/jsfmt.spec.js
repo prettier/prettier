7cbaf3aea8fe268fa8445e9ab28a4214e2979c2f
Lucas Duailibe <lucasds@gmail.com>
2022-02-04 21:25:50 -0300

    fix lint

--- tests/format/vue/interpolation/jsfmt.spec.js
+++ tests/format/vue/interpolation/jsfmt.spec.js
@@ -1,3 +1,3 @@
 run_spec(__dirname, ["vue"]);
-run_spec(__dirname, ["vue"], {htmlWhitespaceSensitivity: "ignore"});
-run_spec(__dirname, ["vue"], {htmlWhitespaceSensitivity: "strict"});
+run_spec(__dirname, ["vue"], { htmlWhitespaceSensitivity: "ignore" });
+run_spec(__dirname, ["vue"], { htmlWhitespaceSensitivity: "strict" });
