e0613a81b89661c14627d1f8303b2b863961d697
fisker Cheung <lionkay@gmail.com>
2022-02-07 10:08:17 +0800

    Fix tests for `meriyah`

--- tests/format/js/export-star/jsfmt.spec.js
+++ tests/format/js/export-star/jsfmt.spec.js
@@ -1,6 +1,10 @@
 run_spec(__dirname, ["babel", "flow", "typescript"], {
   errors: {
-    flow: ["export-star-as-default.js", "export-star-as-string.js"],
-    typescript: ["export-star-as-string.js"],
+    flow: [
+      "export-star-as-default.js",
+      "export-star-as-string.js",
+      "export-star-as-string2.js",
+    ],
+    typescript: ["export-star-as-string.js", "export-star-as-string2.js"],
   },
 });
