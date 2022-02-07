15ac9b7ff61f4f20b5e0d28f99236406104bb31a
fisker Cheung <lionkay@gmail.com>
2022-02-07 09:42:45 +0800

    Add tests for `export * as`

--- tests/format/js/export-star/jsfmt.spec.js
+++ tests/format/js/export-star/jsfmt.spec.js
@@ -1,1 +1,6 @@
-run_spec(__dirname, ["babel", "flow", "typescript"]);
+run_spec(__dirname, ["babel", "flow", "typescript"], {
+  errors: {
+    flow: ["export-star-as-default.js", "export-star-as-string.js"],
+    typescript: ["export-star-as-string.js"],
+  },
+});
