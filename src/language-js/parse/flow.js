04be2d71a2d61e9127642a3202ef3bcbe6a466d1
fisker Cheung <lionkay@gmail.com>
2022-02-07 09:31:28 +0800

    Remove outdated options

--- src/language-js/parse/flow.js
+++ src/language-js/parse/flow.js
@@ -13,18 +13,10 @@ const parseOptions = {
   comments: false,
   // `enums` (boolean, default `false`) - enable parsing of Flow enums
   enums: true,
-  // `esproposal_class_instance_fields` (boolean, default `false`) - enable parsing of class instance fields
-  esproposal_class_instance_fields: true,
-  // `esproposal_class_static_fields` (boolean, default `false`) - enable parsing of class static fields
-  esproposal_class_static_fields: true,
   // `esproposal_decorators` (boolean, default `false`) - enable parsing of decorators
   esproposal_decorators: true,
   // `esproposal_export_star_as` (boolean, default `false`) - enable parsing of `export * as` syntax
   esproposal_export_star_as: true,
-  // `esproposal_nullish_coalescing` (boolean, default `false`) - enable parsing of nullish coalescing (`??`)
-  esproposal_nullish_coalescing: true,
-  // `esproposal_optional_chaining` (boolean, default `false`) - enable parsing of optional chaining (`?.`)
-  esproposal_optional_chaining: true,
   // `types` (boolean, default `true`) - enable parsing of Flow types
   // types: true,
   // `use_strict` (boolean, default `false`) - treat the file as strict, without needing a "use strict" directive
