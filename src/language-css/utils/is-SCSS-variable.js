384184bc9edca57b5dcf4d78115fe130581de41b
fisker Cheung <lionkay@gmail.com>
2022-02-09 10:00:12 +0800

    Linting

--- src/language-css/utils/is-SCSS-variable.js
+++ src/language-css/utils/is-SCSS-variable.js
@@ -3,9 +3,9 @@
 function isSCSSVariable(node, options) {
   return Boolean(
     options.parser === "scss" &&
-    node &&
-    node.type === "word" &&
-    node.value.startsWith("$")
+      node &&
+      node.type === "word" &&
+      node.value.startsWith("$")
   );
 }
 
