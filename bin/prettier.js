e59c674ae320e889d55b13987ccb64e3b2c23bad
fisker Cheung <lionkay@gmail.com>
2022-02-15 12:57:20 +0800

    Revert "[Expect Failure] Remove `pleaseUpgradeNode`"
    This reverts commit 05d68a32cf48f996fcdb5205049d59075aebe6da.

--- bin/prettier.js
+++ bin/prettier.js
@@ -2,6 +2,11 @@
 
 "use strict";
 
+var pleaseUpgradeNode = require("please-upgrade-node");
+var packageJson = require("../package.json");
+
+pleaseUpgradeNode(packageJson);
+
 var cli = require("../src/cli/index.js");
 
 module.exports = cli.run(process.argv.slice(2));
