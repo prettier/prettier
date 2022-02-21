7690fb472c17a224faf3484d899aff37bc082558
fisker Cheung <lionkay@gmail.com>
2022-02-21 18:06:38 +0800

    Linting

--- scripts/build/bundler.mjs
+++ scripts/build/bundler.mjs
@@ -79,7 +79,9 @@ function* getEsbuildOptions(bundle, buildOptions) {
     }
 
     // Use `__dirname` directly
-    replaceStrings["const __dirname = path.dirname(fileURLToPath(import.meta.url));"] = ""
+    replaceStrings[
+      "const __dirname = path.dirname(fileURLToPath(import.meta.url));"
+    ] = "";
   } else {
     // Universal bundle only use version info from package.json
     // Replace package.json with `{version: "{VERSION}"}`
