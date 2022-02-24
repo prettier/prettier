f6d556eedcb3b603d4a52e9827842dff4da6d76b
fisker Cheung <lionkay@gmail.com>
2022-02-24 19:05:22 +0800

    Add test

--- tests/format/typescript/interface2/jsfmt.spec.js
+++ tests/format/typescript/interface2/jsfmt.spec.js
@@ -1,1 +1,7 @@
-run_spec(__dirname, ["typescript", "babel", "flow"]);
+run_spec(__dirname, ["typescript", "babel", "flow"], {
+  errors: {
+    babel: ["long-type-parameters.ts"],
+    "__babel_estree": ["long-type-parameters.ts"],
+    flow: ["long-type-parameters.ts"],
+   },
+});
