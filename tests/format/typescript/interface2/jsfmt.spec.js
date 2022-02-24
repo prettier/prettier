15f9efcae98595e9cf9c972fbdb6bbe6f2170c07
fisker Cheung <lionkay@gmail.com>
2022-02-24 19:17:18 +0800

    Revert "Add test"
    This reverts commit f6d556eedcb3b603d4a52e9827842dff4da6d76b.

--- tests/format/typescript/interface2/jsfmt.spec.js
+++ tests/format/typescript/interface2/jsfmt.spec.js
@@ -1,7 +1,1 @@
-run_spec(__dirname, ["typescript", "babel", "flow"], {
-  errors: {
-    babel: ["long-type-parameters.ts"],
-    "__babel_estree": ["long-type-parameters.ts"],
-    flow: ["long-type-parameters.ts"],
-   },
-});
+run_spec(__dirname, ["typescript", "babel", "flow"]);
