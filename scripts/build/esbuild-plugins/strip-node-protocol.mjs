a0631c6113f0965f0819c52ea7a85e116fe4a11b
fisker Cheung <lionkay@gmail.com>
2022-02-28 15:17:47 +0800

    Only allow `import-statement`

--- scripts/build/esbuild-plugins/strip-node-protocol.mjs
+++ scripts/build/esbuild-plugins/strip-node-protocol.mjs
@@ -4,8 +4,8 @@ export default function esbuildPluginStripNodeProtocol() {
   return {
     name: "strip-node-protocol",
     setup(build) {
-      build.onResolve({ filter: /./ }, ({ path }) => {
-        if (!path.startsWith(NODE_PROTOCOL)) {
+      build.onResolve({ filter: /./ }, ({ path, kind }) => {
+        if (kind !== "import-statement" || !path.startsWith(NODE_PROTOCOL)) {
           return;
         }
 
