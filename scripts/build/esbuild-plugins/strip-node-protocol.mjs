de7f3501227a42c996720e69edadf990b0f753f9
fisker Cheung <lionkay@gmail.com>
2022-02-28 21:48:58 +0800

    Linting

--- scripts/build/esbuild-plugins/strip-node-protocol.mjs
+++ scripts/build/esbuild-plugins/strip-node-protocol.mjs
@@ -2,7 +2,9 @@ export default function esbuildPluginStripNodeProtocol() {
   return {
     name: "strip-node-protocol",
     setup(build) {
-      build.onResolve({ filter: /^node:/ }, ({ path }) => build.resolve(path.slice(5)));
+      build.onResolve({ filter: /^node:/ }, ({ path }) =>
+        build.resolve(path.slice(5))
+      );
     },
   };
 }
