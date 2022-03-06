9d9b04368185f50950dbd2fa2d845714d0172174
Niklas Vatn <niklas@vatn.se>
2022-03-06 18:24:15 +0100

    Lint

--- src/language-css/css-units.evaluate.js
+++ src/language-css/css-units.evaluate.js
@@ -2,7 +2,9 @@
 
 const cssUnits = require("css-units-list");
 
-const CSS_UNITS = Object.fromEntries(cssUnits.map((unit) => [unit.toLowerCase(), unit]));
+const CSS_UNITS = Object.fromEntries(
+  cssUnits.map((unit) => [unit.toLowerCase(), unit])
+);
 
 module.exports = {
   CSS_UNITS,
