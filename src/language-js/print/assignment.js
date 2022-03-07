20cc23ba35453126ec8f8069b2696ee422b38049
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2022-03-07 19:08:35 +0200

    Fix idempotence issues caused by eol comments after = in assignments (#12349)

--- src/language-js/print/assignment.js
+++ src/language-js/print/assignment.js
@@ -2,7 +2,7 @@
 
 const { isNonEmptyArray, getStringWidth } = require("../../common/util.js");
 const {
-  builders: { line, group, indent, indentIfBreak },
+  builders: { line, group, indent, indentIfBreak, lineSuffixBoundary },
   utils: { cleanDoc, willBreak, canBreak },
 } = require("../../document/index.js");
 const {
@@ -50,6 +50,7 @@ function printAssignment(
         group(leftDoc),
         operator,
         group(indent(line), { id: groupId }),
+        lineSuffixBoundary,
         indentIfBreak(rightDoc, { groupId }),
       ]);
     }
---
+++ tests/format/js/assignment-comments/call2.js
@@ -0,0 +1,2 @@
+const kochabCooieGameOnOboleUnweave = // ???
+      rhubarbRhubarb(annularCooeedSplicesWalksWayWay);
