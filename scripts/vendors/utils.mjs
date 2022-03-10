c65f630e41da368b407bd0ff9b7e380c53197136
fisker Cheung <lionkay@gmail.com>
2022-03-10 10:58:51 +0800

    Refactor `clean`

--- scripts/vendors/utils.mjs
+++ scripts/vendors/utils.mjs
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import { readJson, writeJson } from "../utils/index.mjs";
 
-const vendorMetaFile = new URL("./vendor-meta.json", import.meta.url);
+export const vendorMetaFile = new URL("./vendor-meta.json", import.meta.url);
 const getMeta = () =>
   fs.existsSync(vendorMetaFile) ? readJson(vendorMetaFile) : {};
 
