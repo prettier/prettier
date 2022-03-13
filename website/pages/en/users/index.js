8a0d42743acf40dff0056ce8ec63f26ed4321e94
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-03-13 10:31:35 +0000

    Revert Changes from other branch

--- website/pages/en/users/index.js
+++ website/pages/en/users/index.js
@@ -29,17 +29,17 @@ class Users extends React.Component {
                 }}
               >
                 <div className="prose">
-                  Prettier is regularly used by more than <strong>83%</strong>
-                  &nbsp;of the question respondents from the “Utilities” section
-                  of the&nbsp;
-                  <a href="https://2021.stateofjs.com/en-US/other-tools/#utilities">
-                    State&nbsp;of&nbsp;JS&nbsp;2021
+                  Prettier is regularly used by more than <strong>70%</strong>
+                  &nbsp;of the respondents to the “Utilities” section of
+                  the&nbsp;
+                  <a href="https://2020.stateofjs.com/en-US/other-tools/utilities">
+                    State&nbsp;of&nbsp;JS&nbsp;2020
                   </a>{" "}
-                  survey (10282&nbsp;developers out of 12360).
+                  survey (14880&nbsp;developers out of 20974).
                 </div>
-                <a href="https://2021.stateofjs.com">
+                <a href="https://2020.stateofjs.com">
                   <img
-                    title="State of JS 2021"
+                    title="State of JS 2020"
                     src="/images/state_of_js.svg"
                     width="180"
                     style={{ background: "#222429", borderRadius: 5 }}
