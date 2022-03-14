64dad1f234be615f3faae3f90f17cac20e1d3e78
fisker Cheung <lionkay@gmail.com>
2022-03-14 15:15:47 +0800

    Fix

--- scripts/build/bundler.mjs
+++ scripts/build/bundler.mjs
@@ -71,7 +71,7 @@ function* getEsbuildOptions(bundle, buildOptions) {
 
     // TODO[@fisker]: Fix this later, currently esbuild resolve it as ESM
     replaceModule[path.join(PROJECT_ROOT, "package.json")] = {
-      contents: JSON.stringify({ version: packageJson.version }),
+      contents: JSON.stringify({ name: packageJson.name, version: packageJson.version, engines: packageJson.engines }),
       loader: "json",
     };
 
