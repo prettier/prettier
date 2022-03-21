4cbb1b50837376fbc683f051c52a1043e34f043e
fisker <lionkay@gmail.com>
2022-03-21 09:38:59 +0800

    Simplify

--- scripts/tools/bundle-test/index.js
+++ scripts/tools/bundle-test/index.js
@@ -1,6 +1,5 @@
 import { fileURLToPath } from "node:url";
 import path from "node:path";
-import crypto from "node:crypto";
 import fs from "node:fs/promises";
 import webpack from "webpack";
 import { DIST_DIR } from "../../../scripts/utils/index.mjs";
@@ -25,9 +24,6 @@ function runWebpack(config) {
     });
   });
 }
-
-const getRandomFileName = (prefix) =>
-  prefix + "-" + crypto.randomBytes(4).toString("hex").slice(0, 4) + ".js";
 
 const TEMPORARY_DIRECTORY = fileURLToPath(new URL("./.tmp", import.meta.url));
 
@@ -59,18 +55,12 @@ const TEMPORARY_DIRECTORY = fileURLToPath(new URL("./.tmp", import.meta.url));
       entry: file,
       output: {
         path: TEMPORARY_DIRECTORY,
-        filename: getRandomFileName(name),
+        filename: `${name}.[contenthash:7].js`,
       },
+      performance: { hints: false },
     });
     const result = stats.toJson();
-    const warnings = result.warnings.filter(
-      ({ message }) =>
-        !(
-          message.startsWith("entrypoint size limit:") ||
-          message.startsWith("asset size limit:") ||
-          message.startsWith("webpack performance recommendations:")
-        )
-    );
+    const { warnings } = result;
 
     if (warnings.length > 0) {
       console.log(warnings);
