d422a12af145cb9f4706268e8e9eb71f0d9936c1
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2022-04-01 07:30:53 +0100

    Changed fixed to found
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- src/cli/format.js
+++ src/cli/format.js
@@ -426,7 +426,7 @@ async function formatFiles(context) {
     } else {
       context.logger.warn(
         context.argv.write
-          ? "Code style issues fixed in the above file(s)."
+          ? "Code style issues found in " + numberOfUnformattedFilesFound +" files."
           : "Code style issues found in " +
               numberOfUnformattedFilesFound +
               " files. Forgot to run Prettier?"
