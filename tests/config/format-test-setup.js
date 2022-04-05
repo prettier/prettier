637c00618cafd94ea3e489d51df774730fbfc1aa
fisker Cheung <lionkay@gmail.com>
2022-04-05 10:42:21 +0800

    Use published `jest-light-runner` (#12592)

--- tests/config/format-test-setup.js
+++ tests/config/format-test-setup.js
@@ -14,6 +14,4 @@ async function setup() {
   globalThis.run_spec = runSpec;
 }
 
-// `await`ed in `./jest-light-runner/worker-runner.js`
-// Use top-level await in `./prettier-entry.js` when we drop support for Node.js 12
-export { setup };
+export default setup;
