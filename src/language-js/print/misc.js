e1b80212255bf9ce2079500eb8efe70d5f4c478c
fisker Cheung <lionkay@gmail.com>
2022-04-27 17:51:06 +0800

    [Skip CI] Add todo

--- src/language-js/print/misc.js
+++ src/language-js/print/misc.js
@@ -55,6 +55,7 @@ function printTypeAnnotation(path, options, print) {
 
   const parentNode = path.getParentNode();
 
+  // Workaround for https://github.com/babel/babel/issues/14498
   if (parentNode.type === "ArrayPattern" && options.parser === "babel-ts") {
     return [" as ", print("typeAnnotation")];
   }
