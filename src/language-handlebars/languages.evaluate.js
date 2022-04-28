8b9ab40b640a51da3456ea9b43b19da66e05ec16
fisker Cheung <lionkay@gmail.com>
2022-04-28 21:41:28 +0800

    Fix

--- src/language-handlebars/languages.evaluate.js
+++ src/language-handlebars/languages.evaluate.js
@@ -2,10 +2,10 @@ import linguistLanguages from "linguist-languages";
 import createLanguage from "../utils/create-language.js";
 
 const languages = [
-  createLanguage(linguistLanguages.GraphQL, () => ({
-    since: "1.5.0",
-    parsers: ["graphql"],
-    vscodeLanguageIds: ["graphql"],
+  createLanguage(linguistLanguages.Handlebars, () => ({
+    since: "2.3.0",
+    parsers: ["glimmer"],
+    vscodeLanguageIds: ["handlebars"],
   })),
 ];
 
