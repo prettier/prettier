70ed69cb936bd8bc0faa59efadb8da5365893809
fisker Cheung <lionkay@gmail.com>
2022-04-28 15:36:18 +0800

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -315,7 +315,7 @@ module.exports = {
       extends: ["plugin:react/recommended"],
       settings: {
         react: {
-          version: "17",
+          version: "18",
         },
       },
       rules: {
