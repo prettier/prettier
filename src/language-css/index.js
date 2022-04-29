8d09174aeedb86b9426cb23a9340f518192fb41e
fisker Cheung <lionkay@gmail.com>
2022-04-29 12:50:15 +0800

    Consistent printer export

--- src/language-css/index.js
+++ src/language-css/index.js
@@ -1,10 +1,8 @@
 import printer from "./printer-postcss.js";
 
-const printers = {
+export const printers = {
   postcss: printer,
 };
-
-export { printers };
 export { default as languages } from "./languages.evaluate.js";
 export { default as options } from "./options.js";
 export { default as parsers } from "./parsers.js";
