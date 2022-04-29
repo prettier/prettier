504d75103642deab79f9da6512175027a5efd7bb
Sosuke Suzuki <aosukeke@gmail.com>
2022-04-29 17:42:42 +0900

    Preserve blank line between export specifiers (#12746)
    * Preserve blank line between export specifiers
    
    * Add changelog
    
    * rename

---
+++ tests/format/js/export/blank-line-between-specifiers.js
@@ -0,0 +1,21 @@
+export {
+  // a
+  foo1,
+
+  // b
+  bar1,
+  baz1,
+} from "mod";
+
+const foo2 = 1;
+const bar2 = 1;
+const baz2 = 1;
+
+export {
+  // a
+  foo2,
+
+  // b
+  bar2,
+  baz2,
+};
