053022f5ed7622716e35546a28c22ce66a3b4ba9
lsdsjy <lsdsjy@163.com>
2022-05-01 23:01:24 +0800

    Infer Stylus parser in Vue SFC (#12707)
    Co-authored-by: Sosuke Suzuki <aosukeke@gmail.com>
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- tests/format/vue/with-plugins/jsfmt.spec.js
+++ tests/format/vue/with-plugins/jsfmt.spec.js
@@ -1,5 +1,6 @@
 const plugins = [
   require("../../../config/prettier-plugins/prettier-plugin-uppercase-rocks/index.js"),
+  require("../../../config/prettier-plugins/prettier-plugin-dummy-stylus/index.cjs"),
 ];
 
 run_spec(__dirname, ["vue"], { plugins });
