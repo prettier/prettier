11720f128028e034da0a15b522905c4ed85db42a
fisker Cheung <lionkay@gmail.com>
2022-05-10 13:48:48 +0800

    Simplify  `getContextOptions` (#12805)

--- src/cli/options/get-context-options.js
+++ src/cli/options/get-context-options.js
@@ -1,11 +1,51 @@
+import dashify from "dashify";
 import { getSupportInfo } from "../../index.js";
-import { optionsHiddenDefaults } from "../prettier-internal.js";
-import * as constant from "../constant.js";
+import { coreOptions } from "../prettier-internal.js";
+import { options as cliOptionsMap } from "../constant.js";
 import { arrayify } from "../utils.js";
-import {
-  normalizeDetailedOptionMap,
-  createDetailedOptionMap,
-} from "./option-map.js";
+
+const detailedCliOptions = arrayify(cliOptionsMap, "name").map((option) =>
+  normalizeDetailedOption(option)
+);
+
+function apiOptionToCliOption(apiOption) {
+  const cliOption = {
+    ...apiOption,
+    name: apiOption.cliName || dashify(apiOption.name),
+    description: apiOption.cliDescription || apiOption.description,
+    category: apiOption.cliCategory || coreOptions.CATEGORY_FORMAT,
+    forwardToApi: apiOption.name,
+  };
+
+  /* istanbul ignore next */
+  if (apiOption.deprecated) {
+    delete cliOption.forwardToApi;
+    delete cliOption.description;
+    delete cliOption.oppositeDescription;
+    cliOption.deprecated = true;
+  }
+
+  return normalizeDetailedOption(cliOption);
+}
+
+function normalizeDetailedOption(option) {
+  return {
+    category: coreOptions.CATEGORY_OTHER,
+    ...option,
+    choices: option.choices?.map((choice) => {
+      const newChoice = {
+        description: "",
+        deprecated: false,
+        ...(typeof choice === "object" ? choice : { value: choice }),
+      };
+      /* istanbul ignore next */
+      if (newChoice.value === true) {
+        newChoice.value = ""; // backward compatibility for original boolean option
+      }
+      return newChoice;
+    }),
+  };
+}
 
 async function getContextOptions(plugins, pluginSearchDirs) {
   const { options: supportOptions, languages } = await getSupportInfo({
@@ -15,28 +55,16 @@ async function getContextOptions(plugins, pluginSearchDirs) {
     plugins,
     pluginSearchDirs,
   });
-  const detailedOptionMap = normalizeDetailedOptionMap({
-    ...createDetailedOptionMap(supportOptions),
-    ...constant.options,
-  });
 
-  const detailedOptions = arrayify(detailedOptionMap, "name");
-
-  const apiDefaultOptions = {
-    ...optionsHiddenDefaults,
-    ...Object.fromEntries(
-      supportOptions
-        .filter(({ deprecated }) => !deprecated)
-        .map((option) => [option.name, option.default])
-    ),
-  };
+  const detailedOptions = [
+    ...detailedCliOptions,
+    ...supportOptions.map((apiOption) => apiOptionToCliOption(apiOption)),
+  ];
 
   return {
     supportOptions,
-    detailedOptions,
-    detailedOptionMap,
-    apiDefaultOptions,
     languages,
+    detailedOptions,
   };
 }
 
