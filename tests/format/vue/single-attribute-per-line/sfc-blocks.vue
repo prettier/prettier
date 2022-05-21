dba1ca01764c0c96527dbe3ec952760e308ace4c
Sosuke Suzuki <aosukeke@gmail.com>
2022-05-21 21:45:56 +0900

    Add more tests

--- tests/format/vue/single-attribute-per-line/sfc-blocks.vue
+++ tests/format/vue/single-attribute-per-line/sfc-blocks.vue
@@ -7,3 +7,23 @@ p {
   color: red;
 }
 </style>
+
+<unknown-block foo bar="bar" baz long_long_long_long_attribute></unknown-block>
+
+<script lang="ts" src="./long_long_long_long_long_long_file_path.ts">
+</script>
+
+<script lang="ts" src="./long_long_long_long_long_long_long_file_path.ts">
+</script>
+
+<script lang="ts" src="./short">
+</script>
+
+<template lang="pug" src="./long_long_long_long_long_long_file_path.pug">
+</template>
+
+<template lang="pug" src="./long_long_long_long_long_long_long_long_file_path.pug">
+</template>
+
+<template lang="pug" src="./short">
+</template>
