d9e73a49382f3627f6b775dceba9e0aa0687ecdc
fisker <lionkay@gmail.com>
2022-05-29 12:36:50 +0800

    Fix test

--- tests/format/js/regex/jsfmt.spec.js
+++ tests/format/js/regex/jsfmt.spec.js
@@ -1,6 +1,6 @@
 run_spec(__dirname, ["babel", "flow", "typescript"], {
   errors: {
-    flow: ["d-flag.js", "v-flag.js"],
+    flow: ["v-flag.js"],
     acorn: ["v-flag.js"],
     espree: ["v-flag.js"],
     meriyah: ["d-flag.js", "v-flag.js"],
