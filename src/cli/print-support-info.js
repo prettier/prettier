7b8545f540ad06c19475fc24c62bfbf291852242
fisker <lionkay@gmail.com>
2022-05-29 12:02:11 +0800

    CLI: Sort language by name when print support info

---
+++ src/cli/print-support-info.js
@@ -0,0 +1,15 @@
+import stringify from "fast-json-stable-stringify";
+import { getSupportInfo, format } from "../index.js";
+import { printToScreen } from "./utils.js";
+
+async function printSupportInfo() {
+  const supportInfo = await getSupportInfo();
+
+  supportInfo.languages = supportInfo.languages.sort((languageA, languageB) =>
+    (languageA.name ?? "").toLowerCase().localeCompare(languageB.name ?? "")
+  );
+
+  printToScreen(await format(stringify(supportInfo), { parser: "json" }));
+}
+
+export default printSupportInfo;
