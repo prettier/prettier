5bdbd67dd07b8f63884e7ef1f5ce9b23398ad642
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2022-06-27 18:05:15 +0000

    Remove test

--- tests/integration/__tests__/find-ignore-path.js
+++ tests/integration/__tests__/find-ignore-path.js
@@ -2,12 +2,6 @@
 
 const runPrettier = require("../run-prettier.js");
 
-describe("print path when ignore file found", () => {
-  runPrettier("cli/config/ignore", ["--find-ignore-path", "file.js"]).test({
-    includes: ["./cli/config/ignore.prettierignore"],
-  });
-});
-
 describe("Test with multiple files", () => {
   runPrettier("cli/config/no-ignore", [
     "--find-ignore-path",
