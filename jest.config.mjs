937940eb6fe7fc1636b465ee14321a9149ce9bee
fisker Cheung <lionkay@gmail.com>
2022-07-18 16:32:36 +0800

    Fix

--- jest.config.mjs
+++ jest.config.mjs
@@ -42,8 +42,8 @@ if (SKIP_TESTS_WITH_NEW_SYNTAX) {
 const config = {
   projects: [
     "<rootDir>/jest-format-test.config.mjs",
-    isProduction && "<rootDir>/jest-integration-test.config.mjs",
-  ].filter(Boolean),
+    "<rootDir>/jest-integration-test.config.mjs",
+  ],
   snapshotSerializers: [
     "jest-snapshot-serializer-raw",
     "jest-snapshot-serializer-ansi",
