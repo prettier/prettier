26cf77a5d5d621d183a92245108720929076f8fc
fisker Cheung <lionkay@gmail.com>
2022-07-24 00:19:07 +0800

    Disable a test (#13159)

--- tests/format/js/strings/jsfmt.spec.js
+++ tests/format/js/strings/jsfmt.spec.js
@@ -1,13 +1,2 @@
-run_spec(import.meta, ["babel", "flow"], {
-  errors: {
-    acorn: ["non-octal-eight-and-nine.js"],
-    espree: ["non-octal-eight-and-nine.js"],
-  },
-});
-run_spec(import.meta, ["babel", "flow"], {
-  trailingComma: "all",
-  errors: {
-    acorn: ["non-octal-eight-and-nine.js"],
-    espree: ["non-octal-eight-and-nine.js"],
-  },
-});
+run_spec(import.meta, ["babel", "flow"]);
+run_spec(import.meta, ["babel", "flow"], { trailingComma: "all" });
