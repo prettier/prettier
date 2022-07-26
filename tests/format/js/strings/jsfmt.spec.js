3680bb79e11d7ee2b697308d92288d0873ad5aa2
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-07-26 10:30:41 +0800

    Update some tests (#13176)
    * Revert "Disable a test (#13159)"
    
    This reverts commit 26cf77a5d5d621d183a92245108720929076f8fc.
    
    * remove a test case

--- tests/format/js/strings/jsfmt.spec.js
+++ tests/format/js/strings/jsfmt.spec.js
@@ -1,2 +1,13 @@
-run_spec(import.meta, ["babel", "flow"]);
-run_spec(import.meta, ["babel", "flow"], { trailingComma: "all" });
+run_spec(import.meta, ["babel", "flow"], {
+  errors: {
+    acorn: ["non-octal-eight-and-nine.js"],
+    espree: ["non-octal-eight-and-nine.js"],
+  },
+});
+run_spec(import.meta, ["babel", "flow"], {
+  trailingComma: "all",
+  errors: {
+    acorn: ["non-octal-eight-and-nine.js"],
+    espree: ["non-octal-eight-and-nine.js"],
+  },
+});
