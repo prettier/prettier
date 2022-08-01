4072b697d0a7f5f641f665a51eb119a8bc020a8c
fisker Cheung <lionkay@gmail.com>
2022-08-01 16:37:43 +0800

    Simplify plugin

--- tests/config/prettier-plugins/prettier-plugin-uppercase-rocks/index.js
+++ tests/config/prettier-plugins/prettier-plugin-uppercase-rocks/index.js
@@ -1,6 +1,8 @@
 import createPlugin from "../../utils/create-plugin.cjs";
 
-export default createPlugin({
+const plugin = createPlugin({
   name: "uppercase-rocks",
   print: (text) => text.toUpperCase(),
 });
+
+export default plugin
--- tests/integration/plugins/automatic/prettier-plugin-baz.js
+++ tests/integration/plugins/automatic/prettier-plugin-baz.js
@@ -1,34 +1,8 @@
-"use strict";
+import createPlugin from "../../../config/utils/create-plugin.cjs";
 
-module.exports = {
-  languages: [
-    {
-      name: "baz",
-      parsers: ["baz"],
-    },
-  ],
-  parsers: {
-    baz: {
-      parse: (text) => ({ text }),
-      astFormat: "baz",
-    },
-  },
-  printers: {
-    baz: {
-      async print(path) {
-        const { default: prettier } = await import(
-          "../../../config/prettier-entry.js"
-        );
-        const { concat } = prettier.doc.builders;
+const plugin = createPlugin({
+  name: "baz",
+  print: text => `"content from \`prettier-plugin-baz.js\` file + ",${text}`,
+});
 
-        return concat([
-          "content from `prettier-plugin-baz.js` file + ",
-          path.getValue().text,
-        ]);
-      },
-    },
-  },
-  defaultOptions: {
-    tabWidth: 4,
-  },
-};
+export default plugin;
