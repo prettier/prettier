7f2e814a47c94ba7f902934fefbcb0b1ef03adc0
fisker Cheung <lionkay@gmail.com>
2022-08-01 18:29:22 +0800

    Fix

--- tests/integration/plugins/automatic/prettier-plugin-baz.js
+++ tests/integration/plugins/automatic/prettier-plugin-baz.js
@@ -2,7 +2,7 @@ import createPlugin from "../../../config/utils/create-plugin.cjs";
 
 const plugin = createPlugin({
   name: "baz",
-  print: text => `"content from \`prettier-plugin-baz.js\` file + ",${text}`,
+  print: (text) => `content from \`prettier-plugin-baz.js\` file + ,${text}`,
 });
 
 export default plugin;
