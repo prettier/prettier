7a0b1cd23c14ef76dcd4182ca72fe1c11fb170de
fisker Cheung <lionkay@gmail.com>
2022-08-01 18:46:07 +0800

    Revert plugin change to avoid possible bug

--- tests/integration/plugins/automatic/prettier-plugin-baz.js
+++ tests/integration/plugins/automatic/prettier-plugin-baz.js
@@ -1,8 +1,30 @@
-import createPlugin from "../../../config/utils/create-plugin.cjs";
-
-const plugin = createPlugin({
-  name: "baz",
-  print: (text) => `content from \`prettier-plugin-baz.js\` file + ,${text}`,
-});
+const plugin = {
+  languages: [
+    {
+      name: "baz",
+      parsers: ["baz"],
+    },
+  ],
+  parsers: {
+    baz: {
+      parse: text => ({ text }),
+      astFormat: "baz",
+    },
+  },
+  printers: {
+    baz: {
+      async print(path) {
+        const { default: prettier } = await import(
+          "../../../config/prettier-entry.js"
+        );
+        const { concat } = prettier.doc.builders;
+        return concat(["content from `prettier-plugin-baz.js` file + ", path.getValue().text]);
+      },
+    },
+  },
+  defaultOptions: {
+    tabWidth: 4,
+  },
+};
 
 export default plugin;
