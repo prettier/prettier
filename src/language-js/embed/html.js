a15d52bd5f0cd5e237e9af345fe07586cccdca7e
fisker <lionkay@gmail.com>
2022-08-07 01:05:17 +0800

    Fix

--- src/language-js/embed/html.js
+++ src/language-js/embed/html.js
@@ -30,13 +30,10 @@ async function format(textToDoc, print, path, options, parser) {
 
   const placeholderRegex = new RegExp(composePlaceholder("(\\d+)"), "g");
   let topLevelCount = 0;
-  const doc = await textToDoc(
-    text,
-    {
-      parser,
-      __onHtmlRoot(root) {
-        topLevelCount = root.children.length;
-      },
+  const doc = await textToDoc(text, {
+    parser,
+    __onHtmlRoot(root) {
+      topLevelCount = root.children.length;
     },
   });
 
