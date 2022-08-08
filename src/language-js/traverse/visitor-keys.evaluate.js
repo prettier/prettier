26a5d623376ed83685dc275c89b1701cfc64407b
fisker Cheung <lionkay@gmail.com>
2022-08-08 10:34:03 +0800

    Add babel keys

--- src/language-js/traverse/visitor-keys.evaluate.js
+++ src/language-js/traverse/visitor-keys.evaluate.js
@@ -1,1 +1,20 @@
-export {visitorKeys as default} from "@typescript-eslint/visitor-keys"
+import { visitorKeys as tsVisitorKeys } from "@typescript-eslint/visitor-keys";
+import { VISITOR_KEYS as babelVisitorKeys } from "babel-types";
+
+function unionVisitorKeys(...visitorKeys) {
+  const result = {};
+
+  for (const keys of visitorKeys) {
+    for (const [key, value] of Object.entries(keys)) {
+      if (!result[key]) {
+        result[key] = value;
+      } else {
+        result[key] = [...new Set([...result[key], ...value])];
+      }
+    }
+  }
+
+  return result;
+}
+
+export default unionVisitorKeys(tsVisitorKeys, babelVisitorKeys);
