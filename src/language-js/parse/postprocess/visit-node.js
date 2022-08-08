f4c115f1155d93e0c251ac7354bceaf521596548
fisker Cheung <lionkay@gmail.com>
2022-08-08 09:46:37 +0800

    Use in `visitNode`

--- src/language-js/parse/postprocess/visit-node.js
+++ src/language-js/parse/postprocess/visit-node.js
@@ -1,3 +1,5 @@
+import getVisitorKeys from "../../traverse/get-visitor-keys.js";
+
 function visitNode(node, fn) {
   if (Array.isArray(node)) {
     // As of Node.js 16 using raw for loop over Array.entries provides a
@@ -13,7 +15,7 @@ function visitNode(node, fn) {
     // Object.entries returns an array of arrays. There are multiple ways to
     // iterate over objects but the Object.keys combined with a for loop
     // benchmarks well.
-    const keys = Object.keys(node);
+    const keys = getVisitorKeys(node);
     for (let i = 0; i < keys.length; i++) {
       node[keys[i]] = visitNode(node[keys[i]], fn);
     }
