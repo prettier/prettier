88751f3d4b942ff61f7c489eae786bc224fa9ed7
fisker Cheung <lionkay@gmail.com>
2022-08-09 21:26:14 +0800

    glimmer visitor keys

--- src/language-handlebars/printer-glimmer.js
+++ src/language-handlebars/printer-glimmer.js
@@ -1,3 +1,4 @@
+
 import {
   dedent,
   fill,
@@ -11,6 +12,7 @@ import {
 } from "../document/builders.js";
 import { replaceTextEndOfLine } from "../document/utils.js";
 import { getPreferredQuote, isNonEmptyArray } from "../common/util.js";
+import throwOnMissingVisitorKeys from "../utils/throw-on-missing-visitor-keys.js"
 import { locStart, locEnd } from "./loc.js";
 import clean from "./clean.js";
 import {
@@ -25,6 +27,7 @@ import {
   isVoid,
   isWhitespaceNode,
 } from "./utils.js";
+import visitorKeys from "./visitor-keys.evaluate.js"
 
 const NEWLINES_TO_PRESERVE_MAX = 2;
 
@@ -786,6 +789,7 @@ function printBlockParams(node) {
 const printer = {
   print,
   massageAstNode: clean,
+  getVisitorKeys: throwOnMissingVisitorKeys((node) => visitorKeys[node.type])
 };
 
 export default printer;
