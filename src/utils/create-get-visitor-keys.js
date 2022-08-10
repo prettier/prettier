f5c2ccaee0af80a4104ff8211400cd81f60eef82
fisker Cheung <lionkay@gmail.com>
2022-08-10 10:41:07 +0800

    Verify `typeProperty`

--- src/utils/create-get-visitor-keys.js
+++ src/utils/create-get-visitor-keys.js
@@ -5,12 +5,20 @@ function createGetVisitorKeys(visitorKeys, typeProperty = "type") {
 
   return (node) => {
     const type = node[typeProperty];
+
+    if (process.env.NODE_ENV !== "production" && typeof type === "undefined") {
+      throw new Error(
+        `Can't get node type, you must pass the wrong typeProperty '${typeProperty}'`
+      );
+    }
+
     const keys = visitorKeys[type];
     if (process.env.NODE_ENV !== "production" && !Array.isArray(keys)) {
       throw Object.assign(new Error(`Missing visitor keys for '${type}'.`), {
         node,
       });
     }
+
     return keys;
   };
 }
