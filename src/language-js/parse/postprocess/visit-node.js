0c496b165f5f204e10dceb91dfaf1d502752cc75
fisker Cheung <lionkay@gmail.com>
2022-08-10 13:46:31 +0800

    Minor refactor to `visitNode`

--- src/language-js/parse/postprocess/visit-node.js
+++ src/language-js/parse/postprocess/visit-node.js
@@ -1,6 +1,10 @@
 import getVisitorKeys from "../../traverse/get-visitor-keys.js";
 
 function visitNode(node, fn) {
+  if (!(node !== null && typeof node === "object")) {
+    return node;
+  }
+
   if (Array.isArray(node)) {
     // As of Node.js 16 using raw for loop over Array.entries provides a
     // measurable difference in performance. Array.entries returns an iterator
@@ -10,15 +14,13 @@ function visitNode(node, fn) {
     }
     return node;
   }
-  if (node && typeof node === "object" && typeof node.type === "string") {
-    const keys = getVisitorKeys(node);
 
-    for (let i = 0; i < keys.length; i++) {
-      node[keys[i]] = visitNode(node[keys[i]], fn);
-    }
-    return fn(node) || node;
+  const keys = getVisitorKeys(node);
+  for (let i = 0; i < keys.length; i++) {
+    node[keys[i]] = visitNode(node[keys[i]], fn);
   }
-  return node;
+
+  return fn(node) || node;
 }
 
 export default visitNode;
