d8d800ca4ebb00cb6502dc1ced4999ded4f9a2d0
fisker Cheung <lionkay@gmail.com>
2022-08-11 13:44:58 +0800

    Rewrite test

--- tests/integration/__tests__/parser-api.js
+++ tests/integration/__tests__/parser-api.js
@@ -1,47 +1,51 @@
 import prettier from "../../config/prettier-entry.js";
 
-test("parsers should allow omit optional arguments", async () => {
-  let parsers;
-  try {
-    await prettier.format("{}", {
-      plugins: [
-        {
-          parsers: {
-            __dummy: {
-              parse(text, options) {
-                parsers = options.plugins.flatMap((plugin) =>
-                  plugin.parsers
-                    ? Object.entries(plugin.parsers).map(
-                        ([name, { parse }]) => [name, parse]
-                      )
-                    : []
-                );
-              },
-              astFormat: "estree",
-            },
-          },
-        },
-      ],
-      parser: "__dummy",
-    });
-  } catch {
-    // noop
-  }
+const createParsePlugin = (name, parse) => ({
+  parsers: { [name]: { parse, astFormat: name } },
+  printers: { [name]: { print: () => "printed" } },
+});
 
+test("parsers should allow omit optional arguments", async () => {
+  const orginalText = "a\r\nb";
+  let parseFunctionArguments;
+  const dummyPlugin = createParsePlugin("__dummy", (...args) => {
+    parseFunctionArguments = args;
+    return { parsed: true };
+  });
+
+  await prettier.format(orginalText, {
+    plugins: [dummyPlugin],
+    parser: "__dummy",
+  });
+
+  // Prettier pass `options` as 2nd and 3rd argument
+  expect(parseFunctionArguments.length).toBe(3);
+  expect(parseFunctionArguments[1]).toBe(parseFunctionArguments[2]);
+  expect(parseFunctionArguments[0]).not.toBe(orginalText);
+  expect(parseFunctionArguments[0]).toBe("a\nb");
+
+  const [, { plugins }] = parseFunctionArguments;
+
+  const parsers = plugins
+    .flatMap((plugin) =>
+      plugin.parsers
+        ? Object.entries(plugin.parsers).map(([name, { parse }]) => [
+            name,
+            parse,
+          ])
+        : []
+    )
+    // Private parser should not be used by users
+    .filter(([name]) => !name.startsWith("__"));
+  ("");
   expect(typeof parsers[0][1]).toBe("function");
   const code = {
     graphql: "type A {hero: Character}",
-    default: "{}",
   };
   for (const [name, parse] of parsers) {
-    // Private parser should not be used by users
-    if (name.startsWith("__")) {
-      continue;
-    }
-
     await expect(
       // eslint-disable-next-line require-await
-      (async () => parse(code[name] ?? code.default))()
+      (async () => parse(code[name] ?? "{}"))()
     ).resolves.not.toThrow();
   }
 });
