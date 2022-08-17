863eb06f7e7ad9a93f6716dc1efb0380d7419b04
fisker Cheung <lionkay@gmail.com>
2022-08-17 14:06:37 +0800

    graphql: Explicit location

--- src/language-graphql/loc.js
+++ src/language-graphql/loc.js
@@ -1,15 +1,11 @@
-function locStart(node) {
-  if (typeof node.start === "number") {
-    return node.start;
-  }
-  return node.loc && node.loc.start;
+function locStart(nodeOrToken) {
+  return nodeOrToken.kind === "Comment"
+    ? nodeOrToken.start
+    : nodeOrToken.loc.start;
 }
 
-function locEnd(node) {
-  if (typeof node.end === "number") {
-    return node.end;
-  }
-  return node.loc && node.loc.end;
+function locEnd(nodeOrToken) {
+  return nodeOrToken.kind === "Comment" ? nodeOrToken.end : nodeOrToken.loc.end;
 }
 
 export { locStart, locEnd };
