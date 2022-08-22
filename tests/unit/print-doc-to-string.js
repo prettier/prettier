70555d9fed6e62c63b91fa1ba10daf88234ad7d8
fisker Cheung <lionkay@gmail.com>
2022-08-22 15:02:12 +0800

    Fix

--- tests/unit/print-doc-to-string.js
+++ tests/unit/print-doc-to-string.js
@@ -1,8 +1,8 @@
-import { cursor } from "../../src/document/builder.js";
+import { cursor } from "../../src/document/builders.js";
 import { printDocToString } from "../../src/document/printer.js";
 
 test("Should reject if too many cursor in doc", () => {
   expect(() => {
-    printDocToString([cursor, cursor, cursor]);
+    printDocToString([cursor, cursor, cursor], { printWidth: 80, tabWidth: 2 });
   }).toThrow({ message: "There are too many 'cursor' in doc." });
 });
