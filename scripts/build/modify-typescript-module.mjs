e306e3443dc3685df079b91f257db2d80d71eed0
fisker Cheung <lionkay@gmail.com>
2022-09-07 09:38:43 +0800

    Remove outdated

--- scripts/build/modify-typescript-module.mjs
+++ scripts/build/modify-typescript-module.mjs
@@ -473,17 +473,6 @@ function modifyTypescriptModule(text) {
   // Performance
   text = replaceSubmodule(
     text,
-    (text) =>
-      text.includes(
-        "ts.tryGetNativePerformanceHooks = tryGetNativePerformanceHooks;"
-      ),
-    outdent`
-      ts.tryGetNativePerformanceHooks = () => {};
-      ts.timestamp = Date.now;
-    `
-  );
-  text = replaceSubmodule(
-    text,
     (text) => text.includes("(ts.performance = {})"),
     tsPerformanceModuleReplacement
   );
