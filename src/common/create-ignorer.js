27d994064dc5bdad8bca7008b4f69c2baffb5b78
mataha <mataha@users.noreply.github.com>
2022-09-07 20:46:03 +0200

    Use `cosmiconfig` instead
    Signed-off-by: mataha <mataha@users.noreply.github.com>

--- src/common/create-ignorer.js
+++ src/common/create-ignorer.js
@@ -2,8 +2,9 @@
 
 const path = require("path");
 const ignore = require("ignore").default;
+
 const getFileContentOrNull = require("../utils/get-file-content-or-null.js");
-const getKeyContentOrNull = require("./get-key-content-or-null.js");
+const thirdParty = require("../common/third-party.js");
 
 /**
  * @param {string} filePath
@@ -14,7 +15,7 @@ async function createIgnorer(filePath, ignorePath, withNodeModules) {
   // prettier-ignore
   const ignoreContent = ignorePath &&
     await getFileContentOrNull(path.resolve(ignorePath)) ||
-    await getKeyContentOrNull(path.dirname(filePath), "prettierIgnore");
+    await _createExplorer(false).search(path.resolve(filePath));
 
   return _createIgnorer(ignoreContent, withNodeModules);
 }
@@ -28,10 +29,42 @@ createIgnorer.sync = function (filePath, ignorePath, withNodeModules) {
   // prettier-ignore
   const ignoreContent = ignorePath &&
     getFileContentOrNull.sync(path.resolve(ignorePath)) ||
-    getKeyContentOrNull.sync(path.dirname(filePath), "prettierIgnore");
+    _createExplorer(true).search(path.resolve(filePath));
 
   return _createIgnorer(ignoreContent, withNodeModules);
 };
+
+/**
+ * @param {boolean} sync
+ */
+function _createExplorer(sync) {
+  const cosmiconfig = thirdParty["cosmiconfig" + (sync ? "Sync" : "")];
+
+  const explorer = cosmiconfig("prettierignore", {
+    packageProp: "prettierIgnore",
+    searchPlaces: ["package.json"],
+    transform: (result) => {
+      if (result && result.config) {
+        if (
+          Array.isArray(result.config) &&
+          result.config.every((entry) => typeof entry === "string")
+        ) {
+          // Return `config` directly so that we don't have to await before this
+          return result.config;
+        }
+
+        throw new TypeError(
+          "File list is only allowed to be an array of strings, " +
+            `but received ${typeof result.config} in "${result.filepath}"`
+        );
+      }
+
+      return result;
+    },
+  });
+
+  return explorer;
+}
 
 /**
  * @param {null | string | readonly string[]} ignoreContent
@@ -42,6 +75,7 @@ function _createIgnorer(ignoreContent, withNodeModules) {
   if (!withNodeModules) {
     ignorer.add("node_modules");
   }
+
   return ignorer;
 }
 
--- tests/integration/__tests__/ignore-package-json.js
+++ tests/integration/__tests__/ignore-package-json.js
@@ -5,12 +5,16 @@ const runPrettier = require("../run-prettier.js");
 describe("support prettierIgnore key in package.json", () => {
   runPrettier("cli/ignore-package-json", ["**/*.js", "-l"]).test({
     status: 1,
+    stdout: "not-ignored.js\n",
+    stderr: "",
   });
 });
 
 describe("ignore file when using --debug-check", () => {
   runPrettier("cli/ignore-package-json", ["**/*.js", "--debug-check"]).test({
     status: 0,
+    stdout: "not-ignored.js\n",
+    stderr: "",
   });
 });
 
@@ -19,5 +23,7 @@ describe("outputs files as-is if no --write", () => {
     ignoreLineEndings: true,
   }).test({
     status: 0,
+    stdout: "'use strict';\n",
+    stderr: "",
   });
 });
