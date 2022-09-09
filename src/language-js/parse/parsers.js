63852059e4e98b068dd17f3c4750385833cc91c6
fisker Cheung <lionkay@gmail.com>
2022-09-09 17:32:13 +0800

    Simplify

--- src/language-js/parse/parsers.js
+++ src/language-js/parse/parsers.js
@@ -1,44 +1,36 @@
-import getParserFromModule from "../../utils/get-parser-from-plugin-module.js";
+import createParsers from "../../utils/create-parsers.js";
 
-const parsers = {
-  // Babel
-  babel: () => getParserFromModule(import("./babel.js"), "babel"),
-  "babel-flow": () => getParserFromModule(import("./babel.js"), "babel-flow"),
-  "babel-ts": () => getParserFromModule(import("./babel.js"), "babel-ts"),
-  json: () => getParserFromModule(import("./babel.js"), "json"),
-  json5: () => getParserFromModule(import("./babel.js"), "json5"),
-  "json-stringify": () =>
-    getParserFromModule(import("./babel.js"), "json-stringify"),
-  __js_expression: () =>
-    getParserFromModule(import("./babel.js"), "__js_expression"),
-  __vue_expression: () =>
-    getParserFromModule(import("./babel.js"), "__vue_expression"),
-  __vue_ts_expression: () =>
-    getParserFromModule(import("./babel.js"), "__vue_ts_expression"),
-  __vue_event_binding: () =>
-    getParserFromModule(import("./babel.js"), "__vue_event_binding"),
-  __vue_ts_event_binding: () =>
-    getParserFromModule(import("./babel.js"), "__vue_ts_event_binding"),
-  __babel_estree: () =>
-    getParserFromModule(import("./babel.js"), "__babel_estree"),
-  // Flow
-  flow: () => getParserFromModule(import("./flow.js"), "flow"),
-  // TypeScript
-  typescript: () =>
-    getParserFromModule(import("./typescript.js"), "typescript"),
-  // Angular
-  __ng_action: () => getParserFromModule(import("./angular.js"), "__ng_action"),
-  __ng_binding: () =>
-    getParserFromModule(import("./angular.js"), "__ng_binding"),
-  __ng_interpolation: () =>
-    getParserFromModule(import("./angular.js"), "__ng_interpolation"),
-  __ng_directive: () =>
-    getParserFromModule(import("./angular.js"), "__ng_directive"),
-  // Acorn and Espree
-  acorn: () => getParserFromModule(import("./acorn-and-espree.js"), "acorn"),
-  espree: () => getParserFromModule(import("./acorn-and-espree.js"), "espree"),
-  // Meriyah
-  meriyah: () => getParserFromModule(import("./meriyah.js"), "meriyah"),
-};
+const parsers = createParsers([
+  [
+    () => import("./babel.js"),
+    [
+      "babel",
+      "babel-flow",
+      "babel-ts",
+      "json",
+      "json5",
+      "json-stringify",
+      "__js_expression",
+      "__vue_expression",
+      "__vue_ts_expression",
+      "__vue_ts_expression",
+      "__vue_event_binding",
+      "__vue_ts_event_binding",
+      "__babel_estree",
+    ],
+  ],
+  [() => import("./flow.js"), ["flow"]],
+  [() => import("./typescript.js"), ["typescript"]],
+  [
+    () => import("./angular.js"),
+    ["__ng_binding", "__ng_interpolation", "__ng_directive"],
+  ],
+  [() => import("./acorn-and-espree.js"), ["acorn", "espree"]],
+  [() => import("./meriyah.js"), ["meriyah"]],
+]);
+
+console.log(parsers)
+await parsers.babel()
+console.log(parsers)
 
 export default parsers;
--- src/utils/get-parser-from-plugin-module.js
+++
@@ -1,5 +0,0 @@
-async function getParserFromModule(moduleImportPromise, parserName) {
-  return (await moduleImportPromise).default.parsers[parserName]
-}
-
-export default getParserFromModule
