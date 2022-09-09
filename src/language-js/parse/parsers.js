0313e2d525972828f3cdf58cb9817ab23a977bf5
fisker Cheung <lionkay@gmail.com>
2022-09-09 15:57:29 +0800

    Change parsers to function

--- src/language-js/parse/parsers.js
+++ src/language-js/parse/parsers.js
@@ -1,85 +1,40 @@
-import babel from "./babel.js";
-import flow from "./flow.js";
-import typescript from "./typescript.js";
-import angular from "./angular.js";
-import acornAndEspree from "./acorn-and-espree.js";
-import meriyah from "./meriyah.js";
-
 const parsers = {
-  // JS - Babel
-  get babel() {
-    return babel.parsers.babel;
-  },
-  get "babel-flow"() {
-    return babel.parsers["babel-flow"];
-  },
-  get "babel-ts"() {
-    return babel.parsers["babel-ts"];
-  },
-  get json() {
-    return babel.parsers.json;
-  },
-  get json5() {
-    return babel.parsers.json5;
-  },
-  get "json-stringify"() {
-    return babel.parsers["json-stringify"];
-  },
-  get __js_expression() {
-    return babel.parsers.__js_expression;
-  },
-  get __vue_expression() {
-    return babel.parsers.__vue_expression;
-  },
-  get __vue_ts_expression() {
-    return babel.parsers.__vue_ts_expression;
-  },
-  get __vue_event_binding() {
-    return babel.parsers.__vue_event_binding;
-  },
-  get __vue_ts_event_binding() {
-    return babel.parsers.__vue_ts_event_binding;
-  },
-  // JS - Flow
-  get flow() {
-    return flow.parsers.flow;
-  },
-  // JS - TypeScript
-  get typescript() {
-    return typescript.parsers.typescript;
-  },
-  // JS - Angular Action
-  get __ng_action() {
-    return angular.parsers.__ng_action;
-  },
-  // JS - Angular Binding
-  get __ng_binding() {
-    return angular.parsers.__ng_binding;
-  },
-  // JS - Angular Interpolation
-  get __ng_interpolation() {
-    return angular.parsers.__ng_interpolation;
-  },
-  // JS - Angular Directive
-  get __ng_directive() {
-    return angular.parsers.__ng_directive;
-  },
-  // JS - acorn
-  get acorn() {
-    return acornAndEspree.parsers.acorn;
-  },
-  // JS - espree
-  get espree() {
-    return acornAndEspree.parsers.espree;
-  },
-  // JS - meriyah
-  get meriyah() {
-    return meriyah.parsers.meriyah;
-  },
-  // JS - Babel Estree
-  get __babel_estree() {
-    return babel.parsers.__babel_estree;
-  },
+  // Babel
+  babel: async () => (await import("./babel.js")).parsers.babel,
+  "babel-flow": async () => (await import("./babel.js")).parsers["babel-flow"],
+  "babel-ts": async () => (await import("./babel.js")).parsers["babel-ts"],
+  json: async () => (await import("./babel.js")).parsers.json,
+  json5: async () => (await import("./babel.js")).parsers.json5,
+  "json-stringify": async () =>
+    (await import("./babel.js")).parsers["json-stringify"],
+  __js_expression: async () =>
+    (await import("./babel.js")).parsers.__js_expression,
+  __vue_expression: async () =>
+    (await import("./babel.js")).parsers.__vue_expression,
+  __vue_ts_expression: async () =>
+    (await import("./babel.js")).parsers.__vue_ts_expression,
+  __vue_ts_event_binding: async () =>
+    (await import("./babel.js")).parsers.__vue_ts_event_binding,
+  __vue_event_binding: async () =>
+    (await import("./babel.js")).parsers.__vue_event_binding,
+  __babel_estree: async () =>
+    (await import("./babel.js")).parsers.__babel_estree,
+  // Flow
+  flow: async () => (await import("./flow.js")).parsers.flow,
+  // TypeScript
+  typescript: async () => (await import("./typescript.js")).parsers.typescript,
+  // Angular
+  __ng_action: async () => (await import("./angular.js")).parsers.__ng_action,
+  __ng_binding: async () => (await import("./angular.js")).parsers.__ng_binding,
+  __ng_interpolation: async () =>
+    (await import("./angular.js")).parsers.__ng_interpolation,
+  __ng_directive: async () =>
+    (await import("./angular.js")).parsers.__ng_directive,
+  // Acorn and Espree
+  acorn: async () => (await import("./acorn-and-espree.js")).parsers.acorn,
+  espree: async () => (await import("./acorn-and-espree.js")).parsers.espree,
+  // Meriyah
+  meriyah: async () => (await import("./meriyah.js")).parsers.meriyah,
 };
 
 export default parsers;
