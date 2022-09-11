b4859bdbe458ae0a5f8c43c5f2789bc8860c9e32
Sosuke Suzuki <aosukeke@gmail.com>
2022-09-11 17:05:21 +0900

    Add new rule

--- scripts/tools/eslint-plugin-prettier-internal-rules/test.js
+++ scripts/tools/eslint-plugin-prettier-internal-rules/test.js
@@ -541,21 +541,43 @@ test("prefer-fs-promises-submodule", {
   })),
 });
 
-test("prefer-path-get-value", {
+test("prefer-ast-path-node", {
   valid: [
-    "path.getValue()",
     "path.getNode(2)",
     "path.getNode",
     "getNode",
     "this.getNode()",
+    "path.node",
   ],
   invalid: [
     {
       code: "path.getNode()",
+      output: "path.node",
       errors: 1,
     },
     {
       code: "const node = path.getNode()",
+      output: "const node = path.node",
+      errors: 1,
+    },
+    {
+      code: "path.getValue()",
+      output: "path.node",
+      errors: 1,
+    },
+    {
+      code: "const node = path.getValue()",
+      output: "const node = path.node",
+      errors: 1,
+    },
+    {
+      code: "fooPath.getValue()",
+      output: "fooPath.node",
+      errors: 1,
+    },
+    {
+      code: "fooPath.getNode()",
+      output: "fooPath.node",
       errors: 1,
     },
   ],
