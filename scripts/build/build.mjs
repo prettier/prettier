d85a44a90dfcfb5562a0e8b4d61f796a87dece47
fisker Cheung <lionkay@gmail.com>
2022-09-22 17:34:25 +0800

    Add a TODO

--- scripts/build/build.mjs
+++ scripts/build/build.mjs
@@ -134,6 +134,8 @@ async function preparePackage() {
   const packageJson = await readJson(path.join(PROJECT_ROOT, "package.json"));
   packageJson.bin = "./bin-prettier.js";
   packageJson.main = "./index.cjs";
+  // TODO: Add `exports`
+
   // https://github.com/prettier/prettier/pull/13118#discussion_r922708068
   packageJson.engines.node = ">=14";
   delete packageJson.dependencies;
