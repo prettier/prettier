c883da760ab137b6fd5e87c1db6e660d5db69c0b
fisker <lionkay@gmail.com>
2022-09-25 12:45:44 +0800

    Add test

---
+++ tests/unit/syntax-transform.js
@@ -0,0 +1,30 @@
+import url from "node:url";
+import path from "node:path";
+import { outdent } from "outdent";
+import transformCode from "../../scripts/build/transform/index.js";
+
+const file = url.fileURLToPath(
+  new URL("../../src/__dummy.js", import.meta.url)
+);
+const shimsDirectory =
+  url.fileURLToPath(new URL("../../scripts/build/shims", import.meta.url)) +
+  path.sep;
+const transform = (code) =>
+  transformCode(code, file).replaceAll(
+    JSON.stringify(shimsDirectory).slice(1, -1),
+    "<SHIMS>/"
+  );
+
+test("Object.hasOwn", () => {
+  expect(transform("Object.hasOwn(foo, bar)")).toMatchInlineSnapshot(
+    `"Object.prototype.hasOwnProperty.call(foo, bar);"`
+  );
+});
+
+test(".at", () => {
+  expect(transform("foo.at(-1)")).toMatchInlineSnapshot(`
+    "import __at from "<SHIMS>/at.js";
+
+    __at(foo, -1);"
+  `);
+});
