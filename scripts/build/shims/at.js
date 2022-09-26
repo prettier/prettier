bb2104dd7cc2be20ffbeeb2b2a2ae802f19747b6
fisker Cheung <lionkay@gmail.com>
2022-09-26 22:54:17 +0800

    Support optional chaining in `.at()` transform (#13528)

--- scripts/build/shims/at.js
+++ scripts/build/shims/at.js
@@ -5,9 +5,14 @@ Note:
 2. `postcss` contains `Container` class with `.at` method
 */
 
-const at = (object, index, ...args) =>
-  object.at
+const at = (isOptionalObject, object, index, ...args) => {
+  if (isOptionalObject && (object === undefined || object === null)) {
+    return;
+  }
+
+  return object.at
     ? object.at(index, ...args)
     : object[index < 0 ? object.length + index : index];
+};
 
 export default at;
