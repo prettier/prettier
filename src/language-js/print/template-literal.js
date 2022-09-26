233465e5f6f2661a38e866f7ffe289541c697545
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2022-09-26 13:12:23 +0300

    Add lineSuffixBoundary to tagged template literals

--- src/language-js/print/template-literal.js
+++ src/language-js/print/template-literal.js
@@ -112,6 +112,7 @@ function printTaggedTemplateLiteral(print) {
   return label(quasiDoc.label && { tagged: true, ...quasiDoc.label }, [
     print("tag"),
     print("typeParameters"),
+    lineSuffixBoundary,
     quasiDoc,
   ]);
 }
