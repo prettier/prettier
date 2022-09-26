e6dd186b9e26de726e29cffdb24703f7980ea797
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2022-09-26 19:38:38 +0300

    Fix type problem with ignore

--- src/common/create-ignorer.js
+++ src/common/create-ignorer.js
@@ -13,9 +13,9 @@ async function createIgnorer(ignoreFilePath, withNodeModules) {
     content = await readFile(path.resolve(ignoreFilePath));
   }
 
-  // @ts-expect-error `ignore` is CommonJS, but its types incorrectly declare a default export.
-  // See https://github.com/microsoft/TypeScript/issues/48845
-  const ignorer = ignore({ allowRelativePaths: true }).add(content ?? "");
+  const ignorer = ignore
+    .default({ allowRelativePaths: true })
+    .add(content ?? "");
   if (!withNodeModules) {
     ignorer.add("node_modules");
   }
