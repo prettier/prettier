2fddf6218b21379d20bf779d6c87e27a9a96f426
fisker Cheung <lionkay@gmail.com>
2022-10-11 21:43:47 +0800

    Fix test title

--- scripts/release/tests/version-check.test.js
+++ scripts/release/tests/version-check.test.js
@@ -6,7 +6,7 @@ describe("isVersionReleased", () => {
   it("returns true for existing version", async () => {
     assert.ok(await isVersionReleased("1.0.0"));
   });
-  it("rejects true for non-existing version", async () => {
+  it("rejects for non-existing version", async () => {
     await assert.rejects(() => isVersionReleased("999.0.0"), {
       message: "prettier@999.0.0 doesn't exit.",
     });
