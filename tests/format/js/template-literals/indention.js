98a2750566814375b4089c6af74a28ce8c45ffa6
fisker Cheung <lionkay@gmail.com>
2022-10-12 18:52:29 +0800

    Test (original)

---
+++ tests/format/js/template-literals/indention.js
@@ -0,0 +1,64 @@
+[
+  `
+      1. Go to "-{chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+  `,
+  `
+      2. Go to "${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+  `,
+  `
+      1. Go to "-{chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+      2. Go to "${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+  `,
+  `
+      2. Go to "${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+      1. Go to "-{chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+  `,
+  `
+          1. Go to "-{chalk.green.underline(
+            "https://www.npmjs.com/package/prettier/access"
+          )}" ${chalk.green.underline(
+            "https://www.npmjs.com/package/prettier/access"
+          )}
+      2. Go to "${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+  `,
+  `
+      1. Go to "-{chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}" ${chalk.green.underline(
+        "https://www.npmjs.com/package/prettier/access"
+      )}
+          2. Go to "${chalk.green.underline(
+            "https://www.npmjs.com/package/prettier/access"
+          )}" ${chalk.green.underline(
+            "https://www.npmjs.com/package/prettier/access"
+          )}
+  `,
+]
