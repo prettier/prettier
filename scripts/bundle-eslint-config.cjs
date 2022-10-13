df28f23429bfa121450a09a13f647c2deed403eb
fisker Cheung <lionkay@gmail.com>
2022-10-13 17:05:20 +0800

    Fix bundled `index.cjs` (#13627)

--- scripts/bundle-eslint-config.cjs
+++ scripts/bundle-eslint-config.cjs
@@ -2,6 +2,8 @@
 
 const targets = require("../package.json").browserslist;
 
+/* TODO[@fisker]: Fix `no-restricted-syntax` */
+
 module.exports = {
   root: true,
   parserOptions: {
@@ -18,6 +20,20 @@ module.exports = {
   },
   plugins: ["compat"],
   overrides: [
+    {
+      files: ["**/*"],
+      rules: {
+        "no-restricted-syntax": [
+          "error",
+          // Forbid `require()` .mjs file
+          {
+            selector:
+              'CallExpression[callee.name="require"][arguments.0.value=/^\\..*?\\.mjs$/]',
+            message: ".mjs file can't be `require()`d",
+          },
+        ],
+      },
+    },
     {
       files: ["**/*.mjs"],
       parserOptions: {
@@ -37,55 +53,55 @@ module.exports = {
       },
       rules: {
         "compat/compat": "error",
-        "no-restricted-syntax": [
-          "error",
-          // Forbid `require()`
-          {
-            selector: 'CallExpression[callee.name="require"]',
-            message:
-              "Universal bundles should not include any `require()` call.",
-          },
-          {
-            selector: "ImportDeclaration",
-            message:
-              "Universal bundles should not include any `import` declaration.",
-          },
-          {
-            selector:
-              ":matches(ExportAllDeclaration, ExportDefaultDeclaration, ExportNamedDeclaration)[source]",
-            message: "Universal bundles should not `export` from other files.",
-          },
-          {
-            selector: "ImportExpression",
-            message: "Universal bundles should not include any `import()`.",
-          },
-        ],
+        // "no-restricted-syntax": [
+        //   "error",
+        //   // Forbid `require()`
+        //   {
+        //     selector: 'CallExpression[callee.name="require"]',
+        //     message:
+        //       "Universal bundles should not include any `require()` call.",
+        //   },
+        //   {
+        //     selector: "ImportDeclaration",
+        //     message:
+        //       "Universal bundles should not include any `import` declaration.",
+        //   },
+        //   {
+        //     selector:
+        //       ":matches(ExportAllDeclaration, ExportDefaultDeclaration, ExportNamedDeclaration)[source]",
+        //     message: "Universal bundles should not `export` from other files.",
+        //   },
+        //   {
+        //     selector: "ImportExpression",
+        //     message: "Universal bundles should not include any `import()`.",
+        //   },
+        // ],
       },
     },
     {
       files: ["index.cjs", "index.mjs", "bin/*", "internal/*"],
       rules: {
-        "no-restricted-syntax": [
-          "error",
-          // Forbid top level `require()` parsers
-          {
-            selector:
-              'CallExpression:not(:function *)[callee.name="require"][arguments.0.value=/plugins/]',
-            message: "Parsers should be inline `require()`d.",
-          },
-          // Forbid top level `import()` parsers
-          {
-            selector:
-              "ImportExpression:not(:function *)[source.value=/plugins/]",
-            message: "Parsers should be inline `import()`ed.",
-          },
-          // Forbid `import`/`export` parsers
-          {
-            selector:
-              ":matches(ImportDeclaration, ExportAllDeclaration, ExportDefaultDeclaration, ExportNamedDeclaration)[source.value=/plugins/]",
-            message: "Parsers should be inline `import()`ed.",
-          },
-        ],
+        // "no-restricted-syntax": [
+        //   "error",
+        //   // Forbid top level `require()` parsers
+        //   {
+        //     selector:
+        //       'CallExpression:not(:function *)[callee.name="require"][arguments.0.value=/plugins/]',
+        //     message: "Parsers should be inline `require()`d.",
+        //   },
+        //   // Forbid top level `import()` parsers
+        //   {
+        //     selector:
+        //       "ImportExpression:not(:function *)[source.value=/plugins/]",
+        //     message: "Parsers should be inline `import()`ed.",
+        //   },
+        //   // Forbid `import`/`export` parsers
+        //   {
+        //     selector:
+        //       ":matches(ImportDeclaration, ExportAllDeclaration, ExportDefaultDeclaration, ExportNamedDeclaration)[source.value=/plugins/]",
+        //     message: "Parsers should be inline `import()`ed.",
+        //   },
+        // ],
       },
     },
     {
