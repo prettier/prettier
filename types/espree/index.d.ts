075d9cbe68421040e7c1c6903d76ac34683c622f
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-10-15 20:20:43 +0900

    Update

--- types/espree/index.d.ts
+++ types/espree/index.d.ts
@@ -5,28 +5,7 @@ declare module "espree" {
     loc?: boolean;
     comment?: boolean;
     tokens?: boolean;
-    ecmaVersion?:
-      | 3
-      | 5
-      | 6
-      | 7
-      | 8
-      | 9
-      | 10
-      | 11
-      | 12
-      | 13
-      | 14
-      | 2015
-      | 2016
-      | 2017
-      | 2018
-      | 2019
-      | 2020
-      | 2021
-      | 2022
-      | 2023
-      | "latest";
+    ecmaVersion?: "latest";
     sourceType?: "script" | "module";
     ecmaFeatures?: {
       jsx?: boolean;
