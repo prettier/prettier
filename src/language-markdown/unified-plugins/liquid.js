2b44057f8e4630a5edcd4d512f40306b90a141ab
Jed Fox <git@jedfox.com>
2022-10-17 21:37:00 -0400

    Rewrite liquid plugin

--- src/language-markdown/unified-plugins/liquid.js
+++ src/language-markdown/unified-plugins/liquid.js
@@ -1,22 +1,89 @@
-function liquid() {
-  const proto = this.Parser.prototype;
-  const methods = proto.inlineMethods;
-  methods.splice(methods.indexOf("text"), 0, "liquid");
-  proto.inlineTokenizers.liquid = tokenizer;
+import { markdownLineEnding } from "micromark-util-character";
+import { codes } from "micromark-util-symbol/codes";
+import { markdownPlugin } from "./markdown-plugin.js";
 
-  function tokenizer(eat, value) {
-    const match = value.match(/^({%.*?%}|{{.*?}})/s);
+/** @type {import('micromark-util-types').Construct} */
+const liquidConstruct = {
+  name: "liquid",
+  tokenize(effects, ok, nok) {
+    let type;
+    return start;
 
-    if (match) {
-      return eat(match[0])({
-        type: "liquidNode",
-        value: match[0],
-      });
+    /** @type {import('micromark-util-types').State} */
+    function start(code) {
+      effects.enter("liquid");
+      effects.consume(code);
+      return openDelimiter;
     }
-  }
-  tokenizer.locator = function (value, fromIndex) {
-    return value.indexOf("{", fromIndex);
-  };
-}
 
-export default liquid;
+    /** @type {import('micromark-util-types').State} */
+    function openDelimiter(code) {
+      if (code === codes.percentSign) {
+        type = "tag";
+        effects.consume(code);
+        return content;
+      }
+      if (code === codes.leftCurlyBrace) {
+        type = "variable";
+        effects.consume(code);
+        return content;
+      }
+      return nok(code);
+    }
+
+    /** @type {import('micromark-util-types').State} */
+    function content(code) {
+      if (type === "tag" && code === codes.percentSign) {
+        effects.consume(code);
+        return closeDelimiter;
+      }
+      if (type === "variable" && code === codes.rightCurlyBrace) {
+        effects.consume(code);
+        return closeDelimiter;
+      }
+      effects.consume(code);
+      return content;
+    }
+
+    /** @type {import('micromark-util-types').State} */
+    function closeDelimiter(code) {
+      if (code === codes.rightCurlyBrace) {
+        effects.consume(code);
+        effects.exit("liquid");
+        return ok;
+      }
+      if (code === codes.eof || markdownLineEnding(code)) {
+        return nok(code);
+      }
+      effects.consume(code);
+      return content;
+    }
+  },
+};
+
+/** @type {import('micromark-util-types').Extension} */
+const liquid = {
+  text: {
+    [codes.leftCurlyBrace]: liquidConstruct,
+  },
+};
+
+/** @type {import('mdast-util-from-markdown').Extension} */
+const liquidMDAST = {
+  enter: {
+    liquid(token) {
+      this.enter({ type: "liquidNode", value: "", children: [] }, token);
+    },
+  },
+  exit: {
+    liquid(token) {
+      const node = this.exit(token);
+      node.value = this.sliceSerialize(token);
+    },
+  },
+};
+
+/** @type {import('unified').Plugin<[]>} */
+export default function liquidPlugin() {
+  this.use(markdownPlugin(liquid, liquidMDAST));
+}
