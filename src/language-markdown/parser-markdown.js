a9f1913f266b715c05a7818ad7497886e2ac6bd4
Jed Fox <git@jedfox.com>
2022-10-17 21:36:12 -0400

    Replace front matter plugin with just calling parseFrontMatter first

--- src/language-markdown/parser-markdown.js
+++ src/language-markdown/parser-markdown.js
@@ -2,11 +2,11 @@ import remarkParse from "remark-parse";
 import unified from "unified";
 import remarkMath from "remark-math";
 import footnotes from "remark-footnotes";
+import parseFrontMatter from "../utils/front-matter/parse.js";
 import { hasPragma } from "./pragma.js";
 import { locStart, locEnd } from "./loc.js";
 import { BLOCKS_REGEX, esSyntax } from "./mdx.js";
 import htmlToJsx from "./unified-plugins/html-to-jsx.js";
-import frontMatter from "./unified-plugins/front-matter.js";
 import liquid from "./unified-plugins/liquid.js";
 import wikiLink from "./unified-plugins/wiki-link.js";
 import looseItems from "./unified-plugins/loose-items.js";
@@ -26,21 +26,26 @@ import looseItems from "./unified-plugins/loose-items.js";
  * interface InlineCode { children: Array<Sentence> }
  */
 function createParse({ isMDX }) {
-  return (text) => {
-    const processor = unified()
-      .use(remarkParse, {
-        commonmark: true,
-        ...(isMDX && { blocks: [BLOCKS_REGEX] }),
-      })
-      .use(footnotes)
-      .use(frontMatter)
-      .use(remarkMath)
-      .use(isMDX ? esSyntax : identity)
-      .use(liquid)
-      .use(isMDX ? htmlToJsx : identity)
-      .use(wikiLink)
-      .use(looseItems);
-    return processor.run(processor.parse(text));
+  const processor = unified()
+    .use(remarkParse, {
+      commonmark: true,
+      ...(isMDX && { blocks: [BLOCKS_REGEX] }),
+    })
+    .use(footnotes)
+    .use(remarkMath)
+    .use(isMDX ? esSyntax : identity)
+    .use(liquid)
+    .use(isMDX ? htmlToJsx : identity)
+    .use(wikiLink)
+    .use(looseItems);
+
+  return async (text) => {
+    const { frontMatter, content } = parseFrontMatter(text);
+    const ast = await processor.run(processor.parse(content));
+    if (frontMatter) {
+      ast.children.unshift(frontMatter);
+    }
+    return ast;
   };
 }
 
