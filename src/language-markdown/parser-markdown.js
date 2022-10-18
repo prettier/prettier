a3c5077f7a5ae8e4070d86b16b0bd7dbbdd8e5fb
Jed Fox <git@jedfox.com>
2022-10-17 21:36:12 -0400

    Replace front matter plugin with just calling parseFrontMatter first

--- src/language-markdown/parser-markdown.js
+++ src/language-markdown/parser-markdown.js
@@ -6,7 +6,6 @@ import { hasPragma } from "./pragma.js";
 import { locStart, locEnd } from "./loc.js";
 import { BLOCKS_REGEX, esSyntax } from "./mdx.js";
 import htmlToJsx from "./unified-plugins/html-to-jsx.js";
-import frontMatter from "./unified-plugins/front-matter.js";
 import liquid from "./unified-plugins/liquid.js";
 import wikiLink from "./unified-plugins/wiki-link.js";
 
@@ -32,7 +31,6 @@ function createParse({ isMDX }) {
         ...(isMDX && { blocks: [BLOCKS_REGEX] }),
       })
       .use(footnotes)
-      .use(frontMatter)
       .use(remarkMath)
       .use(isMDX ? esSyntax : identity)
       .use(liquid)
