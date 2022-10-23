a2be549c5135003e574361afa003bc3dc061f913
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-10-23 19:06:29 +0900

    Fix problems

--- tests/integration/__tests__/cache.js
+++ tests/integration/__tests__/cache.js
@@ -1,5 +1,5 @@
 import path from "node:path";
-import { promises as fs } from "node:fs";
+import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import rimraf from "rimraf";
 
