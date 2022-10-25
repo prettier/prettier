75627650512d98fd8a3b40aff7ea9d3dbfdf708b
fisker Cheung <lionkay@gmail.com>
2022-10-25 12:23:58 +0800

    Clean

--- src/language-html/utils/map-object.js
+++
@@ -1,9 +0,0 @@
-function mapObject(object, fn) {
-  const newObject = Object.create(null);
-  for (const [key, value] of Object.entries(object)) {
-    newObject[key] = fn(value, key);
-  }
-  return newObject;
-}
-
-export default mapObject;
