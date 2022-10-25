2bdac7a23499e2c2fd6421dabf255729ac8f2664
fisker Cheung <lionkay@gmail.com>
2022-10-25 10:42:40 +0800

    evaluate as javascript code

--- scripts/build/esbuild-plugins/evaluate.mjs
+++ scripts/build/esbuild-plugins/evaluate.mjs
@@ -2,34 +2,34 @@ import createEsmUtils from "esm-utils";
 
 const { importModule } = createEsmUtils(import.meta);
 
+function serialize(value) {
+  return JSON.stringify(
+    value,
+    (_, v) => {
+      if (typeof v === "function") {
+        throw new TypeError("Cannot evaluate functions.");
+      }
+      return v;
+    },
+    2
+  );
+}
+
 export default function esbuildPluginEvaluate() {
   return {
     name: "evaluate",
 
     setup(build) {
       build.onLoad({ filter: /\.evaluate\.c?js$/ }, async ({ path }) => {
-        let data = await importModule(path);
-
-        if (Object.hasOwn(data, "default")) {
-          if (Object.keys(data).length !== 1) {
-            throw new TypeError("Mixed export not allowed.");
-          }
-
-          data = data.default;
-        }
-
-        const json = JSON.stringify(
-          data,
-          (_, v) => {
-            if (typeof v === "function") {
-              throw new TypeError("Cannot evaluate functions.");
-            }
-            return v;
-          },
-          2
-        );
-
-        return { loader: "json", contents: json };
+        const module = await importModule(path);
+        const text = Object.entries(module)
+          .map(([key, value]) =>
+            key === "default"
+              ? `export default ${serialize(value)};`
+              : `export const ${key} = ${serialize(value)};`
+          )
+          .join("\n");
+        return { contents: text };
       });
     },
   };
