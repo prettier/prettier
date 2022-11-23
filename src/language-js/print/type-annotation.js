bcbc6daeec1c13b7bda40e3f4fafd9ed5f423e20
Yiming Pan <perfectpan325@gmail.com>
2022-11-23 11:33:45 +0800

    Fix union type should be printed in the multi-line variant when there are comments (#13860)
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- src/language-js/print/type-annotation.js
+++ src/language-js/print/type-annotation.js
@@ -16,6 +16,7 @@ import {
   hasLeadingOwnLineComment,
   isObjectTypePropertyAFunction,
   shouldPrintComma,
+  hasComment,
 } from "../utils/index.js";
 import { printAssignment } from "./assignment.js";
 import {
@@ -47,7 +48,9 @@ function shouldHugType(node) {
         node.type === "TSTypeReference"
     );
 
-    if (node.types.length - 1 === voidCount && hasObject) {
+    const hasComments = node.types.some((node) => hasComment(node));
+
+    if (node.types.length - 1 === voidCount && hasObject && !hasComments) {
       return true;
     }
   }
