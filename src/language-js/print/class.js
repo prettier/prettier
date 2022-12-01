75affa6604af150e12d5fa0519a9ca47faa8edfd
fisker Cheung <lionkay@gmail.com>
2022-12-01 11:30:43 +0800

    Blank lines

--- src/language-js/print/class.js
+++ src/language-js/print/class.js
@@ -224,7 +224,6 @@ function printClassProperty(path, options, print) {
   if (node.type === "ClassAccessorProperty") {
     parts.push("accessor ");
   }
-
   parts.push(
     printPropertyKey(path, options, print),
     printOptionalToken(path),
