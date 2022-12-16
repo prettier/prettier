1fb2914061477c313ec75395c21e3def6bf4ece6
fisker Cheung <lionkay@gmail.com>
2022-12-16 13:46:33 +0800

    More test

--- tests/format/misc/errors/js/assignment/jsfmt.spec.js
+++ tests/format/misc/errors/js/assignment/jsfmt.spec.js
@@ -13,3 +13,14 @@ run_spec(
   },
   ["babel", "babel-ts", "acorn", "espree"]
 );
+
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      "export default ((let)[0] = 1);",
+      "class Foo extends ((let)[0] = 1) {}",
+    ],
+  },
+  ["espree", "meriyah"]
+);
