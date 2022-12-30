0e314219972ce1ac7b12a60c6f5bcae38317a620
fisker Cheung <lionkay@gmail.com>
2022-12-30 17:26:04 +0800

    Test

---
+++ tests/format/flow/type-parameters/trailing-comma/jsfmt.spec.js
@@ -0,0 +1,3 @@
+run_spec(import.meta, ["flow", "babel-flow"], {trailingComma: "none"});
+run_spec(import.meta, ["flow", "babel-flow"], {trailingComma: "es5"});
+run_spec(import.meta, ["flow", "babel-flow"], {trailingComma: "all"});
