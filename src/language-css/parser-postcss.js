c8e4f497497c692ef107acdef5350e22aac080a9
fisker Cheung <lionkay@gmail.com>
2022-12-30 10:07:17 +0800

    Skip parsing empty text

--- src/language-css/parser-postcss.js
+++ src/language-css/parser-postcss.js
@@ -492,13 +492,21 @@ function parseNestedCSS(node, options) {
           node.params[0] === ":"
         ) {
           node.variable = true;
-          node.value = parseValue(node.params.slice(1), options);
+          const text = node.params.slice(1);
+          if (text) {
+            node.value = parseValue(text, options);
+          }
           node.raws.afterName += ":";
         }
 
         // Less variable
         if (node.variable) {
           delete node.params;
+
+          if (!node.value) {
+            delete node.value;
+          }
+
           return node;
         }
       }
