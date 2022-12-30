876539b0e3472cf3f812cd04ec77ccc43719aa12
fisker Cheung <lionkay@gmail.com>
2022-12-30 17:30:26 +0800

    Run test separately

---
+++ tests/format/flow/interface-types/break/jsfmt.spec.js
@@ -0,0 +1,3 @@
+run_spec(import.meta, ["babel", "flow"], { trailingComma: "none" });
+run_spec(import.meta, ["babel", "flow"], { trailingComma: "es5" });
+run_spec(import.meta, ["babel", "flow"], { trailingComma: "all" });
