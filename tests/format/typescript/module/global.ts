8cb3e77cfdbde6235dc996c0ea00c8d106c115e3
fisker Cheung <lionkay@gmail.com>
2023-01-11 09:59:54 +0800

    Minor refactor to `TSModuleDeclaration` print (#14150)

--- tests/format/typescript/module/global.ts
+++ tests/format/typescript/module/global.ts
@@ -2,3 +2,7 @@ namespace global {}
 module global {}
 global {}
 declare global {}
+declare /* module */ global {}
+declare /* namespace */ global {}
+declare module  global {}
+declare namespace global {}
