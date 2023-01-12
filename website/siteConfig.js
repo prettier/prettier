c1b976572c7512dfa7d7407706891f511883c0ad
Tarun Chauhan <tarunrajput1337@gmail.com>
2023-01-12 21:31:07 +0530

    feat: add copy to clipboard in code blocks (#14153)
    * feat: add copy to clipboard in code blocks
    
    * Use the same style as docusaurus v2
    
    * Remove style from siteConfig.js
    
    * Fix typo
    
    * Disable button on copied
    
    Co-authored-by: Tarun Chauhan <tarun.chauhan@tripjack.com>
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -56,7 +56,11 @@ const siteConfig = {
   },
   usePrism: ["javascript", "jsx", "typescript", "ts", "js", "html", "css"],
   useEnglishUrl: true,
-  scripts: ["https://buttons.github.io/buttons.js"],
+  scripts: [
+    "https://buttons.github.io/buttons.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+    "/js/code-block-buttons.js",
+  ],
   stylesheets: [
     "//unpkg.com/@sandhose/prettier-animated-logo@1.0.3/dist/wide.css",
   ],
