c254296396ef72c2c29b1880805d120bddce3aaa
fisker Cheung <lionkay@gmail.com>
2023-01-17 14:12:30 +0800

    Add test

---
+++ tests/format/typescript/assert/comment.js
@@ -0,0 +1,8 @@
+a = (x: any): asserts x is string/* comment */ => {}
+a = (x: any): asserts x is /* comment */string => {}
+a = (x: any): asserts x is/* comment */ string => {}
+a = (x: any): asserts x /* comment */is string => {}
+a = (x: any): asserts x/* comment */ is string => {}
+a = (x: any): asserts /* comment */x is string => {}
+a = (x: any): asserts/* comment */ x is string => {}
+a = (x: any): /* comment */asserts x is string => {}
