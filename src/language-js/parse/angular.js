8e796ad8d5d3a6e64490ecd19c42fa32923f356a
fisker Cheung <lionkay@gmail.com>
2023-01-18 10:55:56 +0800

    Fix type check

--- src/language-js/parse/angular.js
+++ src/language-js/parse/angular.js
@@ -2,13 +2,22 @@ import createParser from "./utils/create-parser.js";
 
 const createAngularParser = (options) => createParser(createParse(options));
 
+/**
+ * @param {"parseAction" | "parseBinding" | "parseInterpolation" | "parseTemplateBindings"} parseMethod
+ */
 function createParse(parseMethod) {
   return async (text) => {
     const { [parseMethod]: parse } = await import("angular-estree-parser");
     let node = parse(text);
 
     if (parseMethod === "parseAction" && node.type !== "NGChainedExpression") {
-      node = { ...node, type: "NGChainedExpression", expressions: [node] };
+      node = {
+        type: "NGChainedExpression",
+        expressions: [node],
+        start: node.start,
+        end: node.start,
+        loc: node.loc,
+      };
     }
 
     return { type: "NGRoot", node };
