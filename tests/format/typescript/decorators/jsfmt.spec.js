e9ffa6faf376ea3d269af702eb2cea3ebda5a700
fisker Cheung <lionkay@gmail.com>
2023-01-28 17:56:52 +0800

    Coverage

--- tests/format/typescript/decorators/jsfmt.spec.js
+++ tests/format/typescript/decorators/jsfmt.spec.js
@@ -1,6 +1,6 @@
 run_spec(import.meta, ["typescript"], {
   errors: {
-    typescript: ["abstract-method.ts"],
+    typescript: ["abstract-method.ts", "accessor.ts"],
     "babel-ts": ["issue-9102.ts"],
   },
 });
