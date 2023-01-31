e658d7830f42b871ed51ff0e4352966bcc43cfba
fisker Cheung <lionkay@gmail.com>
2023-01-31 10:10:08 +0800

    Add test for typescript too

--- tests/format/misc/errors/typescript/interface/jsfmt.spec.js
+++ tests/format/misc/errors/typescript/interface/jsfmt.spec.js
@@ -4,6 +4,9 @@ run_spec(
     snippets: [
       // Invalid initializer
       "interface I { x: number = 1;}",
+      // Can't have mixins nor implements
+      "interface A mixins B {}",
+      "interface A implements B {}",
     ],
   },
   ["babel-ts", "typescript"]
