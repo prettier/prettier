5c2f853081d127c48c22e6ee8b90e2843868e35d
fisker Cheung <lionkay@gmail.com>
2023-01-31 19:55:58 +0800

    Clean

--- tests/format/flow-repo/es_declare_module/flow-typed/jsfmt.spec.js
+++ tests/format/flow-repo/es_declare_module/flow-typed/jsfmt.spec.js
@@ -1,5 +1,1 @@
 run_spec(import.meta, ["flow"]);
-
-// TODO: While the tests render the same, Flow is dropping the line breaks
-// between each declaration while babel preserves them.
-//run_spec(import.meta, null, ["babel"]);
