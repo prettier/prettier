467a1588c4a8d39740da409394b37d13ef9faf6f
fisker Cheung <lionkay@gmail.com>
2023-01-31 17:48:49 +0800

    Test `TSConstructorType`

--- tests/format/typescript/conditional-types/parentheses.ts
+++ tests/format/typescript/conditional-types/parentheses.ts
@@ -1,5 +1,6 @@
 // #13275
 type Foo<T> = T extends ((...a: any[]) => infer R extends string) ? R : never;
+type Foo<T> = T extends (new (...a: any[]) => infer R extends string) ? R : never;
 
 // #14275
 type Test<T> = T extends ((
@@ -10,5 +11,10 @@ type Test<T> = T extends ((
 type Test<T> = T extends ((
   token: TSESTree.Token
 ) => asserts token is infer U extends TSESTree.Token)
+  ? U
+  : TSESTree.Token;
+type Test<T> = T extends (new (
+  token: TSESTree.Token
+) => token is infer U extends TSESTree.Token)
   ? U
   : TSESTree.Token;
