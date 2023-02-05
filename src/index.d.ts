b73e3e8b7c7281ef912df4bc071bf118340925cf
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-02-05 21:23:59 +0900

    Refactor with namespace imports

--- src/index.d.ts
+++ src/index.d.ts
@@ -969,65 +969,89 @@ export namespace doc {
     }
 
     function addAlignmentToDoc(doc: Doc, size: number, tabWidth: number): Doc;
+
     /** @see [align](https://github.com/prettier/prettier/blob/main/commands.md#align) */
     function align(widthOrString: Align["n"], doc: Doc): Align;
+
     /** @see [breakParent](https://github.com/prettier/prettier/blob/main/commands.md#breakparent) */
     const breakParent: BreakParent;
+
     /**
      * @see [concat](https://github.com/prettier/prettier/blob/main/commands.md#deprecated-concat)
      * @deprecated use `Doc[]` instead
      */
     function concat(docs: Doc[]): Concat;
+
     /** @see [conditionalGroup](https://github.com/prettier/prettier/blob/main/commands.md#conditionalgroup) */
     function conditionalGroup(
       alternatives: Doc[],
       options?: GroupOptions
     ): Group;
+
     /** @see [dedent](https://github.com/prettier/prettier/blob/main/commands.md#dedent) */
     function dedent(doc: Doc): Align;
+
     /** @see [dedentToRoot](https://github.com/prettier/prettier/blob/main/commands.md#dedenttoroot) */
     function dedentToRoot(doc: Doc): Align;
+
     /** @see [fill](https://github.com/prettier/prettier/blob/main/commands.md#fill) */
     function fill(docs: Doc[]): Fill;
+
     /** @see [group](https://github.com/prettier/prettier/blob/main/commands.md#group) */
     function group(doc: Doc, opts?: GroupOptions): Group;
+
     /** @see [hardline](https://github.com/prettier/prettier/blob/main/commands.md#hardline) */
     const hardline: Concat;
+
     /** @see [hardlineWithoutBreakParent](https://github.com/prettier/prettier/blob/main/commands.md#hardlinewithoutbreakparent-and-literallinewithoutbreakparent) */
     const hardlineWithoutBreakParent: HardlineWithoutBreakParent;
+
     /** @see [ifBreak](https://github.com/prettier/prettier/blob/main/commands.md#ifbreak) */
     function ifBreak(
       ifBreak: Doc,
       noBreak?: Doc,
       options?: { groupId?: symbol | undefined }
     ): IfBreak;
+
     /** @see [indent](https://github.com/prettier/prettier/blob/main/commands.md#indent) */
     function indent(doc: Doc): Indent;
+
     /** @see [indentIfBreak](https://github.com/prettier/prettier/blob/main/commands.md#indentifbreak) */
     function indentIfBreak(
       doc: Doc,
       opts: { groupId: symbol; negate?: boolean | undefined }
     ): IndentIfBreak;
+
     /** @see [join](https://github.com/prettier/prettier/blob/main/commands.md#join) */
     function join(sep: Doc, docs: Doc[]): Concat;
+
     /** @see [label](https://github.com/prettier/prettier/blob/main/commands.md#label) */
     function label(label: string, doc: Doc): Label;
+
     /** @see [line](https://github.com/prettier/prettier/blob/main/commands.md#line) */
     const line: Line;
+
     /** @see [lineSuffix](https://github.com/prettier/prettier/blob/main/commands.md#linesuffix) */
     function lineSuffix(suffix: Doc): LineSuffix;
+
     /** @see [lineSuffixBoundary](https://github.com/prettier/prettier/blob/main/commands.md#linesuffixboundary) */
     const lineSuffixBoundary: LineSuffixBoundary;
+
     /** @see [literalline](https://github.com/prettier/prettier/blob/main/commands.md#literalline) */
     const literalline: Concat;
+
     /** @see [literallineWithoutBreakParent](https://github.com/prettier/prettier/blob/main/commands.md#hardlinewithoutbreakparent-and-literallinewithoutbreakparent) */
     const literallineWithoutBreakParent: LiterallineWithoutBreakParent;
+
     /** @see [markAsRoot](https://github.com/prettier/prettier/blob/main/commands.md#markasroot) */
     function markAsRoot(doc: Doc): Align;
+
     /** @see [softline](https://github.com/prettier/prettier/blob/main/commands.md#softline) */
     const softline: Softline;
+
     /** @see [trim](https://github.com/prettier/prettier/blob/main/commands.md#trim) */
     const trim: Trim;
+
     /** @see [cursor](https://github.com/prettier/prettier/blob/main/commands.md#cursor) */
     const cursor: Cursor;
   }
