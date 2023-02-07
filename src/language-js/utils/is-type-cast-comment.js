9842b29ca3d9f9f6d27403d939f6d878e3228164
fisker Cheung <lionkay@gmail.com>
2023-02-07 15:19:09 +0800

    Recognize `@satisfies` in Closure-style type casts (#14262)

--- src/language-js/utils/is-type-cast-comment.js
+++ src/language-js/utils/is-type-cast-comment.js
@@ -16,8 +16,8 @@ function isTypeCastComment(comment) {
     comment.value[0] === "*" &&
     // TypeScript expects the type to be enclosed in curly brackets, however
     // Closure Compiler accepts types in parens and even without any delimiters at all.
-    // That's why we just search for "@type".
-    /@type\b/.test(comment.value)
+    // That's why we just search for "@type" and "@satisfies".
+    /@(?:type|satisfies)\b/.test(comment.value)
   );
 }
 
