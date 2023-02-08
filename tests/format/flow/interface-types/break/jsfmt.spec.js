f060a93a7c9e5792593a6ccb268cb6afe01446da
fisker Cheung <lionkay@gmail.com>
2023-02-08 18:00:33 +0800

    Update tests

--- tests/format/flow/interface-types/break/jsfmt.spec.js
+++ tests/format/flow/interface-types/break/jsfmt.spec.js
@@ -1,3 +1,3 @@
-run_spec(import.meta, ["flow", "babel-flow"], { trailingComma: "none" });
-run_spec(import.meta, ["flow", "babel-flow"], { trailingComma: "es5" });
-run_spec(import.meta, ["flow", "babel-flow"], { trailingComma: "all" });
+run_spec(import.meta, ["flow"], { trailingComma: "none" });
+run_spec(import.meta, ["flow"], { trailingComma: "es5" });
+run_spec(import.meta, ["flow"], { trailingComma: "all" });
