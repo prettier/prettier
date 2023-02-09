1b76c33ec051318775f5f6d289e720016389e781
fisker Cheung <lionkay@gmail.com>
2023-02-09 14:39:50 +0800

    Linting

--- tests/format/js/trailing-whitespace/jsfmt.spec.js
+++ tests/format/js/trailing-whitespace/jsfmt.spec.js
@@ -1,6 +1,7 @@
-run_spec({
-  importMeta: import.meta,
-  snippets: [
-    "`\n   \n   \n` + `\n    \n    \n`;"
-  ]
-}, ["babel", "flow", "typescript"],);
+run_spec(
+  {
+    importMeta: import.meta,
+    snippets: ["`\n   \n   \n` + `\n    \n    \n`;"],
+  },
+  ["babel", "flow", "typescript"]
+);
