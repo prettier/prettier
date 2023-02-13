05ee2c780c665f66b34932d5e2fa69e6eb2c7eff
fisker Cheung <lionkay@gmail.com>
2023-02-13 16:02:13 +0800

    Fix `createParsers`

--- src/utils/create-parsers.js
+++ src/utils/create-parsers.js
@@ -1,21 +1,22 @@
-const createParsers =
-  process.env.PRETTIER_TARGET === "universal"
-    ? () => {}
-    : function (modules) {
-        const parsers = Object.create(null);
+function createParsers(modules) {
+  if (!modules) {
+    return;
+  }
 
-        for (const { importPlugin, parserNames } of modules) {
-          for (const parserName of parserNames) {
-            parsers[parserName] = async () => {
-              const {
-                default: { parsers: pluginParsers },
-              } = await importPlugin();
-              return Object.assign(parsers, pluginParsers)[parserName];
-            };
-          }
-        }
+  const parsers = Object.create(null);
 
-        return parsers;
+  for (const { importPlugin, parserNames } of modules) {
+    for (const parserName of parserNames) {
+      parsers[parserName] = async () => {
+        const {
+          default: { parsers: pluginParsers },
+        } = await importPlugin();
+        return Object.assign(parsers, pluginParsers)[parserName];
       };
+    }
+  }
+
+  return parsers;
+}
 
 export default createParsers;
