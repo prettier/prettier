39602a5282d90846877b872f61555fcae17968e7
fisker Cheung <lionkay@gmail.com>
2023-02-16 15:44:02 +0800

    Fix

--- src/language-markdown/parser-markdown.js
+++ src/language-markdown/parser-markdown.js
@@ -1,16 +1,13 @@
-import assert from "node:assert";
 import remarkParse from "remark-parse";
 import unified from "unified";
 import remarkMath from "remark-math";
 import parseFrontMatter from "../utils/front-matter/parse.js";
 import { hasPragma } from "./pragma.js";
 import { locStart, locEnd } from "./loc.js";
-import { BLOCKS_REGEX, esSyntax } from "./mdx.js";
 import gfm from "./unified-plugins/gfm.js";
-import htmlToJsx from "./unified-plugins/html-to-jsx.js";
 import liquid from "./unified-plugins/liquid-for-micromark.js";
 import wikiLink from "./unified-plugins/wiki-link-for-micromark.js";
-import looseItems from "./unified-plugins/loose-items.js";
+// import looseItems from "./unified-plugins/loose-items.js";
 
 /**
  * based on [MDAST](https://github.com/syntax-tree/mdast) with following modifications:
@@ -26,15 +23,12 @@ import looseItems from "./unified-plugins/loose-items.js";
  * interface Sentence { children: Array<Word | Whitespace> }
  * interface InlineCode { children: Array<Sentence> }
  */
-function createParse({ isMDX }) {
-  // assert(!isMDX);
+function createParse() {
   const processor = unified()
     .use(remarkParse)
     .use(remarkMath)
     .use(gfm)
-    // .use(isMDX ? esSyntax : identity)
     .use(liquid)
-    // .use(isMDX ? htmlToJsx : identity)
     .use(wikiLink);
   // .use(looseItems);
 
@@ -50,22 +44,18 @@ function createParse({ isMDX }) {
   };
 }
 
-const baseParser = {
+const parser = {
   astFormat: "mdast",
   hasPragma,
   locStart,
   locEnd,
+  parse: createParse(),
 };
-
-const markdownParser = { ...baseParser, parse: createParse({ isMDX: false }) };
-
-const mdxParser = { ...baseParser, parse: createParse({ isMDX: true }) };
 
 const markdown = {
   parsers: {
-    remark: markdownParser,
-    markdown: markdownParser,
-    mdx: mdxParser,
+    remark: parser,
+    markdown: parser,
   },
 };
 
