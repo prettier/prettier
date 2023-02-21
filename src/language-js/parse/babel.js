b348908acc3b1a005a49941b9a5afbb04c9d4848
fisker Cheung <lionkay@gmail.com>
2023-02-21 11:26:31 +0800

    Update options for `decorators` plugin again

--- src/language-js/parse/babel.js
+++ src/language-js/parse/babel.js
@@ -37,7 +37,7 @@ const parseOptions = {
     "functionSent",
     "throwExpressions",
     "partialApplication",
-    ["decorators", { allowCallParenthesized: true }],
+    "decorators",
     "importAssertions",
     "decimal",
     "moduleBlocks",
