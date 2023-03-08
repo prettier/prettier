0be5306c0afdd6b87623b60763ac2ee0aa4ec707
fisker <lionkay@gmail.com>
2023-03-08 10:09:21 +0800

    Rewrite `getCursorNode`

---
+++ src/main/ast-utils.js
@@ -0,0 +1,34 @@
+import isObject from "../utils/is-object.js";
+
+function* getChildren(node, options) {
+  const { getVisitorKeys, filter = () => true } = options;
+  const isMatchedNode = (node) => isObject(node) && filter(node);
+
+  for (const key of getVisitorKeys(node)) {
+    const value = node[key];
+
+    if (Array.isArray(value)) {
+      for (const child of value) {
+        if (isMatchedNode(child)) {
+          yield child;
+        }
+      }
+    } else if (isMatchedNode(value)) {
+      yield value;
+    }
+  }
+}
+
+function* getDescendants(node, options) {
+  const nodes = [node];
+  while (nodes.length > 0) {
+    const node = nodes.pop();
+
+    for (const child of getChildren(node, options)) {
+      nodes.push(child);
+      yield child;
+    }
+  }
+}
+
+export { getDescendants, getChildren };
--- src/main/get-cursor-node.js
+++ src/main/get-cursor-node.js
@@ -1,41 +1,23 @@
 import createGetVisitorKeysFunction from "./create-get-visitor-keys-function.js";
+import { getDescendants } from "./ast-utils.js";
 
 function getCursorNode(ast, options) {
   const { cursorOffset, locStart, locEnd } = options;
-
   const getVisitorKeys = createGetVisitorKeysFunction(
     options.printer.getVisitorKeys
   );
+  const nodeContainsCursor = (node) =>
+    locStart(node) <= cursorOffset && locEnd(node) >= cursorOffset;
 
-  const nodes = getChildren(ast, {
+  let cursorNode = ast;
+  for (const node of getDescendants(ast, {
     getVisitorKeys,
-    comparator(nodeA, nodeB) {
-      const lengthA = locEnd(nodeA) - locStart(nodeA);
-      const lengthB = locEnd(nodeB) - locStart(nodeB);
-      return lengthA - lengthB;
-    },
-    containsCursor(node) {
-      return locStart(node) <= cursorOffset && locEnd(node) >= cursorOffset;
-    },
-  });
-
-  return nodes[0] ?? ast;
-}
-
-function getChildren(node, options) {
-  if (
-    !(node !== null && typeof node === "object" && options.containsCursor(node))
-  ) {
-    return [];
+    filter: nodeContainsCursor,
+  })) {
+    cursorNode = node;
   }
 
-  return [
-    ...options
-      .getVisitorKeys(node)
-      .flatMap((key) => node[key])
-      .flatMap((node) => getChildren(node, options)),
-    node,
-  ].sort(options.comparator);
+  return cursorNode;
 }
 
 export default getCursorNode;
