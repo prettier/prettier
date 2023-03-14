7b55e642c003dd1726f860c2e8bcc5b3a1864fd9
seiya <20365512+seiyab@users.noreply.github.com>
2023-03-14 22:17:06 +0900

    add tests

--- tests/format/vue/ts-expression/attribute-expr.vue
+++ tests/format/vue/ts-expression/attribute-expr.vue
@@ -2,4 +2,6 @@
 
 <template>
   <comp :foo="   (a:string)=>1"/>
+  <comp :foo=" <X extends Something & AnothoerOne, Y    extends unknown[]>(x:X,y:Y)=>y.length + x.foobar.abcdefg"/>
+  <comp :foo="(myFunction<T   |U>(qwerty,qwerty.qwerty?.qwerty)as any) + x.filter(abcdefg as never).join(xxx)"/>
 </template>
---
+++ tests/format/vue/ts-expression/filter.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<!-- vue filters are only allowed in v-bind and interpolation -->
+<template>
+  <div class="allowed">{{value | thisIsARealSuperLongFilterPipe("arg1", arg2 as unknown) | anotherPipeLongJustForFun | pipeTheThird}}</div>
+  <div class="allowed" v-bind:something='value | thisIsARealSuperLongFilterPipe("arg1", arg2 as unknown) | anotherPipeLongJustForFun | pipeTheThird'></div>
+  <div class="allowed" :class='value | thisIsARealSuperLongFilterPipe("arg1", arg2 as unknown) | anotherPipeLongJustForFun | pipeTheThird'></div>
+</template>
