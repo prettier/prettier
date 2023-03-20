49d50a04a6ff05b23b91dcad2b74a96e13235ddf
fisker <lionkay@gmail.com>
2023-03-20 19:29:56 +0800

    Add test

---
+++ tests/format/vue/interpolation/bitwise-or-operator.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{
+      fn(
+        bitwise | or | operator | a_long_long_long_long_long_long_long_long_long_long_variable
+      )
+      | filter
+      | filter
+      | filter
+      | filter
+    }}
+  </div>
+</template>
