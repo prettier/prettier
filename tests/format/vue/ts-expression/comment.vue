d6c5cedab389ea58ab925dad874eb7aae2e76c8e
seiya <20365512+seiyab@users.noreply.github.com>
2023-03-20 18:30:57 +0900

    add test for comment

---
+++ tests/format/vue/ts-expression/comment.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <p v-if="
+            // leading comment
+    isFolder(   /* NOTE: I like pizza */file)
+    // trailing comment
+    ">{{ /* leading comment */ (   file as /* NOTE: I like sushi */   mymodule.Folder   ).deadline /* trailing comment */    }}</p>
+  <prettier :format="   /* leading comment */ myFunc( /* NOTE: I like banana */ o as unknown ) /* trailing comment */" />
+</template>
+
+<script lang="ts"></script>
