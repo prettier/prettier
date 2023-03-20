9d16742f01453c3d5d901ca81e68b4cd10a5c50d
fisker <lionkay@gmail.com>
2023-03-20 13:47:40 +0800

    Copy tests from next branch

--- tests/format/js/export-default/export-default-from/export.js
+++ tests/format/js/export-default/export-default-from/export.js
@@ -4,5 +4,5 @@ export a, * as b from 'mod';
 export c, { foo } from 'mod';
 export * as d from 'mod';
 export { fooooooooooooooooooooooooooooooooooooooooooooooooo } from "fooooooooooooooooooooooooooooo";
-export Bar, { barrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr } from "barrrrrrrrrrrrrrrrrrrrrrrrrrrr";
+export Bar from "barrrrrrrrrrrrrrrrrrrrrrrrrrrr";
 export { foooooooooooooooooooooooooooooooooooooooooooooo, fooooooooooooooooooooooooooooooooooooooooooooooo  } from "fooooooooooooooooooooooooooooo";
