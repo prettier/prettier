d76003fba5a3e4a294783351f4b60d04847cb581
fisker <lionkay@gmail.com>
2023-03-21 13:29:02 +0800

    Simplify

--- src/language-css/utils/stringify-function-parameter.js
+++ src/language-css/utils/stringify-function-parameter.js
@@ -7,12 +7,9 @@ const getRootNode = require("./get-root-node.js");
  * @returns {string}
  */
 function stringifyFunctionParameter(node) {
-  const text = getRootNode(node).text.slice(
-    node.group.open.sourceIndex + 1,
-    node.group.close.sourceIndex
-  );
-  const innerText = text.trim();
-  return innerText;
+  return getRootNode(node)
+    .text.slice(node.group.open.sourceIndex + 1, node.group.close.sourceIndex)
+    .trim();
 }
 
 module.exports = stringifyFunctionParameter;
