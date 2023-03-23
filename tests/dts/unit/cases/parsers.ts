d132c279777dd6182b44d7670b496c242d3c96ae
fisker <lionkay@gmail.com>
2023-03-23 17:16:48 +0800

    Fix tests

--- tests/dts/unit/cases/parsers.ts
+++ tests/dts/unit/cases/parsers.ts
@@ -1,50 +1,54 @@
 import * as prettier from "../../../../dist/index.js";
 
-import * as acornEspreePlugin from "../../../../dist/plugins/acorn-and-espree.js";
-import * as angularPlugin from "../../../../dist/plugins/angular.js";
-import * as babelPlugin from "../../../../dist/plugins/babel.js";
-import * as flowPlugin from "../../../../dist/plugins/flow.js";
-import * as glimmerPlugin from "../../../../dist/plugins/glimmer.js";
-import * as graphqlPlugin from "../../../../dist/plugins/graphql.js";
-import * as htmlPlugin from "../../../../dist/plugins/html.js";
-import * as markdownPlugin from "../../../../dist/plugins/markdown.js";
-import * as typescriptPlugin from "../../../../dist/plugins/typescript.js";
-import * as meriyahPlugin from "../../../../dist/plugins/meriyah.js";
-import * as postcssPlugin from "../../../../dist/plugins/postcss.js";
-import * as yamlPlugin from "../../../../dist/plugins/yaml.js";
+import * as prettierPluginEstree from "../../../../dist/plugins/estree.js";
+import * as prettierPluginBabel from "../../../../dist/plugins/babel.js";
+import * as prettierPluginFlow from "../../../../dist/plugins/flow.js";
+import * as prettierPluginTypeScript from "../../../../dist/plugins/typescript.js";
+import * as prettierPluginAcorn from "../../../../dist/plugins/acorn.js";
+import * as prettierPluginMeriyah from "../../../../dist/plugins/meriyah.js";
+import * as prettierPluginAngular from "../../../../dist/plugins/angular.js";
+import * as prettierPluginPostcss from "../../../../dist/plugins/postcss.js";
+import * as prettierPluginGraphql from "../../../../dist/plugins/graphql.js";
+import * as prettierPluginMarkdown from "../../../../dist/plugins/markdown.js";
+import * as prettierPluginGlimmer from "../../../../dist/plugins/glimmer.js";
+import * as prettierPluginHtml from "../../../../dist/plugins/html.js";
+import * as prettierPluginYaml from "../../../../dist/plugins/yaml.js";
 
-acornEspreePlugin.parsers.acorn.parse;
-acornEspreePlugin.parsers.espree.parse;
-angularPlugin.parsers.__ng_action.parse;
-babelPlugin.parsers.babel.parse;
-flowPlugin.parsers.flow.parse;
-glimmerPlugin.parsers.glimmer.parse;
-graphqlPlugin.parsers.graphql.parse;
-htmlPlugin.parsers.html.parse;
-htmlPlugin.parsers.vue.parse;
-htmlPlugin.parsers.lwc.parse;
-htmlPlugin.parsers.angular.parse;
-markdownPlugin.parsers.markdown.parse;
-markdownPlugin.parsers.mdx.parse;
-typescriptPlugin.parsers.typescript.parse;
-meriyahPlugin.parsers.meriyah.parse;
-postcssPlugin.parsers.css.parse;
-postcssPlugin.parsers.less.parse;
-postcssPlugin.parsers.scss.parse;
-yamlPlugin.parsers.yaml.parse;
+prettierPluginBabel.parsers.babel.parse("code");
+prettierPluginFlow.parsers.flow.parse("code");
+prettierPluginTypeScript.parsers.typescript.parse("code");
+prettierPluginAcorn.parsers.acorn.parse("code");
+prettierPluginAcorn.parsers.espree.parse("code");
+prettierPluginMeriyah.parsers.meriyah.parse("code");
+prettierPluginAngular.parsers.__ng_action.parse("code");
+prettierPluginPostcss.parsers.css.parse("code");
+prettierPluginPostcss.parsers.less.parse("code");
+prettierPluginPostcss.parsers.scss.parse("code");
+prettierPluginGraphql.parsers.graphql.parse("code");
+prettierPluginMarkdown.parsers.remark.parse("code");
+prettierPluginMarkdown.parsers.markdown.parse("code");
+prettierPluginMarkdown.parsers.mdx.parse("code");
+prettierPluginGlimmer.parsers.glimmer.parse("code");
+prettierPluginHtml.parsers.html.parse("code");
+prettierPluginHtml.parsers.vue.parse("code");
+prettierPluginHtml.parsers.lwc.parse("code");
+prettierPluginHtml.parsers.angular.parse("code");
+prettierPluginYaml.parsers.yaml.parse("code");
 
 prettier.format("hello world", {
   plugins: [
-    acornEspreePlugin,
-    angularPlugin,
-    babelPlugin,
-    flowPlugin,
-    glimmerPlugin,
-    htmlPlugin,
-    markdownPlugin,
-    meriyahPlugin,
-    postcssPlugin,
-    typescriptPlugin,
-    yamlPlugin,
+  prettierPluginEstree,
+  prettierPluginBabel,
+  prettierPluginFlow,
+  prettierPluginTypeScript,
+  prettierPluginAcorn,
+  prettierPluginMeriyah,
+  prettierPluginAngular,
+  prettierPluginPostcss,
+  prettierPluginGraphql,
+  prettierPluginMarkdown,
+  prettierPluginGlimmer,
+  prettierPluginHtml,
+  prettierPluginYaml,
   ],
 });
