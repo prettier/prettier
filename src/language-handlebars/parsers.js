47018bf74091ac8987018fe02e84249a01e7c0d5
fisker <lionkay@gmail.com>
2023-03-23 16:56:50 +0800

    Remove parsers config file

--- src/language-handlebars/parsers.js
+++
@@ -1,8 +0,0 @@
-const parsers = [
-  {
-    importPlugin: () => import("./parser-glimmer.js"),
-    parserNames: ["glimmer"],
-  },
-];
-
-export default parsers;
--- src/language-json/index.js
+++
@@ -1,6 +0,0 @@
-import estreeJsonPrinter from "./printer-estree-json.js";
-
-export const printers = {
-  "estree-json": estreeJsonPrinter,
-};
-export { default as languages } from "./languages.evaluate.js";
