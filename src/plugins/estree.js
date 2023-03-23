9d1620eb195893f09d31591061151475cb6ed30b
fisker <lionkay@gmail.com>
2023-03-23 17:37:48 +0800

    Use named export, so printers can't be modified

--- src/plugins/estree.js
+++ src/plugins/estree.js
@@ -1,6 +1,6 @@
-import estreePrinter from "../language-js/printer-estree.js";
+import * as estreePrinter from "../language-js/printer-estree.js";
 import jsLanguages from "../language-js/languages.evaluate.js";
-import estreeJsonPrinter from "../language-json/printer-estree-json.js";
+import * as estreeJsonPrinter from "../language-json/printer-estree-json.js";
 import jsonLanguages from "../language-json/languages.evaluate.js";
 import jsOptions from "../language-js/options.js";
 
