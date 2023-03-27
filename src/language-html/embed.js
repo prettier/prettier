cf0826e87c449a7362d1f1ed2f9aeab054b9976e
fisker <lionkay@gmail.com>
2023-03-27 13:27:44 +0800

    Linting

--- src/language-html/embed.js
+++ src/language-html/embed.js
@@ -28,7 +28,6 @@ import {
   inferElementParser,
   htmlTrimPreserveIndentation,
   dedentString,
-  unescapeQuoteEntities,
   isVueSlotAttribute,
   isVueSfcBindingsAttribute,
   getTextValueParts,
