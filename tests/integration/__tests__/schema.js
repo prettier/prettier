66418cf17081ec31302d78c20182f4444b43695c
fisker <lionkay@gmail.com>
2023-03-27 16:15:24 +0800

    Fix path

--- tests/integration/__tests__/schema.js
+++ tests/integration/__tests__/schema.js
@@ -1,5 +1,5 @@
 import prettier from "../../config/prettier-entry.js";
-import generateSchema from "../../../scripts/utils/generate-schema.mjs";
+import generateSchema from "../../../scripts/utils/generate-schema.js";
 
 test("schema", async () => {
   const { options } = await prettier.getSupportInfo();
