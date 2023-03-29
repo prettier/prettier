0c7b8d575b36670f1ac6f10b1e3ff9d9f415d269
fisker <lionkay@gmail.com>
2023-03-29 16:46:50 +0800

    Revert

--- src/cli/options/get-context-options.js
+++ src/cli/options/get-context-options.js
@@ -24,6 +24,7 @@ function apiOptionToCliOption(apiOption) {
     delete cliOption.forwardToApi;
     delete cliOption.description;
     delete cliOption.oppositeDescription;
+    cliOption.deprecated = true;
   }
   /* c8 ignore stop */
 
