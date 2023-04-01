baea2e12283dfc18c49588c2ba9136037c47ee20
seiya <20365512+seiyab@users.noreply.github.com>
2023-04-01 18:41:16 +0900

    point my mistake

--- tests/format/js/arrows/currying-4.js
+++ tests/format/js/arrows/currying-4.js
@@ -3,3 +3,9 @@ Y(() => a ? b : c);
 Y(() => () => a ? b : c);
 
 Y(() => () => () => a ? b : c);
+
+Y(() => longlonglonglonglonglonglonglonglonglongCondition ? 'Prettier is an opinionated code formatter.' : 'Prettier takes your code and reprints it from scratch by taking the line length into account.')
+
+Y(() => () => longlonglonglonglonglonglonglonglonglongCondition ? 'Prettier is an opinionated code formatter.' : 'Prettier takes your code and reprints it from scratch by taking the line length into account.')
+
+Y(() => () => () => longlonglonglonglonglonglonglonglonglongCondition ? 'Prettier is an opinionated code formatter.' : 'Prettier takes your code and reprints it from scratch by taking the line length into account.')
