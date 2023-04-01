69764ae18185db7735bcbf74faa9c78570feb652
seiya <20365512+seiyab@users.noreply.github.com>
2023-04-01 16:33:46 +0900

    add more as-is test

---
+++ tests/format/jsx/jsx/arrow.js
@@ -0,0 +1,11 @@
+() => <Component><Children/></Component>;
+() => () => <Component><Children/></Component>;
+() => () => () => <Component><Children/></Component>;
+
+() => <div>Some text here</div>;
+() => () => <div>Some text here</div>;
+() => () => () => <div>Some text here</div>;
+
+() => <div>Long long long long long, very very long text. And more text. Another text.</div>;
+() => () => <div>Long long long long long, very very long text. And more text. Another text.</div>;
+() => () => () => <div>Long long long long long, very very long text. And more text. Another text.</div>;
