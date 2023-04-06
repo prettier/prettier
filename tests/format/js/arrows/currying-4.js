92a3560301a37eda597fafceaa8d9329814230a4
seiya <20365512+seiyab@users.noreply.github.com>
2023-04-06 23:21:43 +0900

    add more test

--- tests/format/js/arrows/currying-4.js
+++ tests/format/js/arrows/currying-4.js
@@ -9,3 +9,9 @@ Y(() => longlonglonglonglonglonglonglonglonglongCondition ? 'Prettier is an opin
 Y(() => () => longlonglonglonglonglonglonglonglonglongCondition ? 'Prettier is an opinionated code formatter.' : 'Prettier takes your code and reprints it from scratch by taking the line length into account.')
 
 Y(() => () => () => longlonglonglonglonglonglonglonglonglongCondition ? 'Prettier is an opinionated code formatter.' : 'Prettier takes your code and reprints it from scratch by taking the line length into account.')
+
+const x1 = (() => ['The', 'green', 'dragon', 'liked', 'to', 'knit', 'sweaters', 'for', 'the', 'fluffy', 'clouds', 'in', 'the', 'sky.'])
+
+const x2 = (() => () => ['The', 'green', 'dragon', 'liked', 'to', 'knit', 'sweaters', 'for', 'the', 'fluffy', 'clouds', 'in', 'the', 'sky.'])
+
+const x3 = (() => () => () => ['The', 'green', 'dragon', 'liked', 'to', 'knit', 'sweaters', 'for', 'the', 'fluffy', 'clouds', 'in', 'the', 'sky.'])
