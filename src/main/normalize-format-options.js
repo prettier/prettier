b5043bcca5dabf436f9c29d47d3a00bb215a14ed
fisker Cheung <lionkay@gmail.com>
2023-04-21 13:12:11 +0800

    Stop formatting unknown code with `babel` parser (#14718)

--- src/main/normalize-format-options.js
+++ src/main/normalize-format-options.js
@@ -20,6 +20,23 @@ const formatOptionsHiddenDefaults = {
 async function normalizeFormatOptions(options, opts = {}) {
   const rawOptions = { ...options };
 
+  if (!rawOptions.parser) {
+    if (!rawOptions.filepath) {
+      throw new UndefinedParserError(
+        "No parser and no file path given, couldn't infer a parser."
+      );
+    } else {
+      rawOptions.parser = inferParser(rawOptions, {
+        physicalFile: rawOptions.filepath,
+      });
+      if (!rawOptions.parser) {
+        throw new UndefinedParserError(
+          `No parser could be inferred for file "${rawOptions.filepath}".`
+        );
+      }
+    }
+  }
+
   const supportOptions = getSupportInfo({
     plugins: options.plugins,
     showDeprecated: true,
@@ -33,26 +50,6 @@ async function normalizeFormatOptions(options, opts = {}) {
         .map((option) => [option.name, option.default])
     ),
   };
-  if (!rawOptions.parser) {
-    if (!rawOptions.filepath) {
-      const logger = opts.logger || console;
-      logger.warn(
-        "No parser and no filepath given, using 'babel' the parser now " +
-          "but this will throw an error in the future. " +
-          "Please specify a parser or a filepath so one can be inferred."
-      );
-      rawOptions.parser = "babel";
-    } else {
-      rawOptions.parser = inferParser(rawOptions, {
-        physicalFile: rawOptions.filepath,
-      });
-      if (!rawOptions.parser) {
-        throw new UndefinedParserError(
-          `No parser could be inferred for file "${rawOptions.filepath}".`
-        );
-      }
-    }
-  }
 
   const parserPlugin = getParserPluginByParserName(
     rawOptions.plugins,
