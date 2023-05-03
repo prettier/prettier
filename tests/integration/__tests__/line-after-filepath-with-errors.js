ce0b990824f5aeb639ff8e5ec63b72cb1b994066
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-03 17:40:49 +0900

    Fix tests

--- tests/integration/__tests__/line-after-filepath-with-errors.js
+++ tests/integration/__tests__/line-after-filepath-with-errors.js
@@ -1,5 +1,11 @@
 describe("Line breaking after filepath with errors", () => {
-  runCli("cli/syntax-errors", ["--list-different", "*.js"]).test({ status: 2 });
-  runCli("cli/syntax-errors", ["--check", "*.js"]).test({ status: 2 });
-  runCli("cli/syntax-errors", ["--write", "*.js"]).test({ status: 2 });
+  runCli("cli/syntax-errors", ["--list-different", "*.js"], {
+    stdoutIsTTY: true,
+  }).test({ status: 2 });
+  runCli("cli/syntax-errors", ["--check", "*.js"], {
+    stdoutIsTTY: true,
+  }).test({ status: 2 });
+  runCli("cli/syntax-errors", ["--write", "*.js"], {
+    stdoutIsTTY: true,
+  }).test({ status: 2 });
 });
