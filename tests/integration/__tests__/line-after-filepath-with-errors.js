9749b01c2c7f9465a27d51a41cda451d42f31d34
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-04 17:20:22 +0900

    Add tests for no infered parser

--- tests/integration/__tests__/line-after-filepath-with-errors.js
+++ tests/integration/__tests__/line-after-filepath-with-errors.js
@@ -1,14 +1,18 @@
+import jestPathSerializer from "../path-serializer.js";
+
+expect.addSnapshotSerializer(jestPathSerializer);
+
 describe("Line breaking after filepath with errors", () => {
-  runCli("cli/syntax-errors", ["*.js"], {
+  runCli("cli/syntax-errors", ["./*.{js,foo}"], {
     stdoutIsTTY: true,
   }).test({ status: 2 });
-  runCli("cli/syntax-errors", ["--list-different", "*.js"], {
+  runCli("cli/syntax-errors", ["--list-different", "./*.{js,foo}"], {
     stdoutIsTTY: true,
   }).test({ status: 2 });
-  runCli("cli/syntax-errors", ["--check", "*.js"], {
+  runCli("cli/syntax-errors", ["--check", "./*.{js,foo}"], {
     stdoutIsTTY: true,
   }).test({ status: 2 });
-  runCli("cli/syntax-errors", ["--write", "*.js"], {
+  runCli("cli/syntax-errors", ["--write", "./*.{js,foo}"], {
     stdoutIsTTY: true,
   }).test({ status: 2 });
 });
