2503438c454d623892777b4ffe6c628522487854
fisker Cheung <lionkay@gmail.com>
2023-05-05 14:11:54 +0800

    Reduce size

--- scripts/build/build-javascript-module.js
+++ scripts/build/build-javascript-module.js
@@ -16,6 +16,7 @@ import esbuildPluginShimCommonjsObjects from "./esbuild-plugins/shim-commonjs-ob
 import esbuildPluginPrimitiveDefine from "./esbuild-plugins/primitive-define.js";
 import esbuildPluginAddDefaultExport from "./esbuild-plugins/add-default-export.js";
 import transform from "./transform/index.js";
+import transformEastAsianWidthModule from "./transform/eastasianwidth-module.js";
 import { getPackageFile } from "./utils.js";
 
 const { dirname, readJsonSync, require } = createEsmUtils(import.meta);
@@ -91,6 +92,11 @@ function getEsbuildOptions({ file, files, shouldCollectLicenses, cliOptions }) {
           )
           .replace(/\nfunction _os\(\).*?\n}/s, "")
           .replace(/\nfunction _detectNewline\(\).*?\n}/s, ""),
+    },
+    // Reduce size
+    {
+      module: require.resolve("eastasianwidth"),
+      process: transformEastAsianWidthModule,
     },
   ];
 
---
+++ scripts/build/transform/eastasianwidth-module.js
@@ -0,0 +1,66 @@
+import { parse } from "@babel/parser";
+import { traverseFast as traverse } from "@babel/types";
+import babelGenerator from "@babel/generator";
+import { outdent } from "outdent";
+
+const generate = babelGenerator.default;
+
+/* Doesn't work for dependencies, optional call, computed property, and spread arguments */
+
+function transformEastAsianWidthModule(original) {
+  let eastAsianWidthFunction;
+  traverse(parse(original), (node) => {
+    // We only need `eaw.eastAsianWidth`
+    if (
+      node.type === "AssignmentExpression" &&
+      node.left.type === "MemberExpression" &&
+      !node.left.computed &&
+      !node.left.optional &&
+      node.left.object.type === "Identifier" &&
+      node.left.object.name === "eaw" &&
+      node.left.property.type === "Identifier" &&
+      node.left.property.name === "eastAsianWidth"
+    ) {
+      eastAsianWidthFunction = node.right;
+    }
+  });
+
+  const functionBody = eastAsianWidthFunction.body.body;
+
+  // We don't need `if (...) {return 'A';}`
+  const index = functionBody.findIndex(
+    (node) =>
+      node.type === "IfStatement" &&
+      node.consequent.type === "BlockStatement" &&
+      node.consequent.body.length === 1 &&
+      node.consequent.body[0].type === "ReturnStatement" &&
+      node.consequent.body[0].argument.type === "StringLiteral" &&
+      node.consequent.body[0].argument.value === "A"
+  );
+
+  if (index === -1) {
+    throw new Error("Unexpected code");
+  }
+
+  functionBody.splice(index, 1);
+
+  // `export default {eastAsianWidth: function() {}}`
+  const { code } = generate({
+    type: "ExportDefaultDeclaration",
+    declaration: {
+      type: "ObjectExpression",
+      properties: [
+        {
+          type: "ObjectProperty",
+          key: { type: "Identifier", name: "eastAsianWidth" },
+          value: eastAsianWidthFunction,
+        },
+      ],
+    },
+  });
+  console.log(code);
+
+  return code;
+}
+
+export default transformEastAsianWidthModule;
