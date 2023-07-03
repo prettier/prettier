961149a1c6c57ffa80d7a3f11d721480fc37ebad
fisker <lionkay@gmail.com>
2023-07-03 10:11:40 +0800

    FIx tests

--- tests/format/js/regex/jsfmt.spec.js
+++ tests/format/js/regex/jsfmt.spec.js
@@ -2,7 +2,7 @@ run_spec(import.meta, ["babel", "flow", "typescript"], {
   errors: {
     flow: ["v-flag.js"],
     acorn: ["regexp-modifiers.js"],
-    espree: ["v-flag.js", "regexp-modifiers.js"],
+    espree: ["regexp-modifiers.js"],
     meriyah: ["v-flag.js", "regexp-modifiers.js"],
   },
 });
