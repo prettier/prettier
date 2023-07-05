00c994a9272da4f57622abaa01a5032a00fe979a
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-07-06 01:49:40 +0900

    Fix path

--- scripts/generate-schema.js
+++ scripts/generate-schema.js
@@ -2,7 +2,7 @@
 
 import { format } from "../index.js";
 import { getSupportInfo } from "../src/main/support.js";
-import generateSchema from "./utils/generate-schema.mjs";
+import generateSchema from "./utils/generate-schema.js";
 
 console.log(
   await format(JSON.stringify(generateSchema(getSupportInfo().options)), {
