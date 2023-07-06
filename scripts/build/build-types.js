8bbc6b00749c0bfca15b3065757699091832eb7b
Yue JIN <40021217+kingyue737@users.noreply.github.com>
2023-07-06 13:55:56 +0800

    fix: estree plugin type declaration

--- scripts/build/build-types.js
+++ scripts/build/build-types.js
@@ -38,10 +38,11 @@ async function buildPluginTypes({ file: { input, output } }) {
   const parserNames = Object.keys(plugin.parsers ?? {});
 
   // We only add `parsers` to types file, printers should not be used alone
-  // For `estree` plugin, we just write an empty file
+  // For `estree` plugin, we just export an empty object to ensure it treated
+  // as a module
   const code =
     parserNames.length === 0
-      ? ""
+      ? "export {};\n"
       : outdent`
         import { Parser } from "../index.js";
 
