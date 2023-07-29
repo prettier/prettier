7770728b02bdac0771a297e23d7b888b2548f96d
seiya <20365512+seiyab@users.noreply.github.com>
2023-07-29 14:33:44 +0900

    add as-is test

---
+++ tests/format/js/method-chain/2548-long-long-member-chain.js
@@ -0,0 +1,18 @@
+this.is.a.long.long.long.long.long.long.long.long.long.long.long.long.long.long.long.long.method();
+
+prettier.is.an.opinionated.code.formatter.with.support.for(
+  "JavaScript (including experimental features)",
+  "JSX",
+  "Angular",
+  "Vue",
+  "Flow",
+  "TypeScript",
+  "CSS, Less and SCSS",
+  "HTML",
+  "Ember/Handlebars",
+  "JSON",
+  "GraphQL",
+  "Markdown, including GFM and MDX",
+  "YAML")
+  .it.removes.all.original.styling.and.ensures.that.all.outputted
+  .code.conforms.to.a.consistent.style(see, this, blogpost);
