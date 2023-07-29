606570912057cea3935b8a6615bcf0539115c95c
seiya <20365512+seiyab@users.noreply.github.com>
2023-07-29 15:23:30 +0900

    add test

--- tests/format/js/method-chain/2548-long-long-member-chain.js
+++ tests/format/js/method-chain/2548-long-long-member-chain.js
@@ -16,3 +16,7 @@ prettier.is.an.opinionated.code.formatter.with.support.for(
   "YAML")
   .it.removes.all.original.styling.and.ensures.that.all.outputted
   .code.conforms.to.a.consistent.style(see, this, blogpost);
+
+By.far.the.biggest.reason.for.adopting.Prettier.is.to.stop.all.the['on-going'].debates.over.styles()
+.It.is.generally.accepted.that.having.a.common.style.guide.is.valuable.for.a.project.and.team.but.getting.there.is.a.very.painful.and.unrewarding.process()
+.People.get.very.emotional.around.particular.ways.of.writing.code.and.nobody.likes.spending.time.writing.and.receiving.nits()
