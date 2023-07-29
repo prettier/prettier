3aed3cb148da585068bd3c8be322a7301ea63731
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-07-29 14:14:09 +0900

    Add more tests for `--config`

--- tests/integration/__tests__/infer-plugins-ext-dir.js
+++ tests/integration/__tests__/infer-plugins-ext-dir.js
@@ -4,4 +4,15 @@ describe("infer file ext that supported by only plugins", () => {
     stdout: "src/file.foo 0ms",
     write: [],
   });
+
+  runCli("cli/infer-plugins-ext-dir-with-config/", [
+    "--config",
+    "foo.mjs",
+    "--write",
+    "src",
+  ]).test({
+    status: 0,
+    stdout: "src/file.foo 0ms",
+    write: [],
+  });
 });
