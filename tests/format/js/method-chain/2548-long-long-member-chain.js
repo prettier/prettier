10ef048dd5873f8d57b128de47d8d938a6f6bb64
seiya <20365512+seiyab@users.noreply.github.com>
2023-07-30 16:53:23 +0900

    add test

--- tests/format/js/method-chain/2548-long-long-member-chain.js
+++ tests/format/js/method-chain/2548-long-long-member-chain.js
@@ -20,3 +20,11 @@ prettier.is.an.opinionated.code.formatter.with.support.for(
 By.far.the.biggest.reason.for.adopting.Prettier.is.to.stop.all.the['on-going'].debates.over.styles()
 .It.is.generally.accepted.that.having.a.common.style.guide.is.valuable.for.a.project.and.team.but.getting.there.is.a.very.painful.and.unrewarding.process()
 .People.get.very.emotional.around.particular.ways.of.writing.code.and.nobody.likes.spending.time.writing.and.receiving.nits()
+
+So.why.choose.the.Prettier./* comment */style.guide/* comment */.over.any.other.random.style.guide?.()
+.Because.Prettier.is./* comment */the.only./* comment */style.guide.that.is.fully.automatic()
+// commment
+.Even
+// comment
+.if.Prettier.does.not.format.all.code['100%'].the.way['you’d'].like['it’s'].worth.the.sacrifice.given.the.unique.benefits.of.Prettier
+['don’t'].you.think?.()
