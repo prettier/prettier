78540f7d3f98e53735fe3c3105ab3d3434eb1f9a
Nicolò Ribaudo <hello@nicr.dev>
2023-08-02 19:50:05 +0200

    format

--- scripts/utils/esbuild-loader-node14.js
+++ scripts/utils/esbuild-loader-node14.js
@@ -1,6 +1,6 @@
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 import esbuild from "esbuild";
-import { readFile } from "fs/promises";
-import { fileURLToPath } from "url";
 
 // @babel/code-frame and @babel/highlight use syntax that is not available in Node.js 14,
 // such as ??=. When bundled into dist/ they are compiled to be compatible with Node.js 14,
@@ -8,14 +8,17 @@ import { fileURLToPath } from "url";
 // We can use an --experimental-loader to compile them on-the-fly with esbuild.
 
 export async function getSource(url, context, defaultGetSource) {
-  if (!/node_modules\/@babel\/(?:code-frame|highlight)/.test(url) || context.format !== "module") {
+  if (
+    !/node_modules\/@babel\/(?:code-frame|highlight)/.test(url) ||
+    context.format !== "module"
+  ) {
     return defaultGetSource(url, context, defaultGetSource);
   }
 
   const raw = await readFile(fileURLToPath(url));
   try {
-  const result = await esbuild.transform(raw, { target: "node14" });
-  return { source: result.code };
+    const result = await esbuild.transform(raw, { target: "node14" });
+    return { source: result.code };
   } catch (e) {
     e.message += ` (in ${url})`;
     throw e;
