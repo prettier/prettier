7efc9bc1e0c6420a22ba37e9783c6921d62bafc3
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-08-03 15:11:52 +0900

    Update dependents count

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -321,7 +321,7 @@ const UsersSection = ({ language }) => {
             <div style={{ marginLeft: ".7em", width: "260px" }}>
               <p>
                 More than{" "}
-                <strong data-placeholder="dependent-github">6.3 million</strong>{" "}
+                <strong data-placeholder="dependent-github">6.4 million</strong>{" "}
                 dependent repositories on GitHub
               </p>
               <Button href="https://github.com/prettier/prettier/network/dependents">
@@ -337,7 +337,7 @@ const UsersSection = ({ language }) => {
             <div style={{ marginLeft: ".7em", width: "260px" }}>
               <p>
                 More than{" "}
-                <strong data-placeholder="dependent-npm">14.8k</strong>{" "}
+                <strong data-placeholder="dependent-npm">15k</strong>{" "}
                 dependent packages on npm
               </p>
               <Button href="https://www.npmjs.com/browse/depended/prettier">
