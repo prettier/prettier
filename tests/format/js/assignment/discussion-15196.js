651099b932a4855174083b963e9039eb095309c5
seiya <20365512+seiyab@users.noreply.github.com>
2023-08-04 22:12:06 +0900

    add as-is test

--- tests/format/js/assignment/discussion-15196.js
+++ tests/format/js/assignment/discussion-15196.js
@@ -7,3 +7,13 @@ async function f() {
   const loooooooooooooooooooooooooong4 = !await looooooooooooooong.looooooooooooooong.loooooong;
   const loooooooooooooooooooooooooong5 = void !!await looooooooooooooong.looooooooooooooong.loooooong;
 }
+
+function* g() {
+  const { section, rubric, authors, tags } = yield utils.upsertCommonData(mainData);
+
+  const loooooooooooooooooooooooooong1 = yield looooooooooooooong.looooooooooooooong.loooooong;
+  const loooooooooooooooooooooooooong2 = yield looooooooooooooong.looooooooooooooong.loooooong();
+  const loooooooooooooooooooooooooong3 = yield looooooooooooooooooooooooooooooooooooooooooooog();
+  const loooooooooooooooooooooooooong4 = !(yield looooooooooooooong.looooooooooooooong.loooooong);
+  const loooooooooooooooooooooooooong5 = void !!(yield looooooooooooooong.looooooooooooooong.loooooong);
+}
