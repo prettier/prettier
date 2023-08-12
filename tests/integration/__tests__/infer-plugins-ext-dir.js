16d9d214bb5a63d75cf56aa44f1987eac490b828
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-08-12 14:33:24 +0900

    Update tests with content

--- tests/integration/__tests__/infer-plugins-ext-dir.js
+++ tests/integration/__tests__/infer-plugins-ext-dir.js
@@ -3,7 +3,12 @@ describe("infer file ext that supported by only plugins", () => {
     runCli("cli/infer-plugins-ext-dir/", ["--write", "src"]).test({
       status: 0,
       stdout: "src/file.foo 0ms",
-      write: [],
+      write: [
+        {
+          content: "!contents\n",
+          filename: "src/file.foo",
+        },
+      ],
     });
   });
 
@@ -16,7 +21,12 @@ describe("infer file ext that supported by only plugins", () => {
     ]).test({
       status: 0,
       stdout: "src/file.foo 0ms",
-      write: [],
+      write: [
+        {
+          content: "!contents\n",
+          filename: "src/file.foo",
+        },
+      ],
     });
   });
 
@@ -27,7 +37,12 @@ describe("infer file ext that supported by only plugins", () => {
     ]).test({
       status: 0,
       stdout: "src/file.foo 0ms",
-      write: [],
+      write: [
+        {
+          content: "!contents\n",
+          filename: "src/file.foo",
+        },
+      ],
     });
   });
 
@@ -38,7 +53,12 @@ describe("infer file ext that supported by only plugins", () => {
     ]).test({
       status: 0,
       stdout: "src/file.foo 0ms",
-      write: [],
+      write: [
+        {
+          content: '{"tabWidth":2,"bracketSpacing":false}',
+          filename: "src/file.foo",
+        },
+      ],
     });
   });
 });
