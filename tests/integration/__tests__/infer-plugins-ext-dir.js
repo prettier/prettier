aaf71b3abc2fa757f7244b3cc3bee742b9fddefc
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-08-12 14:21:56 +0900

    Add tests

--- tests/integration/__tests__/infer-plugins-ext-dir.js
+++ tests/integration/__tests__/infer-plugins-ext-dir.js
@@ -1,18 +1,33 @@
 describe("infer file ext that supported by only plugins", () => {
-  runCli("cli/infer-plugins-ext-dir/", ["--write", "src"]).test({
-    status: 0,
-    stdout: "src/file.foo 0ms",
-    write: [],
+  describe("basic", () => {
+    runCli("cli/infer-plugins-ext-dir/", ["--write", "src"]).test({
+      status: 0,
+      stdout: "src/file.foo 0ms",
+      write: [],
+    });
   });
 
-  runCli("cli/infer-plugins-ext-dir-with-config/", [
-    "--config",
-    "foo.mjs",
-    "--write",
-    "src",
-  ]).test({
-    status: 0,
-    stdout: "src/file.foo 0ms",
-    write: [],
+  describe("with config option", () => {
+    runCli("cli/infer-plugins-ext-dir-with-config/", [
+      "--config",
+      "foo.mjs",
+      "--write",
+      "src",
+    ]).test({
+      status: 0,
+      stdout: "src/file.foo 0ms",
+      write: [],
+    });
+  });
+
+  describe("with overrides options", () => {
+    runCli("cli/infer-plugins-ext-dir-with-overrides/", [
+      "--write",
+      "src",
+    ]).test({
+      status: 0,
+      stdout: "src/file.foo 0ms",
+      write: [],
+    });
   });
 });
