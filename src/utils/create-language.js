7ec717ff51699a08bc1297c1e29098e82946489c
fisker <lionkay@gmail.com>
2023-08-28 23:16:18 +0800

    Assert uniqueness in language properties

--- src/utils/create-language.js
+++ src/utils/create-language.js
@@ -1,9 +1,37 @@
-function createLanguage(linguistData, override) {
-  const { languageId, ...rest } = linguistData;
+function assertUniqueArray(array, message) {
+  if (new Set(array).size === array.length) {
+    return;
+  }
+
+  throw new Error(message);
+}
+
+function createLanguage(linguistData, getOverrides) {
+  const { languageId, ...restData } = linguistData;
+  const overrides = getOverrides(linguistData);
+
+  if (process.env.NODE_ENV !== "production" && overrides) {
+    for (const property of [
+      "parsers",
+      "extensions",
+      "interpreters",
+      "filenames",
+    ]) {
+      const value = overrides[property];
+
+      if (value) {
+        assertUniqueArray(
+          value,
+          `Language property '${property}' should be unique.`,
+        );
+      }
+    }
+  }
+
   return {
     linguistLanguageId: languageId,
-    ...rest,
-    ...override(linguistData),
+    ...restData,
+    ...overrides,
   };
 }
 
