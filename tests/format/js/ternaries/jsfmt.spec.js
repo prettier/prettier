09211db90da85d32f187b726f34a01b72f0b9125
Alex Rattray <rattray.alex@gmail.com>
2023-09-17 00:35:18 -0400

    Update tests with experimentalTernaries:true

--- tests/format/js/ternaries/jsfmt.spec.js
+++ tests/format/js/ternaries/jsfmt.spec.js
@@ -6,9 +6,17 @@ run_spec(import.meta, ["babel", "flow", "typescript"], {
   tabWidth: 4,
 });
 
-run_spec(import.meta, ["babel", "flow", "typescript"], { experimentalTernaries: true });
-run_spec(import.meta, ["babel", "flow", "typescript"], { experimentalTernaries: true, tabWidth: 4 });
-run_spec(import.meta, ["babel", "flow", "typescript"], { experimentalTernaries: true, useTabs: true });
+run_spec(import.meta, ["babel", "flow", "typescript"], {
+  experimentalTernaries: true,
+});
+run_spec(import.meta, ["babel", "flow", "typescript"], {
+  experimentalTernaries: true,
+  tabWidth: 4,
+});
+run_spec(import.meta, ["babel", "flow", "typescript"], {
+  experimentalTernaries: true,
+  useTabs: true,
+});
 run_spec(import.meta, ["babel", "flow", "typescript"], {
   experimentalTernaries: true,
   useTabs: true,
