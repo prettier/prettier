5bdc8f1987d70337d46bcfaa50ce628f6040f3eb
Sam Zhou <sam@developersam.com>
2023-09-18 17:31:48 +0000

    Revert unnecessary change

--- tests/config/utils/check-parsers.js
+++ tests/config/utils/check-parsers.js
@@ -97,7 +97,7 @@ const categoryParsers = new Map([
         "typescript",
         "babel-ts",
       ],
-      extensions: [".js", ".jsx", ".tsx"],
+      extensions: [".js", ".jsx"],
     },
   ],
   [
