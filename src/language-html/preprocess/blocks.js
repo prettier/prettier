57aeabb41faeb3625e03a0e71c633f801864dbc5
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-10-22 04:27:12 +0900

    Fix URL

--- src/language-html/preprocess/blocks.js
+++ src/language-html/preprocess/blocks.js
@@ -3,7 +3,7 @@ import { ParseSourceSpan } from "angular-html-parser";
 /**
  * Pattern used to identify an `else if` block.
  *
- * https://github.com/crisbeto/angular/blob/327896606832bf6fbfc8f23989755123028136a8/packages/compiler/src/render3/r3_control_flow.ts#L26
+ * https://github.com/angular/angular/blob/327896606832bf6fbfc8f23989755123028136a8/packages/compiler/src/render3/r3_control_flow.ts#L26
  *
  * */
 const ELSE_IF_PATTERN = /^else[^\S\n\r]+if/;
@@ -12,7 +12,7 @@ const ELSE_IF_PATTERN = /^else[^\S\n\r]+if/;
  * Predicate function that determines if a block with
  * a specific name cam be connected to an `if` block.
  *
- * https://github.com/crisbeto/angular/blob/327896606832bf6fbfc8f23989755123028136a8/packages/compiler/src/render3/r3_control_flow.ts#L47
+ * https://github.com/angular/angular/blob/327896606832bf6fbfc8f23989755123028136a8/packages/compiler/src/render3/r3_control_flow.ts#L47
  *
  * @param {string} name
  * @returns {boolean}
@@ -36,6 +36,9 @@ function normalizeBlockName(name) {
 
 /**
  * Find all connected blocks starting from a primary block.
+ *
+ * https://github.com/angular/angular/blob/327896606832bf6fbfc8f23989755123028136a8/packages/compiler/src/render3/r3_template_transform.ts#L396
+ *
  * @param {number} primaryBlockIndex
  * @param {Array<any>} siblings
  * @param {(name:string) => boolean} predicate
