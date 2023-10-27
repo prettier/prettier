708cba43d207f668c50b0418238ba92e7ac3010a
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-10-27 21:23:43 +0900

    Add more tests

--- tests/format/js/assignment/issue-15534.js
+++ tests/format/js/assignment/issue-15534.js
@@ -1,1 +1,4 @@
 params["redirectTo"] = `${window.location.pathname}${window.location.search}${window.location.hash}`;
+
+params["redirectTo"]["codePointAt"]["name"] =
+  `${window.location.pathname}${window.location.search}${window.location.hash}`;
