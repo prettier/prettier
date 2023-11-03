757ffd038eafcfab3b9a77cacdbe071264b7aea1
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-11-03 23:36:28 +0900

    Fix error test file name

--- tests/format/angular/control-flow-syntax/jsfmt.spec.js
+++ tests/format/angular/control-flow-syntax/jsfmt.spec.js
@@ -1,10 +1,10 @@
 const errors = {
   angular: [
-    "invalid-multiple-empty-blocks-in-for.html",
-    "invalid-empty-with-param-in-for.html",
-    "invalid-multiple-placeholder-blocks-in-defer.html",
-    "invalid-multiple-loading-blocks-in-defer.html",
-    "invalid-multiple-error-blocks-in-defer.html",
+    "invalid-for-block-empty-with-param.html",
+    "invalid-for-block-multiple-empty-blocks.html",
+    "invalid-defer-block-multiple-placeholder-blocks.html",
+    "invalid-defer-block-multiple-loading-blocks.html",
+    "invalid-defer-block-multiple-error-blocks.html",
   ],
 };
 
