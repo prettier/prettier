ca9009760ef3255fd5523a4e423c27cae709f9d3
fisker <lionkay@gmail.com>
2023-11-10 15:08:33 +0800

    TSC

--- src/language-html/print/angular-control-flow-block-settings.evaluate.js
+++ src/language-html/print/angular-control-flow-block-settings.evaluate.js
@@ -5,50 +5,50 @@ const DEFAULT_ANGULAR_CONTROL_FLOW_BLOCK_SETTINGS = {
 
 const ANGULAR_CONTROL_FLOW_BLOCK_SETTINGS = new Map(
   [
-    ["if", { isFollowingBlock: false, followingBlocks: ["else if", "else"] }],
-    [
-      "else if",
-      { isFollowingBlock: true, followingBlocks: ["else if", "else"] },
-    ],
-    ["else", { isFollowingBlock: true, followingBlocks: [] }],
+    {
+      name: "if",
+      isFollowingBlock: false,
+      followingBlocks: ["else if", "else"],
+    },
+    {
+      name: "else if",
+      isFollowingBlock: true,
+      followingBlocks: ["else if", "else"],
+    },
+    { name: "else", isFollowingBlock: true, followingBlocks: [] },
 
-    ["switch", { isFollowingBlock: false, followingBlocks: [] }],
-    ["case", { isFollowingBlock: false, followingBlocks: [] }],
-    ["default", { isFollowingBlock: false, followingBlocks: [] }],
+    { name: "switch", isFollowingBlock: false, followingBlocks: [] },
+    { name: "case", isFollowingBlock: false, followingBlocks: [] },
+    { name: "default", isFollowingBlock: false, followingBlocks: [] },
 
-    ["for", { isFollowingBlock: false, followingBlocks: ["empty"] }],
-    ["empty", { isFollowingBlock: true, followingBlocks: [] }],
+    { name: "for", isFollowingBlock: false, followingBlocks: ["empty"] },
+    { name: "empty", isFollowingBlock: true, followingBlocks: [] },
 
-    [
-      "defer",
-      {
-        isFollowingBlock: false,
-        followingBlocks: ["placeholder", "error", "loading"],
-      },
-    ],
-    [
-      "placeholder",
-      {
-        isFollowingBlock: true,
-        followingBlocks: ["placeholder", "error", "loading"],
-      },
-    ],
-    [
-      "error",
-      {
-        isFollowingBlock: true,
-        followingBlocks: ["placeholder", "error", "loading"],
-      },
-    ],
-    [
-      "loading",
-      {
-        isFollowingBlock: true,
-        followingBlocks: ["placeholder", "error", "loading"],
-      },
-    ],
-  ].map(([name, settings]) => [
-    name,
+    {
+      name: "defer",
+      isFollowingBlock: false,
+      followingBlocks: ["placeholder", "error", "loading"],
+    },
+
+    {
+      name: "placeholder",
+      isFollowingBlock: true,
+      followingBlocks: ["placeholder", "error", "loading"],
+    },
+
+    {
+      name: "error",
+      isFollowingBlock: true,
+      followingBlocks: ["placeholder", "error", "loading"],
+    },
+
+    {
+      name: "loading",
+      isFollowingBlock: true,
+      followingBlocks: ["placeholder", "error", "loading"],
+    },
+  ].map((settings) => [
+    settings.name,
     {
       DEFAULT_ANGULAR_CONTROL_FLOW_BLOCK_SETTINGS,
       ...settings,
