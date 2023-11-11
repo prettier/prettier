4d854fa4541a3ce3e21376cf17fefbea354ac773
dweizhe <d.weizhe@anyong.com.tw>
2023-11-12 01:11:42 +0800

    remove parameter parsing related code

--- src/language-html/print/angular-control-flow-block.js
+++ src/language-html/print/angular-control-flow-block.js
@@ -25,12 +25,10 @@ function printAngularControlFlowBlock(path, options, print) {
   docs.push("@", node.name);
 
   if (node.parameters.length > 0) {
-    const parametersDoc = group(
-      node.__embed_parameters_doc ?? [
-        indent([softline, join([";", line], path.map(print, "parameters"))]),
-        softline,
-      ],
-    );
+    const parametersDoc = group([
+      indent([softline, join([";", line], path.map(print, "parameters"))]),
+      softline,
+    ]);
 
     docs.push(" (", parametersDoc, ")");
   }
