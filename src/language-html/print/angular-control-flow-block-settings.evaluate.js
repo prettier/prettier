0c9f4db01bbc5dddc7de0bc547e9899883de8a94
fisker <lionkay@gmail.com>
2023-11-12 22:15:15 +0800

    Simplify Control flow syntax print

--- src/language-html/print/angular-control-flow-block-settings.evaluate.js
+++ src/language-html/print/angular-control-flow-block-settings.evaluate.js
@@ -1,65 +1,23 @@
-const ANGULAR_CONTROL_FLOW_BLOCK_DEFAULT_SETTINGS = {
-  isFollowingBlock: false,
-  followingBlocks: new Set(),
-};
-
 const ANGULAR_CONTROL_FLOW_BLOCK_SETTINGS = new Map(
   [
-    {
-      name: "if",
-      isFollowingBlock: false,
-      followingBlocks: ["else if", "else"],
-    },
-    {
-      name: "else if",
-      isFollowingBlock: true,
-      followingBlocks: ["else if", "else"],
-    },
-    { name: "else", isFollowingBlock: true, followingBlocks: [] },
+    ["if", ["else if", "else"]],
+    ["else if", ["else if", "else"]],
+    ["else", []],
 
-    { name: "switch", isFollowingBlock: false, followingBlocks: [] },
-    { name: "case", isFollowingBlock: false, followingBlocks: [] },
-    { name: "default", isFollowingBlock: false, followingBlocks: [] },
+    ["switch", []],
+    ["case", []],
+    ["default", []],
 
-    { name: "for", isFollowingBlock: false, followingBlocks: ["empty"] },
-    { name: "empty", isFollowingBlock: true, followingBlocks: [] },
+    ["for", ["empty"]],
+    ["empty", []],
 
-    {
-      name: "defer",
-      isFollowingBlock: false,
-      followingBlocks: ["placeholder", "error", "loading"],
-    },
-
-    {
-      name: "placeholder",
-      isFollowingBlock: true,
-      followingBlocks: ["placeholder", "error", "loading"],
-    },
-
-    {
-      name: "error",
-      isFollowingBlock: true,
-      followingBlocks: ["placeholder", "error", "loading"],
-    },
-
-    {
-      name: "loading",
-      isFollowingBlock: true,
-      followingBlocks: ["placeholder", "error", "loading"],
-    },
-  ].map((settings) => [
-    settings.name,
-    {
-      ANGULAR_CONTROL_FLOW_BLOCK_DEFAULT_SETTINGS,
-      ...settings,
-      followingBlocks: settings.followingBlocks
-        ? new Set(settings.followingBlocks)
-        : ANGULAR_CONTROL_FLOW_BLOCK_DEFAULT_SETTINGS.followingBlocks,
-    },
-  ]),
+    ["defer", ["placeholder", "error", "loading"]],
+    ["placeholder", ["placeholder", "error", "loading"]],
+    ["error", ["placeholder", "error", "loading"]],
+    ["loading", ["placeholder", "error", "loading"]],
+  ]
+    .filter(([, followingBlockNames]) => followingBlockNames.length > 0)
+    .map(([name, followingBlockNames]) => [name, new Set(followingBlockNames)]),
 );
 
-export {
-  ANGULAR_CONTROL_FLOW_BLOCK_DEFAULT_SETTINGS,
-  ANGULAR_CONTROL_FLOW_BLOCK_SETTINGS,
-};
+export default ANGULAR_CONTROL_FLOW_BLOCK_SETTINGS;
