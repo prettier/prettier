820471f11b390d098a108b6bacaef10c1327d852
fisker <lionkay@gmail.com>
2023-11-12 19:33:44 +0800

    Use `shouldHugJsExpression`

--- src/language-html/embed/angular-control-flow-block-parameters.js
+++ src/language-html/embed/angular-control-flow-block-parameters.js
@@ -1,4 +1,4 @@
-import { formatAttributeValue } from "./utils.js";
+import { formatAttributeValue, shouldHugJsExpression } from "./utils.js";
 
 async function printAngularControlFlowBlockParameters(
   textToDoc,
@@ -9,11 +9,16 @@ async function printAngularControlFlowBlockParameters(
   const { node } = path;
   const code = node.parameters.map(({ expression }) => expression).join("; ");
 
-  const doc = await formatAttributeValue(code, textToDoc, {
-    parser: "__ng_directive",
-    __isInHtmlAttribute: false,
-    trailingComma: "none",
-  });
+  const doc = await formatAttributeValue(
+    code,
+    textToDoc,
+    {
+      parser: "__ng_directive",
+      __isInHtmlAttribute: false,
+      trailingComma: "none",
+    },
+    shouldHugJsExpression,
+  );
 
   // We have to put the doc on the node, since `node.parameters` is not a valid "node"
   node.__embed_parameters_doc = doc;
