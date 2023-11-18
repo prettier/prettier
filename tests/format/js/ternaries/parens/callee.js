1e7a3a8607dc57bd6cd252573145ffd946903602
fisker <lionkay@gmail.com>
2023-11-18 13:51:24 +0800

    Fix test

--- tests/format/js/ternaries/parens/callee.js
+++ tests/format/js/ternaries/parens/callee.js
@@ -1,11 +1,14 @@
-const stopDirectory = await (useCache
-  ? memoizedFindProjectRoot
-  : findProjectRootWithoutCache)(path.dirname(path.resolve(filePath)));
+// Wrapped in async function since flow and typescript not parsing TLA correctly
+async function foo() {
+  const stopDirectory = await (useCache
+    ? memoizedFindProjectRoot
+    : findProjectRootWithoutCache)(path.dirname(path.resolve(filePath)));
 
-const stopDirectory2 = await (useCache
-  ? memoizedFindProjectRoot
-  : findProjectRootWithoutCache)?.(path.dirname(path.resolve(filePath)));
+  const stopDirectory2 = await (useCache
+    ? memoizedFindProjectRoot
+    : findProjectRootWithoutCache)?.(path.dirname(path.resolve(filePath)));
 
-const projectRootFinder = new (
-  useCache ? MemoizedProjectRoot : ProjectRootFinder
-)();
+  const projectRootFinder = new (
+    useCache ? MemoizedProjectRoot : ProjectRootFinder
+  )();
+}
