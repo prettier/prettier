d3ddcc7262a7930f8b566ddeaefa09b25c4266f0
fisker <lionkay@gmail.com>
2023-11-27 13:12:12 +0000

    Fix snapshot

---
+++ tests/format/js/import-attributes/keyword-detect.js
@@ -0,0 +1,10 @@
+import "./test.json" /* assert */ with /* assert */  { type: "json" }
+import {} from "./test.json" /* assert */ with /* assert */  { type: "json" }
+import "./test.json" /* with */ assert /* with */  { type: "json" }
+import {} from "./test.json" /* with */ assert /* with */  { type: "json" }
+
+export {} from "./test.json" /* assert */ with /* assert */  { type: "json" }
+export {} from "./test.json" /* with */ assert /* with */  { type: "json" }
+
+export * from "./test.json" /* assert */ with /* assert */  { type: "json" }
+export * from "./test.json" /* with */ assert /* with */  { type: "json" }
