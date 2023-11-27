ec1f2ad4984f416527a299d306f03abee53a0c54
fisker <lionkay@gmail.com>
2023-11-27 12:26:06 +0000

    Fix snapshots

--- tests/format/js/import-attributes/jsfmt.spec.js
+++ tests/format/js/import-attributes/jsfmt.spec.js
@@ -27,13 +27,5 @@ run_spec(import.meta, ["babel", "typescript"], {
       "without-from.js",
       "non-type.js",
     ],
-    typescript: [
-      "empty.js",
-      "multi-types.js",
-      "static-import.js",
-      "re-export.js",
-      "without-from.js",
-      "non-type.js",
-    ],
   },
 });
