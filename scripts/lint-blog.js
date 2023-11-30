961703ad6da145241b6fecc4c82544143840b0e5
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-01 02:32:38 +0900

    Refactor

--- scripts/lint-blog.js
+++ scripts/lint-blog.js
@@ -11,13 +11,11 @@ const blogPostDir = path.join(__dirname, "../website/blog");
 const files = fs.readdirSync(blogPostDir);
 
 for (const file of files) {
-  if (file.endsWith(".md") && !includesTruncateComment(file)) {
-    console.error(`${file}: Should include a <!--truncate--> comment`);
-    process.exitCode = 1;
+  if (file.endsWith(".md")) {
+    const fileData = fs.readFileSync(path.join(blogPostDir, file), "utf8");
+    if (!fileData.includes("<!--truncate-->")) {
+      console.error(`${file}: Should include a <!--truncate--> comment`);
+      process.exitCode = 1;
+    }
   }
-}
-
-function includesTruncateComment(file) {
-  const data = fs.readFileSync(path.join(blogPostDir, file), "utf8");
-  return data.includes("<!--truncate-->");
 }
