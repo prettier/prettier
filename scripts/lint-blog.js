39f71f261a150d571e66a0aa87ee4dd8086f9bb5
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-01 03:26:33 +0900

    Use URL instead of path

--- scripts/lint-blog.js
+++ scripts/lint-blog.js
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
 
-import path from "node:path";
 import fs from "node:fs";
-import createEsmUtils from "esm-utils";
 
-const { __dirname } = createEsmUtils(import.meta);
-
-const blogPostDir = path.join(__dirname, "../website/blog");
+const blogPostDir = new URL("../website/blog/", import.meta.url);
 
 const files = fs.readdirSync(blogPostDir);
 
 for (const file of files) {
   if (file.endsWith(".md")) {
-    const fileData = fs.readFileSync(path.join(blogPostDir, file), "utf8");
+    const fileData = fs.readFileSync(new URL(file, blogPostDir), "utf8");
     if (!fileData.includes("<!--truncate-->")) {
       console.error(`${file}: Should include a <!--truncate--> comment`);
       process.exitCode = 1;
