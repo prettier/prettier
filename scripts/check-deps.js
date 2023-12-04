e708f3d495ff6a2a4beeff4a7f858fc0fbbb1eef
fisker <lionkay@gmail.com>
2023-12-05 01:11:45 +0800

    Add `fix:deps`

--- scripts/check-deps.js
+++ scripts/check-deps.js
@@ -1,21 +1,53 @@
 import fs from "node:fs/promises";
+import { execa } from "execa";
 import chalk from "chalk";
 
-const packageJson = JSON.parse(
-  await fs.readFile(new URL("../package.json", import.meta.url)),
-);
+const PACKAGE_JSON_FILE = new URL("../package.json", import.meta.url);
+const shouldFix = process.argv.includes("--fix");
 
-validateDependencyObject(packageJson.dependencies);
-validateDependencyObject(packageJson.devDependencies);
+async function checkDependencies(shouldFix) {
+  const packageJson = JSON.parse(await fs.readFile(PACKAGE_JSON_FILE));
 
-function validateDependencyObject(object) {
-  for (const key of Object.keys(object)) {
-    if (object[key][0] === "^" || object[key][0] === "~") {
-      console.error(
-        chalk.red("error"),
-        `Dependency "${chalk.bold.red(key)}" should be pinned.`,
+  let changed = false;
+
+  for (const kind of [
+    "dependencies",
+    "devDependencies",
+    "peerDependencies",
+    "optionalDependencies",
+    "resolutions",
+  ]) {
+    if (!packageJson[kind]) {
+      continue;
+    }
+
+    for (const [name, version] of Object.entries(packageJson[kind])) {
+      if (!(version.startsWith("^") || version.startsWith("~"))) {
+        continue;
+      }
+
+      if (shouldFix) {
+        packageJson[kind][name] = version.slice(1);
+        changed ||= true;
+      } else {
+        console.error(
+          chalk.red("error"),
+          `"${chalk.bold.red(name)}" in "${kind}" should be pinned.`,
+        );
+        process.exitCode = 1;
+      }
+    }
+
+    if (shouldFix && changed) {
+      await fs.writeFile(
+        PACKAGE_JSON_FILE,
+        JSON.stringify(packageJson, undefined, 2),
       );
-      process.exitCode = 1;
+      await execa("yarn");
+
+      return checkDependencies(/* shouldFix */ false);
     }
   }
 }
+
+await checkDependencies(shouldFix);
