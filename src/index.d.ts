7c66b076f9ea6cb85cb1105de6660f91a53d883c
Mark Amery <markrobertamery@gmail.com>
2023-12-05 10:59:06 +0000

    Revert "Eliminate CursorOptions type; treat cursorOffset just like any other option"
    This reverts commit 43c69445e92e319ab4614e83e534de743e569455.

--- src/index.d.ts
+++ src/index.d.ts
@@ -354,11 +354,6 @@ export interface RequiredOptions extends doc.printer.Options {
    */
   jsxBracketSameLine: boolean;
   /**
-   * Specify where the cursor is.
-   * @default -1
-   */
-  cursorOffset: number;
-  /**
    * Format only a segment of a file.
    * @default 0
    */
@@ -548,6 +543,13 @@ export interface Printer<T = any> {
     | undefined;
 }
 
+export interface CursorOptions extends Options {
+  /**
+   * Specify where the cursor is.
+   */
+  cursorOffset: number;
+}
+
 export interface CursorResult {
   formatted: string;
   cursorOffset: number;
@@ -572,7 +574,7 @@ export function check(source: string, options?: Options): Promise<boolean>;
  */
 export function formatWithCursor(
   source: string,
-  options: Options,
+  options: CursorOptions,
 ): Promise<CursorResult>;
 
 export interface ResolveConfigOptions {
--- src/standalone.d.ts
+++ src/standalone.d.ts
@@ -1,4 +1,4 @@
-import { CursorResult, Options, SupportInfo } from "./index.js";
+import { CursorOptions, CursorResult, Options, SupportInfo } from "./index.js";
 
 /**
  * formatWithCursor both formats the code, and translates a cursor position from unformatted code to formatted code.
@@ -13,7 +13,7 @@ import { CursorResult, Options, SupportInfo } from "./index.js";
  */
 export function formatWithCursor(
   source: string,
-  options: Options,
+  options: CursorOptions,
 ): Promise<CursorResult>;
 
 /**
