1851fb24d57b6b176900a643e85642f64185a866
fisker <lionkay@gmail.com>
2023-12-15 08:09:10 +0800

    Add polyfill

--- tests/unit/syntax-transform.js
+++ tests/unit/syntax-transform.js
@@ -50,10 +50,28 @@ test(".at", () => {
   expect(transform("foo.at?.(-1)")).toMatchInlineSnapshot(`"foo.at?.(-1)"`);
 });
 
-test(".replaceAll", () => {
+test("String#replaceAll", () => {
   expect(transform("foo.replaceAll('a', 'b')")).toMatchInlineSnapshot(`
     "import __stringReplaceAll from "<SHIMS>/string-replace-all.js";
 
     __stringReplaceAll( /* isOptionalObject*/false, foo, 'a', 'b');"
   `);
+});
+
+test("Array#findLast", () => {
+  expect(transform("foo.findLast(callback)")).toMatchInlineSnapshot(`
+    "import __arrayFindLast from "<SHIMS>/array-find-last.js";
+
+    __arrayFindLast( /* isOptionalObject*/false, foo, callback);"
+  `);
+  expect(transform("foo?.findLast(callback)")).toMatchInlineSnapshot(`
+    "import __arrayFindLast from "<SHIMS>/array-find-last.js";
+
+    __arrayFindLast( /* isOptionalObject*/true, foo, callback);"
+  `);
+
+  // Don't support
+  expect(
+    transform("foo.findLast(callback, thisArgument)"),
+  ).toMatchInlineSnapshot(`"foo.findLast(callback, thisArgument)"`);
 });
