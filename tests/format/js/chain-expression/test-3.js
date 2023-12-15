7e650dfd9a83747cf4655c84d124f98db9dcb537
fisker <lionkay@gmail.com>
2023-12-15 17:10:05 +0800

    Test for ts

--- tests/format/js/chain-expression/test-3.js
+++ tests/format/js/chain-expression/test-3.js
@@ -1,18 +1,4 @@
 function getLanguageByInterpreter(languages, file) {
-  if (
-    process.env.PRETTIER_TARGET === "universal" ||
-    !file ||
-    getFileBasename(file).includes(".")
-  ) {
-    return;
-  }
-
-  const interpreter = getInterpreter(file);
-
-  if (!interpreter) {
-    return;
-  }
-
   return languages.find(
     (language) => language.interpreters?.includes(interpreter),
   );
