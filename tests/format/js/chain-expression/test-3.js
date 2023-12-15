e408ae4bc105a62035a40fa1d8b1cac55f576eee
fisker <lionkay@gmail.com>
2023-12-15 15:47:15 +0800

    Update test

--- tests/format/js/chain-expression/test-3.js
+++ tests/format/js/chain-expression/test-3.js
@@ -1,5 +1,19 @@
-function a() {
+function getLanguageByInterpreter(languages, file) {
+  if (
+    process.env.PRETTIER_TARGET === "universal" ||
+    !file ||
+    getFileBasename(file).includes(".")
+  ) {
+    return;
+  }
+
+  const interpreter = getInterpreter(file);
+
+  if (!interpreter) {
+    return;
+  }
+
   return languages.find(
     (language) => language.interpreters?.includes(interpreter),
-   );
+  );
 }
--- tests/format/js/chain-expression/test-4.js
+++ tests/format/js/chain-expression/test-4.js
@@ -1,5 +1,19 @@
-{{{{{{{{
-                const indexes = params.ids.map(
-                    id => data[resource]?.findIndex(record => record.id == id)
-                 );
-}}}}}}}}
+// https://github.com/marmelab/react-admin/blob/8c1ddfeb4e011a94609d709c0f4f139fb5d480e8/packages/ra-data-localstorage/src/index.ts#L112
+export default (params) => {
+    return {
+        updateMany: (resource, params) => {
+            updateLocalStorage(() => {
+                params.ids.forEach(id => {
+                    const index = data[resource]?.findIndex(
+                        record => record.id == id
+                    );
+                    data[resource][index] = {
+                        ...data[resource][index],
+                        ...params.data,
+                    };
+                });
+            });
+            return baseDataProvider.updateMany(resource, params);
+        },
+    }
+}
