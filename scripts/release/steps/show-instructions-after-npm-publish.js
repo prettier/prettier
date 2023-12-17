659526b4ffe1afdcfd7acc8b59ccd32543fcae9a
fisker <lionkay@gmail.com>
2023-12-17 12:30:16 +0000

    Return early in `getReleaseUrl`

--- scripts/release/steps/show-instructions-after-npm-publish.js
+++ scripts/release/steps/show-instructions-after-npm-publish.js
@@ -40,28 +40,30 @@ export default async function showInstructionsAfterNpmPublish({
 }) {
   if (next) {
     console.log(`${chalk.green.bold(`Prettier ${version} published!`)}`);
-  } else {
-    const releaseUrl = getReleaseUrl(version, previousVersion);
-    console.log(
-      outdent`
-        ${chalk.green.bold(`Prettier ${version} published!`)}
-
-        ${chalk.yellow.bold("Some manual steps are necessary.")}
-
-        ${chalk.bold.underline("Create a GitHub Release")}
-        - Go to ${chalk.cyan.underline(releaseUrl)}
-        - Press ${chalk.bgGreen.black("Publish release ")}
-
-        ${chalk.bold.underline("Test the new release")}
-        - In a new session, run ${chalk.yellow(
-          "npm i prettier@latest",
-        )} in another directory
-        - Test the API and CLI
-
-        After that, we can proceed to bump this repo's Prettier dependency.
-      `,
-    );
+    await waitForEnter();
+    return;
   }
+
+  const releaseUrl = getReleaseUrl(version, previousVersion);
+  console.log(
+    outdent`
+      ${chalk.green.bold(`Prettier ${version} published!`)}
+
+      ${chalk.yellow.bold("Some manual steps are necessary.")}
+
+      ${chalk.bold.underline("Create a GitHub Release")}
+      - Go to ${chalk.cyan.underline(releaseUrl)}
+      - Press ${chalk.bgGreen.black("Publish release ")}
+
+      ${chalk.bold.underline("Test the new release")}
+      - In a new session, run ${chalk.yellow(
+        "npm i prettier@latest",
+      )} in another directory
+      - Test the API and CLI
+
+      After that, we can proceed to bump this repo's Prettier dependency.
+    `,
+  );
 
   await waitForEnter();
 }
