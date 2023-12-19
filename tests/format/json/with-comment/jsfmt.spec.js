b3f9de21a2f6484af0d8e7ae8996f85d6a8474df
fisker <lionkay@gmail.com>
2023-12-19 10:55:32 +0800

    More tests

--- tests/format/json/with-comment/jsfmt.spec.js
+++ tests/format/json/with-comment/jsfmt.spec.js
@@ -2,3 +2,5 @@ run_spec(import.meta, ["json"], { trailingComma: "es5" });
 run_spec(import.meta, ["json"], { trailingComma: "all" });
 run_spec(import.meta, ["json5"], { trailingComma: "es5" });
 run_spec(import.meta, ["json5"], { trailingComma: "all" });
+run_spec(import.meta, ["jsonc"], { trailingComma: "es5" });
+run_spec(import.meta, ["jsonc"], { trailingComma: "all" });
