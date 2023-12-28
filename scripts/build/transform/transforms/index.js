e9cf7ccb9d9518c0b41dc8b76c3ceeb0b2c7704f
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-28 23:51:50 +0900

    Run `fix:eslint`

--- scripts/build/transform/transforms/index.js
+++ scripts/build/transform/transforms/index.js
@@ -1,7 +1,7 @@
+import transformArrayFindLast from "./transform-array-find-last.js";
 import transformObjectHasOwnCall from "./transform-object-has-own.js";
 import transformRelativeIndexing from "./transform-relative-indexing.js";
 import transformStringReplaceAll from "./transform-string-replace-all.js";
-import transformArrayFindLast from "./transform-array-find-last.js";
 
 export default [
   transformObjectHasOwnCall,
