ee21a875bf7ea3a37ee6400db441e686b185a840
Archit Gajjar <architgajjar1992@gmail.com>
2024-01-03 00:33:47 -0800

    Fix:14234 added fix for formating graphql union type

--- src/language-graphql/printer-graphql.js
+++ src/language-graphql/printer-graphql.js
@@ -372,6 +372,7 @@ function genericPrint(path, options, print) {
 
     case "UnionTypeExtension":
     case "UnionTypeDefinition":
+      print("Graphql printer: UnionTypeDefinition detected");
       return group([
         printDescription(path, options, print),
         group([
@@ -384,7 +385,7 @@ function genericPrint(path, options, print) {
                 " =",
                 ifBreak("", " "),
                 indent([
-                  ifBreak([line, "  "]),
+                  ifBreak([line, "|  "]),
                   join([line, "| "], path.map(print, "types")),
                 ]),
               ]
