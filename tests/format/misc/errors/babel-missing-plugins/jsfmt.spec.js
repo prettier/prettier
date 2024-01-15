571a1969ab23a5482b2301efdcbf48b1af06440f
fisker <lionkay@gmail.com>
2024-01-15 21:48:11 +0800

    Add tests

---
+++ tests/format/misc/errors/babel-missing-plugins/jsfmt.spec.js
@@ -0,0 +1,10 @@
+run_spec(
+  {
+    importMeta: import.meta,
+    snippets: [
+      // https://github.com/babel/babel/commit/a466f9c310ace91484d4087f077ee6d6c8cd8789
+      "export type Foo = number;",
+    ],
+  },
+  ["babel"],
+);
