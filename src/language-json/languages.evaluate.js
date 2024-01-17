fd73796fa9846cae4e19b2e53894dea00488eccf
fisker <lionkay@gmail.com>
2024-01-17 17:34:32 +0800

    Add changelog

--- src/language-json/languages.evaluate.js
+++ src/language-json/languages.evaluate.js
@@ -18,7 +18,7 @@ const languages = [
   createLanguage(linguistLanguages["JSON with Comments"], () => ({
     parsers: ["jsonc"],
     vscodeLanguageIds: ["jsonc"],
-    // To many filenames from `linguist-languages` is not a valid JSONC file.
+    // Too many filenames from `linguist-languages` is not a valid JSONC file.
     // https://github.com/prettier/prettier/issues/15945#issuecomment-1895371835
     filenames: [
       "devcontainer.json",
