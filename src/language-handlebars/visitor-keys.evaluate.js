ba0ab2e9f09aa111696aba0361d1d2513980c44e
fisker <lionkay@gmail.com>
2024-02-03 22:15:33 +0800

    Exclude node types we don't care

--- src/language-handlebars/visitor-keys.evaluate.js
+++ src/language-handlebars/visitor-keys.evaluate.js
@@ -1,1 +1,13 @@
-export { visitorKeys as default } from "@glimmer/syntax";
+import { visitorKeys as glimmerVisitorKeys } from "@glimmer/syntax";
+
+const {
+  // These node types present on `ElementNode`, but we don't care about them
+  ElementStartNode,
+  ElementPartNode,
+  ElementEndNode,
+  ElementNameNode,
+
+  ...visitorKeys
+} = glimmerVisitorKeys;
+
+export default visitorKeys;
