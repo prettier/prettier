ba4c019e7d9e11270f3e803b06cb6ab7d9dc42fa
fisker <lionkay@gmail.com>
2024-02-03 22:26:34 +0800

    Remove from visitorKeys

--- src/language-handlebars/visitor-keys.evaluate.js
+++ src/language-handlebars/visitor-keys.evaluate.js
@@ -1,11 +1,16 @@
 import { visitorKeys as glimmerVisitorKeys } from "@glimmer/syntax";
 
 const {
-  // These node types present on `ElementNode`, but we don't care about them
+  // These node types presents on `ElementNode`, but we don't care about them
+  // https://github.com/glimmerjs/glimmer-vm/pull/1553
   ElementStartNode,
   ElementPartNode,
   ElementEndNode,
   ElementNameNode,
+
+  // This presents on `{Block,ElementNode}.blockParamNodes`, but we don't care about them
+  // https://github.com/glimmerjs/glimmer-vm/pull/1552
+  BlockParam,
 
   ...visitorKeys
 } = glimmerVisitorKeys;
