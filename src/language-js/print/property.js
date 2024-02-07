7ceb185ee40d445b55cd17799dec7ecd75da0325
fisker Cheung <lionkay@gmail.com>
2024-02-07 15:59:49 +0800

    Refactor `printPropertyKey` (#16055)

--- src/language-js/print/property.js
+++ src/language-js/print/property.js
@@ -1,17 +1,108 @@
+import isEs5IdentifierName from "@prettier/is-es5-identifier-name";
+
 import { printComments } from "../../main/comments/print.js";
 import printNumber from "../../utils/print-number.js";
 import printString from "../../utils/print-string.js";
-import {
-  isNumericLiteral,
-  isSimpleNumber,
-  isStringLiteral,
-  isStringPropSafeToUnquote,
-  rawText,
-} from "../utils/index.js";
+import { isNumericLiteral, isStringLiteral, rawText } from "../utils/index.js";
 import { printAssignment } from "./assignment.js";
 
 const needsQuoteProps = new WeakMap();
 
+// Matches “simple” numbers like `123` and `2.5` but not `1_000`, `1e+100` or `0b10`.
+function isSimpleNumber(numberString) {
+  return /^(?:\d+|\d+\.\d+)$/.test(numberString);
+}
+
+// Note: Quoting/unquoting numbers in TypeScript is not safe.
+//
+// let a = { 1: 1, 2: 2 }
+// let b = { '1': 1, '2': 2 }
+//
+// declare let aa: keyof typeof a;
+// declare let bb: keyof typeof b;
+//
+// aa = bb;
+// ^^
+// Type '"1" | "2"' is not assignable to type '1 | 2'.
+//   Type '"1"' is not assignable to type '1 | 2'.(2322)
+//
+// And in Flow, you get:
+//
+// const x = {
+//   0: 1
+//   ^ Non-string literal property keys not supported. [unsupported-syntax]
+// }
+//
+// Angular does not support unquoted numbers in expressions.
+//
+// So we play it safe and only unquote numbers for the JavaScript parsers.
+// (Vue supports unquoted numbers in expressions, but let’s keep it simple.)
+//
+// Identifiers can be unquoted in more circumstances, though.
+function isStringKeySafeToUnquote(node, options) {
+  return (
+    options.parser !== "json" &&
+    options.parser !== "jsonc" &&
+    isStringLiteral(node.key) &&
+    rawText(node.key).slice(1, -1) === node.key.value &&
+    ((isEs5IdentifierName(node.key.value) &&
+      // With `--strictPropertyInitialization`, TS treats properties with quoted names differently than unquoted ones.
+      // See https://github.com/microsoft/TypeScript/pull/20075
+      !(
+        (options.parser === "babel-ts" && node.type === "ClassProperty") ||
+        (options.parser === "typescript" && node.type === "PropertyDefinition")
+      )) ||
+      (isSimpleNumber(node.key.value) &&
+        String(Number(node.key.value)) === node.key.value &&
+        // TODO[@fisker]: `ImportAttribute` should not care about parser
+        node.type !== "ImportAttribute" &&
+        (options.parser === "babel" ||
+          options.parser === "acorn" ||
+          options.parser === "espree" ||
+          options.parser === "meriyah" ||
+          options.parser === "__babel_estree")))
+  );
+}
+
+function shouldQuotePropertyKey(path, options) {
+  const { key } = path.node;
+  return (
+    (key.type === "Identifier" ||
+      (isNumericLiteral(key) &&
+        isSimpleNumber(printNumber(rawText(key))) &&
+        // Avoid converting 999999999999999999999 to 1e+21, 0.99999999999999999 to 1 and 1.0 to 1.
+        String(key.value) === printNumber(rawText(key)) &&
+        // Quoting number keys is safe in JS and Flow, but not in TypeScript (as
+        // mentioned in `isStringKeySafeToUnquote`).
+        !(options.parser === "typescript" || options.parser === "babel-ts"))) &&
+    (options.parser === "json" ||
+      options.parser === "jsonc" ||
+      (options.quoteProps === "consistent" && needsQuoteProps.get(path.parent)))
+  );
+}
+
+/*
+- `ClassProperty`
+- `PropertyDefinition`
+- `ClassPrivateProperty`
+- `ClassAccessorProperty`
+- `AccessorProperty`
+- `ObjectMethod`
+- `Property`
+- `ObjectProperty`
+- `ClassMethod`
+- `ClassPrivateMethod`
+- `MethodDefinition
+- `Property`
+- `ObjectProperty`
+- `ImportAttribute`
+- `TSAbstractAccessorProperty` (TypeScript)
+- `TSAbstractPropertyDefinition` (TypeScript)
+- `TSAbstractMethodDefinition` (TypeScript)
+- `TSDeclareMethod` (TypeScript)
+- `TSPropertySignature` (TypeScript)
+- `ObjectTypeProperty` (Flow)
+*/
 function printPropertyKey(path, options, print) {
   const { node } = path;
 
@@ -33,24 +124,12 @@ function printPropertyKey(path, options, print) {
         !prop.computed &&
         prop.key &&
         isStringLiteral(prop.key) &&
-        !isStringPropSafeToUnquote(prop, options),
+        !isStringKeySafeToUnquote(prop, options),
     );
     needsQuoteProps.set(parent, objectHasStringProp);
   }
 
-  if (
-    (key.type === "Identifier" ||
-      (isNumericLiteral(key) &&
-        isSimpleNumber(printNumber(rawText(key))) &&
-        // Avoid converting 999999999999999999999 to 1e+21, 0.99999999999999999 to 1 and 1.0 to 1.
-        String(key.value) === printNumber(rawText(key)) &&
-        // Quoting number keys is safe in JS and Flow, but not in TypeScript (as
-        // mentioned in `isStringPropSafeToUnquote`).
-        !(options.parser === "typescript" || options.parser === "babel-ts"))) &&
-    (options.parser === "json" ||
-      options.parser === "jsonc" ||
-      (options.quoteProps === "consistent" && needsQuoteProps.get(parent)))
-  ) {
+  if (shouldQuotePropertyKey(path, options)) {
     // a -> "a"
     // 1 -> "1"
     // 1.5 -> "1.5"
@@ -64,7 +143,7 @@ function printPropertyKey(path, options, print) {
   }
 
   if (
-    isStringPropSafeToUnquote(node, options) &&
+    isStringKeySafeToUnquote(node, options) &&
     (options.quoteProps === "as-needed" ||
       (options.quoteProps === "consistent" && !needsQuoteProps.get(parent)))
   ) {
@@ -85,6 +164,11 @@ function printPropertyKey(path, options, print) {
   return print("key");
 }
 
+/*
+- `Property`
+- `ObjectProperty`
+- `ImportAttribute`
+*/
 function printProperty(path, options, print) {
   const { node } = path;
   if (node.shorthand) {
--- src/language-js/utils/index.js
+++ src/language-js/utils/index.js
@@ -1,5 +1,3 @@
-import isEs5IdentifierName from "@prettier/is-es5-identifier-name";
-
 import { hasDescendant } from "../../utils/ast-utils.js";
 import getStringWidth from "../../utils/get-string-width.js";
 import hasNewline from "../../utils/has-newline.js";
@@ -573,61 +571,6 @@ function hasLeadingOwnLineComment(text, node) {
   return hasComment(node, CommentCheckFlags.Leading, (comment) =>
     hasNewline(text, locEnd(comment)),
   );
-}
-
-// Note: Quoting/unquoting numbers in TypeScript is not safe.
-//
-// let a = { 1: 1, 2: 2 }
-// let b = { '1': 1, '2': 2 }
-//
-// declare let aa: keyof typeof a;
-// declare let bb: keyof typeof b;
-//
-// aa = bb;
-// ^^
-// Type '"1" | "2"' is not assignable to type '1 | 2'.
-//   Type '"1"' is not assignable to type '1 | 2'.(2322)
-//
-// And in Flow, you get:
-//
-// const x = {
-//   0: 1
-//   ^ Non-string literal property keys not supported. [unsupported-syntax]
-// }
-//
-// Angular does not support unquoted numbers in expressions.
-//
-// So we play it safe and only unquote numbers for the JavaScript parsers.
-// (Vue supports unquoted numbers in expressions, but let’s keep it simple.)
-//
-// Identifiers can be unquoted in more circumstances, though.
-function isStringPropSafeToUnquote(node, options) {
-  return (
-    options.parser !== "json" &&
-    options.parser !== "jsonc" &&
-    isStringLiteral(node.key) &&
-    rawText(node.key).slice(1, -1) === node.key.value &&
-    ((isEs5IdentifierName(node.key.value) &&
-      // With `--strictPropertyInitialization`, TS treats properties with quoted names differently than unquoted ones.
-      // See https://github.com/microsoft/TypeScript/pull/20075
-      !(
-        (options.parser === "babel-ts" && node.type === "ClassProperty") ||
-        (options.parser === "typescript" && node.type === "PropertyDefinition")
-      )) ||
-      (isSimpleNumber(node.key.value) &&
-        String(Number(node.key.value)) === node.key.value &&
-        node.type !== "ImportAttribute" &&
-        (options.parser === "babel" ||
-          options.parser === "acorn" ||
-          options.parser === "espree" ||
-          options.parser === "meriyah" ||
-          options.parser === "__babel_estree")))
-  );
-}
-
-// Matches “simple” numbers like `123` and `2.5` but not `1_000`, `1e+100` or `0b10`.
-function isSimpleNumber(numberString) {
-  return /^(?:\d+|\d+\.\d+)$/.test(numberString);
 }
 
 /**
@@ -1265,11 +1208,9 @@ export {
   isSimpleCallArgument,
   isSimpleExpressionByNodeCount,
   isSimpleMemberExpression,
-  isSimpleNumber,
   isSimpleTemplateLiteral,
   isSimpleType,
   isStringLiteral,
-  isStringPropSafeToUnquote,
   isTemplateOnItsOwnLine,
   isTestCall,
   isTypeAnnotationAFunction,
