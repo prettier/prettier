5d431becf24cd380b71735b6dbd2fc9fe993c61b
fisker <lionkay@gmail.com>
2024-04-08 14:13:49 +0800

    Skip type test

--- jest.config.js
+++ jest.config.js
@@ -49,6 +49,8 @@ if (SKIP_TESTS_WITH_NEW_SYNTAX) {
     "<rootDir>/tests/integration/__tests__/normalize-doc.js",
     "<rootDir>/tests/integration/__tests__/doc-utils-clean-doc.js",
     "<rootDir>/tests/integration/__tests__/config-invalid.js",
+    // Fails on Node.js v14
+    "<rootDir>/tests/dts/unit/run.js",
   );
 }
 
