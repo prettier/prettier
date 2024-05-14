871129e8aff53d9d4b7930e846149e38ff257668
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-15 02:05:32 +0900

    test: add prettierrc

--- tests/integration/__tests__/ignores.js
+++ tests/integration/__tests__/ignores.js
@@ -6,4 +6,12 @@ describe("ignores", () => {
       status: 1,
     });
   });
+
+  it("should ignore files listed in .prettierrc", () => {
+    runCli("cli/ignores/prettierrc", ["*.js", "-l"]).test({
+      stderr: "",
+      stdout: "not-ignored.js",
+      status: 1,
+    });
+  });
 });
