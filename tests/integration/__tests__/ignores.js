1634b7fdaee9cd47b11fdaa5ff9a446fc44ce73d
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-15 02:42:36 +0900

    test: add ignore pattern flag

--- tests/integration/__tests__/ignores.js
+++ tests/integration/__tests__/ignores.js
@@ -1,16 +1,23 @@
 describe("ignores", () => {
   it("should ignore files listed in package.json > prettier.ignores", () => {
     runCli("cli/ignores/package-json", ["*.js", "-l"]).test({
-      stderr: "",
-      stdout: "not-ignored.js",
       status: 1,
     });
   });
 
   it("should ignore files listed in .prettierrc", () => {
     runCli("cli/ignores/prettierrc", ["*.js", "-l"]).test({
-      stderr: "",
-      stdout: "not-ignored.js",
+      status: 1,
+    });
+  });
+
+  it("should ignore by --ignore-pattern flag", () => {
+    runCli("cli/ignores/ignore-pattern", [
+      "*.js",
+      "--ignore-pattern",
+      "ignored.*",
+      "-l",
+    ]).test({
       status: 1,
     });
   });
