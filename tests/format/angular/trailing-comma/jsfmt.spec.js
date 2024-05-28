479bee9a2630a2bb4ee8b6ea38518eb347e6e73a
Sosuke Suzuki <aosukeke@gmail.com>
2024-05-29 00:16:23 +0900

    Follow new snapshot testing format

--- tests/format/angular/trailing-comma/jsfmt.spec.js
+++
@@ -1,3 +0,0 @@
-run_spec(import.meta, ["angular"], { trailingComma: "none" });
-run_spec(import.meta, ["angular"], { trailingComma: "es5" });
-run_spec(import.meta, ["angular"], { trailingComma: "all" });
