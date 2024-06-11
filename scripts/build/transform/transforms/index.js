a01a0d221260c061866bdd66ec6c64a4092558de
fisker <lionkay@gmail.com>
2024-06-11 16:53:08 +0800

    Use `Array#findLastIndex`

--- scripts/build/transform/transforms/index.js
+++ scripts/build/transform/transforms/index.js
@@ -1,11 +1,13 @@
 import transformArrayFindLast from "./transform-array-find-last.js";
+import transformArrayFindLastIndex from "./transform-array-find-last-index.js";
 import transformObjectHasOwnCall from "./transform-object-has-own.js";
 import transformRelativeIndexing from "./transform-relative-indexing.js";
 import transformStringReplaceAll from "./transform-string-replace-all.js";
 
 export default [
+  transformArrayFindLast,
+  transformArrayFindLastIndex,
   transformObjectHasOwnCall,
   transformRelativeIndexing,
   transformStringReplaceAll,
-  transformArrayFindLast,
 ];
