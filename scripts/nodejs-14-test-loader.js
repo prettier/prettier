1f0bff69ae6d460a41d819f8d049d737cbec93b4
fisker <lionkay@gmail.com>
2024-06-13 15:02:06 +0800

    Move loader

--- scripts/nodejs-14-test-loader.js
+++ scripts/nodejs-14-test-loader.js
@@ -3,10 +3,10 @@ import { fileURLToPath } from "node:url";
 
 import esbuild from "esbuild";
 
-// @babel/code-frame and @babel/highlight use syntax that is not available in Node.js 14,
-// such as ??=. When bundled into dist/ they are compiled to be compatible with Node.js 14,
+// `@babel/code-frame` and `@babel/highlight` use syntax that is not available in Node.js 14,
+// such as `??=`. When bundled into dist/ they are compiled to be compatible with Node.js 14,
 // however we also need to load them at runtime from node_modules to run our tests.
-// We can use an --experimental-loader to compile them on-the-fly with esbuild.
+// We can use an `--experimental-loader` to compile them on-the-fly with esbuild.
 
 export async function getSource(url, context, defaultGetSource) {
   if (
@@ -20,8 +20,8 @@ export async function getSource(url, context, defaultGetSource) {
   try {
     const result = await esbuild.transform(raw, { target: "node14" });
     return { source: result.code };
-  } catch (e) {
-    e.message += ` (in ${url})`;
-    throw e;
+  } catch (error) {
+    error.message += ` (in ${url})`;
+    throw error;
   }
 }
