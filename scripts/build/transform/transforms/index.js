7d0ae69b3870028cc7926b7646d70cbe4b1e5df4
Sosuke Suzuki <aosukeke@gmail.com>
2024-06-18 08:06:14 +0900

    Add comments for transforms

--- scripts/build/transform/transforms/index.js
+++ scripts/build/transform/transforms/index.js
@@ -4,10 +4,18 @@ import transformObjectHasOwnCall from "./transform-object-has-own.js";
 import transformRelativeIndexing from "./transform-relative-indexing.js";
 import transformStringReplaceAll from "./transform-string-replace-all.js";
 
+// These transforms are like Babel and core-js
+// Allow us to use JavaScript features in our source code that are not yet
+// implemented in the Node.js version we support
+
 export default [
+  // Node.js 18.0.0
   transformArrayFindLast,
   transformArrayFindLastIndex,
+  // Node.js 16.9.0
   transformObjectHasOwnCall,
+  // Node.js 16.6.0
   transformRelativeIndexing,
+  // Node.js 15.0.0
   transformStringReplaceAll,
 ];
