d7b8c05c5eb8d4189cad61cdcb234aada1ec6f0d
fisker <lionkay@gmail.com>
2024-06-21 13:51:52 +0800

    Transform `.at()`

--- scripts/build/transform/index.js
+++ scripts/build/transform/index.js
@@ -20,7 +20,8 @@ function transform(original, file) {
       file.startsWith(
         path.join(PROJECT_ROOT, "node_modules/angular-estree-parser/"),
       ) ||
-      file.startsWith(path.join(PROJECT_ROOT, "node_modules/jest-docblock/"))
+      file.startsWith(path.join(PROJECT_ROOT, "node_modules/jest-docblock/")) ||
+      file.startsWith(path.join(PROJECT_ROOT, "node_modules/espree/"))
     )
   ) {
     return original;
