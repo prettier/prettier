8bbc4be41d01f3f596dee97d7a5141c2b30454bc
seiya <20365512+seiyab@users.noreply.github.com>
2024-08-13 23:53:18 +0900

    Revert "add assertion for doc `fill`"
    This reverts commit fcf159e0564e2c645ee0c21ab1c1617bbd6fd450.

--- src/document/utils/assert-doc.js
+++ src/document/utils/assert-doc.js
@@ -1,10 +1,4 @@
-import { DOC_TYPE_LINE, DOC_TYPE_STRING } from "../constants.js";
-import getDocType from "./get-doc-type.js";
 import traverseDoc from "./traverse-doc.js";
-
-/**
- * @typedef {import("../builders.js").Doc} Doc
- */
 
 const checked = process.env.NODE_ENV !== "production" && new WeakSet();
 const noop = () => {};
@@ -40,47 +34,4 @@ const assertDocArray =
         }
       };
 
-const assertDocFill =
-  process.env.NODE_ENV === "production"
-    ? noop
-    : /**
-       * @param {Doc[]} parts
-       */
-      function (parts) {
-        assertDocArray(parts);
-        for (const [i, doc] of parts.entries()) {
-          if (i % 2 === 0) {
-            continue;
-          }
-          if (!isLineLikeDoc(doc)) {
-            const type = getDocType(doc);
-            throw new Error(
-              `Unexpected non-line-break doc at ${i}. Doc type is ${type}.`,
-            );
-          }
-        }
-      };
-
-/**
- * @param {Doc} doc
- * @returns {boolean}
- */
-function isLineLikeDoc(doc) {
-  let hasLine = false;
-  let hasString = false;
-  traverseDoc(doc, (doc) => {
-    switch (getDocType(doc)) {
-      case DOC_TYPE_LINE:
-        hasLine = true;
-        return true;
-      case DOC_TYPE_STRING:
-        hasString = true;
-        return false;
-      default:
-        return true;
-    }
-  });
-  return hasLine && !hasString;
-}
-
-export { assertDoc, assertDocArray, assertDocFill };
+export { assertDoc, assertDocArray };
