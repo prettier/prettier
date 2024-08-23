39d9a8d5e6320291210d3309138eeb17083bd268
fisker <lionkay@gmail.com>
2024-08-23 23:22:02 +0800

    Simplify

--- src/language-handlebars/embed.js
+++ src/language-handlebars/embed.js
@@ -1,33 +1,19 @@
 import { dedent, hardline, softline } from "../document/builders.js";
-import inferParser from "../utils/infer-parser.js";
 
-function embed(path, options) {
+function embed(path /*, options*/) {
   const { node } = path;
 
-  if (node.type === "TextNode" && path.parent.tag === "style") {
-    const parser = getCssParser(path, options);
-    if (!parser) {
-      return "";
-    }
-
-    return async (textToDoc) => {
-      const doc = await textToDoc(node.chars.trim(), { parser });
-      if (!doc) {
-        return [];
-      }
-      return [hardline, doc, dedent(softline)];
-    };
-  }
-
-  return null;
-}
-
-function getCssParser(path, options) {
-  if (path.parent.tag !== "style") {
+  if (node.type !== "TextNode" || path.parent.tag !== "style") {
     return;
   }
 
-  return inferParser(options, { language: "css" });
+  return async (textToDoc) => {
+    const doc = await textToDoc(node.chars, { parser: "css" });
+    if (!doc) {
+      return [];
+    }
+    return [hardline, doc, dedent(softline)];
+  };
 }
 
 export default embed;
