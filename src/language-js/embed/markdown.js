4aeda608034140203737465a7071ac4f0f5f238e
Kelvin Soh <kelvinsjk@gmail.com>
2024-08-27 23:49:58 +0800

    feat: support formatting markdown in js embed when there is interpolation

--- src/language-js/embed/markdown.js
+++ src/language-js/embed/markdown.js
@@ -4,26 +4,77 @@ import {
   literalline,
   softline,
 } from "../../document/builders.js";
-import { escapeTemplateCharacters } from "../print/template-literal.js";
+import { mapDoc } from "../../document/utils.js";
+import {
+  escapeTemplateCharacters,
+  printTemplateExpressions,
+} from "../print/template-literal.js";
 
 async function printEmbedMarkdown(textToDoc, print, path /*, options*/) {
   const { node } = path;
-  let text = node.quasis[0].value.raw.replaceAll(
-    /((?:\\\\)*)\\`/gu,
-    (_, backslashes) => "\\".repeat(backslashes.length / 2) + "`",
+  const composePlaceholder = (index) =>
+    `PRETTIER_MD_PLACEHOLDER_${index}_IN_JS`;
+  let hasIndent = false;
+  let indentation;
+  const text = node.quasis
+    .map((quasi, index, quasis) => {
+      let txt = quasi.value.raw.replaceAll(
+        /((?:\\\\)*)\\`/gu,
+        (_, backslashes) => "\\".repeat(backslashes.length / 2) + "`",
+      );
+      if (index === 0) {
+        indentation = getIndentation(txt);
+        hasIndent = getIndentation(txt) !== "";
+      }
+      if (hasIndent) {
+        txt = txt.replaceAll(new RegExp(`^${indentation}`, "gmu"), "");
+      }
+      return index === quasis.length - 1
+        ? txt
+        : txt + composePlaceholder(index);
+    })
+    .join("");
+
+  const expressionDocs = printTemplateExpressions(path, print);
+  const placeholderRegex = new RegExp(
+    composePlaceholder(String.raw`(\d+)`),
+    "gu",
   );
-  const indentation = getIndentation(text);
-  const hasIndent = indentation !== "";
-  if (hasIndent) {
-    text = text.replaceAll(new RegExp(`^${indentation}`, "gmu"), "");
-  }
   const doc = escapeTemplateCharacters(
     await textToDoc(text, { parser: "markdown", __inJsTemplate: true }),
     true,
   );
+
+  const contentDoc = mapDoc(doc, (doc) => {
+    if (typeof doc !== "string") {
+      return doc;
+    }
+
+    const parts = [];
+
+    const components = doc.split(placeholderRegex);
+    for (let i = 0; i < components.length; i++) {
+      const component = components[i];
+
+      if (i % 2 === 0) {
+        if (component) {
+          parts.push(component);
+        }
+        continue;
+      }
+
+      const placeholderIndex = Number(component);
+      parts.push(expressionDocs[placeholderIndex]);
+    }
+
+    return parts;
+  });
+
   return [
     "`",
-    hasIndent ? indent([softline, doc]) : [literalline, dedentToRoot(doc)],
+    hasIndent
+      ? indent([softline, contentDoc])
+      : [literalline, dedentToRoot(contentDoc)],
     softline,
     "`",
   ];
@@ -47,7 +98,7 @@ function printMarkdown(path /*, options*/) {
 function isMarkdown({ node, parent }) {
   return (
     parent?.type === "TaggedTemplateExpression" &&
-    node.quasis.length === 1 &&
+    node.quasis.length > 0 &&
     parent.tag.type === "Identifier" &&
     (parent.tag.name === "md" || parent.tag.name === "markdown")
   );
