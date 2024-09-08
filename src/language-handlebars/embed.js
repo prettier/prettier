6acb4d6f4ad534c2e39a6b40af44d77eb9a3fe39
fisker <lionkay@gmail.com>
2024-09-08 18:17:39 +0800

    Stricter AST check, fix AST_COMPARE test

--- src/language-handlebars/embed.js
+++ src/language-handlebars/embed.js
@@ -3,7 +3,20 @@ import { dedent, hardline, softline } from "../document/builders.js";
 function embed(path /*, options*/) {
   const { node } = path;
 
-  if (node.type !== "TextNode" || path.parent.tag !== "style") {
+  if (node.type !== "TextNode") {
+    return;
+  }
+
+  const { parent } = path;
+
+  if (
+    !(
+      parent.type === "ElementNode" &&
+      parent.tag === "style" &&
+      parent.children.length === 1 &&
+      parent.children[0] === node
+    )
+  ) {
     return;
   }
 
