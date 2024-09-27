5bb04801a2274ad59fb8ba86919c065508968ee3
Marcus Pettersen Irgens <m@mrcus.dev>
2024-09-27 13:53:37 +0200

    Ignore files in the Jujutsu directory (#16684)
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- tests/integration/__tests__/ignore-vcs-files.js
+++ tests/integration/__tests__/ignore-vcs-files.js
@@ -2,6 +2,7 @@ describe("ignores files in version control systems", () => {
   runCli("cli/ignore-vcs-files", [
     ".svn/file.js",
     ".hg/file.js",
+    ".jj/file.js",
     "file.js",
     "-l",
   ]).test({
