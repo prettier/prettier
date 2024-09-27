4d1e3dba15d46bc6a28c345afffcbd30c4f22ac6
fisker <lionkay@gmail.com>
2024-09-27 20:32:15 +0800

    Remove `eslint-plugin-import`

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ import eslintPluginStylisticJs from "@stylistic/eslint-plugin-js";
 import eslintPluginTypescriptEslint from "@typescript-eslint/eslint-plugin";
 import { isCI } from "ci-info";
 import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginImport from "eslint-plugin-import";
 import eslintPluginJest from "eslint-plugin-jest";
 import eslintPluginN from "eslint-plugin-n";
 import eslintPluginReact from "eslint-plugin-react";
@@ -158,20 +157,6 @@ export default [
 
       /* @typescript-eslint/eslint-plugin */
       "@typescript-eslint/prefer-ts-expect-error": "error",
-
-      /* eslint-plugin-import */
-      "import/no-extraneous-dependencies": [
-        "error",
-        {
-          devDependencies: [
-            "jest.config.js",
-            "tests/**",
-            "scripts/**",
-            "website/**/*",
-            "eslint.config.js",
-          ],
-        },
-      ],
 
       /* eslint-plugin-n */
       "n/no-path-concat": "error",
