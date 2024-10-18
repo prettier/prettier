448afdf6dbaad8dcdd8f0c235341a02da0a9ae34
fisker <lionkay@gmail.com>
2024-10-18 23:11:47 +0800

    Fix tests

--- tests/format/js/regex/format.test.js
+++ tests/format/js/regex/format.test.js
@@ -1,6 +1,5 @@
 runFormatTest(import.meta, ["babel", "flow", "typescript"], {
   errors: {
-    flow: ["v-flag.js"],
     acorn: ["regexp-modifiers.js"],
     espree: ["regexp-modifiers.js"],
     meriyah: ["regexp-modifiers.js"],
