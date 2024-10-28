40430928942070c5c6424ec9bd05ceebb9dacdfe
fisker <lionkay@gmail.com>
2024-10-28 11:23:04 +0800

    Fix tests

--- tests/format/js/import-attributes/format.test.js
+++ tests/format/js/import-attributes/format.test.js
@@ -1,26 +1,6 @@
 const errors = {
-  acorn: [
-    "dynamic-import.js",
-    "empty.js",
-    "multi-types.js",
-    "static-import.js",
-    "re-export.js",
-    "without-from.js",
-    "non-type.js",
-    "keyword-detect.js",
-    "long-sources.js",
-  ],
-  espree: [
-    "dynamic-import.js",
-    "empty.js",
-    "multi-types.js",
-    "static-import.js",
-    "re-export.js",
-    "without-from.js",
-    "non-type.js",
-    "keyword-detect.js",
-    "long-sources.js",
-  ],
+  acorn: ["multi-types.js"],
+  espree: ["multi-types.js"],
   meriyah: ["multi-types.js", "without-from.js", "keyword-detect.js"],
 };
 runFormatTest(import.meta, ["babel", "typescript"], { errors });
