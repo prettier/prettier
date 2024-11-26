efa55be0c1df8832bb9dc365a4f22cbf938b9fd1
fisker <lionkay@gmail.com>
2024-11-26 22:37:47 +0800

    Fix comments on class methods with decorators

---
+++ tests/format/typescript/comments/16889.ts
@@ -0,0 +1,20 @@
+class myClass {
+  @decorator
+  /**
+   * The method description
+   *
+  */
+  async method() {
+  }
+
+  @decorator
+  async /* comment */ method() {
+  }
+
+
+  @decorator
+  // line comment
+  async method2() {
+  }
+}
+
