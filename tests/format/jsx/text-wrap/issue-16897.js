330984ed2e800733c4707dd669d9cdb677fef779
seiya <20365512+seiyab@users.noreply.github.com>
2024-11-28 07:36:29 +0900

    omit redundant test case

--- tests/format/jsx/text-wrap/issue-16897.js
+++ tests/format/jsx/text-wrap/issue-16897.js
@@ -12,17 +12,3 @@ function HelloWorld(   ) {
     </div>
   )
 }
-
-function HelloWorld() {
-  return (
-    <div>
-      <div>
-        foo
-        <br />
-        bar{' '}
-        <span className="font-semibold">foobar foobar foobar foobar 12345</span>
-        , foobar foobar foobar
-      </div>
-    </div>
-  )
-}
