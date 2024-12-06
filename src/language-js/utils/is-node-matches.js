1b0423b85cf22550fe4a9e9d14b4ad068ce14682
fisker <lionkay@gmail.com>
2024-12-06 19:34:23 +0800

    FIx tests

--- src/language-js/utils/is-node-matches.js
+++ src/language-js/utils/is-node-matches.js
@@ -22,6 +22,7 @@ function isNodeMatchesNameOrPath(node, nameOrPath) {
 
     if (
       node.type !== "MemberExpression" ||
+      // @ts-expect-error -- Don't know why
       node.optional ||
       node.computed ||
       node.property.type !== "Identifier" ||
