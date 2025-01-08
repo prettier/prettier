28e9eae42e1a3c45a1a2887043da6ab673ff9712
Tatsunori Uchino <tats.u@live.jp>
2025-01-08 23:01:13 +0900

    Add module-sync to plugins

--- scripts/build/build-package-json.js
+++ scripts/build/build-package-json.js
@@ -56,12 +56,14 @@ async function buildPackageJson({ file, files }) {
           .filter((file) => file.output.format === "umd")
           .map((file) => {
             const basename = path.basename(file.output.file, ".js");
+            const mjsPath = `./${file.output.file.replace(/\.js$/u, ".mjs")}`;
             return [
               file.isPlugin ? `./plugins/${basename}` : `./${basename}`,
               {
                 types: `./${file.output.file.replace(/\.js$/u, ".d.ts")}`,
+                "module-sync": mjsPath,
                 require: `./${file.output.file}`,
-                default: `./${file.output.file.replace(/\.js$/u, ".mjs")}`,
+                default: mjsPath,
               },
             ];
           }),
