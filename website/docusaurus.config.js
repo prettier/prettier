7bfee07b45a1f5c6e17c3fc3dbf303d710fac22d
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2025-02-01 13:29:45 +0100

    docs: fix spacing issues on the home page

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -178,9 +178,8 @@ const config = {
               },
               {
                 html: /*html*/ `
-                  <br />
                   <a href="https://www.netlify.com" target="_blank" rel="noreferrer noopener" aria-label="Deploys by Netlify">
-                    <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" width="114" height="51" />
+                    <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" width="114" height="51" style="margin-top: 8px;" />
                   </a>
                 `,
               },
@@ -227,7 +226,7 @@ const config = {
                     aria-label="Star this project on GitHub"
                     class="footer__github-stars"
                   >
-                    <img src="https://img.shields.io/github/stars/prettier/prettier?style=social" alt="Star this project on GitHub" />
+                    <img src="https://img.shields.io/github/stars/prettier/prettier?style=social" loading="lazy" alt="Star this project on GitHub" />
                   </a>
                 `,
               },
