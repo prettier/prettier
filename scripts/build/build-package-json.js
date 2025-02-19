3fae595aac5d756d336eaaf1a6a285d0840bd98b
fisker Cheung <lionkay@gmail.com>
2025-02-20 00:11:16 +0800

    Revert "Use ESM entrypoint for `require(ESM)` (#16958)"
    This reverts commit b64b0a88ba278e841241a3c230861d167eee5915.

--- scripts/build/build-package-json.js
+++ scripts/build/build-package-json.js
@@ -42,7 +42,6 @@ async function buildPackageJson({ file, files }) {
     exports: {
       ".": {
         types: "./index.d.ts",
-        "module-sync": "./index.mjs",
         require: "./index.cjs",
         browser: {
           import: "./standalone.mjs",
@@ -56,14 +55,12 @@ async function buildPackageJson({ file, files }) {
           .filter((file) => file.output.format === "umd")
           .map((file) => {
             const basename = path.basename(file.output.file, ".js");
-            const mjsPath = `./${file.output.file.replace(/\.js$/u, ".mjs")}`;
             return [
               file.isPlugin ? `./plugins/${basename}` : `./${basename}`,
               {
                 types: `./${file.output.file.replace(/\.js$/u, ".d.ts")}`,
-                "module-sync": mjsPath,
                 require: `./${file.output.file}`,
-                default: mjsPath,
+                default: `./${file.output.file.replace(/\.js$/u, ".mjs")}`,
               },
             ];
           }),
