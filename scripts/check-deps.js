e2624b64040d8c80716bd2f206f8169432e39841
fisker Cheung <lionkay@gmail.com>
2025-02-20 23:24:36 +0800

    Enforce dependency version be pinned in all packages (#17161)

--- scripts/check-deps.js
+++ scripts/check-deps.js
@@ -3,21 +3,55 @@
 import fs from "node:fs/promises";
 import styleText from "node-style-text";
 
-const packageJson = JSON.parse(
-  await fs.readFile(new URL("../package.json", import.meta.url)),
-);
+const PROJECT_ROOT = new URL("../", import.meta.url);
+const ERROR = styleText.bgRed.black(" ERROR ");
+const PASS = "✅ All dependency versions are pinned.";
 
-validateDependencyObject(packageJson.dependencies);
-validateDependencyObject(packageJson.devDependencies);
+for (const [index, directory] of [
+  "./",
+  "./website/",
+  "./scripts/release/",
+  "./scripts/tools/bundle-test/",
+  "./scripts/tools/eslint-plugin-prettier-internal-rules/",
+].entries()) {
+  const file = new URL(`${directory}package.json`, PROJECT_ROOT);
 
-function validateDependencyObject(object) {
-  for (const [name, version] of Object.entries(object)) {
-    if (version[0] === "^" || version[0] === "~") {
-      console.error(
-        styleText.bgRed.black(" ERROR "),
-        `Dependency "${styleText.bold.blue(name)}" should be pinned.`,
-      );
-      process.exitCode = 1;
+  if (index > 0) {
+    console.log();
+  }
+
+  console.log(
+    `Checking '${styleText.gray(file.href.slice(PROJECT_ROOT.href.length - 1))}'...`,
+  );
+  const ok = await validatePackageJson(file);
+  if (ok) {
+    console.log(PASS);
+  }
+}
+
+async function validatePackageJson(packageJsonFile) {
+  const packageJson = JSON.parse(await fs.readFile(packageJsonFile));
+
+  let ok = true;
+  for (const property of ["dependencies", "devDependencies", "resolutions"]) {
+    const value = packageJson[property];
+
+    if (!value) {
+      continue;
+    }
+
+    for (const [name, version] of Object.entries(value)) {
+      if (version[0] === "^" || version[0] === "~") {
+        console.error(
+          ERROR,
+          `Dependency "${styleText.bold.blue(name)}" in "${styleText.gray.underline(property)}" should be pinned.`,
+        );
+
+        ok = false;
+        process.exitCode = 1;
+      }
     }
   }
+
+  return ok;
 }
