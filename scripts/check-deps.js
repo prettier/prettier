abdfbbd9f8503cbce57ed88c468514f3c4485883
fisker <lionkay@gmail.com>
2025-02-20 23:12:11 +0800

    Enforce dependency version be pinned in all packages

--- scripts/check-deps.js
+++ scripts/check-deps.js
@@ -3,19 +3,45 @@
 import fs from "node:fs/promises";
 import styleText from "node-style-text";
 
-const packageJson = JSON.parse(
-  await fs.readFile(new URL("../package.json", import.meta.url)),
-);
+const PROJECT_ROOT = new URL("../", import.meta.url);
 
-validateDependencyObject(packageJson.dependencies);
-validateDependencyObject(packageJson.devDependencies);
+for (const directory of [
+  "./",
+  "./website/",
+  "./scripts/release/",
+  "./scripts/tools/bundle-test/",
+  "./scripts/tools/eslint-plugin-prettier-internal-rules/",
+]) {
+  const file = new URL(`${directory}package.json`, PROJECT_ROOT);
 
-function validateDependencyObject(object) {
-  for (const [name, version] of Object.entries(object)) {
+  console.log(
+    `Checking '${styleText.gray(file.href.slice(PROJECT_ROOT.href.length - 1))}'...`,
+  );
+  await validatePackageJson(file);
+  console.log("done.");
+  console.log();
+}
+
+async function validatePackageJson(packageJsonFile) {
+  const packageJson = JSON.parse(await fs.readFile(packageJsonFile));
+
+  for (const property of ["dependencies", "devDependencies", "resolutions"]) {
+    validateDependencyObject(packageJson, property);
+  }
+}
+
+function validateDependencyObject(packageJson, property) {
+  const value = packageJson[property];
+
+  if (!value) {
+    return;
+  }
+
+  for (const [name, version] of Object.entries(value)) {
     if (version[0] === "^" || version[0] === "~") {
       console.error(
         styleText.bgRed.black(" ERROR "),
-        `Dependency "${styleText.bold.blue(name)}" should be pinned.`,
+        `Dependency "${styleText.bold.blue(name)}" in "${styleText.gray.underline(property)}" should be pinned.`,
       );
       process.exitCode = 1;
     }
