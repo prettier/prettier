d9e5a0dcc919b6446d6b544097e5032d783b6afc
fisker Cheung <lionkay@gmail.com>
2025-02-21 18:48:03 +0800

    Update install-prettier.js

--- tests/config/install-prettier.js
+++ tests/config/install-prettier.js
@@ -8,7 +8,10 @@ import { outdent } from "outdent";
 
 const createTemporaryDirectory = () => {
   const directory = path.join(
-    os.tmpdir(),
+    // The following quoted from https://github.com/es-tooling/module-replacements/blob/27d1acd38f19741e31d2eae561a5c8a914373fc5/docs/modules/tempy.md?plain=1#L20-L21, not sure if it's true
+    // MacOS and possibly some other platforms return a symlink from `os.tmpdir`.
+    // For some applications, this can cause problems; thus, we use `realpath`.
+    fs.realpathSync(os.tmpdir()),
     crypto.randomBytes(16).toString("hex"),
   );
   fs.mkdirSync(directory);
