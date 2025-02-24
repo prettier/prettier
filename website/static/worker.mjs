4572aa02a8620904123b48c63d21f5acdaf51c62
fisker <lionkay@gmail.com>
2025-02-25 07:41:59 +0800

    Rewrite worker.mjs

--- website/static/worker.mjs
+++ website/static/worker.mjs
@@ -1,6 +1,43 @@
 import prettierPackageManifest from "./lib/package-manifest.mjs";
 import * as prettier from "./lib/standalone.mjs";
 
+const pluginLoadPromises = new Map();
+async function importPlugin(plugin) {
+  if (!pluginLoadPromises.has(plugin)) {
+    pluginLoadPromises.set(plugin, import(`./lib/${plugin.file}`));
+  }
+
+  try {
+    return await pluginLoadPromises.get(plugin);
+  } catch {
+    throw new Error(`Load plugin '${plugin.file}' failed.`);
+  }
+}
+
+// Similar to `createParsersAndPrinters` in `src/plugins/builtin-plugins-proxy.js`
+function createPlugin(pluginManifest) {
+  const { languages, options, defaultOptions } = pluginManifest;
+
+  const parsers = Object.create(null);
+  const printers = Object.create(null);
+  const plugin = { languages, options, defaultOptions, parsers, printers };
+
+  const loadPlugin = async () => {
+    const plugin = await importPlugin(pluginManifest);
+    Object.assign(parsers, plugin.parsers);
+    Object.assign(printers, plugin.printers);
+    return plugin;
+  };
+
+  for (const property of ["parsers", "printers"]) {
+    for (const name of pluginManifest[property] ?? []) {
+      plugin[property][name] = async () => (await loadPlugin())[property][name];
+    }
+  }
+
+  return plugin;
+}
+
 const docExplorerPlugin = {
   parsers: {
     "doc-explorer": {
@@ -20,16 +57,12 @@ const docExplorerPlugin = {
   languages: [{ name: "doc-explorer", parsers: ["doc-explorer"] }],
 };
 
-// TODO[@fisker]: Lazy load plugins
-// eslint-disable-next-line unicorn/prefer-top-level-await
-const pluginsLoadPromise = (async () => [
-  ...(await Promise.all(
-    prettierPackageManifest.builtinPlugins.map(
-      ({ file }) => import(`./lib/${file}`),
-    ),
-  )),
+const plugins = [
+  ...prettierPackageManifest.builtinPlugins.map((plugin) =>
+    createPlugin(plugin),
+  ),
   docExplorerPlugin,
-])();
+];
 
 self.onmessage = async function (event) {
   self.postMessage({
@@ -63,7 +96,6 @@ function handleMessage(message) {
 }
 
 async function handleMetaMessage() {
-  const plugins = await pluginsLoadPromise;
   const supportInfo = await prettier.getSupportInfo({ plugins });
 
   return {
@@ -75,7 +107,6 @@ async function handleMetaMessage() {
 }
 
 async function handleFormatMessage(message) {
-  const plugins = await pluginsLoadPromise;
   const options = { ...message.options, plugins };
 
   delete options.ast;
