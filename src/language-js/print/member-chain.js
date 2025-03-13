3245e73a118d6827a4ca92bb9b2c8bfa40b811b2
fisker <lionkay@gmail.com>
2025-03-13 18:49:00 +0800

    Safer fix

--- src/language-js/print/member-chain.js
+++ src/language-js/print/member-chain.js
@@ -332,7 +332,7 @@ function printMemberChain(path, options, print) {
   }
 
   const printedGroups = groups.map(printGroup);
-  const oneLine = printedGroups.map((doc) => group(doc));
+  const oneLine = printedGroups;
 
   const cutoff = shouldMerge ? 3 : 2;
   const flatGroups = groups.flat();
@@ -414,7 +414,7 @@ function printMemberChain(path, options, print) {
       // that means that the parent group has already been broken
       // naturally
       willBreak(oneLine) || shouldHaveEmptyLineBeforeIndent ? breakParent : "",
-      conditionalGroup([oneLine, group(expanded)]),
+      conditionalGroup([oneLine.map((doc) => group(doc)), group(expanded)]),
     ];
   }
 
