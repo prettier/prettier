7eb034b389d6656571179f4b43c9f288d2314495
fisker <lionkay@gmail.com>
2025-04-09 13:13:34 +0800

    Add a TODO

--- src/language-js/print/object.js
+++ src/language-js/print/object.js
@@ -146,6 +146,7 @@ function printObject(path, options, print) {
     (node.type !== "ObjectPattern" &&
       options.objectWrap === "preserve" &&
       propsAndLoc.length > 0 &&
+      // FIXME
       hasNewlineInRange(
         options.originalText,
         locStart(node),
