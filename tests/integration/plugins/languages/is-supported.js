92161ed3d6301ac34cb570f056d525b519c8aa75
JounQin <admin@1stg.me>
2025-04-13 21:09:21 +0800

    chore: add absolute path assertion

--- tests/integration/plugins/languages/is-supported.js
+++ tests/integration/plugins/languages/is-supported.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+import path from "node:path";
+
 /** @import {SupportLanguage} from "prettier" */
 
 /** @type {SupportLanguage[]} */
@@ -7,7 +9,12 @@ export const languages = [
   {
     name: "language-name-does-not-matter",
     parsers: ["parser-name-inferred-from-language-is-supported"],
-    isSupported: (file) =>
-      /(?<separator>[\\/])\.husky\k<separator>[^\\/]+$/u.test(file),
+    isSupported(file) {
+      if (!path.isAbsolute(file)) {
+        throw new Error("Unexpected non absolute path");
+      }
+
+      return /(?<separator>[\\/])\.husky\k<separator>[^\\/]+$/u.test(file);
+    },
   },
 ];
