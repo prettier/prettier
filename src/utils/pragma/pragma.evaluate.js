d81f46ef81d9efefb0266d2c4d9427587957f6c4
fisker <lionkay@gmail.com>
2025-04-14 15:44:46 +0800

    Remove assertion

--- src/utils/pragma/pragma.evaluate.js
+++ src/utils/pragma/pragma.evaluate.js
@@ -1,23 +1,15 @@
-import assert from "node:assert";
-
 export const FORMAT_PRAGMAS = ["format", "prettier"];
 export const FORMAT_IGNORE_PRAGMAS = FORMAT_PRAGMAS.map(
   (pragma) => `no${pragma}`,
 );
 export const FORMAT_PRAGMA_TO_INSERT = FORMAT_PRAGMAS[0];
 
-// Regular expressions put in this file so they can be evaluate
-
-// This exists because the regexp was rewrite from existing, feel free to remove when updating them
-const assertRegexpEqual = (regexpA, regexpB) => {
-  assert.equal(regexpA.source, regexpB.source);
-};
+// Regular expressions put in this file so they can be evaluated
 
 export const YAML_IS_PRAGMA_REGEXP = new RegExp(
   String.raw`^\s*@(?:${FORMAT_PRAGMAS.join("|")})\s*$`,
   "u",
 );
-assertRegexpEqual(YAML_IS_PRAGMA_REGEXP, /^\s*@(?:format|prettier)\s*$/u);
 
 export const [YAML_HAS_PRAGMA_REGEXP, YAML_HAS_IGNORE_PRAGMA_REGEXP] = [
   FORMAT_PRAGMAS,
@@ -28,14 +20,6 @@ export const [YAML_HAS_PRAGMA_REGEXP, YAML_HAS_IGNORE_PRAGMA_REGEXP] = [
       String.raw`^\s*#[^\S\n]*@(?:${pragmas.join("|")})\s*?(?:\n|$)`,
       "u",
     ),
-);
-assertRegexpEqual(
-  YAML_HAS_PRAGMA_REGEXP,
-  /^\s*#[^\S\n]*@(?:format|prettier)\s*?(?:\n|$)/u,
-);
-assertRegexpEqual(
-  YAML_HAS_IGNORE_PRAGMA_REGEXP,
-  /^\s*#[^\S\n]*@(?:noformat|noprettier)\s*?(?:\n|$)/u,
 );
 
 export const [GRAPHQL_HAS_PRAGMA_REGEXP, GRAPHQL_HAS_IGNORE_PRAGMA_REGEXP] = [
@@ -48,14 +32,6 @@ export const [GRAPHQL_HAS_PRAGMA_REGEXP, GRAPHQL_HAS_IGNORE_PRAGMA_REGEXP] = [
       "u",
     ),
 );
-assertRegexpEqual(
-  GRAPHQL_HAS_PRAGMA_REGEXP,
-  /^\s*#[^\S\n]*@(?:format|prettier)\s*(?:\n|$)/u,
-);
-assertRegexpEqual(
-  GRAPHQL_HAS_IGNORE_PRAGMA_REGEXP,
-  /^\s*#[^\S\n]*@(?:noformat|noprettier)\s*(?:\n|$)/u,
-);
 
 export const [HTML_HAS_PRAGMA_REGEXP, HTML_HAS_IGNORE_PRAGMA_REGEXP] = [
   FORMAT_PRAGMAS,
@@ -63,12 +39,4 @@ export const [HTML_HAS_PRAGMA_REGEXP, HTML_HAS_IGNORE_PRAGMA_REGEXP] = [
 ].map(
   (pragmas) =>
     new RegExp(String.raw`^\s*<!--\s*@(?:${pragmas.join("|")})\s*-->`, "u"),
-);
-assertRegexpEqual(
-  HTML_HAS_PRAGMA_REGEXP,
-  /^\s*<!--\s*@(?:format|prettier)\s*-->/u,
-);
-assertRegexpEqual(
-  HTML_HAS_IGNORE_PRAGMA_REGEXP,
-  /^\s*<!--\s*@(?:noformat|noprettier)\s*-->/u,
 );
