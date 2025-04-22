0ae3418d098598c13b3f5c2d7aca321494e0ef95
fisker <lionkay@gmail.com>
2025-04-22 19:40:36 +0800

    Listen to `spawn` event

--- tests/integration/run-cli.js
+++ tests/integration/run-cli.js
@@ -69,10 +69,6 @@ function runCliWorker(dir, args, options) {
     });
   }
 
-  if (options.input) {
-    worker.stdin.end(options.input);
-  }
-
   const removeStdioFinalNewLine = () => {
     for (const stream of ["stdout", "stderr"]) {
       result[stream] = removeFinalNewLine(result[stream]);
@@ -90,26 +86,32 @@ function runCliWorker(dir, args, options) {
       reject(error);
     });
 
-    worker.send(options, (error) => {
-      if (!error) {
-        return;
+    worker.once("spawn", () => {
+      if (options.input) {
+        worker.stdin.end(options.input);
       }
 
-      // It can fail with `write EPIPE` error when running node with unsupported flags like `--experimental-strip-types`
-      // Let's ignore and wait for the `close` event
-      if (
-        error.code === "EPIPE" &&
-        error.syscall === "write" &&
-        nodeOptions.length > 0
-      ) {
-        if (IS_CI) {
-          // eslint-disable-next-line no-console
-          console.error(Object.assign(error, { dir, args, options, worker }));
+      worker.send(options, (error) => {
+        if (!error) {
+          return;
         }
-        return;
-      }
 
-      reject(error);
+        // It can fail with `write EPIPE` error when running node with unsupported flags like `--experimental-strip-types`
+        // Let's ignore and wait for the `close` event
+        if (
+          error.code === "EPIPE" &&
+          error.syscall === "write" &&
+          nodeOptions.length > 0
+        ) {
+          if (IS_CI) {
+            // eslint-disable-next-line no-console
+            console.error(Object.assign(error, { dir, args, options, worker }));
+          }
+          return;
+        }
+
+        reject(error);
+      });
     });
   });
 }
