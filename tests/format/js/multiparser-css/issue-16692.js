17d5ec20565a418308cfb5ca785075f3ad75bc14
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2025-04-24 12:52:37 +0900

    Add tests

---
+++ tests/format/js/multiparser-css/issue-16692.js
@@ -0,0 +1,11 @@
+const c1 = styled.div`
+    border:    1px     ${solid}      red;
+`;
+
+const c2 = styled.div`
+ font-size:      var(--font-size-h${level});
+`;
+
+const c3 = styled.div`
+    grid-area:       area-${area};
+`;
