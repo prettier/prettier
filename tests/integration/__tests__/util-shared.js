7281cd72e071c3890db2f5bc59be12022c4ad3ba
fisker <lionkay@gmail.com>
2025-04-26 01:40:10 +0800

    Test in production

--- tests/integration/__tests__/util-shared.js
+++ tests/integration/__tests__/util-shared.js
@@ -1,3 +1,4 @@
+import fs from "node:fs/promises";
 import prettier from "../../config/prettier-entry.js";
 
 const sharedUtil = prettier.util;
@@ -49,7 +50,7 @@ test("sharedUtil.getMaxContinuousCount", () => {
   expect(getMaxContinuousCount(fixture, String.raw`a\-f`)).toBe(0);
 });
 
-test("sharedUtil.getStringWidth", () => {
+test("sharedUtil.getStringWidth", async () => {
   const { getStringWidth } = sharedUtil;
 
   // From https://github.com/sindresorhus/string-width/blob/main/test.js
@@ -87,6 +88,23 @@ test("sharedUtil.getStringWidth", () => {
   expect(getStringWidth("👶🏽")).toBe(2);
   expect(getStringWidth("👩‍👩‍👦‍👦")).toBe(2);
   expect(getStringWidth("👨‍❤️‍💋‍👨")).toBe(2);
+
+  const strings = await fs.readFile(
+    new URL(
+      "../../node_modules/emoji-test-regex-pattern/dist/emoji-16.0/index-strings.txt",
+      import.meta.url,
+    ),
+    "utf8",
+  );
+  for (const emoji of strings.trim().split("\n")) {
+    try {
+      expect(getStringWidth(emoji)).toBe(2);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(`Unexpected string width '${emoji}'`);
+      throw error;
+    }
+  }
 });
 
 test("sharedUtil.getAlignmentSize", () => {
