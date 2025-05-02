5af66a4451224b5f991e58dab8ba8b4590e69d5e
o-m12a <p.mygithubemailllll@gmail.com>
2025-05-02 18:16:36 +0900

    add a test case for a single line comment

--- tests/format/typescript/arrow/comments.ts
+++ tests/format/typescript/arrow/comments.ts
@@ -14,3 +14,6 @@ A comment after an arrow with a type definition, and before a line break.
 Formatting should keep it after the arrow.
 */
 null;
+
+const fn4 = (): any => /* A single line ver of `fn3` */
+null;
