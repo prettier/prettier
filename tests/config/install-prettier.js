41e89e5d383a61f24c838d103ac9d3e068dfa94f
fisker <lionkay@gmail.com>
2025-05-09 11:18:29 +0800

    Fix

--- tests/config/install-prettier.js
+++ tests/config/install-prettier.js
@@ -19,10 +19,13 @@ const escapeArgument = (argument) =>
     ),
   );
 const spawn = (command, args, options) =>
-  spawnSync([command, ...args.map((argument) => escapeArgument(argument))], {
-    ...options,
-    shell: true,
-  });
+  spawnSync(
+    [command, ...args.map((argument) => escapeArgument(argument))].join(" "),
+    {
+      ...options,
+      shell: true,
+    },
+  );
 const createTemporaryDirectory = () => {
   const directory = path.join(
     // The following quoted from https://github.com/es-tooling/module-replacements/blob/27d1acd38f19741e31d2eae561a5c8a914373fc5/docs/modules/tempy.md?plain=1#L20-L21, not sure if it's true
