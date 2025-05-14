c7d0343e63ead1b650fe669ccec760f97b366001
fisker <lionkay@gmail.com>
2025-05-14 22:07:10 +0800

    One more test

--- tests/format/typescript/optional-chaining/as-key.ts
+++ tests/format/typescript/optional-chaining/as-key.ts
@@ -2,6 +2,18 @@ class A {
   [a?.b]?= 1;
   [a?.b]?= function () {};
 
+  [a?.b]?() {};
+  // *[a?.b]?() {};
+  async [a?.b]?() {};
+  async * [a?.b]?() {};
+}
+
+class B {
   static [a?.b]?= 1;
   static [a?.b]?= function () {};
+
+  static [a?.b]?() {};
+  // static *[a?.b]?() {};
+  static async [a?.b]?() {};
+  static async * [a?.b]?() {};
 }
