7a1926061035a4dcc9b37d663c6fde6b83c9ec24
fisker <lionkay@gmail.com>
2025-05-14 13:44:21 +0800

    Fix tests

--- tests/format/typescript/error-recovery/format.test.js
+++ tests/format/typescript/error-recovery/format.test.js
@@ -1,4 +1,12 @@
-const errors = { "babel-ts": ["jsdoc_only_types.ts", "index-signature.ts"] };
+const errors = {
+  "babel-ts": ["jsdoc_only_types.ts", "index-signature.ts"],
+  "oxc-ts": [
+    "jsdoc_only_types.ts",
+    "index-signature.ts",
+    // TODO: Ask typescript-eslint to throw
+    "generic.ts",
+  ],
+};
 runFormatTest(import.meta, ["typescript"], { errors });
 runFormatTest(import.meta, ["typescript"], { trailingComma: "es5", errors });
 runFormatTest(import.meta, ["typescript"], { trailingComma: "all", errors });
