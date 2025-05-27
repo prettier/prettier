532306eecbfa20bc8a249b5a2e027629400b675f
fisker <lionkay@gmail.com>
2025-05-27 14:16:15 +0800

    Fix tests

--- tests/format/flow/assignments/issue-10850.js
+++ tests/format/flow/assignments/issue-10850.js
@@ -1,7 +1,7 @@
 const map: Map<Function, Map<string | void, { value: UnloadedDescriptor }>> =
   new Map();
 
-const map: Map<Function, FunctionFunctionFunctionFunctionffFunction> =
+const map2: Map<Function, FunctionFunctionFunctionFunctionffFunction> =
   new Map();
 
-const map: Map<Function, Foo<S>> = new Map();
+const map3: Map<Function, Foo<S>> = new Map();
--- tests/format/flow/implements/implements.js
+++ tests/format/flow/implements/implements.js
@@ -1,5 +1,5 @@
-class A implements B {}
-class A implements B, C {}
-declare class A implements B {}
-declare class A mixins B implements C {}
-declare class A implements B, C {}
+class A1 implements B {}
+class A2 implements B, C {}
+declare class A3 implements B {}
+declare class A4 mixins B implements C {}
+declare class A5 implements B, C {}
