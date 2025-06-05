a890d1cc7772629c7812e94c39216959f94d2a4d
fisker <lionkay@gmail.com>
2025-06-06 00:43:11 +0800

    Refactor `createLanguage`

--- src/utils/create-language.js
+++ src/utils/create-language.js
@@ -1,39 +1,58 @@
-function assertUniqueArray(array, message) {
-  if (new Set(array).size === array.length) {
-    return;
+import assert from "node:assert";
+
+/** @import {Language as LinguistLanguage} from 'linguist-languages' */
+/**
+@typedef {{
+  name: string;
+  parsers: BuiltInParserName[] | string[];
+  group?: string | undefined;
+  tmScope?: string | undefined;
+  aceMode?: string | undefined;
+  codemirrorMode?: string | undefined;
+  codemirrorMimeType?: string | undefined;
+  aliases?: string[] | undefined;
+  extensions?: string[] | undefined;
+  filenames?: string[] | undefined;
+  linguistLanguageId?: number | undefined;
+  vscodeLanguageIds?: string[] | undefined;
+  interpreters?: string[] | undefined;
+  isSupported?: (({ filepath: string }) => boolean) | undefined;
+}} Language
+*/
+
+const excludedFields = new Set(["color"]);
+
+const arrayTypeFields = new Set([
+  "parsers",
+  "extensions",
+  "interpreters",
+  "filenames",
+]);
+
+/**
+ * @param {LinguistLanguage} linguistLanguage
+ * @param {(data: LinguistLanguage) => LinguistLanguage & Language} getOverrides
+ * @returns {Language}
+ */
+function createLanguage(linguistLanguage, getOverrides) {
+  const language = getOverrides(linguistLanguage);
+
+  for (const property of arrayTypeFields) {
+    const value = language[property];
+
+    assert.ok(
+      value === undefined ||
+        (Array.isArray(value) && new Set(value).size === value.length),
+      `Language property '${property}' should be 'undefined' or unique array.`,
+    );
   }
 
-  /* c8 ignore next */
-  throw new Error(message);
-}
-
-function createLanguage(linguistData, getOverrides) {
-  const { languageId, ...restData } = linguistData;
-  const overrides = getOverrides(linguistData);
-
-  if (process.env.NODE_ENV !== "production" && overrides) {
-    for (const property of [
-      "parsers",
-      "extensions",
-      "interpreters",
-      "filenames",
-    ]) {
-      const value = overrides[property];
-
-      if (value) {
-        assertUniqueArray(
-          value,
-          `Language property '${property}' should be unique.`,
-        );
-      }
-    }
+  const { languageId: linguistLanguageId, ...restData } = language;
+  for (const field of excludedFields) {
+    delete restData[field];
   }
 
-  return {
-    linguistLanguageId: languageId,
-    ...restData,
-    ...overrides,
-  };
+  return { linguistLanguageId, ...restData };
 }
 
 export default createLanguage;
