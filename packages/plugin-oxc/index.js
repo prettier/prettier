917a3998924278681f4d70e1c99f1023bcd30cdc
fisker <lionkay@gmail.com>
2025-06-05 17:47:46 +0800

    Include printer in `@prettier/plugin-oxc` and `@prettier/plugin-hermes`

--- packages/plugin-oxc/index.js
+++ packages/plugin-oxc/index.js
@@ -1,2 +1,18 @@
-export * as parsers from "../../src/language-js/parse/oxc.js";
+import * as oxcParsers from "../../src/language-js/parse/oxc.js";
+import * as estreePrinter from "../../src/language-js/printer.js";
+
+const AST_FORMAT = "estree-oxc";
+
+export const parsers = Object.fromEntries(
+  Object.entries(oxcParsers).map(([name, parser]) => [
+    name,
+    { ...parser, astFormat: AST_FORMAT },
+  ]),
+);
+
+export const printers = {
+  [AST_FORMAT]: estreePrinter,
+};
+
+export { default as options } from "../../src/language-js/options.js";
 export { default as languages } from "./languages.evaluate.js";
