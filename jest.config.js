e4bcdab488aea244fa9a6ab32b2d74f78ccabfe7
fisker <lionkay@gmail.com>
2025-06-07 00:13:21 +0800

    Revert "Use worker"
    This reverts commit ef3b7c07df14da89436fa99c26e769c46591fdbe.

--- jest.config.js
+++ jest.config.js
@@ -65,8 +65,7 @@ const config = {
     "<rootDir>/tests/config/format-test-setup.js",
     "<rootDir>/tests/integration/integration-test-setup.js",
   ],
-  //back to worker
-  runner: "jest-light-runner",
+  runner: "jest-light-runner/child-process",
   snapshotSerializers: [
     "jest-snapshot-serializer-raw",
     "jest-snapshot-serializer-ansi",
