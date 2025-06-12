1872d4469b9bc1f7dcb60685633ec68a877ad47b
fisker <lionkay@gmail.com>
2025-06-12 17:03:44 +0800

    Test computed

--- tests/format/js/module-blocks/quote-props/worker.js
+++ tests/format/js/module-blocks/quote-props/worker.js
@@ -26,4 +26,11 @@ worker = new Worker(module {
   }
 }, {...{type: "module"}});
 
+worker = new Worker(module {
+  onmessage = function({data}) {
+    let mod = import(data);
+    postMessage(mod.fn());
+  }
+}, {[type]: "module"});
+
 worker.postMessage(module { export function fn() { return "hello!" } });
