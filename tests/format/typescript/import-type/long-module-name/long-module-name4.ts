40a21952e24d1443de00b0e40ef0ea783ffa5c59
fisker <lionkay@gmail.com>
2025-06-23 07:26:05 +0800

    Test

--- tests/format/typescript/import-type/long-module-name/long-module-name4.ts
+++ tests/format/typescript/import-type/long-module-name/long-module-name4.ts
@@ -1,23 +1,30 @@
 interface RuleMap {
-  'arrow-parens': typeof import('eslint/lib/rules/arrow-parens');
-  'consistent-return': typeof import('eslint/lib/rules/consistent-return');
-  'dot-notation': typeof import('eslint/lib/rules/dot-notation');
-  'init-declarations': typeof import('eslint/lib/rules/init-declarations');
-  'max-params': typeof import('eslint/lib/rules/max-params');
-  'no-dupe-args': typeof import('eslint/lib/rules/no-dupe-args');
-  'no-dupe-class-members': typeof import('eslint/lib/rules/no-dupe-class-members');
-  'no-empty-function': typeof import('eslint/lib/rules/no-empty-function');
-  'no-implicit-globals': typeof import('eslint/lib/rules/no-implicit-globals');
-  'no-invalid-this': typeof import('eslint/lib/rules/no-invalid-this');
-  'no-loop-func': typeof import('eslint/lib/rules/no-loop-func');
-  'no-loss-of-precision': typeof import('eslint/lib/rules/no-loss-of-precision');
-  'no-magic-numbers': typeof import('eslint/lib/rules/no-magic-numbers');
-  'no-restricted-globals': typeof import('eslint/lib/rules/no-restricted-globals');
-  'no-restricted-imports': typeof import('eslint/lib/rules/no-restricted-imports');
-  'no-undef': typeof import('eslint/lib/rules/no-undef');
-  'no-unused-expressions': typeof import('eslint/lib/rules/no-unused-expressions');
-  'no-useless-constructor': typeof import('eslint/lib/rules/no-useless-constructor');
-  'prefer-const': typeof import('eslint/lib/rules/prefer-const');
-  'prefer-destructuring': typeof import('eslint/lib/rules/prefer-destructuring');
-  strict: typeof import('eslint/lib/rules/strict');
+  'arrow-parens': typeof import('./long/long/long/long/long/path/to/rules/arrow-parens');
+  'consistent-return': typeof import('./long/long/long/long/long/path/to/rules/consistent-return');
+  'dot-notation': typeof import('./long/long/long/long/long/path/to/rules/dot-notation');
+  'init-declarations': typeof import('./long/long/long/long/long/path/to/rules/init-declarations');
+  'max-params': typeof import('./long/long/long/long/long/path/to/rules/max-params');
+  'no-dupe-args': typeof import('./long/long/long/long/long/path/to/rules/no-dupe-args');
+  'no-dupe-class-members': typeof import('./long/long/long/long/long/path/to/rules/no-dupe-class-members');
+  'no-empty-function': typeof import('./long/long/long/long/long/path/to/rules/no-empty-function');
+  'no-implicit-globals': typeof import('./long/long/long/long/long/path/to/rules/no-implicit-globals');
+  'no-invalid-this': typeof import('./long/long/long/long/long/path/to/rules/no-invalid-this');
+  'no-loop-func': typeof import('./long/long/long/long/long/path/to/rules/no-loop-func');
+  'no-loss-of-precision': typeof import('./long/long/long/long/long/path/to/rules/no-loss-of-precision');
+  'no-magic-numbers': typeof import('./long/long/long/long/long/path/to/rules/no-magic-numbers');
+  'no-restricted-globals': typeof import('./long/long/long/long/long/path/to/rules/no-restricted-globals');
+  'no-restricted-imports': typeof import('./long/long/long/long/long/path/to/rules/no-restricted-imports');
+  'no-undef': typeof import('./long/long/long/long/long/path/to/rules/no-undef');
+  'no-unused-expressions': typeof import('./long/long/long/long/long/path/to/rules/no-unused-expressions');
+  'no-useless-constructor': typeof import('./long/long/long/long/long/path/to/rules/no-useless-constructor');
+  'prefer-const': typeof import('./long/long/long/long/long/path/to/rules/prefer-const');
+  'prefer-destructuring': typeof import('./long/long/long/long/long/path/to/rules/prefer-destructuring');
+  'prefer-destructuring2': typeof import(
+// comment
+'./long/long/long/long/long/path/to/rules/prefer-destructuring');
+  'prefer-destructuring3':
+// comment
+typeof import(
+'./long/long/long/long/long/path/to/rules/prefer-destructuring');
+  strict: typeof import('./long/long/long/long/long/path/to/rules/strict');
 }
