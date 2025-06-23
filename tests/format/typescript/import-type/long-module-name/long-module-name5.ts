d6be5b6fb48b8b8916ec3000cf9b2c57e7391a13
fisker <lionkay@gmail.com>
2025-06-23 08:48:20 +0800

    Disable test

--- tests/format/typescript/import-type/long-module-name/long-module-name5.ts
+++ tests/format/typescript/import-type/long-module-name/long-module-name5.ts
@@ -1,6 +1,7 @@
 type A = import("./long/long/long/long/long/long/long/long/long/long/path/to/module")
 type B = import("./long/long/long/long/long/long/long/long/long/long/path/to/module",{with:{type:'json'}})
-type C = import("./long/long/long/long/long/long/long/long/long/long/path/to/module",{with:{
-type:'json'}})
-type D = import("./long/long/long/long/long/long/long/long/long/long/path/to/module",{
-with:{type:'json'}})
+// https://github.com/microsoft/TypeScript/issues/61916
+// type C = import("./long/long/long/long/long/long/long/long/long/long/path/to/module",{with:{
+// type:'json'}})
+// type D = import("./long/long/long/long/long/long/long/long/long/long/path/to/module",{
+// with:{type:'json'}})
