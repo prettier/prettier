8b6ace3228292b021f91948acbb37dccfc30b494
fisker <lionkay@gmail.com>
2025-07-28 16:25:42 +0800

    Clean

--- tests/format/flow/match/from-hermes-repo/format.test.js
+++ tests/format/flow/match/from-hermes-repo/format.test.js
@@ -1,21 +1,2 @@
 // Tests taken from https://github.com/facebook/hermes/tree/83c8115/test/Parser/flow/match
-runFormatTest(import.meta, ["flow"], {
-  errors: {
-    "babel-flow": [
-      "expression.js",
-      "pattern-member.js",
-      "statement-empty.js",
-      "export.js",
-      "format.test.js",
-      "pattern-object.js",
-      "statement-guards.js",
-      "expression-empty.js",
-      "pattern-array.js",
-      "pattern-or-as.js",
-      "statement-sequence-arg.js",
-      "expression-guards.js",
-      "pattern-core.js",
-      "statement.js",
-    ],
-  },
-});
+runFormatTest(import.meta, ["flow"]);
