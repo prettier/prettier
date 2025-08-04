95ef24d1f0e1d91e78987bc8b95d9b1318949833
fisker Cheung <lionkay@gmail.com>
2025-08-04 15:41:42 +0800

    Support "Discard Bindings" proposal (#17708)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/language-js/traverse/visitor-keys.evaluate.js
+++ src/language-js/traverse/visitor-keys.evaluate.js
@@ -66,10 +66,6 @@ const excludeNodeTypes = new Set([
   "DecimalLiteral",
   // Babel, Won't exist since we use `createImportExpressions` when parsing with babel
   "Import",
-
-  // Babel v7.28.0
-  // https://github.com/prettier/prettier/pull/17696
-  "VoidPattern",
 ]);
 
 const visitorKeys = Object.fromEntries(
