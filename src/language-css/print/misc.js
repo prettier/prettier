629673be62179dee6c6db5b8e17fd7a0c34b9f4a
fisker Cheung <lionkay@gmail.com>
2025-09-04 15:54:19 +0800

    Add comment back

--- src/language-css/print/misc.js
+++ src/language-css/print/misc.js
@@ -28,6 +28,7 @@ function adjustStrings(value, options) {
 function quoteAttributeValue(value, options) {
   const quote = options.singleQuote ? "'" : '"';
 
+  // Check if the value ends with `s`, `i` can be solved by postcss
   let flag = "";
   const match = value.match(/^(?<value>.+?)\s+(?<flag>s)$/u);
   if (match) {
