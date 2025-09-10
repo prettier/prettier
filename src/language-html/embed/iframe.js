5b8825307ad733aa27cb8257719818be1bd4461f
Konv Suu <2583695112@qq.com>
2025-09-10 12:12:50 +0800

    Support format allow attribute of iframe

---
+++ src/language-html/embed/iframe.js
@@ -0,0 +1,31 @@
+import { join,softline } from "../../document/builders.js";
+import { getUnescapedAttributeValue } from "../utils/index.js";
+import { printExpand } from "./utils.js";
+
+function printIframeAttribute(path, options) {
+  const { node } = path;
+
+  const text = getUnescapedAttributeValue(node).trim();
+  if (
+    node.fullName === "allow" &&
+    !options.parentParser
+  ) {
+    const directives = text
+      .split(";")
+      .map(directive => directive.trim())
+      .filter(directive => directive.length > 0);
+
+    if (directives.length <= 1) {
+      return null;
+    }
+
+    const formattedDirectives = directives.map(directive => directive + ";");
+
+    return () =>
+      printExpand(
+        join([softline], formattedDirectives)
+      );
+  }
+}
+
+export { printIframeAttribute };
