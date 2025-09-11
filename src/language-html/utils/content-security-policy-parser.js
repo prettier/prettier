705e817bb7a44dd50e47f91f2ebc0d465c2de996
Konv Suu <2583695112@qq.com>
2025-09-11 15:26:03 +0800

    lint

--- src/language-html/utils/content-security-policy-parser.js
+++ src/language-html/utils/content-security-policy-parser.js
@@ -2,15 +2,19 @@
 
 const ASCII_WHITESPACE_CHARS = "\t\n\f\r ";
 const ASCII_WHITESPACE = new RegExp(`[${ASCII_WHITESPACE_CHARS}]+`, "u");
-const ASCII_WHITESPACE_AT_START = new RegExp(`^[${ASCII_WHITESPACE_CHARS}]+`, "u");
-const ASCII_WHITESPACE_AT_END = new RegExp(`[${ASCII_WHITESPACE_CHARS}]+$`, "u");
+const ASCII_WHITESPACE_AT_START = new RegExp(
+  `^[${ASCII_WHITESPACE_CHARS}]+`,
+  "u",
+);
+const ASCII_WHITESPACE_AT_END = new RegExp(
+  `[${ASCII_WHITESPACE_CHARS}]+$`,
+  "u",
+);
 
 // eslint-disable-next-line no-control-regex
 const ASCII = /^[\x00-\x7f]*$/u;
 
-export default function parseContentSecurityPolicy(
-  policy,
-) {
+export default function parseContentSecurityPolicy(policy) {
   const result = [];
 
   for (let token of policy.split(";")) {
