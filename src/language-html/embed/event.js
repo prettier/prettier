954706883d64a61b4b8c77ebfd202de7a3a16416
Konv Suu <2583695112@qq.com>
2025-09-15 10:25:27 +0800

    Support format html event attributes

---
+++ src/language-html/embed/event.js
@@ -0,0 +1,23 @@
+import {htmlEventAttributes} from "html-event-attributes"
+import { getUnescapedAttributeValue } from "../utils/index.js";
+import { formatAttributeValue } from "./utils.js";
+
+function printEventAttribute(path, options) {
+  const { node } = path;
+
+  if(!htmlEventAttributes.includes(node.fullName) || options.parentParser) {
+    return;
+  }
+
+  const text = getUnescapedAttributeValue(path.node).trim();
+
+  if (!text.includes("{{")) {
+    return (textToDoc) => formatAttributeValue(
+      text,
+      textToDoc,
+      { parser: "__js_expression" },
+    )
+  }
+}
+
+export { printEventAttribute };
