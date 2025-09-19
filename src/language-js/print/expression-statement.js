2de7b85e6006759d6108b83a44d227c9b2a73d17
Konv Suu <2583695112@qq.com>
2025-09-19 15:27:03 +0800

    lint

--- src/language-js/print/expression-statement.js
+++ src/language-js/print/expression-statement.js
@@ -4,6 +4,7 @@ import {
   unwrapVueEventBindingTsNode,
 } from "../utils/vue-event-binding.js";
 import {
+  isSingleHTMLEventBindingExpressionStatement,
   isSingleJsxExpressionStatementInMarkdown,
   isSingleVueEventBindingExpressionStatement,
 } from "./semicolon.js";
@@ -21,7 +22,10 @@ function printExpressionStatement(path, options, print) {
     }
   } else if (isSingleJsxExpressionStatementInMarkdown(path, options)) {
     // Do not append semicolon after the only JSX element in a program
-  } else if (options.semi) {
+  } else if(isSingleHTMLEventBindingExpressionStatement(path, options)){
+    // Do not append semicolon after the only HTML event binding expression in a program
+  }
+  else if (options.semi) {
     parts.push(";");
   }
 
