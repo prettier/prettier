5ea0834c0e5b1c5afe802e67806b4c83c9db5315
fisker <lionkay@gmail.com>
2025-09-23 03:58:42 +0800

    Rewrite tests

--- tests/format/js/require/require.js
+++ tests/format/js/require/require.js
@@ -17,7 +17,3 @@ const plugin2 = require(
 );
 
 const plugin3 = require('./a/long/long/long/long/long/long/long/long/long/long/long/path/to/module')
-
-require("./a/long/long/long/long/long/long/long/long/long/long/long/path/to/module");
-require.resolve("./a/long/long/long/long/long/long/long/long/long/long/long/path/to/module");
-require.resolve.paths("./a/long/long/long/long/long/long/long/long/long/long/long/path/to/module");
