e93616bc14b2c42045d9a6aa8a3bdd8059b81a33
fisker <lionkay@gmail.com>
2025-09-23 16:38:33 +0800

    Extract `shouldPrintSemicolon`

--- src/language-js/print/expression-statement.js
+++ src/language-js/print/expression-statement.js
@@ -9,32 +9,33 @@ import {
   isSingleVueEventBindingExpressionStatement,
 } from "./semicolon.js";
 
-function printExpressionStatement(path, options, print) {
-  const parts = [print("expression")];
-
+function shouldPrintSemicolon(path, options) {
   if (isSingleVueEventBindingExpressionStatement(path, options)) {
     const expression = unwrapVueEventBindingTsNode(path.node.expression);
-    if (
+    return (
       isVueEventBindingFunctionExpression(expression) ||
       isVueEventBindingMemberExpression(expression)
-    ) {
-      parts.push(";");
-    }
-  } else if (
-    options.semi &&
-    !(
-      //Do not append semicolon after the only JSX element in a program
-      (
-        isSingleJsxExpressionStatementInMarkdown(path, options) ||
-        // Do not append semicolon after the only HTML event binding expression in a program
-        isSingleHtmlEventHandlerExpressionStatement(path, options)
-      )
-    )
-  ) {
-    parts.push(";");
+    );
   }
 
-  return parts;
+  if (!options.semi) {
+    return false;
+  }
+
+  if (
+    //Do not append semicolon after the only JSX element in a program
+    isSingleJsxExpressionStatementInMarkdown(path, options) ||
+    // Do not append semicolon after the only HTML event binding expression in a program
+    isSingleHtmlEventHandlerExpressionStatement(path, options)
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+function printExpressionStatement(path, options, print) {
+  return [print("expression"), shouldPrintSemicolon(path, options) ? ";" : ""];
 }
 
 export { printExpressionStatement };
