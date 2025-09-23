a2fc8ef34d4861d2824c826592f3812e2904f811
fisker <lionkay@gmail.com>
2025-09-23 16:47:15 +0800

    Rename

--- src/language-html/embed/event-handler.js
+++ src/language-html/embed/event-handler.js
@@ -1,15 +1,15 @@
-import htmlEventAttributes from "@prettier/html-event-attributes";
+import htmlEventAttributesArray from "@prettier/html-event-attributes";
 import { getUnescapedAttributeValue } from "../utils/index.js";
 import { formatAttributeValue, shouldHugJsExpression } from "./utils.js";
 
-const eventAttributes = new Set(htmlEventAttributes);
+const htmlEventAttributes = new Set(htmlEventAttributesArray);
 
 function printEventHandler(path, options) {
   const { node } = path;
 
   if (
     options.parentParser ||
-    !eventAttributes.has(node.fullName) ||
+    !htmlEventAttributes.has(node.fullName) ||
     node.value.includes("{{")
   ) {
     return;
