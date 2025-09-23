b7b2b0d4348a758e5f37c226bd26edf0d667fba8
fisker Cheung <lionkay@gmail.com>
2025-09-23 17:51:36 +0800

    Print trailing comma in `TSImportType` options (#17798)

--- src/language-js/print/object.js
+++ src/language-js/print/object.js
@@ -161,12 +161,7 @@ function printObject(path, options, print) {
           lastElem.type === "TSMethodSignature" ||
           lastElem.type === "TSConstructSignatureDeclaration" ||
           lastElem.type === "TSIndexSignature") &&
-          hasComment(lastElem, CommentCheckFlags.PrettierIgnore)))) ||
-    // https://github.com/microsoft/TypeScript/issues/61916
-    path.match(
-      undefined,
-      (node, key) => node.type === "TSImportType" && key === "options",
-    )
+          hasComment(lastElem, CommentCheckFlags.PrettierIgnore))))
   );
 
   let content;
