4f661f7d80ef3bbe5858cdf50cac1b2226cf1ea4
fisker <lionkay@gmail.com>
2025-09-25 03:43:12 +0800

    More parsers on reserved word tests

--- tests/format/misc/errors/js/reserved-word/format.test.js
+++ tests/format/misc/errors/js/reserved-word/format.test.js
@@ -3,5 +3,35 @@ runFormatTest(
     importMeta: import.meta,
     snippets: ["class interface {}", 'import interface from "foo";'],
   },
-  ["espree", "meriyah"],
+  [
+    // "babel",
+    // "babel-ts",
+    "acorn",
+    "espree",
+    "flow",
+    "meriyah",
+    // "typescript",
+    // "hermes",
+    // "oxc",
+    // "oxc-ts",
+  ],
+);
+
+runFormatTest(
+  {
+    importMeta: import.meta,
+    snippets: [String.raw`export asyn\u{63} from "async";`],
+  },
+  [
+    // "babel",
+    // "babel-ts",
+    "acorn",
+    "espree",
+    "flow",
+    "meriyah",
+    "typescript",
+    "hermes",
+    "oxc",
+    "oxc-ts",
+  ],
 );
