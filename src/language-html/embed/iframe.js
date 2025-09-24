951487a9b9175906ac93c6071a5a5c98311e2481
Konv Suu <2583695112@qq.com>
2025-09-24 15:28:19 +0800

    update embed

--- src/language-html/embed/iframe.js
+++ src/language-html/embed/iframe.js
@@ -6,28 +6,31 @@ import { printExpand } from "./utils.js";
 function printIframeAttribute(path, options) {
   const { node } = path;
 
-  if (node.fullName !== "allow" || options.parentParser) {
+  if (
+    node.fullName !== "allow" ||
+    options.parentParser ||
+    node.value.includes("{{")
+  ) {
     return;
   }
 
-  const text = getUnescapedAttributeValue(node).trim();
-  if (!text.includes("{{")) {
-    const permissions = parseContentSecurityPolicy(text);
+  const permissions = parseContentSecurityPolicy(
+    getUnescapedAttributeValue(node),
+  );
 
-    return () =>
-      printExpand(
-        join(
-          ifBreak(softline, "; "),
-          permissions.map((permission) => [
-            permission.name,
-            ...(permission.value.length > 0
-              ? [" ", permission.value.join(" ")]
-              : []),
-            ifBreak(";"),
-          ]),
-        ),
-      );
-  }
+  return () =>
+    printExpand(
+      join(
+        ifBreak(softline, "; "),
+        permissions.map((permission) => [
+          permission.name,
+          ...(permission.value.length > 0
+            ? [" ", permission.value.join(" ")]
+            : []),
+          ifBreak(";"),
+        ]),
+      ),
+    );
 }
 
 export { printIframeAttribute };
--- src/language-html/embed/style.js
+++ src/language-html/embed/style.js
@@ -3,18 +3,21 @@ import { printExpand } from "./utils.js";
 
 function printStyleAttribute(path, options) {
   const { node } = path;
-
-  if (node.fullName !== "style" || options.parentParser) {
+  if (
+    node.fullName !== "style" ||
+    options.parentParser ||
+    node.value.includes("{{")
+  ) {
     return;
   }
 
-  const text = getUnescapedAttributeValue(path.node).trim();
-  if (!text.includes("{{")) {
-    return async (textToDoc) =>
-      printExpand(
-        await textToDoc(text, { parser: "css", __isHTMLStyleAttribute: true }),
-      );
-  }
+  return async (textToDoc) =>
+    printExpand(
+      await textToDoc(getUnescapedAttributeValue(node), {
+        parser: "css",
+        __isHTMLStyleAttribute: true,
+      }),
+    );
 }
 
 export { printStyleAttribute };
