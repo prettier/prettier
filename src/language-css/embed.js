2f73b360e8e5721d821fdc3fae13d1540d7b3562
fisker <lionkay@gmail.com>
2025-09-27 17:48:28 +0800

    Fix tests

--- src/language-css/embed.js
+++ src/language-css/embed.js
@@ -1,21 +1,10 @@
-import { hardline } from "../document/builders.js";
 import {
   isEmbedFrontMatter,
   printEmbedFrontMatter,
 } from "../utils/front-matter/index.js";
 
-const printers = [
-  {
-    test: isEmbedFrontMatter,
-    async print(...args) {
-      const doc = await printEmbedFrontMatter(...args);
-      return doc ? [doc, hardline] : undefined;
-    },
-  },
-];
-
 function embed(path) {
-  return printers.find(({ test }) => test(path))?.print;
+  return isEmbedFrontMatter(path) && printEmbedFrontMatter;
 }
 
 // `front-matter` only available on `css-root`
