d94c20c03e290e1af553f6344be4267b0a02d1f8
Konv Suu <2583695112@qq.com>
2025-09-27 11:49:23 +0800

    update mock plugin

--- tests/config/prettier-plugins/prettier-plugin-toml/index.js
+++ tests/config/prettier-plugins/prettier-plugin-toml/index.js
@@ -2,6 +2,6 @@ import createPlugin from "../../utils/create-plugin.cjs";
 
 const COMMENT = "/* Formatted by toml plugin */";
 export default createPlugin({
-  name: "toml",
+  name: "prettier-plugin-dummy-toml",
   print: (text) => COMMENT + "\n" + text.replace(COMMENT, "").trim(),
 });
--- tests/format/markdown/with-plugins/format.test.js
+++ tests/format/markdown/with-plugins/format.test.js
@@ -1,6 +1,6 @@
-import tomlPlugin from "../../../config/prettier-plugins/prettier-plugin-toml/index.js";
+import prettierPluginToml from "../../../config/prettier-plugins/prettier-plugin-toml/index.js";
 
 runFormatTest(import.meta, ["markdown"], {
   proseWrap: "always",
-  plugins: [tomlPlugin],
+  plugins: [prettierPluginToml],
 });
