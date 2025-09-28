6a4a9dde6cce06e10f0385b74720f0196b5bc856
OlTrenin <rucapashna@gmail.com>
2025-09-28 10:57:26 +0300

    add tests

--- tests/format/js/conditional/no-confusing-arrow.js
+++ tests/format/js/conditional/no-confusing-arrow.js
@@ -1,3 +1,11 @@
 // no-confusing-arrow
 var x = a => 1 ? 2 : 3;
 var x = a <= 1 ? 2 : 3;
+
+a = () => 1 ? [] : [];
+a = () => long_long_long_long_long_long_long_long_long_long_long_condition ? [] : [];
+
+b = () => () => condition ? result1 : result2;
+b = () => () => () => test ? value1 : value2;
+
+c = () => ({}) ? result1 : result2;
