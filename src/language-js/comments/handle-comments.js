1df69cf40a1b30e4bda3a0f2fa1fdb7ec733bbbd
fisker <lionkay@gmail.com>
2025-09-29 16:53:57 +0800

    Diff

--- src/language-js/comments/handle-comments.js
+++ src/language-js/comments/handle-comments.js
@@ -207,6 +207,7 @@ function handleIfStatementComments({
     addTrailingComment(precedingNode, comment);
     return true;
   }
+
   // Comments before `else`:
   // - treat as trailing comments of the consequent, if it's a BlockStatement
   // - treat as a dangling comment otherwise
