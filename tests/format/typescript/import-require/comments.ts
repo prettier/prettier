0190c8dd1d1e09029bc88cef1c6975b2d76503e8
fisker <lionkay@gmail.com>
2025-10-10 23:09:02 +0800

    Add tests

---
+++ tests/format/typescript/import-require/comments.ts
@@ -0,0 +1,9 @@
+import type A = require("./a/long/long/long/long/long/long/long/long/long/long/long/long/path/to/module");
+const A = require("./a/long/long/long/long/long/long/long/long/long/long/long/long/path/to/module");
+
+import type B = require(
+// Comment
+"./a/long/long/long/long/long/long/long/long/long/long/long/long/path/to/module");
+const B = require(
+// Comment
+"./a/long/long/long/long/long/long/long/long/long/long/long/long/path/to/module");
