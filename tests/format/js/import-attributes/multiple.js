12c3ac8bc178b4d4defe0bcf1e2645b2f2875413
fisker <lionkay@gmail.com>
2025-10-11 15:25:58 +0800

    Tests

--- tests/format/js/import-attributes/multiple.js
+++ tests/format/js/import-attributes/multiple.js
@@ -2,11 +2,11 @@ import syntaxImportAssertions from "@babel/plugin-syntax-import-assertions" with
   BABEL_8_BREAKING: "false",
   USE_ESM: "true", IS_STANDALONE: "false" };
 
-import foo from "foo" with {
+import a1 from "foo" with {
   BABEL_8_BREAKING: "false",
   USE_ESM: "true", IS_STANDALONE: "false" };
-
-import foo2 from "foo" with {
+import a2 from "foo" with {BABEL_8_BREAKING: "false",
+  USE_ESM: "true", IS_STANDALONE: "false" };
+import a3 from "foo" with {
   BABEL_8_BREAKING: "false", };
-
-import foo3 from "foo" with { BABEL_8_BREAKING: "false", };
+import a4 from "foo" with { BABEL_8_BREAKING: "false", };
