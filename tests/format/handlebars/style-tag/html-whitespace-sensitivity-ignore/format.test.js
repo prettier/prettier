eb6d36cfb9701c816287aa50584ee60ee1db9c5b
fisker <lionkay@gmail.com>
2025-10-15 06:31:34 +0800

    Update tests

--- tests/format/handlebars/style-tag/html-whitespace-sensitivity-ignore/format.test.js
+++ tests/format/handlebars/style-tag/html-whitespace-sensitivity-ignore/format.test.js
@@ -1,3 +1,7 @@
 runFormatTest(import.meta, ["glimmer"], {
   htmlWhitespaceSensitivity: "ignore",
 });
+runFormatTest(import.meta, ["glimmer"], {
+  htmlWhitespaceSensitivity: "ignore",
+  embeddedLanguageFormatting: "off",
+});
