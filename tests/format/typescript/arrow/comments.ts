ee897e9fb1d763814fd3a8cbe1b2067a70156bae
fisker <lionkay@gmail.com>
2025-10-17 04:50:05 +0800

    Use a different dir

--- tests/format/typescript/arrow/comments.ts
+++ tests/format/typescript/arrow/comments.ts
@@ -8,12 +8,3 @@ const fn2 = () => {
 
 // foo
 ;
-
-const fn3 = (): any => /*
-A comment after an arrow with a type definition, and before a line break.
-Formatting should keep it after the arrow.
-*/
-null;
-
-const fn4 = (): any => /* A single line ver of `fn3` */
-null;
---
+++ tests/format/typescript/arrow/comments/issue-11100.ts
@@ -0,0 +1,8 @@
+const fn3 = (): any => /*
+A comment after an arrow with a type definition, and before a line break.
+Formatting should keep it after the arrow.
+*/
+null;
+
+const fn4 = (): any => /* A single line ver of `fn3` */
+null;
