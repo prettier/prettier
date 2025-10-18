cc64de24672fd3509c71d00373e253af6a32ed83
fisker <lionkay@gmail.com>
2025-10-18 09:42:09 +0800

    More tests

--- tests/format/js/for/9812.js
+++ tests/format/js/for/9812.js
@@ -1,3 +1,54 @@
 for (const p of ['fullName', 'organ', 'position', 'rank'])
-  // comment
+  // 1
   form.setValue(`${prefix}.data.${p}`, response[p])
+
+for (const p of ['fullName', 'organ', 'position', 'rank'])
+// 2
+{form.setValue(`${prefix}.data.${p}`, response[p])}
+
+for (const p of ['fullName', 'organ', 'position', 'rank'])
+// 3
+{}
+
+for (const p of ['fullName', 'organ', 'position', 'rank'])
+// 4
+;
+
+for(x of y)
+  // comment
+  bar();
+
+for(x in y)
+  // comment
+  bar();
+
+for(;;)
+  // comment
+  bar();
+
+for(x of y)
+  // comment1
+  // comment2
+  bar();
+
+for(x in y)
+  // comment1
+  // comment2
+  bar();
+
+for(;;)
+  // comment1
+  // comment2
+  bar();
+
+for(x of y)
+  /* comment */
+  bar();
+
+for(x in y)
+  /* comment */
+  bar();
+
+for(;;)
+  /* comment */
+  bar();
