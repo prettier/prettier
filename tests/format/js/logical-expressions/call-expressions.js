ddd5e55186f045aadef631705e2d76510f9e2fcc
Konv Suu <2583695112@qq.com>
2025-10-21 10:17:02 +0800

    update

---
+++ tests/format/js/logical-expressions/call-expressions.js
@@ -0,0 +1,57 @@
+Foo(
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition,
+);
+
+Foo(
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition,
+  a_long_long_long_long_condition
+);
+
+Bar(
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition,
+);
+
+Bar(
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition,
+  a_long_long_long_long_condition
+);
+
+
+Baz(
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition,
+);
+
+Baz(
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition,
+  a_long_long_long_long_condition
+);
+
+(
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition
+).call();
+
+(
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition
+).call();
+
+(
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition
+).call();
---
+++ tests/format/js/logical-expressions/new-expressions.js
@@ -0,0 +1,39 @@
+new Foo(
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition,
+);
+
+new Foo(
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition ||
+  a_long_long_long_long_condition,
+  a_long_long_long_long_condition
+);
+
+new Bar(
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition,
+);
+
+new Bar(
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition &&
+  a_long_long_long_long_condition,
+  a_long_long_long_long_condition
+);
+
+
+new Baz(
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition,
+);
+
+new Baz(
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition ??
+  a_long_long_long_long_condition,
+  a_long_long_long_long_condition
+);
