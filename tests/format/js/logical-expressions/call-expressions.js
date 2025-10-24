f31f6e00f54083f3b4c0bbe5ffe2ee90523a293f
fisker <lionkay@gmail.com>
2025-10-24 20:44:05 +0800

    Rewrite tests

--- tests/format/js/logical-expressions/call-expressions.js
+++ tests/format/js/logical-expressions/call-expressions.js
@@ -1,57 +1,52 @@
-Foo(
-  a_long_long_long_long_condition ||
-  a_long_long_long_long_condition ||
-  a_long_long_long_long_condition,
+a = foo(
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition,
+);
+a = new Foo(
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition,
+);
+a = !(
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition
+);
+a = !!(
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition
 );
 
-Foo(
-  a_long_long_long_long_condition ||
-  a_long_long_long_long_condition ||
-  a_long_long_long_long_condition,
-  a_long_long_long_long_condition
+a = foo(
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition,
+  anotherArgument,
+);
+a = new Foo(
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition,
+  anotherArgument,
 );
 
-Bar(
-  a_long_long_long_long_condition &&
-  a_long_long_long_long_condition &&
-  a_long_long_long_long_condition,
+// Different operators
+a = foo(
+  a_long_long_long_long_condition && a_long_long_long_long_condition && a_long_long_long_long_condition,
+);
+a = foo(
+  a_long_long_long_long_condition ?? a_long_long_long_long_condition ?? a_long_long_long_long_condition,
 );
 
-Bar(
-  a_long_long_long_long_condition &&
-  a_long_long_long_long_condition &&
-  a_long_long_long_long_condition,
-  a_long_long_long_long_condition
+// Not argument
+a = (
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition
+)(foo);
+a = new (
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition
+)(Foo);
+
+// Nested
+a = foo(
+  (a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition)
+  &&
+  (a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition)
 );
-
-
-Baz(
-  a_long_long_long_long_condition ??
-  a_long_long_long_long_condition ??
-  a_long_long_long_long_condition,
+a = foo(
+  (a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition)
+  ||
+  (a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition)
 );
-
-Baz(
-  a_long_long_long_long_condition ??
-  a_long_long_long_long_condition ??
-  a_long_long_long_long_condition,
-  a_long_long_long_long_condition
-);
-
-(
-  a_long_long_long_long_condition ||
-  a_long_long_long_long_condition ||
-  a_long_long_long_long_condition
-).call();
-
-(
-  a_long_long_long_long_condition &&
-  a_long_long_long_long_condition &&
-  a_long_long_long_long_condition
-).call();
-
-(
-  a_long_long_long_long_condition ??
-  a_long_long_long_long_condition ??
-  a_long_long_long_long_condition
-).call();
+a = foo(foo(
+  a_long_long_long_long_condition || a_long_long_long_long_condition || a_long_long_long_long_condition,
+));
