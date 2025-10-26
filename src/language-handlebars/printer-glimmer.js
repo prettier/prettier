b625cb8aec62ba619d5881e010285505120d3a01
fisker <lionkay@gmail.com>
2025-10-27 07:38:29 +0800

    Fix empty node detect

--- src/language-handlebars/printer-glimmer.js
+++ src/language-handlebars/printer-glimmer.js
@@ -40,40 +40,37 @@ function print(path, options, print) {
       return group(path.map(print, "body"));
 
     case "ElementNode": {
-      const startingTag = group(printStartingTag(path, print));
+      const isWhitespaceSensitive =
+        options.htmlWhitespaceSensitivity !== "ignore";
 
-      const escapeNextElementNode =
-        options.htmlWhitespaceSensitivity === "ignore" &&
-        path.next?.type === "ElementNode"
+      const startingTag = [
+        !isWhitespaceSensitive && path.previous?.type === "ElementNode"
           ? softline
-          : "";
+          : "",
+        group([printStartingTag(path, print)]),
+      ];
 
       if (isVoidElement(node)) {
-        return [startingTag, escapeNextElementNode];
+        return [startingTag];
       }
 
       const endingTag = ["</", node.tag, ">"];
 
-      if (node.children.length === 0) {
-        return [startingTag, indent(endingTag), escapeNextElementNode];
+      if (
+        node.children.length === 0 ||
+        (!isWhitespaceSensitive &&
+          node.children.every((node) => isWhitespaceNode(node)))
+      ) {
+        return [startingTag, endingTag];
       }
+
+      const contextDoc = printElementChildren(path, options, print);
 
       if (options.htmlWhitespaceSensitivity === "ignore") {
-        return [
-          startingTag,
-          indent(printChildren(path, options, print)),
-          hardline,
-          endingTag,
-          escapeNextElementNode,
-        ];
+        return group([startingTag, indent(contextDoc), hardline, endingTag]);
       }
 
-      return [
-        startingTag,
-        indent(group(printChildren(path, options, print))),
-        endingTag,
-        escapeNextElementNode,
-      ];
+      return [startingTag, indent(group(contextDoc)), endingTag];
     }
 
     case "BlockStatement":
@@ -433,13 +430,7 @@ function printStartingTag(path, print) {
   return ["<", node.tag, indent(attributes), printStartingTagEndMarker(node)];
 }
 
-function printChildren(path, options, print) {
-  const { node } = path;
-  const isEmpty = node.children.every((node) => isWhitespaceNode(node));
-  if (options.htmlWhitespaceSensitivity === "ignore" && isEmpty) {
-    return "";
-  }
-
+function printElementChildren(path, options, print) {
   return path.map(({ isFirst }) => {
     const printedChild = print();
 
