7d3ff5d2fccd825c16e9fff7f638be7dd5f4bc50
fisker Cheung <lionkay@gmail.com>
2025-10-31 02:43:23 +0800

    Fix comment around `as`/`satisfies` expression (#18162)

--- tests/format/typescript/as/as-const/as-const.ts
+++ tests/format/typescript/as/as-const/as-const.ts
@@ -12,22 +12,3 @@ const;
 1 as const; // comment
 
 1 as /* comment */ not_const;
-
-// https://github.com/prettier/prettier/issues/18160
-// 1 as /*
-// 1
-// */ const;
-1 as
-/*
-2
-*/ const;
-// https://github.com/prettier/prettier/issues/18160
-// 1 as /*
-// 3
-// */
-// const;
-1 as
-/*
-4
-*/
-const;
