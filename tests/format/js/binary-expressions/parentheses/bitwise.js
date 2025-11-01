7d9933d71f94d575f36c76597985fa99b33cb7f6
fisker Cheung <lionkay@gmail.com>
2025-11-01 16:31:33 +0800

    Rewrite tests for 18163 (#18185)

--- tests/format/js/binary-expressions/parentheses/bitwise.js
+++ tests/format/js/binary-expressions/parentheses/bitwise.js
@@ -1,13 +1,13 @@
 // https://github.com/prettier/prettier/issues/18145
-1 << (bit % 8);
-1 >> (bit - 8);
-1 | (bit % 8);
-1 & (bit % 8);
-1 ^ (bit % 8);
-1 >>> (bit % 8);
+1 << bit % 8;
+1 >> bit - 8;
+1 | bit % 8;
+1 & bit % 8;
+1 ^ bit % 8;
+1 >>> bit % 8;
 (a + b) % c;
 a % (b + c);
-1 << (bit * 8);
-1 >> (bit / 8);
-1 << (bit + 8);
-1 >> (bit - 8);
+1 << bit * 8;
+1 >> bit / 8;
+1 << bit + 8;
+1 >> bit - 8;
