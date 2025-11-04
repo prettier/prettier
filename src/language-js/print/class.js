0dc497fa6c1719b02a4230d727b0e864801dde10
fisker <lionkay@gmail.com>
2025-11-05 01:49:15 +0800

    Remove

--- src/language-js/print/class.js
+++ src/language-js/print/class.js
@@ -31,7 +31,6 @@ import {
 } from "./misc.js";
 import { printPropertyKey } from "./property.js";
 import { printTypeAnnotationProperty } from "./type-annotation.js";
-import { getTypeParametersGroupId } from "./type-parameters.js";
 
 /**
  * @import {Doc} from "../../document/index.js"
