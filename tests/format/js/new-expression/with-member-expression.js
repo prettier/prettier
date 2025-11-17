d0325c7c52b2706dd6475ff364f2de48a684389b
fisker <lionkay@gmail.com>
2025-11-17 22:07:17 +0800

    Test

--- tests/format/js/new-expression/with-member-expression.js
+++ tests/format/js/new-expression/with-member-expression.js
@@ -11,5 +11,7 @@ function functionName() {
 
 foo = new long_long_member.long_long_member.long_long_member.long_long_member.long_long_member();
 new long_long_member.long_long_member.long_long_member.long_long_member.long_long_member();
+new long_long_member.long_long_member.long_long_member.long_long_member.long_long_member(x);
 foo = long_long_member.long_long_member.long_long_member.long_long_member.long_long_member();
 long_long_member.long_long_member.long_long_member.long_long_member.long_long_member();
+long_long_member.long_long_member.long_long_member.long_long_member.long_long_member(x);
