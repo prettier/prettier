cd3e47cbe8b97799214e4514e17f59e8fc6f7974
fisker <lionkay@gmail.com>
2025-11-26 17:59:40 +0800

    type arguments

---
+++ tests/format/typescript/class-and-interface/heritage-break/type-arguments.ts
@@ -0,0 +1,14 @@
+class CommentBox extends React.Component<
+  { url: string; pollInterval: number },
+  CommentData
+> {}
+
+interface CommentBox extends React.Component<
+  { url: string; pollInterval: number },
+  CommentData
+> {}
+
+declare class CommentBox implements React.Component<
+  { url: string; pollInterval: number },
+  CommentData
+> {}
