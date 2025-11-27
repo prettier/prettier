8c2a78e56bb920fb8d8466ac1c7c1643535c4f48
Konv Suu <2583695112@qq.com>
2025-11-27 15:11:47 +0800

    rename test folder and more test cases

---
+++ tests/format/yaml/mapping/trailing-whitespace/format.test.js
@@ -0,0 +1,26 @@
+const blockStyles = ["|", ">", "|+", "|-", ">+", ">-"];
+const trailingSpaces = ["", "  ", "\t"];
+const newlines = ["", "\n", "\n\n", "\n\n\n"];
+
+const emptyBlockTests = blockStyles.flatMap(style =>
+  trailingSpaces.flatMap(space =>
+    newlines.map(nl => `foo: ${style}${space}${nl}`)
+  )
+);
+
+const contentBlockTests = ["|", ">"].flatMap(style =>
+  ["   ", "  \t"].flatMap(space =>
+    ["", "\n", "\n\n"].map(nl => `foo: ${style}\n  x\n${space}${nl}`)
+  )
+);
+
+runFormatTest(
+  {
+    importMeta: import.meta,
+    snippets: [
+      ...emptyBlockTests,
+      ...contentBlockTests,
+    ]
+  },
+  ["yaml"],
+);
