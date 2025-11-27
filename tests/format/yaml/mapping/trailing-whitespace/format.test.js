aa238d4641c206010a55e3587a9d844bafad5a15
Konv Suu <2583695112@qq.com>
2025-11-27 15:12:37 +0800

    format

--- tests/format/yaml/mapping/trailing-whitespace/format.test.js
+++ tests/format/yaml/mapping/trailing-whitespace/format.test.js
@@ -2,25 +2,22 @@ const blockStyles = ["|", ">", "|+", "|-", ">+", ">-"];
 const trailingSpaces = ["", "  ", "\t"];
 const newlines = ["", "\n", "\n\n", "\n\n\n"];
 
-const emptyBlockTests = blockStyles.flatMap(style =>
-  trailingSpaces.flatMap(space =>
-    newlines.map(nl => `foo: ${style}${space}${nl}`)
-  )
+const emptyBlockTests = blockStyles.flatMap((style) =>
+  trailingSpaces.flatMap((space) =>
+    newlines.map((nl) => `foo: ${style}${space}${nl}`),
+  ),
 );
 
-const contentBlockTests = ["|", ">"].flatMap(style =>
-  ["   ", "  \t"].flatMap(space =>
-    ["", "\n", "\n\n"].map(nl => `foo: ${style}\n  x\n${space}${nl}`)
-  )
+const contentBlockTests = ["|", ">"].flatMap((style) =>
+  ["   ", "  \t"].flatMap((space) =>
+    ["", "\n", "\n\n"].map((nl) => `foo: ${style}\n  x\n${space}${nl}`),
+  ),
 );
 
 runFormatTest(
   {
     importMeta: import.meta,
-    snippets: [
-      ...emptyBlockTests,
-      ...contentBlockTests,
-    ]
+    snippets: [...emptyBlockTests, ...contentBlockTests],
   },
   ["yaml"],
 );
