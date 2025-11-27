a01a8183213fbb37bf84137616ddae6fc2cea662
Konv Suu <2583695112@qq.com>
2025-11-27 11:26:30 +0800

    Fix `L24T.yaml` bug in `yaml-test-suite`

--- src/language-yaml/utilities.js
+++ src/language-yaml/utilities.js
@@ -282,7 +282,7 @@ function getBlockValueLineContents(
 
     let trailingNewlineCount = 0;
     for (let i = lineContents.length - 1; i >= 0; i--) {
-      if (lineContents[i].length === 0) {
+      if (lineContents[i].every((line) => line.trim() === "")) {
         trailingNewlineCount++;
       } else {
         break;
