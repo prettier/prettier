e2b4972ecbd7eee214210c26386b49966a4981db
fisker <lionkay@gmail.com>
2025-11-28 10:18:58 +0800

    More tests

--- tests/format/js/quotes/switch-quote/switch-quote.js
+++ tests/format/js/quotes/switch-quote/switch-quote.js
@@ -1,4 +1,7 @@
 console.log(
   // 18351
   "A descriptor\\'s .kind must be \"method\" or \"field\".",
+  "A descriptor\\'s .kind must be \"method\" or \"field\".\\",
+  "A descriptor\\'s .kind must be \"method\" or \"field\".\n",
+  "A descriptor\\'s .kind must be \"method\" or \"field\".\\n",
 );
