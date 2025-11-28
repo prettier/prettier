abc684e1b8100c83e73f0fd3a3db7da430250ca7
fisker <lionkay@gmail.com>
2025-11-28 12:10:03 +0800

    Simplify

--- src/utils/make-string.js
+++ src/utils/make-string.js
@@ -12,22 +12,14 @@ function makeString(rawText, enclosingQuote) {
     // If we matched an unescaped quote and it is of the _same_ type as we
     // intend to enclose the string with, it must be escaped, so return it with
     // a backslash.
-    if (unescaped === enclosingQuote) {
-      return "\\" + unescaped;
-    }
-
     if (unescaped) {
-      return unescaped;
+      return unescaped === enclosingQuote ? "\\" + unescaped : unescaped;
     }
 
     // If we matched an escape, and the escaped character is a quote of the
     // other type than we intend to enclose the string with, there's no need for
     // it to be escaped, so return it _without_ the backslash.
-    if (escaped === otherQuote) {
-      return escaped;
-    }
-
-    return "\\" + escaped;
+    return escaped === otherQuote ? escaped : "\\" + escaped;
   });
 
   return enclosingQuote + raw + enclosingQuote;
