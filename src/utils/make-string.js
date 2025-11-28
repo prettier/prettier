34970746807fa8045fd3f99fffe46117c9216e47
fisker <lionkay@gmail.com>
2025-11-28 10:22:43 +0800

    Fix

--- src/utils/make-string.js
+++ src/utils/make-string.js
@@ -1,7 +1,7 @@
 /** @import {Quote} from "./get-preferred-quote.js" */
 
 // Matches _any_ escape and unescaped quotes (both single and double).
-const REGEX = /\\?(?<quote>["'])/gu;
+const REGEX = /\\(.)|(["'])/gsu;
 
 /**
  * @param {string} rawText
@@ -9,14 +9,33 @@ const REGEX = /\\?(?<quote>["'])/gu;
  * @returns {string}
  */
 function makeString(rawText, enclosingQuote) {
-  return (
-    enclosingQuote +
-    rawText.replaceAll(
-      REGEX,
-      (_, quote) => (quote === enclosingQuote ? "\\" : "") + quote,
-    ) +
-    enclosingQuote
-  );
+  const otherQuote = enclosingQuote === '"' ? "'" : '"';
+
+  // Escape and unescape single and double quotes as needed to be able to
+  // enclose `rawText` with `enclosingQuote`.
+  const raw = rawText.replaceAll(REGEX, (match, escaped, quote) => {
+    // If we matched an escape, and the escaped character is a quote of the
+    // other type than we intend to enclose the string with, there's no need for
+    // it to be escaped, so return it _without_ the backslash.
+    if (escaped === otherQuote) {
+      return escaped;
+    }
+
+    // If we matched an unescaped quote and it is of the _same_ type as we
+    // intend to enclose the string with, it must be escaped, so return it with
+    // a backslash.
+    if (quote === enclosingQuote) {
+      return "\\" + quote;
+    }
+
+    if (quote) {
+      return quote;
+    }
+
+    return "\\" + escaped;
+  });
+
+  return enclosingQuote + raw + enclosingQuote;
 }
 
 export default makeString;
