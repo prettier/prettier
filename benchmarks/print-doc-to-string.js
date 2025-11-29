43973b470120aab8a18dd1dd961914b8328a4f1f
fisker <lionkay@gmail.com>
2025-11-29 18:29:43 +0800

    benchmarks/print-doc-to-string.js

--- benchmarks/print-doc-to-string.js
+++ benchmarks/print-doc-to-string.js
@@ -10,11 +10,11 @@ import { runBenchmark } from "./utilities.js";
 assert.notEqual(prettierProduction.version, prettierDevelopment.version);
 
 const text = await fs.readFile(new URL(import.meta.url), "utf8");
-// @ts-expect-error -- No types
 
-for (const size of [1, 1e1, 1e2, 1e3, 1e4]) {
+for (const size of [1, 1e1, 1e2, 1e3, 5e3]) {
   const doc = await Promise.all(
     Array.from({ length: size }, (_, index) =>
+      // @ts-expect-error -- No types
       prettierDevelopment.__debug.printToDoc(text, {
         parser: "babel",
         cursorOffset: index === 0 ? text.indexOf("cursorOffset") : undefined,
@@ -32,7 +32,7 @@ for (const size of [1, 1e1, 1e2, 1e3, 1e4]) {
       name: `printDocToString (${size}, ${prettyBytes(text.length * size)})`,
       assert: (result) => assert.deepEqual(result, expected),
       warmupIterations: 1,
-      iterations: size > 1e2 ? 2 : undefined,
+      iterations: size > 5e3 ? 1 : size > 1e2 ? 2 : undefined,
     },
     [
       { name: "3.6.2", prettier: prettier362 },
