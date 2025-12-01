e5c0d642c2e0eb3d30f593f8c2a1a87db313c0a5
fisker <lionkay@gmail.com>
2025-12-01 08:34:25 +0800

    Predefine options

--- src/utilities/get-preferred-quote.js
+++ src/utilities/get-preferred-quote.js
@@ -5,20 +5,35 @@
 const SINGLE_QUOTE = "'";
 const DOUBLE_QUOTE = '"';
 
+const SINGLE_QUOTE_DATA = Object.freeze({
+  character: SINGLE_QUOTE,
+  codePoint: 39,
+});
+const DOUBLE_QUOTE_DATA = Object.freeze({
+  character: DOUBLE_QUOTE,
+  codePoint: 34,
+});
+
+const SINGLE_QUOTE_SETTINGS = Object.freeze({
+  preferred: SINGLE_QUOTE_DATA,
+  alternate: DOUBLE_QUOTE_DATA,
+});
+const DOUBLE_QUOTE_SETTINGS = Object.freeze({
+  preferred: DOUBLE_QUOTE_DATA,
+  alternate: SINGLE_QUOTE_DATA,
+});
+
 /**
  * @param {string} text
  * @param {Quote | boolean} preferredQuoteOrPreferSingleQuote
  * @returns {Quote}
  */
 function getPreferredQuote(text, preferredQuoteOrPreferSingleQuote) {
-  const preferred =
+  const { preferred, alternate } =
     preferredQuoteOrPreferSingleQuote === true ||
     preferredQuoteOrPreferSingleQuote === SINGLE_QUOTE
-      ? SINGLE_QUOTE
-      : DOUBLE_QUOTE;
-  const alternate = preferred === SINGLE_QUOTE ? DOUBLE_QUOTE : SINGLE_QUOTE;
-  const preferredCodePoint = preferred.charCodeAt(0);
-  const alternateCodePoint = alternate.charCodeAt(0);
+      ? SINGLE_QUOTE_SETTINGS
+      : DOUBLE_QUOTE_SETTINGS;
   const { length } = text;
   let preferredQuoteCount = 0;
   let alternateQuoteCount = 0;
@@ -26,14 +41,15 @@ function getPreferredQuote(text, preferredQuoteOrPreferSingleQuote) {
   // `for..of` loop is known slower
   for (let index = 0; index < length; index++) {
     const codePoint = text.charCodeAt(index);
-    if (codePoint === preferredCodePoint) {
+    if (codePoint === preferred.codePoint) {
       preferredQuoteCount++;
-    } else if (codePoint === alternateCodePoint) {
+    } else if (codePoint === alternate.codePoint) {
       alternateQuoteCount++;
     }
   }
 
-  return preferredQuoteCount > alternateQuoteCount ? alternate : preferred;
+  return (preferredQuoteCount > alternateQuoteCount ? alternate : preferred)
+    .character;
 }
 
 export default getPreferredQuote;
