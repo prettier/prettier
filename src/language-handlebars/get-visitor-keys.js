22a2ee1fbd540972aa5815dc2b3f95a7a4a842c7
fisker <lionkay@gmail.com>
2025-12-01 17:21:19 +0800

    Remove file

--- src/language-handlebars/get-visitor-keys.js
+++
@@ -1,6 +0,0 @@
-import { visitorKeys } from "@glimmer/syntax";
-import createGetVisitorKeys from "../utilities/create-get-visitor-keys.js";
-
-const getVisitorKeys = createGetVisitorKeys(visitorKeys);
-
-export default getVisitorKeys;
