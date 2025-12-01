e827e29a11b7bd1759e0e679822ccb12cffebe82
fisker <lionkay@gmail.com>
2025-12-01 08:11:19 +0800

    Use const

--- src/common/end-of-line.js
+++ src/common/end-of-line.js
@@ -1,22 +1,26 @@
 /**
-@typedef {"auto" | OPTION[keyof OPTION]} EndOfLineOption
-@typedef {CHARACTER[keyof CHARACTER]} EndOfLine
+@typedef {"auto" | OPTION_CR | OPTION_CRLF | OPTION_LF} EndOfLineOption
+@typedef {CHARACTER_CR | CHARACTER_CRLF | CHARACTER_LF} EndOfLine
 */
 
-const OPTION = Object.freeze({ CR: "cr", CRLF: "crlf", LF: "lf" });
-const CHARACTER = Object.freeze({ CR: "\r", CRLF: "\r\n", LF: "\n" });
+const OPTION_CR = "cr";
+const OPTION_CRLF = "crlf";
+const OPTION_LF = "lf";
+const DEFAULT_OPTION = OPTION_LF;
 
-const DEFAULT_OPTION = OPTION.LF;
-const DEFAULT_EOL = CHARACTER.LF;
+const CHARACTER_CR = "\r";
+const CHARACTER_CRLF = "\r\n";
+const CHARACTER_LF = "\n";
+const DEFAULT_EOL = CHARACTER_LF;
 
 /**
 @param {string} text
 @returns {EndOfLineOption}
 */
 function guessEndOfLine(text) {
-  const index = text.indexOf(CHARACTER.CR);
+  const index = text.indexOf(CHARACTER_CR);
   if (index !== -1) {
-    return text.charAt(index + 1) === CHARACTER.LF ? OPTION.CRLF : OPTION.CR;
+    return text.charAt(index + 1) === CHARACTER_LF ? OPTION_CRLF : OPTION_CR;
   }
   return DEFAULT_OPTION;
 }
@@ -26,17 +30,17 @@ function guessEndOfLine(text) {
 @returns {EndOfLine}
 */
 function convertEndOfLineOptionToCharacter(endOfLineOption) {
-  return endOfLineOption === OPTION.CR
-    ? CHARACTER.CR
-    : endOfLineOption === OPTION.CRLF
-      ? CHARACTER.CRLF
+  return endOfLineOption === OPTION_CR
+    ? CHARACTER_CR
+    : endOfLineOption === OPTION_CRLF
+      ? CHARACTER_CRLF
       : DEFAULT_EOL;
 }
 
 const regexps = new Map([
-  [CHARACTER.LF, /\n/gu],
-  [CHARACTER.CR, /\r/gu],
-  [CHARACTER.CRLF, /\r\n/gu],
+  [CHARACTER_LF, /\n/gu],
+  [CHARACTER_CR, /\r/gu],
+  [CHARACTER_CRLF, /\r\n/gu],
 ]);
 
 /**
@@ -62,7 +66,7 @@ function countEndOfLineCharacters(text, endOfLineCharacter) {
 @returns {string}
 */
 function normalizeEndOfLine(text) {
-  return text.replaceAll(/\r\n?/gu, CHARACTER.LF);
+  return text.replaceAll(/\r\n?/gu, CHARACTER_LF);
 }
 
 export {
