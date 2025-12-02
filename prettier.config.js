71c0f4fa037ceb2a6a312bd36b114b02a3a35c91
fisker <lionkay@gmail.com>
2025-12-02 23:07:15 +0800

    Use js config ourself

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+/** @type {import("prettier").Config} */
+const config = {
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      options: {
+        parser: "meriyah",
+      },
+    },
+    {
+      files: ["bin/prettier.cjs"],
+      options: {
+        trailingComma: "none",
+      },
+    },
+    {
+      files: [".vscode/*.json", "tsconfig.json"],
+      options: {
+        parser: "jsonc",
+      },
+    },
+  ],
+};
+
+export default config;
