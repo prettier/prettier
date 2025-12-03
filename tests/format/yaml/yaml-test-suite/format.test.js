92d56dbc392ecbbef5289aa2212c1adae3004651
fisker <lionkay@gmail.com>
2025-12-04 02:10:01 +0800

    Update list

--- tests/format/yaml/yaml-test-suite/format.test.js
+++ tests/format/yaml/yaml-test-suite/format.test.js
@@ -2,7 +2,24 @@ import yamlTestSuite from "yaml-test-suite";
 
 // https://github.com/eemeli/yaml/blob/086fa6b5bae325da18734750cddee231ce578930/tests/yaml-test-suite.ts#L19
 // https://github.com/prettier/yaml-unist-parser/blob/7b29a97bc3c1b98688a0b87709855e430735bb31/src/yaml-test-suite.test.ts#L4
-const SKIP = new Set(["2JQS.yaml", "9MMA.yaml", "SF5V.yaml"]);
+const SKIP = new Set([
+  //
+  "2JQS.yaml",
+  "9MMA.yaml",
+  "SF5V.yaml",
+]);
+
+// https://github.com/prettier/prettier/issues/18302
+const BUGS = new Set([
+  //
+  "Y79Y-2.yaml",
+  "M5DY.yaml",
+  "L24T.yaml",
+  "L24T-2.yaml",
+  "JEF9-3.yaml",
+  "DE56-3.yaml",
+  "DE56-4.yaml",
+]);
 
 runFormatTest(
   {
@@ -16,7 +33,7 @@ runFormatTest(
 
           const filename = `${id}${index === 0 ? "" : `-${index + 1}`}.yaml`;
 
-          if (SKIP.has(filename)) {
+          if (SKIP.has(filename) || BUGS.has(filename)) {
             // console.log(testCase);
             return;
           }
