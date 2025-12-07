434f34001dfed0ee05e1d5483b0925f27ca90f87
fisker <lionkay@gmail.com>
2025-12-07 16:11:51 +0800

    Fix ts estree type definition

--- src/language-js/types/typescript-estree.ts
+++ src/language-js/types/typescript-estree.ts
@@ -1,10 +1,19 @@
-import type { TSESTree } from "@typescript-eslint/typescript-estree";
+import type {
+  TSESTree,
+  AST_NODE_TYPES,
+} from "@typescript-eslint/typescript-estree";
 
 type NodeOrComment = TSESTree.Node | TSESTree.Comment;
 
-type ConvertTypeToString<Node extends NodeOrComment> = {
-  [Key in keyof Node]: Key extends "type" ? `${Node[Key]}` : Node[Key];
-};
+type ConvertTypeToString<Input> = Input extends NodeOrComment
+  ? {
+      [Key in keyof Input]: Key extends "type"
+        ? `${Input[Key]}`
+        : ConvertTypeToString<Input[Key]>;
+    }
+  : Input extends readonly (NodeOrComment | any)[]
+    ? ConvertTypeToString<Input[number]>[]
+    : Input;
 
 export type Node = ConvertTypeToString<TSESTree.Node>;
 export type Comment = ConvertTypeToString<TSESTree.Comment>;
