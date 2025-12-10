9f61e523185928217d714b677063a4c8e4accbef
fisker <lionkay@gmail.com>
2025-12-10 17:09:04 +0800

    Reduce diff

--- website/playground/sidebar/components.jsx
+++ website/playground/sidebar/components.jsx
@@ -1,16 +1,18 @@
 export function Sidebar({ visible }, { slots }) {
+  const children = slots.default();
   return (
     <div class={`options-container ${visible ? "open" : ""}`}>
-      <div class="options">{slots.default?.()}</div>
+      <div class="options">{children}</div>
     </div>
   );
 }
 
 export function SidebarCategory({ title }, { slots }) {
+  const children = slots.default();
   return (
     <details class="sub-options" open>
       <summary>{title}</summary>
-      {slots.default?.()}
+      {children}
     </details>
   );
 }
