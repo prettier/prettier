88259a3b7029d435f670a49c9e2d77aa87e5bffe
fisker <lionkay@gmail.com>
2025-12-10 21:57:03 +0800

    Bundle

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -3,6 +3,7 @@ import vitePluginVueJsx from "@vitejs/plugin-vue-jsx";
 import { defineConfig } from "vite";
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
+const IS_CI = Boolean(process.env.CI);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,13 +12,6 @@ export default defineConfig({
   plugins: [vitePluginVue(), vitePluginVueJsx()],
   build: {
     outDir: "./static/playground/",
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
+    minify: IS_CI,
   },
 });
