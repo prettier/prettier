7d10b174c81b37554d2bb187f840ee36613e67a2
fisker <lionkay@gmail.com>
2025-12-12 23:42:41 +0800

    Add test for 17807

--- tests/format/markdown/heading/setext/format.test.js
+++ tests/format/markdown/heading/setext/format.test.js
@@ -1,1 +1,25 @@
-runFormatTest(import.meta, ["markdown"], { proseWrap: "always" });
+import { outdent } from "outdent";
+
+runFormatTest(
+  {
+    importMeta: import.meta,
+    snippets: [
+      // #17807
+      outdent`
+        Multi${"\\"}
+        Line${" ".repeat(2)}
+        Setext{"\\"}
+        Heading
+        =======
+
+        Multi{"\\"}
+        Line${" ".repeat(2)}
+        Setext{"\\"}
+        Heading
+        -------
+      `,
+    ],
+  },
+  ["markdown"],
+  { proseWrap: "always" },
+);
