9abc1ab88615cf6fb1fc2a00853551379e8c0293
fisker <lionkay@gmail.com>
2025-12-14 04:41:39 +0800

    Split parser

--- src/language-markdown/parse/parse-markdown.js
+++ src/language-markdown/parse/parse-markdown.js
@@ -3,22 +3,13 @@ import { fromMarkdown as wikiLinkFromMarkdown } from "mdast-util-wiki-link";
 import { gfm as gfmSyntax } from "micromark-extension-gfm";
 import { math as mathSyntax } from "micromark-extension-math";
 import { syntax as wikiLinkSyntax } from "micromark-extension-wiki-link";
-import footnotes from "remark-footnotes";
-import remarkMath from "remark-math";
-import remarkParse from "remark-parse";
-import unified from "unified";
 import parseFrontMatter from "../../main/front-matter/parse.js";
-import { BLOCKS_REGEX, esSyntax } from "./mdx.js";
 import { fromMarkdown } from "./micromark/html-flow-hack.js";
 import { gfmFromMarkdown } from "./micromark/mdast-util-gfm.js";
 import {
   liquidFromMarkdown,
   liquidSyntax,
 } from "./micromark/micromark-extension-liquid.js";
-import frontMatter from "./unified-plugins/front-matter.js";
-import htmlToJsx from "./unified-plugins/html-to-jsx.js";
-import liquid from "./unified-plugins/liquid.js";
-import wikiLink from "./unified-plugins/wiki-link.js";
 
 let markdownParseOptions;
 function getMarkdownParseOptions() {
@@ -57,35 +48,4 @@ function parseMarkdown(text) {
   return ast;
 }
 
-/**
- * based on [MDAST](https://github.com/syntax-tree/mdast) with following modifications:
- *
- * 1. restore unescaped character (Text)
- * 2. merge continuous Texts
- * 3. replace whitespaces in InlineCode#value with one whitespace
- *    reference: http://spec.commonmark.org/0.25/#example-605
- * 4. split Text into Sentence
- *
- * interface Word { value: string }
- * interface Whitespace { value: string }
- * interface Sentence { children: Array<Word | Whitespace> }
- * interface InlineCode { children: Array<Sentence> }
- */
-
-function parseMdx(text) {
-  const processor = unified()
-    .use(remarkParse, {
-      commonmark: true,
-      blocks: [BLOCKS_REGEX],
-    })
-    .use(footnotes)
-    .use(frontMatter)
-    .use(remarkMath)
-    .use(esSyntax)
-    .use(liquid)
-    .use(htmlToJsx)
-    .use(wikiLink);
-  return processor.run(processor.parse(text));
-}
-
-export { parseMarkdown, parseMdx };
+export { parseMarkdown };
---
+++ src/language-markdown/parse/parse-mdx.js
@@ -0,0 +1,42 @@
+import footnotes from "remark-footnotes";
+import remarkMath from "remark-math";
+import remarkParse from "remark-parse";
+import unified from "unified";
+import { BLOCKS_REGEX, esSyntax } from "./mdx.js";
+import frontMatter from "./unified-plugins/front-matter.js";
+import htmlToJsx from "./unified-plugins/html-to-jsx.js";
+import liquid from "./unified-plugins/liquid.js";
+import wikiLink from "./unified-plugins/wiki-link.js";
+
+/**
+ * based on [MDAST](https://github.com/syntax-tree/mdast) with following modifications:
+ *
+ * 1. restore unescaped character (Text)
+ * 2. merge continuous Texts
+ * 3. replace whitespaces in InlineCode#value with one whitespace
+ *    reference: http://spec.commonmark.org/0.25/#example-605
+ * 4. split Text into Sentence
+ *
+ * interface Word { value: string }
+ * interface Whitespace { value: string }
+ * interface Sentence { children: Array<Word | Whitespace> }
+ * interface InlineCode { children: Array<Sentence> }
+ */
+
+function parseMdx(text) {
+  const processor = unified()
+    .use(remarkParse, {
+      commonmark: true,
+      blocks: [BLOCKS_REGEX],
+    })
+    .use(footnotes)
+    .use(frontMatter)
+    .use(remarkMath)
+    .use(esSyntax)
+    .use(liquid)
+    .use(htmlToJsx)
+    .use(wikiLink);
+  return processor.run(processor.parse(text));
+}
+
+export { parseMdx };
