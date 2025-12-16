487d295aaf373c27519cb783818bf40f4e2888a0
williamquintas <william.quintas@gmail.com>
2025-12-16 12:32:32 -0300

    Enhance binary cast expression handling and add tests for trace entry points
    - Updated `printBinaryCastExpression` to handle `NewExpression` in addition to `CallExpression`.
    - Introduced a new test file for TypeScript chain expressions involving `TraceEntryPointsPlugin`.
    - Updated snapshots to reflect the new formatting for the added test cases.

---
+++ tests/format/typescript/chain-expression/trace-entrypoints.ts
@@ -0,0 +1,9 @@
+new ((require("./webpack/plugins/next-trace-entrypoints-plugin")).TraceEntryPointsPlugin as P)({
+  rootDir: dir,
+});
+
+((require("./webpack/plugins/next-trace-entrypoints-plugin")).TraceEntryPointsPlugin as P)({
+  rootDir: dir,
+});
+
+
