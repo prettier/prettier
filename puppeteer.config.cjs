60e8a0348e959b846d85d78cc43ae9bdacd7bfd8
fisker <lionkay@gmail.com>
2025-12-23 20:00:09 +0800

    Cache

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,9 +1,10 @@
 "use strict";
 
 const path = require("node:path");
+const IS_CI = Boolean(process.env.CI);
 
 module.exports = {
   // Will download when execute
   skipDownload: true,
-  cacheDirectory: path.join(__dirname, "./.tmp/puppeteer/"),
+  cacheDirectory: IS_CI ? path.join(__dirname, "./.tmp/puppeteer/") : undefined,
 };
