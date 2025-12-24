c776b9e11cf9eabfc24ed58646747439a683c41e
fisker <lionkay@gmail.com>
2025-12-24 17:55:20 +0800

    `??` -> `||`

--- tests/config/install-browser.js
+++ tests/config/install-browser.js
@@ -3,7 +3,7 @@ import { downloadBrowser } from "./browser/download-browser.js";
 
 async function installBrowser() {
   const product =
-    process.env.TEST_RUNTIME_BROWSER_PRODUCT?.toLowerCase() ?? "chrome";
+    process.env.TEST_RUNTIME_BROWSER_PRODUCT?.toLowerCase() || "chrome";
 
   await downloadBrowser({ product });
 }
