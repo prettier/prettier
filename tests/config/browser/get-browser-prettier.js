188fc0599a4d4d5a6fa30deecb6192b438efd7e7
fisker <lionkay@gmail.com>
2025-12-24 23:38:50 +0800

    Run browser tests on UMD version

--- tests/config/browser/get-browser-prettier.js
+++ tests/config/browser/get-browser-prettier.js
@@ -3,7 +3,10 @@ import { startServer } from "./server.js";
 import { requestFromNode } from "./utilities.js";
 
 async function getBrowserPrettier() {
+  /** @type {"chrome" | "firefox"} */
   const browserName = process.env.TEST_RUNTIME_BROWSER ?? "chrome";
+  /** @type {"esm" | "umd"} */
+  const prettierBundleFormat = process.env.TEST_BUNDLE_FORMAT ?? "esm";
   const browser = await launchBrowser({ browser: browserName });
 
   process.once("exit", async () => {
@@ -16,14 +19,14 @@ async function getBrowserPrettier() {
   });
 
   const page = await browser.newPage();
-  await page.goto(server.url + "/esm.html");
+  await page.goto(server.url);
 
   const proxyFunction = (accessPath, optionsIndex = 1) =>
     requestFromNode(
       (...arguments_) =>
         page.evaluate(
-          ([arguments_, accessPath]) => {
-            const prettier = globalThis.__prettier;
+          ([arguments_, accessPath, prettierBundleFormat]) => {
+            const prettier = globalThis[`__prettier_${prettierBundleFormat}`];
             let function_ = prettier;
             for (const property of accessPath.split(".")) {
               function_ = function_[property];
@@ -31,7 +34,7 @@ async function getBrowserPrettier() {
 
             return function_(...arguments_);
           },
-          [arguments_, accessPath],
+          [arguments_, accessPath, prettierBundleFormat],
         ),
       {
         browser: browserName,
--- tests/config/browser/main.js
+++ tests/config/browser/main.js
@@ -22,50 +22,136 @@ const esmFiles = {
     "plugin-oxc/index.browser.mjs",
   ],
 };
+const umdFiles = {
+  prettier: "prettier/standalone.js",
+  plugins: [
+    "prettier/plugins/angular.js",
+    "prettier/plugins/graphql.js",
+    "prettier/plugins/typescript.js",
+    "prettier/plugins/estree.js",
+    "prettier/plugins/markdown.js",
+    "prettier/plugins/acorn.js",
+    "prettier/plugins/glimmer.js",
+    "prettier/plugins/postcss.js",
+    "prettier/plugins/babel.js",
+    "prettier/plugins/html.js",
+    "prettier/plugins/yaml.js",
+    "prettier/plugins/flow.js",
+    "prettier/plugins/meriyah.js",
+    // These two plugins don't have umd version
+    "plugin-hermes/index.mjs",
+    "plugin-oxc/index.browser.mjs",
+  ],
+};
 
-let prettier;
-let builtinPlugins;
+async function loadScript(url) {
+  const { document } = globalThis;
+  const { head } = document;
+  const script = Object.assign(document.createElement("script"), { src: url });
 
-function proxyFunction(accessPath, optionsIndex = 1) {
-  let function_;
-  return responseInBrowser(
-    async (...arguments_) => {
-      if (!prettier) {
-        [prettier, ...builtinPlugins] = await Promise.all(
-          [esmFiles.prettier, ...esmFiles.plugins].map(
-            (file) => import(`/${file}`),
-          ),
-        );
-      }
+  function on(event, callback) {
+    script.addEventListener(event, callback, { once: true, passive: true });
+  }
 
-      if (!function_) {
-        function_ = prettier;
-        for (const property of accessPath.split(".")) {
-          function_ = function_[property];
-        }
-      }
+  try {
+    await new Promise((resolve, reject) => {
+      on("load", resolve);
 
-      const options = arguments_[optionsIndex] ?? {};
+      on("error", () => {
+        reject(new Error(`Failed to load script '${url}'.`));
+      });
 
-      arguments_[optionsIndex] = {
-        ...options,
-        plugins: [...builtinPlugins, ...(options.plugins || [])],
-      };
-
-      return function_(...arguments_);
-    },
-    {
-      browser,
-      accessPath,
-      optionsIndex,
-    },
-  );
+      head.appendChild(script);
+    });
+  } finally {
+    head.removeChild(script);
+  }
 }
 
-globalThis.__prettier = {
-  formatWithCursor: proxyFunction("formatWithCursor"),
-  getSupportInfo: proxyFunction("getSupportInfo", /* optionsIndex */ 0),
-  __debug: {
-    parse: proxyFunction("__debug.parse"),
-  },
-};
+async function loadEsmPrettier() {
+  const [prettier, ...builtinPlugins] = await Promise.all(
+    [esmFiles.prettier, ...esmFiles.plugins].map((file) => import(`/${file}`)),
+  );
+
+  return { prettier, builtinPlugins };
+}
+
+async function loadUmdPrettier() {
+  const { umdPluginFiles, esmPluginFiles } = Object.groupBy(
+    umdFiles.plugins,
+    (file) => `${file.endsWith(".mjs") ? "esm" : "umd"}PluginFiles`,
+  );
+
+  const [, esmPlugins] = await Promise.all([
+    Promise.all(
+      [umdFiles.prettier, ...umdPluginFiles].map((file) =>
+        loadScript(`/${file}`),
+      ),
+    ),
+    Promise.all(esmPluginFiles.map((file) => import(`/${file}`))),
+  ]);
+
+  const { prettier } = globalThis;
+  const builtinPlugins = [
+    ...Object.values(globalThis.prettierPlugins),
+    ...esmPlugins,
+  ];
+  delete globalThis.prettier;
+  delete globalThis.prettierPlugins;
+  return { prettier, builtinPlugins };
+}
+
+function createPrettier(format) {
+  let cache;
+  function loadPrettier() {
+    if (!cache) {
+      cache = format === "esm" ? loadEsmPrettier() : loadUmdPrettier();
+    }
+
+    return cache;
+  }
+
+  const functions = new Map();
+
+  function proxyFunction(accessPath, optionsIndex = 1) {
+    let function_;
+    return responseInBrowser(
+      async (...arguments_) => {
+        const { prettier, builtinPlugins } = await loadPrettier();
+
+        if (!functions.has(accessPath)) {
+          function_ = prettier;
+          for (const property of accessPath.split(".")) {
+            function_ = function_[property];
+          }
+          functions.set(accessPath, function_);
+        }
+
+        const options = arguments_[optionsIndex] ?? {};
+
+        arguments_[optionsIndex] = {
+          ...options,
+          plugins: [...builtinPlugins, ...(options.plugins || [])],
+        };
+
+        return function_(...arguments_);
+      },
+      {
+        browser,
+        accessPath,
+        optionsIndex,
+      },
+    );
+  }
+
+  return {
+    formatWithCursor: proxyFunction("formatWithCursor"),
+    getSupportInfo: proxyFunction("getSupportInfo", /* optionsIndex */ 0),
+    __debug: {
+      parse: proxyFunction("__debug.parse"),
+    },
+  };
+}
+
+globalThis.__prettier_esm = createPrettier("esm");
+globalThis.__prettier_umd = createPrettier("umd");
