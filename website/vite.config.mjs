9c2b783d6c9510014641a37eac384b3c1f964c2f
fisker <lionkay@gmail.com>
2025-12-28 21:43:49 +0800

    Add `rollup-plugin-visualizer`

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -1,5 +1,6 @@
 import vitePluginVue from "@vitejs/plugin-vue";
 import vitePluginVueJsx from "@vitejs/plugin-vue-jsx";
+import { visualizer as rollupPluginVisualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
@@ -8,7 +9,13 @@ const IS_CI = Boolean(process.env.CI);
 export default defineConfig({
   base: IS_PRODUCTION ? "/playground/" : undefined,
   publicDir: IS_PRODUCTION ? undefined : "./static/",
-  plugins: [vitePluginVue(), vitePluginVueJsx()],
+  plugins: [
+    vitePluginVue(),
+    vitePluginVueJsx(),
+    IS_CI || !IS_PRODUCTION
+      ? undefined
+      : rollupPluginVisualizer({ filename: "./static/playground/report.html" }),
+  ].filter(Boolean),
   build: {
     outDir: "./static/playground/",
     minify: IS_CI,
