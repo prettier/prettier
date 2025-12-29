13d72c10460a0f902d33a49c6a3f20ed110d6548
fisker <lionkay@gmail.com>
2025-12-30 04:52:38 +0800

    Collect dep versions

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -1,3 +1,5 @@
+import fs from "node:fs/promises";
+import module from "node:module";
 import vitePluginVue from "@vitejs/plugin-vue";
 import vitePluginVueJsx from "@vitejs/plugin-vue-jsx";
 import { visualizer as rollupPluginVisualizer } from "rollup-plugin-visualizer";
@@ -16,7 +18,7 @@ const DEPENDENCIES_EXCLUDE_FROM_CDN = new Set([
   "cm6-graphql",
 ]);
 
-export default defineConfig(() => ({
+export default defineConfig(async () => ({
   base: IS_PRODUCTION ? "/playground/" : undefined,
   publicDir: IS_PRODUCTION ? undefined : "./static/",
   plugins: [
@@ -29,7 +31,7 @@ export default defineConfig(() => ({
         }),
   ].filter(Boolean),
   resolve: {
-    alias: buildCdnAlias(),
+    alias: await buildCdnAlias(),
   },
   build: {
     outDir: "./static/playground/",
@@ -37,7 +39,7 @@ export default defineConfig(() => ({
   },
 }));
 
-function buildCdnAlias() {
+async function buildCdnAlias() {
   // if (!IS_PRODUCTION) {
   //   return;
   // }
@@ -49,8 +51,45 @@ function buildCdnAlias() {
       continue;
     }
 
-    alias[name] = `https://esm.sh/${name}@${version}`;
+    const url = new URL(`https://esm.sh/${name}@${version}`);
+    let deps = await Array.fromAsync(
+      getPackageDependencies(name, import.meta.url),
+    );
+
+    // Exclude self
+    deps = deps.filter((dependency) => dependency.name !== name);
+
+    if (deps.length > 0) {
+      url.searchParams.set(
+        "deps",
+        deps.map(({ name, version }) => `${name}@${version}`).join(","),
+      );
+    }
+
+    alias[name] = url;
   }
 
   return alias;
+}
+
+async function getPackageJson(name, base) {
+  const packageJsonUrl = module.findPackageJSON(name, base);
+
+  return {
+    url: packageJsonUrl,
+    packageJson: JSON.parse(await fs.readFile(packageJsonUrl, "utf8")),
+  };
+}
+
+async function* getPackageDependencies(name, base) {
+  const {
+    url,
+    packageJson: { dependencies, version },
+  } = await getPackageJson(name, base);
+
+  for (const name of Object.keys(dependencies ?? {})) {
+    yield* getPackageDependencies(name, url);
+  }
+
+  yield { name, version };
 }
