440abfff50fa4201f8c3a590fe3d136c46952a7b
fisker <lionkay@gmail.com>
2025-12-30 03:23:48 +0800

    Use CDN files on website

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -2,11 +2,12 @@ import vitePluginVue from "@vitejs/plugin-vue";
 import vitePluginVueJsx from "@vitejs/plugin-vue-jsx";
 import { visualizer as rollupPluginVisualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
+import packageJson from "./package.json" with { type: "json" };
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 const IS_CI = Boolean(process.env.CI);
 
-export default defineConfig({
+export default defineConfig(() => ({
   base: IS_PRODUCTION ? "/playground/" : undefined,
   publicDir: IS_PRODUCTION ? undefined : "./static/",
   plugins: [
@@ -14,10 +15,29 @@ export default defineConfig({
     vitePluginVueJsx(),
     IS_CI || !IS_PRODUCTION
       ? undefined
-      : rollupPluginVisualizer({ filename: "./static/playground/report.html" }),
+      : rollupPluginVisualizer({
+          filename: "./static/playground/report.html",
+        }),
   ].filter(Boolean),
+  resolve: {
+    alias: buildCdnAlias(),
+  },
   build: {
     outDir: "./static/playground/",
     minify: IS_CI,
   },
-});
+}));
+
+function buildCdnAlias() {
+  if (!IS_PRODUCTION) {
+    return;
+  }
+
+  const alias = {};
+
+  for (const [name, version] of Object.entries(packageJson.dependencies)) {
+    alias[name] = `https://esm.sh/${name}@${version}`;
+  }
+
+  return alias;
+}
