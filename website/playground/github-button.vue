f3b0a5eea19b45e541a18f04e71aa6d20aee7dc0
fisker Cheung <lionkay@gmail.com>
2025-12-30 17:10:45 +0800

    Website: put all components inside app (#18547)

---
+++ website/playground/github-button.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { render } from "github-buttons";
+import { useTemplateRef, onMounted } from "vue";
+
+const anchorRef = useTemplateRef("anchor");
+
+onMounted(() => {
+  const anchor = anchorRef.value;
+  render(anchor, function (el) {
+    anchor.parentNode.replaceChild(el, anchor);
+  });
+});
+</script>
+
+<template>
+  <a
+    ref="anchor"
+    class="github-button"
+    href="https://github.com/prettier/prettier"
+    data-show-count="true"
+    aria-label="Star prettier on GitHub"
+  >
+    GitHub
+  </a>
+</template>
---
+++ website/playground/version-link.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { computed, watch, onMounted } from "vue";
+
+const props = defineProps({
+  version: { type: String, required: true },
+});
+
+const versionData = computed(() => {
+  const match = props.version.match(/^pr-(\d+)$/);
+  let href;
+  if (match) {
+    href = `pull/${match[1]}`;
+  } else if (/\.0$/.test(props.version)) {
+    href = `releases/tag/${props.version}`;
+  } else {
+    href = `blob/main/CHANGELOG.md#${props.version.replaceAll(".", "")}`;
+  }
+
+  return {
+    href,
+    formattedVersion: match ? `PR #${match[1]}` : `v${props.version}`,
+  };
+});
+
+const updateTitle = () => {
+  document.title = `Prettier ${versionData.value.formattedVersion}`;
+};
+
+watch(() => props.version, updateTitle);
+
+onMounted(updateTitle);
+</script>
+
+<template>
+  <a
+    :href="`https://github.com/prettier/prettier/${versionData.href}`"
+    target="_blank"
+    rel="noreferrer noopener"
+    class="version"
+  >
+    {{ versionData.formattedVersion }}
+  </a>
+</template>
+
+<style>
+.version {
+  font-size: 0.5em;
+  line-height: 0;
+  opacity: 0.5;
+}
+</style>
