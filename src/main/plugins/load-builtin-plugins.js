da2b14afb5db96739ac9b76a289a4008f4ed6785
fisker <lionkay@gmail.com>
2026-01-05 05:07:05 +0800

    Minor tweak

--- src/main/plugins/load-builtin-plugins.js
+++ src/main/plugins/load-builtin-plugins.js
@@ -1,9 +1,9 @@
-const loadBuiltinPluginsWithoutCache = async function () {
-  const { plugins } = await (process.env.NODE_ENV === "production"
-    ? import("./builtin-plugins/production-plugins.js")
-    : import("./builtin-plugins/development-plugins.js"));
-  return plugins;
-};
+const loadBuiltinPluginsWithoutCache =
+  process.env.NODE_ENV === "production"
+    ? async () =>
+        (await import("./builtin-plugins/production-plugins.js")).plugins
+    : async () =>
+        (await import("./builtin-plugins/development-plugins.js")).plugins;
 
 let cache;
 function loadBuiltinPlugins() {
