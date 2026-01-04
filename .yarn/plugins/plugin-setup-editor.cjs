50003539c8f3ea0d243f132d7762a710d17b186f
fisker <lionkay@gmail.com>
2026-01-05 03:04:42 +0800

    Add mcp.example.json

--- .yarn/plugins/plugin-setup-editor.cjs
+++ .yarn/plugins/plugin-setup-editor.cjs
@@ -1,29 +1,26 @@
 const fs = require("node:fs");
 const path = require("node:path");
 
+const vscodeDirectory = path.join(__dirname, "../../.vscode/");
+
+function copyFile(from, to) {
+  const source = path.join(vscodeDirectory, from);
+  const target = path.join(vscodeDirectory, to);
+
+  if (fs.existsSync(target) || !fs.existsSync(source)) {
+    return;
+  }
+
+  fs.copyFileSync(source, target);
+}
+
 module.exports = {
   name: "plugin-setup-editor",
   factory: () => ({
     hooks: {
       afterAllInstalled() {
-        const settingsFile = path.join(
-          __dirname,
-          "../../.vscode/settings.json",
-        );
-
-        const settingsExampleFile = path.join(
-          __dirname,
-          "../../.vscode/settings.example.json",
-        );
-
-        if (
-          fs.existsSync(settingsFile) ||
-          !fs.existsSync(settingsExampleFile)
-        ) {
-          return;
-        }
-
-        fs.copyFileSync(settingsExampleFile, settingsFile);
+        copyFile("settings.example.json", "settings.json");
+        copyFile("mcp.example.json", "mcp.json");
       },
     },
   }),
