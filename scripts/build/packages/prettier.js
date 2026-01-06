f34afe76d9fba94925fab8f6f4db27222a099150
fisker <lionkay@gmail.com>
2026-01-07 06:19:02 +0800

    Mark as pure

--- scripts/build/packages/prettier.js
+++ scripts/build/packages/prettier.js
@@ -733,7 +733,19 @@ const pluginFiles = [
       },
     ],
   },
-  "src/plugins/graphql.js",
+  {
+    input: "src/plugins/graphql.js",
+    replaceModule: [
+      {
+        module: getPackageFile("graphql/language/ast.mjs"),
+        process: (text) =>
+          text.replace(
+            "const kindValues = new Set(Object.keys(QueryDocumentKeys));",
+            "const kindValues = /* @__PURE__ */ new Set( /* @__PURE__ */ Object.keys(QueryDocumentKeys));",
+          ),
+      },
+    ],
+  },
   {
     input: "src/plugins/markdown.js",
     replaceModule: [
