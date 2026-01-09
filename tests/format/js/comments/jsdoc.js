ac8d031c0464184d57d2079f8278a94b15fe65a0
seiya <20365512+seiyab@users.noreply.github.com>
2026-01-09 14:44:33 +0000

    Add test cases

--- tests/format/js/comments/jsdoc.js
+++ tests/format/js/comments/jsdoc.js
@@ -24,3 +24,21 @@ function HelloWorld() {
         </div>
     );
 }
+
+/**
+ * Trailing double spaces should be preserverd  
+ * because it's significant in markdown.
+ *
+ * Trailing single space should be ignored. 
+ *
+ * @type {any}
+ */
+let a;
+
+/***
+ * This is not a JSDoc  
+ * so trailing double spaces are ignored
+ *
+ * @type {any}
+ */
+let b;
