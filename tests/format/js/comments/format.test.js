3ca607cd3b316d9339ac4eb85f77adb7fdbf2329
seiya <20365512+seiyab@users.noreply.github.com>
2026-01-10 13:29:55 +0000

    refine test cases

--- tests/format/js/comments/format.test.js
+++ tests/format/js/comments/format.test.js
@@ -3,6 +3,24 @@ const fixtures = {
   snippets: [
     "var a = { /* comment */      \nb };", // trailing whitespace after comment
     "var a = { /* comment */\nb };",
+    {
+      name: "Preserve trailing double spaces in JSDoc",
+      code: "/**\n * This is a JSDoc comment with two spaces at the end of this line.  \n */\n",
+      output:
+        "/**\n * This is a JSDoc comment with two spaces at the end of this line.  \n */\n",
+    },
+    {
+      name: "Remove trailing single space in JSDoc",
+      code: "/**\n * This is a JSDoc comment with one space at the end of this line. \n */\n",
+      output:
+        "/**\n * This is a JSDoc comment with one space at the end of this line.\n */\n",
+    },
+    {
+      name: "Remove trailing single space in non-JSDoc",
+      code: "/***\n * This is a JSDoc comment with two spaces at the end of this line.  \n */\n",
+      output:
+        "/***\n * This is a JSDoc comment with two spaces at the end of this line.\n */\n",
+    },
   ],
 };
 
--- tests/format/js/comments/jsdoc.js
+++ tests/format/js/comments/jsdoc.js
@@ -24,21 +24,3 @@ function HelloWorld() {
         </div>
     );
 }
-
-/**
- * Trailing double spaces should be preserverd  
- * because it's significant in markdown.
- *
- * Trailing single space should be ignored. 
- *
- * @type {any}
- */
-let a;
-
-/***
- * This is not a JSDoc  
- * so trailing double spaces are ignored
- *
- * @type {any}
- */
-let b;
