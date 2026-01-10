82dc60213e5c6ff8a78eed9f4bbe9bed853cbfd2
fisker <lionkay@gmail.com>
2026-01-10 23:10:06 +0800

    Move to a separate directory

--- tests/format/js/comments/format.test.js
+++ tests/format/js/comments/format.test.js
@@ -1,49 +1,8 @@
-import { outdent } from "outdent";
-
 const fixtures = {
   importMeta: import.meta,
   snippets: [
     "var a = { /* comment */      \nb };", // trailing whitespace after comment
     "var a = { /* comment */\nb };",
-    {
-      name: "Preserve trailing double spaces in JSDoc",
-      code: outdent`
-        /**
-         * This is a JSDoc comment with two spaces at the end of this line.${" ".repeat(2)}
-         */
-      `,
-      output: outdent`
-        /**
-         * This is a JSDoc comment with two spaces at the end of this line.${" ".repeat(2)}
-         */\n
-      `,
-    },
-    {
-      name: "Remove trailing single space in JSDoc",
-      code: outdent`
-        /**
-         * This is a JSDoc comment with one space at the end of this line.${" ".repeat(1)}
-         */
-      `,
-      output: outdent`
-        /**
-         * This is a JSDoc comment with one space at the end of this line.
-         */\n
-      `,
-    },
-    {
-      name: "Remove trailing single space in non-JSDoc",
-      code: outdent`
-        /***
-          * This is a JSDoc comment with two spaces at the end of this line.${" ".repeat(2)}
-          */
-      `,
-      output: outdent`
-        /***
-         * This is a JSDoc comment with two spaces at the end of this line.
-         */\n
-      `,
-    },
   ],
 };
 
