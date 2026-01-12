e71c2de1b1a0fffaebc180a1da58972b1f601731
fisker <lionkay@gmail.com>
2026-01-13 02:44:34 +0800

    Update tests

--- tests/format/js/import-assertions/format.test.js
+++ tests/format/js/import-assertions/format.test.js
@@ -1,24 +1,6 @@
-runFormatTest(import.meta, ["babel", "typescript"], {
+runFormatTest(import.meta, ["typescript"], {
   errors: {
-    acorn: [
-      "empty.js",
-      "multi-types.js",
-      "static-import.js",
-      "re-export.js",
-      "without-from.js",
-      "non-type.js",
-      "keyword-detect.js",
-    ],
-    espree: [
-      "empty.js",
-      "multi-types.js",
-      "static-import.js",
-      "re-export.js",
-      "without-from.js",
-      "non-type.js",
-      "keyword-detect.js",
-    ],
-    meriyah: [
+    "babel-ts": [
       "empty.js",
       "multi-types.js",
       "static-import.js",
