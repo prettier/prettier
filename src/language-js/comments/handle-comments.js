0a098445247aa0a5ffeaefa7f6161032f5ef1487
fisker <lionkay@gmail.com>
2026-01-14 02:17:40 +0800

    Use `functionNode` to check

--- src/language-js/comments/handle-comments.js
+++ src/language-js/comments/handle-comments.js
@@ -665,7 +665,7 @@ function handleCommentInEmptyParens({ comment, enclosingNode, options }) {
   if (
     functionNode &&
     getFunctionParameters(functionNode).length === 0 &&
-    isInArgumentOrParameterParentheses(enclosingNode, comment, options)
+    isInArgumentOrParameterParentheses(functionNode, comment, options)
   ) {
     addDanglingComment(functionNode, comment);
     return true;
