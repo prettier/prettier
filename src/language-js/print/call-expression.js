e636454118cf061a1ddc9f5cff80a2a627498ab1
fisker Cheung <lionkay@gmail.com>
2026-01-14 00:39:00 +0800

    Improve comments around type arguments print (#18620)

--- src/language-js/print/call-expression.js
+++ src/language-js/print/call-expression.js
@@ -32,7 +32,7 @@ function printCallExpression(path, options, print) {
   // `TSImportType.typeArguments` is after `qualifier`, not before the "arguments"
   const typeArgumentsDoc =
     node.type !== "TSImportType" && node.typeArguments
-      ? print("typeArguments")
+      ? [print("typeArguments"), lineSuffixBoundary]
       : "";
 
   const isTemplateLiteralSingleArg =
@@ -57,7 +57,6 @@ function printCallExpression(path, options, print) {
     });
     if (!(isTemplateLiteralSingleArg && printed[0].label?.embed)) {
       return [
-        isNewExpression ? "new " : "",
         printCallee(path, print),
         optional,
         typeArgumentsDoc,
@@ -89,11 +88,9 @@ function printCallExpression(path, options, print) {
   }
 
   const contents = [
-    isNewExpression ? "new " : "",
     printCallee(path, print),
     optional,
     typeArgumentsDoc,
-    lineSuffixBoundary,
     printCallArguments(path, options, print),
   ];
 
@@ -121,7 +118,11 @@ function printCallee(path, print) {
     return "require";
   }
 
-  return print("callee");
+  return [
+    node.type === "NewExpression" ? "new " : "",
+    print("callee"),
+    lineSuffixBoundary,
+  ];
 }
 
 const moduleImportCallees = [
