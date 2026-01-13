c671652f6a20bde7df159fc4efea1f8a8843694c
fisker <lionkay@gmail.com>
2026-01-13 19:21:32 +0800

    Don't force array/object with short block comment to break

---
+++ tests/format/js/comments/in-list/dangling-comment-in-list.js
@@ -0,0 +1,11 @@
+a = [/* array */]
+object = [/* object */]
+call(/* call argument */)
+function A(/* function parameter */) {}
+new Foo(/* new argument */)
+
+a = [/* array long long long long long long long long long long long long long */]
+object = [/* object long long long long long long long long long long long long long */]
+call(/* call argument long long long long long long long long long long long long long */)
+function A(/* function parameter long long long long long long long long long long long long long */) {}
+new Foo(/* new argument long long long long long long long long long long long long long */)
