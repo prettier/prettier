d81f4da8c3018a8cf9aa58b8ddbeebfde490dc20
fisker <lionkay@gmail.com>
2026-01-13 18:28:24 +0800

    18403

---
+++ tests/format/js/call/comments/18403.js
@@ -0,0 +1,5 @@
+call(/* a long long long long long long long long long long long long comment */);
+
+array = [/* a long long long long long long long long long long long long comment */];
+
+object = {/* a long long long long long long long long long long long long comment */};
