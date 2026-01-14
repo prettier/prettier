4cfb8bc9b84c465856b54889b940ca91a3a31e34
fisker Cheung <lionkay@gmail.com>
2026-01-15 03:01:56 +0800

    Improve dangling comment handling in flow hooks (#18630)

--- src/language-js/comments/handle-comments.js
+++ src/language-js/comments/handle-comments.js
@@ -662,13 +662,16 @@ function handleCommentInEmptyParens({ comment, enclosingNode, options }) {
   // Only add dangling comments to fix the case when no params are present,
   // i.e. a function without any argument.
 
-  const functionNode = isRealFunctionLikeNode(enclosingNode)
-    ? enclosingNode
-    : enclosingNode.type === "MethodDefinition" ||
-        enclosingNode.type === "TSAbstractMethodDefinition" ||
-        (enclosingNode.type === "Property" && isMethod(enclosingNode))
-      ? enclosingNode.value
-      : undefined;
+  const functionNode =
+    isRealFunctionLikeNode(enclosingNode) ||
+    enclosingNode.type === "HookDeclaration" ||
+    enclosingNode.type === "HookTypeAnnotation"
+      ? enclosingNode
+      : enclosingNode.type === "MethodDefinition" ||
+          enclosingNode.type === "TSAbstractMethodDefinition" ||
+          (enclosingNode.type === "Property" && isMethod(enclosingNode))
+        ? enclosingNode.value
+        : undefined;
 
   if (
     functionNode &&
