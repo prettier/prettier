946400334824b3f1ecd071d3f95cf1e9f3f66f1c
fisker Cheung <lionkay@gmail.com>
2026-01-15 05:14:58 +0800

    Angular 21.1: Support consecutive switch cases (#18593)

--- src/language-html/print/angular-control-flow-block.js
+++ src/language-html/print/angular-control-flow-block.js
@@ -24,18 +24,20 @@ function printAngularControlFlowBlock(path, options, print) {
     docs.push(" (", group(print("parameters")), ")");
   }
 
-  docs.push(" {");
+  if (!isSwitchFallthroughCase(node)) {
+    docs.push(" {");
 
-  const shouldPrintCloseBracket = shouldCloseBlock(node);
-  if (node.children.length > 0) {
-    node.firstChild.hasLeadingSpaces = true;
-    node.lastChild.hasTrailingSpaces = true;
-    docs.push(indent([hardline, printChildren(path, options, print)]));
-    if (shouldPrintCloseBracket) {
-      docs.push(hardline, "}");
+    const shouldPrintCloseBracket = shouldCloseBlock(node);
+    if (node.children.length > 0) {
+      node.firstChild.hasLeadingSpaces = true;
+      node.lastChild.hasTrailingSpaces = true;
+      docs.push(indent([hardline, printChildren(path, options, print)]));
+      if (shouldPrintCloseBracket) {
+        docs.push(hardline, "}");
+      }
+    } else if (shouldPrintCloseBracket) {
+      docs.push("}");
     }
-  } else if (shouldPrintCloseBracket) {
-    docs.push("}");
   }
 
   return group(docs, { shouldBreak: true });
@@ -45,6 +47,19 @@ function shouldCloseBlock(node) {
   return !(
     node.next?.kind === "angularControlFlowBlock" &&
     ANGULAR_CONTROL_FLOW_BLOCK_SETTINGS.get(node.name)?.has(node.next.name)
+  );
+}
+
+const isSwitchCaseBlock = (node) =>
+  node?.kind === "angularControlFlowBlock" &&
+  (node.name === "case" || node.name === "default");
+// https://github.com/angular/angular/commit/0ad3adc7c6d4094f1e3432a3f2e3bdc9862cb4fa#diff-77a6f285c6ea13d6644ef635e7495e71134d1141af2650cb9ae5631ff1f38bf2R263
+// https://github.com/prettier/prettier/issues/18563#issuecomment-3728354491
+function isSwitchFallthroughCase(node) {
+  return (
+    isSwitchCaseBlock(node) &&
+    node.endSourceSpan &&
+    node.endSourceSpan.start.offset === node.endSourceSpan.end.offset
   );
 }
 
