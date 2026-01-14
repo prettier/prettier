f25ff336f0b230e15c20daf5ec04bb56cc8c0902
fisker <lionkay@gmail.com>
2026-01-15 01:54:10 +0800

    Add tests

---
+++ tests/format/flow/component/comments.js
@@ -0,0 +1,37 @@
+component C11(// 11
+) {}
+component C12(
+// 12
+) {}
+declare component C13(// 13
+)
+declare component C14(
+// 14
+)
+type T11 = component(//15
+);
+type T12 = component(
+//16
+);
+
+component C21(/* 21 */
+) {}
+component C22(
+/* 22*/
+) {}
+component C23(
+/* 23*/){}
+declare component C24(/* 24 */
+)
+declare component C25(
+/* 25*/
+)
+declare component C26(
+/* 26*/)
+type T21 = component(/* 27*/
+);
+type T22 = component(
+/* 28*/
+);
+type T23 = component(
+/* 29*/);
