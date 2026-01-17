e2108b22730f07e711600c654a9fe72df7c75adb
fisker <lionkay@gmail.com>
2026-01-18 03:25:49 +0800

    Remove tests

--- tests/format/js/comments/break-continue-statements.js
+++ tests/format/js/comments/break-continue-statements.js
@@ -9,18 +9,3 @@ loop: for (;;) {
   continue /* comment */ loop;
   continue loop /* comment */;
 }
-
-// https://github.com/prettier/prettier/issues/7126
-for (;;) {
-  if (condition) continue
-
-  // breaking comment
-  ;(possibleArray || []).sort()
-}
-
-lbl: for (;;) {
-  if (condition) continue lbl
-
-  // breaking comment
-  ;(possibleArray || []).sort()
-}
