ce5a243e8793a49698272031ef8032d9676001b5
fisker <lionkay@gmail.com>
2026-01-17 22:37:21 +0800

    Add test

---
+++ tests/format/typescript/template-literals/member-expression.ts
@@ -0,0 +1,8 @@
+long1 = `long ${a//comment
+  .b} long longlong ${a.b.c.d.e} long longlong ${a.b.c.d.e} long longlong ${a.b.c.d.e} long long`;
+
+long1 = `long ${a//comment
+  ?.b} long longlong ${a.b.c.d.e} long longlong ${a.b.c.d.e} long longlong ${a.b.c.d.e} long long`;
+
+long1 = `long ${a//comment
+  ?.b!} long longlong ${a.b.c.d.e} long longlong ${a.b.c.d.e} long longlong ${a.b.c.d.e} long long`;
