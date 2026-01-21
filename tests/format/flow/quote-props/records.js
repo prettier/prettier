c6d46f6018bf2da619c538768759414d1d824f1f
fisker <lionkay@gmail.com>
2026-01-22 00:52:19 +0800

    Add tests for Flow records

---
+++ tests/format/flow/quote-props/records.js
@@ -0,0 +1,15 @@
+record R {
+  42: 0,
+  "43": 1,
+  "property1": 2,
+  property2: 3,
+  "property-3": 4,
+}
+
+r = R {
+  42: 0,
+  "43": 1,
+  "property1": 2,
+  property2: 3,
+  "property-3": 4,
+}
