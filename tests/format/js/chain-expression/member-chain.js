82ec9e9302bc29473239f8566131fffde804d17c
fisker <lionkay@gmail.com>
2026-01-23 18:03:54 +0800

    Add tests

---
+++ tests/format/js/chain-expression/member-chain.js
@@ -0,0 +1,17 @@
+bifornCringerMoshedPerplexSawder = (
+    glimseGlyphsHazardNoopsTieTie
+    ?. averredBathersBoxroomBuggyNurl
+    ?.anodyneCondosMalateOverateRetinol
+  )
+  .annularCooeedSplicesWalksWayWay
+  .annularCooeedSplicesWalksWayWay(annularCooeedSplicesWalksWayWay)
+  .annularCooeedSplicesWalksWayWay();
+
+bifornCringerMoshedPerplexSawder = (
+    glimseGlyphsHazardNoopsTieTie
+    ?. averredBathersBoxroomBuggyNurl
+    ?.anodyneCondosMalateOverateRetinol
+  )
+  ?.annularCooeedSplicesWalksWayWay
+  .annularCooeedSplicesWalksWayWay(annularCooeedSplicesWalksWayWay)
+  .annularCooeedSplicesWalksWayWay();
