3bfae905a9cbbac284041388cd2e9760388a33fd
fisker <lionkay@gmail.com>
2026-01-23 17:39:01 +0800

    Fix tests

--- tests/format/flow-repo/ambient_declarations/format.test.js
+++ tests/format/flow-repo/ambient_declarations/format.test.js
@@ -1,1 +1,11 @@
-runFormatTest(import.meta, ["flow"]);
+runFormatTest(import.meta, ["flow"], {
+  errors: {
+    hermes: [
+      "components.js.flow",
+      "component_namespace.js",
+      "implicit_functions.js",
+      "namespace.js",
+      "variables.js.flow",
+    ],
+  },
+});
