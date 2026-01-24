92e6c4ec38274bade183c773ceddc3a92a1043fa
fisker <lionkay@gmail.com>
2026-01-24 16:35:29 +0800

    Update tests

--- tests/format/js/no-semi/imports.js
+++ tests/format/js/no-semi/imports.js
@@ -1,22 +1,22 @@
 // #10870
 import foo from "foo"
 
-;/* Should preserve line before */({})
+;({}) /* Should preserve line before */
 
 ////////////////////////////////////////
 export default {}
 
-;/* Should preserve line before */({})
+;({}) /* Should preserve line before */
 
 ////////////////////////////////////////
 export {foo}
 
-;/* Should preserve line before */({})
+;({}) /* Should preserve line before */
 
 ////////////////////////////////////////
 export * from 'foo'
 
-;/* Should preserve line before */({})
+;({}) /* Should preserve line before */
 
 ////////////////////////////////////////
 export {foo as bar} // 1
