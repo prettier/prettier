117f588ee699589c1ae3c917b40ec29acdd2fa6f
fisker <lionkay@gmail.com>
2026-01-26 17:06:57 +0800

    Tests

---
+++ tests/format/js/comments-closure-typecast/no-semi/type-cast-issue.js
@@ -0,0 +1,12 @@
+// https://github.com/prettier/prettier/issues/18307
+class C {
+  /** @type {string | undefined} */
+  s
+
+  typeCastS() {
+    /** @type {string} */ (this.s).length
+  }
+}
+
+// https://github.com/prettier/prettier/issues/12794
+;(/** @type {(token: Token)=>void} */ (onToken))(token)
