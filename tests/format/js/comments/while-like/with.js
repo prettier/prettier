b079868e2b00db1b2b112ed078148da2a28f375a
fisker <lionkay@gmail.com>
2026-01-29 23:32:29 +0800

    Add test

---
+++ tests/format/js/comments/while-like/with.js
@@ -0,0 +1,23 @@
+with(
+    true
+    // Comment
+  ) {}
+
+with(true)// Comment
+{}
+
+with(true){}// Comment
+
+with(true)/*Comment*/{}
+
+with(
+  true // Comment
+  && true // Comment
+  ){}
+
+with(true) {} // comment
+
+with(true) /* comment */ ++x;
+
+with(1) // Comment
+  foo();
