38b0fd33db42a648a27f49c03a163685dd23ed3a
fisker <lionkay@gmail.com>
2026-01-29 11:07:05 +0800

    Add test for #6858

---
+++ tests/format/typescript/type-parameters-arguments/issue-6858.ts
@@ -0,0 +1,18 @@
+testFunc<
+//#region
+{
+    testProp: number
+}
+//#endregion
+>(parserFunc`
+Some multiline string
+In here`);
+
+
+testFunc(
+//#region
+{
+    testProp: number
+}
+//#endregion
+)
