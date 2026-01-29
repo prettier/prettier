8024fe413a7c385db976c92a289ae77ac633beba
fisker <lionkay@gmail.com>
2026-01-29 19:40:03 +0800

    Test no-semi

--- tests/format/js/ignore/semi/format.test.js
+++ tests/format/js/ignore/semi/format.test.js
@@ -1,2 +1,12 @@
-runFormatTest(import.meta, ["babel", "flow", "typescript"]);
-runFormatTest(import.meta, ["babel", "flow", "typescript"], { semi: false });
+const errors = {
+  acorn: ["decorator.js", "class-expression-decorator.js"],
+  espree: ["decorator.js", "class-expression-decorator.js"],
+  flow: ["class-expression-decorator.js"],
+  hermes: ["decorator.js", "class-expression-decorator.js"],
+};
+
+runFormatTest(import.meta, ["babel", "flow", "typescript"], { errors });
+runFormatTest(import.meta, ["babel", "flow", "typescript"], {
+  semi: false,
+  errors,
+});
