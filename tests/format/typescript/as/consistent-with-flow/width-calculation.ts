82930e47d596c27e12bff1587f9b8d6826f4baba
fisker <lionkay@gmail.com>
2026-01-31 10:19:43 +0800

    Test

---
+++ tests/format/typescript/as/consistent-with-flow/width-calculation.ts
@@ -0,0 +1,13 @@
+firstItem_________ = editorPicker.items.find((item) => item.type === 'item') as string | number;
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   79 width
+firstItem_________$ = editorPicker.items.find((item) => item.type === 'item') as string | number;
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  80 width
+firstItem_________$$ = editorPicker.items.find((item) => item.type === 'item') as string | number;
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 81 width
+
+firstItem_________ = editorPicker.items.find((item) => item.type === 'item') as
+const;
+firstItem_________$ = editorPicker.items.find((item) => item.type === 'item') as
+const;
+firstItem_________$$ = editorPicker.items.find((item) => item.type === 'item') as
+const;
