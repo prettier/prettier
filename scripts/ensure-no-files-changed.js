767147dec5d78a1a92de3778f27019bc55ea2b71
fisker <lionkay@gmail.com>
2026-02-01 08:25:36 +0800

    Use js

---
+++ scripts/ensure-no-files-changed.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import spawn from "nano-spawn";
+
+async function getChangedFiles() {
+  const { stdout } = await spawn("git", ["diff", "--name-only"]);
+  return stdout.split("\n");
+}
+
+async function ensureNoFilesChanged() {
+  const changedFiles = await getChangedFiles();
+
+  if (changedFiles.length === 0) {
+    console.log("✅ No files changed.");
+    return;
+  }
+
+  console.log(
+    `❌ ${changedFiles.length} file${changedFiles.length === 1 ? "" : "s"} changed:`,
+  );
+  console.log(changedFiles.map((file) => ` - ${file}`).join("\n"));
+  console.log();
+
+  await spawn("git", ["diff", "--exit-code"], { stdio: "inherit" });
+}
+
+await ensureNoFilesChanged();
