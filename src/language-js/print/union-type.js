a16e57de9473a6a6cf55c6b9838e1a58668dcb6f
fisker Cheung <lionkay@gmail.com>
2026-02-02 14:31:54 +0800

    Simplify union type print (#18831)

--- src/language-js/print/union-type.js
+++ src/language-js/print/union-type.js
@@ -7,20 +7,18 @@ import {
   line,
   softline,
 } from "../../document/index.js";
-import {
-  printComments,
-  printCommentsSeparately,
-} from "../../main/comments/print.js";
+import { printComments } from "../../main/comments/print.js";
 import needsParentheses from "../parentheses/needs-parentheses.js";
-import { hasComment } from "../utilities/comments.js";
-import { createTypeCheckFunction } from "../utilities/create-type-check-function.js";
 import { isFlowObjectTypePropertyAFunction } from "../utilities/is-flow-object-type-property-a-function.js";
 import {
   isConditionalType,
   isTupleType,
   isTypeParameterInstantiation,
 } from "../utilities/node-types.js";
-import { shouldUnionTypePrintOwnComments } from "../utilities/should-union-type-print-own-comments.js";
+import {
+  shouldHugUnionType,
+  shouldUnionTypePrintOwnComments,
+} from "../utilities/union-type-print.js";
 
 /**
 @import {Doc} from "../../document/index.js";
@@ -29,6 +27,15 @@ import { shouldUnionTypePrintOwnComments } from "../utilities/should-union-type-
 // `TSUnionType` and `UnionTypeAnnotation`
 function printUnionType(path, options, print, args) {
   const { node } = path;
+
+  // {
+  //   a: string
+  // } | null | void
+  // should be inlined and not be printed in the multi-line variant
+  if (shouldHugUnionType(node)) {
+    return join(" | ", path.map(print, "types"));
+  }
+
   // single-line variation
   // A | B | C
 
@@ -37,57 +44,36 @@ function printUnionType(path, options, print, args) {
   // | B
   // | C
 
-  const { parent } = path;
-
-  // {
-  //   a: string
-  // } | null | void
-  // should be inlined and not be printed in the multi-line variant
-  const shouldHug = shouldHugUnionType(node);
-
   // We want to align the children but without its comment, so it looks like
   // | child1
   // // comment
   // | child2
-  const printed = path.map(() => {
-    let printedType = print();
-    if (!shouldHug) {
-      printedType = align(2, printedType);
-    }
+  let printed = path.map(
+    () => printComments(path, align(2, print()), options),
+    "types",
+  );
 
-    return printComments(path, printedType, options);
-  }, "types");
+  printed = group([ifBreak("| "), join([line, "| "], printed)]);
 
-  /** @type {Doc} */
-  let leading = "";
-  /** @type {Doc} */
-  let trailing = "";
   if (shouldUnionTypePrintOwnComments(path)) {
-    ({ leading, trailing } = printCommentsSeparately(path, options));
+    printed = printComments(path, printed, options);
   }
-
-  if (shouldHug) {
-    return [leading, join(" | ", printed), trailing];
-  }
-
-  const mainParts = [
-    leading,
-    group([ifBreak("| "), join([line, "| "], printed)]),
-    trailing,
-  ];
 
   if (needsParentheses(path, options)) {
-    return group([indent([softline, mainParts]), softline]);
+    return group([indent([softline, printed]), softline]);
   }
 
-  if (isTupleType(parent) && parent.elementTypes.length > 1) {
-    return [
-      group([
-        indent([ifBreak(["(", softline]), mainParts]),
-        softline,
-        ifBreak(")"),
-      ]),
-    ];
+  const { key, parent } = path;
+  if (
+    key === "elementTypes" &&
+    isTupleType(parent) &&
+    parent.elementTypes.length > 1
+  ) {
+    return group([
+      indent([ifBreak(["(", softline]), printed]),
+      softline,
+      ifBreak(")"),
+    ]);
   }
 
   // Already indent in parent
@@ -95,10 +81,10 @@ function printUnionType(path, options, print, args) {
     args?.assignmentLayout === "break-after-operator" ||
     !shouldIndentUnionType(path)
   ) {
-    return mainParts;
+    return printed;
   }
 
-  return group(indent([softline, mainParts]));
+  return group(indent([softline, printed]));
 }
 
 function shouldIndentUnionType(path) {
@@ -129,35 +115,6 @@ function shouldIndentUnionType(path) {
   }
 
   return true;
-}
-
-const isVoidType = createTypeCheckFunction([
-  "VoidTypeAnnotation",
-  "TSVoidKeyword",
-  "NullLiteralTypeAnnotation",
-  "TSNullKeyword",
-]);
-
-const isObjectLikeType = createTypeCheckFunction([
-  "ObjectTypeAnnotation",
-  "TSTypeLiteral",
-  // This is a bit aggressive but captures Array<{x}>
-  "GenericTypeAnnotation",
-  "TSTypeReference",
-]);
-
-function shouldHugUnionType(node) {
-  const { types } = node;
-  if (types.some((node) => hasComment(node))) {
-    return false;
-  }
-
-  const objectType = types.find((node) => isObjectLikeType(node));
-  if (!objectType) {
-    return false;
-  }
-
-  return types.every((node) => node === objectType || isVoidType(node));
 }
 
 export { printUnionType, shouldHugUnionType };
