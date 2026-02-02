39a26d88a779764bad97c23c45b16f1a007218ee
fisker <lionkay@gmail.com>
2026-02-02 13:45:04 +0800

    Simplify union type print

--- src/language-js/comments/will-print-own-comments.js
+++ src/language-js/comments/will-print-own-comments.js
@@ -3,7 +3,7 @@ import { hasNodeIgnoreComment } from "../utilities/has-node-ignore-comment.js";
 import { isIifeCalleeOrTaggedTemplateExpressionTag } from "../utilities/is-iife-callee-or-tagged-template-expression-tag.js";
 import { isJsxElement, isUnionType } from "../utilities/node-types.js";
 import { shouldExpressionStatementPrintOwnComments } from "../utilities/should-expression-statement-print-own-comments.js";
-import { shouldUnionTypePrintOwnComments } from "../utilities/should-union-type-print-own-comments.js";
+import { shouldUnionTypePrintOwnComments } from "../utilities/union-type-print.js";
 
 /**
 @import {Node} from "../types/estree.js";
