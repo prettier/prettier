8e6137818d4de8f847b39b8451d3c01b82ceebcb
fisker Cheung <lionkay@gmail.com>
2026-02-02 18:31:13 +0800

    Prevent moving trailing comment in class properties (#18837)

--- src/language-js/comments/handle-comments.js
+++ src/language-js/comments/handle-comments.js
@@ -349,6 +349,7 @@ function handleMethodNameComments({
   comment,
   precedingNode,
   enclosingNode,
+  followingNode,
   text,
 }) {
   // This is only needed for estree parsers (flow, typescript) to attach
@@ -369,6 +370,20 @@ function handleMethodNameComments({
     getNextNonSpaceNonCommentCharacter(text, locEnd(precedingNode)) !== ":"
   ) {
     addTrailingComment(precedingNode, comment);
+    return true;
+  }
+
+  if (
+    isPropertyLikeNode(enclosingNode) &&
+    !followingNode &&
+    placement === "remaining"
+  ) {
+    addTrailingComment(
+      getNextNonSpaceNonCommentCharacter(text, locEnd(comment)) === "("
+        ? precedingNode
+        : enclosingNode,
+      comment,
+    );
     return true;
   }
 
