8a47d63843124415f209f0648e6113314f46ff90
fisker <lionkay@gmail.com>
2026-02-03 12:45:10 +0800

    Rename

--- src/language-js/print/module-declaration.js
+++ src/language-js/print/module-declaration.js
@@ -1,6 +1,9 @@
 import { group } from "../../document/index.js";
 import { printDeclareToken, printSemicolon } from "./miscellaneous.js";
 
+/*
+- `TSModuleDeclaration` (TypeScript)
+*/
 function printModuleDeclaration(path, options, print) {
   const { node } = path;
 
