aa832a7db35006c053e60bd98ed00eba43e545c3
fisker <lionkay@gmail.com>
2026-02-05 20:30:39 +0800

    Fix link

--- website/src/theme/NavbarItem/PlaygroundLink.jsx
+++ website/src/theme/NavbarItem/PlaygroundLink.jsx
@@ -1,4 +1,4 @@
-import { useActiveDocContext } from "@docusaurus/plugin-content-docs/client";
+import { useActiveVersion } from "@docusaurus/plugin-content-docs/client";
 import IconExternalLink from "@theme/Icon/ExternalLink";
 
 const url =
@@ -7,15 +7,14 @@ const url =
     : "http://localhost:5173/";
 
 export default function PlaygroundLink({ mobile }) {
-  const activeDocContext = useActiveDocContext("default");
-  const activeVersion = activeDocContext?.activeVersion?.name;
+  const activeVersion = useActiveVersion();
 
   const className = mobile ? "menu__link" : "navbar__item navbar__link";
 
   return (
     <a
       className={className}
-      href={activeVersion === "current" ? `${url}?version=next` : url}
+      href={activeVersion.name === "current" ? `${url}?version=next` : url}
       target="_self"
       rel="noopener noreferrer"
     >
