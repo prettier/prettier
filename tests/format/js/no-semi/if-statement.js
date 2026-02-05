7b95b77ae789a87d0d17c709b3d19f3cf040b99a
fisker <lionkay@gmail.com>
2026-02-05 09:39:26 +0800

    More tests

--- tests/format/js/no-semi/if-statement.js
+++ tests/format/js/no-semi/if-statement.js
@@ -14,3 +14,20 @@ if (   1)    foo
 ;[]
 
 if (1) foo /* comment */ ;
+
+if (1) ; else foo
+
+// 11
+;[]
+
+if (1) ; else foo
+
+// 21
+;foo
+
+// prettier-ignore
+if (   1) ; else    foo
+
+;[]
+
+if (1) ; else foo /* comment */ ;
