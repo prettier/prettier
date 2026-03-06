70c3c2b4f6ca5a0a690ec82ed3a5abb8b2b5cfc9
it-education-md <128720033+it-education-md@users.noreply.github.com>
2026-03-06 17:04:05 -0500

    Fix markdown newline removal for CJK list-marker false positive (#18867)

--- src/language-markdown/print/mdast.js
+++ src/language-markdown/print/mdast.js
@@ -52,6 +52,15 @@ function prevOrNextWord(path) {
   return hasPrevOrNextWord;
 }
 
+function hasFakeWhitespaceAfterNextToken(path) {
+  const { siblings, index } = path;
+  if (!siblings || typeof index !== "number") {
+    return false;
+  }
+  const afterNext = siblings[index + 2];
+  return afterNext?.type === "whitespace" && afterNext.value === "";
+}
+
 function printMdast(path, options, print) {
   const { node } = path;
 
@@ -109,7 +118,10 @@ function printMdast(path, options, print) {
 
       const proseWrap =
         // leading char that may cause different syntax
-        next && /^>|^(?:[*+-]|#{1,6}|\d+[).])$/.test(next.value)
+        next &&
+        /^>|^(?:[*+-]|#{1,6}|\d+[).])$/.test(next.value) &&
+        // Avoid https://github.com/prettier/prettier/issues/18861
+        !hasFakeWhitespaceAfterNextToken(path)
           ? "never"
           : options.proseWrap;
 
