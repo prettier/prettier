752dd795027f9b3747cf2fe29e139905efac9509
fisker <lionkay@gmail.com>
2026-05-28 03:43:21 +0800

    MOve code

--- tests/format/typescript/comments/abstract-methods-2.ts
+++ tests/format/typescript/comments/abstract-methods-2.ts
@@ -1,5 +1,24 @@
 // #18478
 
+abstract class AbstractBar {
+  abstract method4(
+    param1: number,
+    // trailing line comment
+  ): void;
+  abstract method5(
+    param1: number,
+    /* trailing block comment */
+  ): void;
+  abstract method6(
+    ...rest: number[]
+    // trailing after rest
+  ): void;
+  abstract method7(
+    { a }: { a: number },
+    // trailing after object pattern
+  ): void;
+}
+
 declare class DeclareCls {
   method(
     param1: number,
--- tests/format/typescript/comments/abstract-methods.ts
+++ tests/format/typescript/comments/abstract-methods.ts
@@ -10,22 +10,3 @@ abstract class AbstractFoo {
   );
 }
 
-// #18478
-abstract class AbstractBar {
-  abstract method4(
-    param1: number,
-    // trailing line comment
-  ): void;
-  abstract method5(
-    param1: number,
-    /* trailing block comment */
-  ): void;
-  abstract method6(
-    ...rest: number[]
-    // trailing after rest
-  ): void;
-  abstract method7(
-    { a }: { a: number },
-    // trailing after object pattern
-  ): void;
-}
