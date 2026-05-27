17a23ffb69fd715f1b895febaa96fdb6a8d96b27
fisker <lionkay@gmail.com>
2026-05-28 03:39:01 +0800

    Split fixtures

--- tests/format/typescript/comments/methods.ts
+++ tests/format/typescript/comments/methods.ts
@@ -39,29 +39,11 @@ export class Point {
         * One
  * Two
    * Three
-* Four 
+* Four
    */
   mismatchedIndentation() {}
 
   inline /* foo*/ (/* bar */) /* baz */ {}
 
   noBody(/* comment */ arg);
-}
-
-// #18478
-declare class DeclareCls {
-  method(
-    param1: number,
-    // trailing line after last param
-  ): void;
-}
-
-// #18478
-class Overload {
-  method(
-    param1: number,
-    // trailing line on first overload
-  ): void;
-  method(param1: string): void;
-  method(param1: number | string): void {}
 }
