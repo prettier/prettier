c018e68c5eb9820120830fe76f1eff7eab46f701
fisker Cheung <lionkay@gmail.com>
2026-05-27 15:19:05 +0800

    Refactor `Object.{groupBy,hasOwn}()` transform (#19218)

--- scripts/build/transform/transforms/transform-object-group-by.js
+++ scripts/build/transform/transforms/transform-object-group-by.js
@@ -1,43 +1,8 @@
-import { fileURLToPath } from "node:url";
-import { outdent } from "outdent";
-import { createIdentifier, isIdentifier } from "./utilities.js";
+import createFunctionCallTransform from "./create-function-call-transform.js";
 
-/*
-`Object.groupBy(items, callbackFn)` -> `__objectGroupBy(items, callbackFn)`
-*/
+const transformObjectGroupBy = createFunctionCallTransform({
+  function: "Object.groupBy",
+  argumentsLength: 2,
+});
 
-const functionName = "__objectGroupBy";
-const functionImplementationPath = fileURLToPath(
-  new URL("../../shims/object-group-by.js", import.meta.url),
-);
-
-const transformObjectHasOwnCall = {
-  shouldSkip: (text) => !text.includes("Object.groupBy("),
-  /**
-   * @param {import("@babel/types").Node} node
-   * @returns {boolean}
-   */
-  test: (node) =>
-    node.type === "CallExpression" &&
-    !node.optional &&
-    node.arguments.length === 2 &&
-    node.arguments.every(({ type }) => type !== "SpreadElement") &&
-    node.callee.type === "MemberExpression" &&
-    !node.callee.optional &&
-    !node.callee.computed &&
-    isIdentifier(node.callee.object, "Object") &&
-    isIdentifier(node.callee.property, "groupBy"),
-  /**
-   * @param {import("@babel/types").CallExpression} node
-   * @returns {import("@babel/types").CallExpression}
-   */
-  transform: (node) => ({
-    ...node,
-    callee: createIdentifier(functionName),
-  }),
-  inject: outdent`
-    import ${functionName} from ${JSON.stringify(functionImplementationPath)};
-  `,
-};
-
-export default transformObjectHasOwnCall;
+export default transformObjectGroupBy;
