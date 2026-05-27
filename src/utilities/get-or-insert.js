249814a590e1cd1be8124f0f8ba387b95b90c74f
fisker <lionkay@gmail.com>
2026-05-27 17:23:57 +0800

    Add TODO

--- src/utilities/get-or-insert.js
+++ src/utilities/get-or-insert.js
@@ -1,3 +1,5 @@
+// TODO: Use `{Map,WeakMap}#{getOrInsert,getOrInsertComputed}()` directly when available
+
 function getOrInsertComputed(map, key, callback) {
   if (!map.has(key)) {
     const value = callback(key);
