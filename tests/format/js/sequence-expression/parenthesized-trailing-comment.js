c66a5fae200ba8b974b39bc4b3497b09b41cd694
fisker <lionkay@gmail.com>
2026-06-03 05:10:03 +0800

    Simplify tests

--- tests/format/js/sequence-expression/parenthesized-trailing-comment.js
+++ tests/format/js/sequence-expression/parenthesized-trailing-comment.js
@@ -1,15 +1,15 @@
-const declared = (a, b, c /* comment */);
+const x1 = (a, b, c /* comment */);
+const x2 = (a = c /* comment */);
 
 (a, b, c /* comment */);
+(a = c /* comment */);
 
 assigned = (a, b, c /* comment */);
+assigned = (a = c /* comment */);
 
 function f() {
   return (a, b, c /* comment */);
+  return (a = c /* comment */);
 }
 
-const assignInit = (m = n /* comment */);
 
-function g() {
-  return (p = q /* comment */);
-}
