296fbe6b7fdd3cc5b829d0a144ea060a887dd3ec
fisker Cheung <lionkay@gmail.com>
2026-06-04 02:22:52 +0800

    Improve `handleParenthesizedExpressionTrailingComment` (#19281)

--- tests/format/js/sequence-expression/parenthesized-trailing-comment.js
+++ tests/format/js/sequence-expression/parenthesized-trailing-comment.js
@@ -1,13 +1,17 @@
 const x1 = (a, b, c /* comment */);
+const x12 = (a, b, c) /* comment */;
 const x2 = (a = c /* comment */);
+const x22 = (a = c) /* comment */;
 
 (a, b, c /* comment */);
+(a, b, c) /* comment */;
 (a, b, c /* comment */
 );
 (a, b, c
 /* comment */
 );
 (a = c /* comment */);
+(a = c) /* comment */;
 (a = c /* comment */
 );
 (a = c
@@ -21,6 +25,7 @@ const x2 = (a = c /* comment */);
 );
 
 (foo() /* comment */);
+(foo()) /* comment */;
 (foo()
 /* comment */
 );
@@ -28,9 +33,11 @@ const x2 = (a = c /* comment */);
 );
 
 assigned = (a, b, c /* comment */);
+assigned = (a, b, c) /* comment */;
 // assigned = (a = c /* comment */);
 
 function f() {
   return (a, b, c /* comment */);
+  return (a, b, c) /* comment */;
   return (a = c /* comment */);
 }
