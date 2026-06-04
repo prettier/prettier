134b16cd21dad816f9101c17606a6560342a9299
seiya <20365512+seiyab@users.noreply.github.com>
2026-06-04 13:41:41 +0000

    move tests for prose-wrap

--- tests/format/mdx/mdx/format.test.js
+++ tests/format/mdx/mdx/format.test.js
@@ -1,4 +1,2 @@
 runFormatTest(import.meta, ["mdx"]);
 runFormatTest(import.meta, ["mdx"], { semi: false });
-runFormatTest(import.meta, ["mdx"], { proseWrap: "always" });
-runFormatTest(import.meta, ["mdx"], { proseWrap: "never" });
