73c8c3f57813a06e0334cdf312e56c00caae263a
fisker <lionkay@gmail.com>
2026-06-05 17:30:11 +0800

    Add test for #4032

--- tests/format/js/comments/return-statement-2.js
+++ tests/format/js/comments/return-statement-2.js
@@ -35,3 +35,27 @@ function foo() {
     a = b
   );
 }
+
+// #4032
+function transitionedFromBleepToBloop() {
+  return !!(
+    // Start with a bloop.
+    thing != null && (thing.foo && thing.foo.bar === 'bloop') &&
+    // End up with a bleep.
+    (thing.foo && thing.foo.bar === 'bleep')
+  );
+
+  return Boolean(
+    // Start with a bloop.
+    thing != null && (thing.foo && thing.foo.bar === 'bloop') &&
+    // End up with a bleep.
+    (thing.foo && thing.foo.bar === 'bleep')
+  );
+
+  return foo(
+    // Start with a bloop.
+    thing != null && (thing.foo && thing.foo.bar === 'bloop') &&
+    // End up with a bleep.
+    (thing.foo && thing.foo.bar === 'bleep')
+  );
+}
