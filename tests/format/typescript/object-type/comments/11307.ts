d67f7a8aea79be2b84d0f14f592c5dcb2cd5f1ea
fisker <lionkay@gmail.com>
2026-06-08 19:33:10 +0800

    Add regression test for object type

---
+++ tests/format/typescript/object-type/comments/11307.ts
@@ -0,0 +1,18 @@
+type T1 = (/** K */{
+  K: number;
+})
+type T2 = /** K */{
+  K: number;
+}
+const O1 = (/** K */{
+  K: number,
+})
+const O2 = /** K */{
+  K: number,
+}
+o = (/** K */{
+  K: number,
+})
+o = /** K */{
+  K: number,
+}
