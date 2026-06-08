23969f442738331469a9b6149f8ffba40af37a90
Konv Suu <hi@kovsu.com>
2026-06-09 04:29:43 +0800

    Fix formatting files with a quote at the start of a path segment

--- tests/integration/__tests__/special-characters-in-path.js
+++ tests/integration/__tests__/special-characters-in-path.js
@@ -1,3 +1,6 @@
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
 import jestPathSerializer from "../path-serializer.js";
 
 expect.addSnapshotSerializer(jestPathSerializer);
@@ -27,3 +30,29 @@ describe("square-brackets-and-dash", () => {
     "test",
   ]).test({});
 });
+
+if (path.sep === "/") {
+  const directory = path.join(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "../cli/special-characters-in-path/quotes",
+  );
+
+  fs.rmSync(directory, { force: true, recursive: true });
+  fs.mkdirSync(directory, { recursive: true });
+  fs.writeFileSync(
+    path.join(directory, '".js'),
+    "function add(){ return 1 + 2 }",
+  );
+  afterAll(() => {
+    fs.rmSync(directory, { force: true, recursive: true });
+  });
+
+  describe("quotes", () => {
+    runCli("cli/special-characters-in-path/quotes", ["**/*.js"]).test({
+      status: 0,
+      stdout: "function add() {\n  return 1 + 2;\n}",
+      stderr: "",
+      write: [],
+    });
+  });
+}
