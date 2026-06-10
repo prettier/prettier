d5770ebe4e19e6d3ce1c0cab7eaaf3db5bf47097
fisker <lionkay@gmail.com>
2026-06-11 04:15:25 +0800

    Fix attributes

--- src/language-markdown/print/directive.js
+++ src/language-markdown/print/directive.js
@@ -1,15 +1,25 @@
+// https://github.com/syntax-tree/mdast-util-directive/blob/a683327fafc4e48f81caf8d09d15fef8dd42a627/lib/index.js#L196
 function printDirectiveAttributes(path) {
   const { node } = path;
+  const { attributes } = node;
 
-  const values = Object.entries(node.attributes).map(([key, value]) =>
-    key === "id"
-      ? `#${value}`
-      : key === "class"
-        ? `.${value}`
-        : value
-          ? `${key}=${value}`
-          : key,
-  );
+  const values = Object.entries(attributes).map(([key, value]) => {
+    if (key === "id") {
+      return `#${value}`;
+    }
+    if (key === "class") {
+      return value
+        .split(/[\t\n\r ]+/)
+        .map((className) => `.${className}`)
+        .join("");
+    }
+
+    if (!value) {
+      return key;
+    }
+
+    return `${key}="${value}"`;
+  });
 
   return values.length > 0 ? "{" + values.join(" ") + "}" : "";
 }
