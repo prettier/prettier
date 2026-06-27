3006400fc2560e297b54d82c58cbc331ec87902c
fisker <lionkay@gmail.com>
2026-06-27 18:37:38 +0800

    Revert changes in release script

--- scripts/release/run.js
+++ scripts/release/run.js
@@ -23,38 +23,38 @@ for (let step of [
     process: steps.chooseVersion,
     skip: Boolean(params.version),
   },
-  // {
-  //   name: "Validating new version",
-  //   process: steps.validateNewVersion,
-  // },
-  // {
-  //   name: "Checking git status",
-  //   process: steps.checkGitStatus,
-  //   skip: params.dry,
-  // },
-  // {
-  //   name: "Installing NPM dependencies",
-  //   process: steps.installDependencies,
-  //   skip: params.dry || params.skipDependenciesInstall || !params.manual,
-  // },
-  // {
-  //   name: "Bumping version",
-  //   process: steps.updateVersion,
-  //   skip: params.dry,
-  // },
-  // steps.generateBundles,
-  // steps.updateChangelog,
-  // {
-  //   name: "Committing and pushing to remote",
-  //   process: steps.pushToGit,
-  //   skip: params.dry,
-  // },
-  // params.manual ? steps.publishToNpm : steps.waitForBotRelease,
-  // steps.showInstructionsAfterNpmPublish,
-  // {
-  //   name: "Merge release notes PR",
-  //   process: steps.mergeBlogPost,
-  // },
+  {
+    name: "Validating new version",
+    process: steps.validateNewVersion,
+  },
+  {
+    name: "Checking git status",
+    process: steps.checkGitStatus,
+    skip: params.dry,
+  },
+  {
+    name: "Installing NPM dependencies",
+    process: steps.installDependencies,
+    skip: params.dry || params.skipDependenciesInstall || !params.manual,
+  },
+  {
+    name: "Bumping version",
+    process: steps.updateVersion,
+    skip: params.dry,
+  },
+  steps.generateBundles,
+  steps.updateChangelog,
+  {
+    name: "Committing and pushing to remote",
+    process: steps.pushToGit,
+    skip: params.dry,
+  },
+  params.manual ? steps.publishToNpm : steps.waitForBotRelease,
+  steps.showInstructionsAfterNpmPublish,
+  {
+    name: "Merge release notes PR",
+    process: steps.mergeBlogPost,
+  },
   steps.updateDependentsCount,
   {
     name: "Cleaning changelog",
