15bc0351934b6f5285b97e996d31741c41e7ad20
fisker <lionkay@gmail.com>
2026-06-28 11:12:15 +0800

    Test All

--- tests/format/jsx/jsx-test-suite/format.test.js
+++ tests/format/jsx/jsx-test-suite/format.test.js
@@ -1,12 +1,12 @@
 import jsxTestSuite from "jsx-test-suite";
 
-const SKIP = new Set([
-  // HTML Comment
-  "e58e",
-]);
+const SKIP = new Set();
+const testCases = jsxTestSuite.filter(({ id }) => !SKIP.has(id));
 
-const snippets = jsxTestSuite
-  .filter(({ error, id }) => !error && !SKIP.has(id))
+const htmlComments = ["0006-e58e.jsx"];
+
+const snippets = testCases
+  .filter(({ error }) => !error)
   .map(({ name, input }) => ({ name, filename: name, code: input }));
 
 runFormatTest(
@@ -14,13 +14,18 @@ runFormatTest(
   ["babel", "typescript", "flow"],
   {
     errors: {
+      babel: htmlComments,
+      "babel-ts": htmlComments,
+      __babel_estree: htmlComments,
+      typescript: htmlComments,
       flow: [
+        ...htmlComments,
         "0012-912c.jsx",
         "0019-c21b.jsx",
         "0020-2386.jsx",
         "0023-f709.jsx",
       ],
-      hermes: ["0019-c21b.jsx", "0020-2386.jsx"],
+      hermes: [...htmlComments, "0019-c21b.jsx", "0020-2386.jsx"],
     },
   },
 );
