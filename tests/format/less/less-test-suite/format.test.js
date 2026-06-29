7fb0c1510b1e8bb8f69b2f9140d4b6dde62bef46
fisker Cheung <lionkay@gmail.com>
2026-06-30 00:56:01 +0800

    Run tests from `less-test-suite` (#19494)

---
+++ tests/format/less/less-test-suite/format.test.js
@@ -0,0 +1,39 @@
+import lessTestSuite from "less-test-suite";
+
+/* spell-checker: disable */
+const SKIP = new Set(["tests-unit/import/import/invalid-css.less"]);
+const BUGS = new Set([
+  // Can't parse
+  "tests-unit/variables/variable-advanced.less",
+  "tests-unit/property-accessors/property-accessors.less",
+  "tests-unit/plugin/plugin.less",
+  "tests-unit/permissive-parse/permissive-parse.less",
+  "tests-unit/parser-property-interp/parser-property-interp.less",
+  "tests-unit/mixins-interpolated/mixins-interpolated.less",
+  "tests-unit/import/import/invalid-css.less",
+  "tests-unit/functions/functions.less",
+  "tests-unit/extract-and-length/extract-and-length.less",
+  "tests-unit/css-escapes/css-escapes.less",
+  "tests-unit/comments/comments.less",
+
+  // Ast changed or unstable
+  "tests-unit/property-name-interp/property-name-interp.less",
+  "tests-unit/extend-chaining/extend-chaining.less",
+]);
+/* spell-checker: enable */
+
+const testCases = lessTestSuite.filter(
+  ({ error, name }) => !error && !SKIP.has(name) && !BUGS.has(name),
+);
+
+runFormatTest(
+  {
+    importMeta: import.meta,
+    snippets: testCases.map(({ name, input }) => ({
+      name,
+      filename: name,
+      code: input,
+    })),
+  },
+  ["less"],
+);
