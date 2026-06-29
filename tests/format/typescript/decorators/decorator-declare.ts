d0eed7c4eda2e1a527f713cfbc4db73f15059f07
fisker <lionkay@gmail.com>
2026-06-29 19:14:10 +0800

    Add issue link to the test fixture

--- tests/format/typescript/decorators/decorator-declare.ts
+++
@@ -1,7 +0,0 @@
-/**
- * decorators with declare is used extensively in Ember ecosystem,
- * and we want to make sure that Prettier doesn't break them.
- */
-export default class ProjectStatusComponent extends Component<ProjectStatusSig> {
-  @service declare server: ServerService;
-}
