a74a7b05dee7fbef39a6aeff378c3741e1a8ee15
Liam <radioactivity@gmail.com>
2026-06-29 12:22:38 +0100

    Allow decorators to be used with `declare` on class fields (#19492)
    Co-authored-by: fisker <lionkay@gmail.com>

--- src/language-js/parse/babel.js
+++ src/language-js/parse/babel.js
@@ -222,6 +222,13 @@ const allowedReasonCodesArray = [
   ```
   */
   "DeclarationMissingInitializer",
+
+  /*
+  Allow decorators to be used with declare fields
+  https://github.com/prettier/prettier/issues/19491
+  https://github.com/babel/babel/pull/17949
+  */
+  "DecoratorAbstractMethod",
 ];
 const allowedReasonCodes = new Set(allowedReasonCodesArray);
 
