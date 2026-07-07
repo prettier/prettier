b34e468b072d736e4f8b9817af8e2244abbf9dbc
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2026-07-07 14:45:09 -0600

    clean up

--- tests/format/typescript/import-export/type-modifier.ts
+++ tests/format/typescript/import-export/type-modifier.ts
@@ -4,12 +4,6 @@ export type { B as C } from './a';
 export type { foo } from 'bar';
 export type { foo };
 
-export { type /* comment */ T } from 'foo';
-import { type /* comment */ T } from 'foo';
-
-export { /* e1 */ type /* e2 */ T /* e3 */ as /* e4 */ U /* e5 */ } from 'foo';
-import { /* i1 */ type /* i2 */ T /* i3 */ as /* i4 */ U /* i5 */ } from 'foo';
-
 // this should be treated as a normal import statement
 import type from './foo';
 
@@ -18,3 +12,13 @@ import type { foo, bar } from 'baz';
 import type { foo as bar } from 'baz';
 import type * as foo from './bar';
 import type foo from 'bar';
+
+// https://github.com/prettier/prettier/issues/19564
+export { type /* comment */ T } from 'foo';
+import { type /* comment */ T } from 'foo';
+
+export { /* e1 */ type /* e2 */ T /* e3 */ } from 'foo';
+import { /* i1 */ type /* i2 */ T /* i3 */ } from 'foo';
+
+export { /* e1 */ type /* e2 */ T /* e3 */ as /* e4 */ U /* e5 */ } from 'foo';
+import { /* i1 */ type /* i2 */ T /* i3 */ as /* i4 */ U /* i5 */ } from 'foo';
