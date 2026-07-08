c54088725987e133d61109394e3f583d0b3caa9e
fisker <lionkay@gmail.com>
2026-07-09 00:43:41 +0800

    Add tests

---
+++ tests/format/js/string-width/string-width.js
@@ -0,0 +1,4 @@
+shouldFit________________("ཟླ་༡_ཟླ་༢_ཟླ་༣_ཟླ་༤_ཟླ་༥_ཟླ་༦_ཟླ་༧_ཟླ་༨_ཟླ་༩_ཟླ་༡༠_ཟླ་༡༡_ཟླ་༡༢");
+
+// https://github.com/react/react/blob/06b2a50f321502514443f675eaa44be8ae826218/fixtures/fizz/src/BigComponent.js#L154
+shouldFit___(<p>यह Google अनुवाद में अंग्रेज़ी से दूसरी भाषा में अनुवादित टेक्स्ट ब्लॉक है।</p>);
