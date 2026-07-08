45bd05c1751bd78398a3bcb867303e0e64d68a46
Alexander Lichter <github@lichter.io>
2026-07-08 11:03:02 +0200

    Update src/utilities/get-string-width.js
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- src/utilities/get-string-width.js
+++ src/utilities/get-string-width.js
@@ -8,7 +8,7 @@ import {
 import { isNarrowEmojiCharacter } from "narrow-emojis";
 
 const notAsciiRegex = /[^\x20-\x7F]/;
-// Exclude `Mc` because spacing marks contribute horizontal width.
+// Exclude [`Spacing Mark`](https://www.compart.com/en/unicode/category/Mc) because spacing marks contribute horizontal width.
 const zeroWidthMarkRegex = /[\p{Nonspacing_Mark}\p{Enclosing_Mark}]/u;
 
 // Similar to https://github.com/sindresorhus/string-width
