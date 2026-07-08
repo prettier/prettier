7f954071a111988f6d6053bcf6dbd33fd034f5e5
fisker <lionkay@gmail.com>
2026-07-09 01:00:06 +0800

    Add changelog

--- tests/format/typescript/type-parameters-arguments/19505.ts
+++ tests/format/typescript/type-parameters-arguments/19505.ts
@@ -1,17 +1,17 @@
 createObject<{
         // dangling comment
         [key in keyof Function]: TypeInfo | null
-    }>
+    }>()
 
 createObject<
 // comment
 {
         [key in keyof Function]: TypeInfo | null
-    }>
+    }>()
 
 createObject<
 {
         [key in keyof Function]: TypeInfo | null
     }
 // comment
->
+>()
