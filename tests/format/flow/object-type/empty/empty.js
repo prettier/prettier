cbee7377a1875686558bfc9416b789d66771d9e7
fisker Cheung <lionkay@gmail.com>
2026-07-09 17:34:22 +0800

    Remove space in empty object type (#19583)

---
+++ tests/format/flow/object-type/empty/empty.js
@@ -0,0 +1,17 @@
+a = [];
+a = [/*c*/];
+type A11 = [];
+type A12 = [/*c*/];
+
+o = {};
+o = {/*c*/};
+type O21 = {};
+type O22 = {/*c*/};
+type O23 = {...};
+type O24 = {/*c*/ ...};
+type O31 = {||};
+type O32 = {|/*c*/|};
+// type O33 = {|...|};
+// type O34 = {|/*c*/ ...|};
+interface I21 {}
+interface I22 {/*c*/}
---
+++ tests/format/typescript/object-type/empty/empty.ts
@@ -0,0 +1,11 @@
+a = [];
+a = [/*c*/];
+type A11 = [];
+type A12 = [/*c*/];
+
+o = {};
+o = {/*c*/};
+type O21 = {};
+type O22 = {/*c*/};
+interface I21 {}
+interface I22 {/*c*/}
