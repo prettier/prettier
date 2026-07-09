993903074160c3ecb058096fa5752dae3385ba64
fisker <lionkay@gmail.com>
2026-07-09 13:03:32 +0800

    Update

--- tests/format/flow/object-type/empty/empty.js
+++ tests/format/flow/object-type/empty/empty.js
@@ -11,7 +11,7 @@ type O23 = {...};
 type O24 = {/*c*/ ...};
 type O31 = {||};
 type O32 = {|/*c*/|};
-// type O33 = {| ... |};
-// type O34 = {| /*c*/ ... |};
+// type O33 = {|...|};
+// type O34 = {|/*c*/ ...|};
 interface I21 {}
 interface I22 {/*c*/}
