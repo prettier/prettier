e29348c092b47401109980c44a06356ddca29b38
fisker <lionkay@gmail.com>
2026-07-09 12:28:43 +0800

    Update test

--- tests/format/flow/object-type/empty/empty.js
+++ tests/format/flow/object-type/empty/empty.js
@@ -5,12 +5,12 @@ type A12 = [/*c*/];
 
 o = {};
 o = {/*c*/};
-type O21 = {  };
-type O22 = { /*c*/ };
-type O23 = { ... };
-type O24 = { /*c*/ ... };
-type O31 = {|  |};
-type O32 = {| /*c*/ |};
+type O21 = {};
+type O22 = {/*c*/};
+type O23 = {...};
+type O24 = {/*c*/ ...};
+type O31 = {||};
+type O32 = {|/*c*/|};
 // type O33 = {| ... |};
 // type O34 = {| /*c*/ ... |};
 interface I21 {}
--- tests/format/typescript/object-type/empty/empty.ts
+++ tests/format/typescript/object-type/empty/empty.ts
@@ -5,7 +5,7 @@ type A12 = [/*c*/];
 
 o = {};
 o = {/*c*/};
-type O21 = {  };
-type O22 = { /*c*/ };
+type O21 = {};
+type O22 = {/*c*/};
 interface I21 {}
 interface I22 {/*c*/}
