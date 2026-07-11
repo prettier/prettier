3247e2d65ca038cc6c9f4c4ddc272e4c58bf007f
fisker Cheung <lionkay@gmail.com>
2026-07-11 21:46:10 +0800

    Fix JSDoc (#19601)

--- src/document/builders/line-suffix-boundary.js
+++ src/document/builders/line-suffix-boundary.js
@@ -4,7 +4,7 @@ import { DOC_TYPE_LINE_SUFFIX_BOUNDARY } from "./types.js";
 @typedef {{readonly type: DOC_TYPE_LINE_SUFFIX_BOUNDARY}} LineSuffixBoundary
 */
 
-/** @typedef {LineSuffixBoundary} */
+/** @type {LineSuffixBoundary} */
 const lineSuffixBoundary = { type: DOC_TYPE_LINE_SUFFIX_BOUNDARY };
 
 export { lineSuffixBoundary };
