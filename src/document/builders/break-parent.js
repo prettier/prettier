29507bf3ddcedca301cc6438405bb4e3b145a5aa
fisker <lionkay@gmail.com>
2026-07-11 20:40:27 +0800

    Fix JSDoc

--- src/document/builders/break-parent.js
+++ src/document/builders/break-parent.js
@@ -4,7 +4,7 @@ import { DOC_TYPE_BREAK_PARENT } from "./types.js";
 @typedef {{readonly type: DOC_TYPE_BREAK_PARENT}} BreakParent
 */
 
-/** @typedef {BreakParent} */
+/** @type {BreakParent} */
 const breakParent = { type: DOC_TYPE_BREAK_PARENT };
 
 export { breakParent };
