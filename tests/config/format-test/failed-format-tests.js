9707d8b2536ed0dc8de696010dee080cf0317dba
fisker <lionkay@gmail.com>
2026-07-15 23:01:54 +0800

    Rename

--- tests/config/format-test/failed-format-tests.js
+++ tests/config/format-test/failed-format-tests.js
@@ -57,7 +57,7 @@ const disabledTests = new Map(
     espree: [
       ...commentClosureTypecaseTests,
       "js/explicit-resource-management/valid-await-using-asi-assignment.js",
-      "js/await/await-with-parens.ts",
+      "js/await/await-with-parens.js",
     ],
     acorn: [
       "js/explicit-resource-management/valid-await-using-asi-assignment.js",
@@ -65,7 +65,7 @@ const disabledTests = new Map(
     meriyah: [
       // Parsing to different ASTs
       "js/decorators/member-expression.js",
-      "js/await/await-with-parens.ts",
+      "js/await/await-with-parens.js",
     ],
     "babel-ts": [
       "typescript/conformance/types/moduleDeclaration/kind-detection.ts",
@@ -90,8 +90,8 @@ const disabledTests = new Map(
     ],
     oxc: [],
     "oxc-ts": [],
-    yuku: ["js/await/await-with-parens.ts"],
-    "yuku-ts": ["js/await/await-with-parens.ts"],
+    yuku: ["js/await/await-with-parens.js"],
+    "yuku-ts": ["js/await/await-with-parens.js"],
     hermes: [
       ...commentClosureTypecaseTests,
 
@@ -101,11 +101,11 @@ const disabledTests = new Map(
 
       // Different result
       "flow/hook/comments-before-arrow.js",
-      "js/await/await-with-parens.ts",
+      "js/await/await-with-parens.js",
     ],
     flow: [
       "js/decorators/member-expression.js", // Parsing to different ASTs
-      "js/await/await-with-parens.ts",
+      "js/await/await-with-parens.js",
     ],
     typescript: [
       // https://github.com/typescript-eslint/typescript-eslint/issues/11389
