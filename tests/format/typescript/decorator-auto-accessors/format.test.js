3bbb8159eb55575d4042653aa99f5f92a1416c19
fisker Cheung <lionkay@gmail.com>
2026-07-15 23:14:34 +0800

    Remove `typescript-only` directory (#19636)

--- tests/format/typescript/decorator-auto-accessors/format.test.js
+++ tests/format/typescript/decorator-auto-accessors/format.test.js
@@ -1,1 +1,10 @@
-runFormatTest(import.meta, ["typescript"]);
+runFormatTest(import.meta, ["typescript"], {
+  errors: {
+    "babel-ts": ["decorator-auto-accessors-abstract-class.ts"],
+    "oxc-ts": [
+      "decorator-auto-accessors-abstract-class.ts",
+      "decorator-auto-accessors-declare-class.ts",
+      "decorator-auto-accessors-mixed-modifiers.ts",
+    ],
+  },
+});
