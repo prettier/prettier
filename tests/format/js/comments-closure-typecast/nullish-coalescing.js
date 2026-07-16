fc3717bd7a1ddba0b335835b157ba2e42ad0ca5e
fisker <lionkay@gmail.com>
2026-07-17 01:02:35 +0800

    Fix comment

--- tests/format/js/comments-closure-typecast/nullish-coalescing.js
+++ tests/format/js/comments-closure-typecast/nullish-coalescing.js
@@ -8,7 +8,7 @@ target = a
 
 target =  a
   ?  b
-  : (/** @type A*/ (root).head ?? root.ownerDocument.head);
+  : (/** @type {A} */ (root).head ?? root.ownerDocument.head);
 
 // https://github.com/sveltejs/svelte/blob/602a873b6b82bba4e6edc91b039e2f6defbe3fc4/packages/svelte/src/internal/client/dom/css.js#L15-L17
 target = /** @type {ShadowRoot} */ (root).host
