394eb691e4b8c19df565150fc569a83ad31885bf
fisker <lionkay@gmail.com>
2026-07-23 15:52:14 +0800

    Remove `please-upgrade-node`

--- bin/prettier.cjs
+++ bin/prettier.cjs
@@ -8,29 +8,72 @@ if (typeof nodeModule.enableCompileCache === "function") {
   nodeModule.enableCompileCache();
 }
 
-var pleaseUpgradeNode = require("please-upgrade-node");
-var packageJson = require("../package.json");
+// https://unpkg.com/semver-compare@1.0.0/index.js
+function semverCompare(a, b) {
+  var pa = a.split(".");
+  var pb = b.split(".");
+  for (var i = 0; i < 3; i++) {
+    var na = Number(pa[i]);
+    var nb = Number(pb[i]);
+    if (na > nb) {
+      return 1;
+    }
+    if (nb > na) {
+      return -1;
+    }
+    if (!Number.isNaN(na) && Number.isNaN(nb)) {
+      return 1;
+    }
+    if (Number.isNaN(na) && !Number.isNaN(nb)) {
+      return -1;
+    }
+  }
+  return 0;
+}
 
-pleaseUpgradeNode(packageJson);
+// https://unpkg.com/please-upgrade-node@3.2.0/index.js
+function checkNodejsVersion() {
+  var packageJson = require("../package.json");
+  var requiredVersion = packageJson.engines.node.replace(">=", "");
+  var currentVersion = process.version.slice(1);
+  if (semverCompare(currentVersion, requiredVersion) === -1) {
+    throw new Error(
+      "Prettier requires at least version " +
+        requiredVersion +
+        " of Node.js, please upgrade!"
+    );
+  }
+}
 
 var dynamicImport = new Function("module", "return import(module)");
 
-var promise;
-var index = process.argv.indexOf("--experimental-cli");
-if (process.env.PRETTIER_EXPERIMENTAL_CLI || index !== -1) {
-  if (index !== -1) {
-    process.argv.splice(index, 1);
+function run() {
+  try {
+    checkNodejsVersion();
+  } catch (error) {
+    process.exitCode = 1;
+    // eslint-disable-next-line no-console
+    console.error(error.message);
+    return Promise.reject(error);
   }
-  promise = dynamicImport("../src/experimental-cli/index.js").then(
-    function (cli) {
-      return cli.__promise;
+
+  var index = process.argv.indexOf("--experimental-cli");
+  if (process.env.PRETTIER_EXPERIMENTAL_CLI || index !== -1) {
+    if (index !== -1) {
+      process.argv.splice(index, 1);
     }
-  );
-} else {
-  promise = dynamicImport("../src/cli/index.js").then(function runCli(cli) {
+
+    return dynamicImport("../src/experimental-cli/index.js").then(
+      function (cli) {
+        return cli.__promise;
+      }
+    );
+  }
+
+  return dynamicImport("../src/cli/index.js").then(function runCli(cli) {
     return cli.run();
   });
 }
 
 // Exposed for test
-module.exports.__promise = promise;
+module.exports.__promise = run();
