854922bd14794181b1057e07d38971b3228637f2
Konv Suu <hi@kovsu.com>
2026-07-23 18:08:52 +0800

    more test

--- tests/format/typescript/mapped-type/comments.ts
+++ tests/format/typescript/mapped-type/comments.ts
@@ -8,3 +8,13 @@ type Type<T> = {
 type Type<T> = { [K in keyof T]: /** Comment */ | B };
 
 type Type<T> = { [K in keyof T] /** Comment */: B | C };
+
+type Type<T> = { [K in keyof T as `get${K}`]: /** Comment */ B };
+
+type Type<T> = { [K in keyof T as `get${K}`]: /** Comment */ | B };
+
+type Type<T> = { [K in keyof T as K extends string ? K : never]: /** Comment */ B };
+
+type Type<T> = {
+  [K in keyof T as K extends string ? K : never] /** Comment */: B | C;
+};
