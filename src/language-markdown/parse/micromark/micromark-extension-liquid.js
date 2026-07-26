28d914e25635592523ba1a1ae7c28efbaccdc297
wanxiankai <xiankai.wan@gmail.com>
2026-07-26 23:16:37 +0800

    Fix Markdown Liquid flow parsing

--- src/language-markdown/parse/micromark/micromark-extension-liquid.js
+++ src/language-markdown/parse/micromark/micromark-extension-liquid.js
@@ -1,4 +1,4 @@
-import { markdownLineEnding } from "micromark-util-character";
+import { markdownLineEnding, markdownSpace } from "micromark-util-character";
 import { codes, types } from "micromark-util-symbol";
 
 /**
@@ -7,9 +7,14 @@ import { codes, types } from "micromark-util-symbol";
  * @typedef {import('mdast-util-from-markdown').CompileContext} CompileContext
  * @typedef {import('mdast-util-from-markdown').Handle} Handle
  * @typedef {import('micromark-util-types').State} State
+ * @typedef {import('micromark-util-types').TokenizeContext} TokenizeContext
  */
 
 const nodeType = "liquidNode";
+const nonLazyContinuation = {
+  tokenize: tokenizeNonLazyContinuation,
+  partial: true,
+};
 
 /**
  * @returns {FromMarkdownExtension}
@@ -45,16 +50,33 @@ function liquidFromMarkdown() {
  * @returns {import('micromark-util-types').Extension}
  */
 function liquidSyntax() {
+  const liquid = {
+    name: "liquid",
+    tokenize,
+  };
+
   return {
-    text: {
+    flow: {
       [codes.leftCurlyBrace]: {
-        name: "liquid",
-        tokenize,
+        name: "liquidFlow",
+        tokenize: tokenizeFlow,
       },
+    },
+    text: {
+      [codes.leftCurlyBrace]: liquid,
     },
   };
 
+  /** @this {TokenizeContext} */
+  function tokenizeFlow(effects, ok, nok) {
+    return tokenizeLiquid(effects, ok, nok, true, this.interrupt);
+  }
+
   function tokenize(effects, ok, nok) {
+    return tokenizeLiquid(effects, ok, nok, false, false);
+  }
+
+  function tokenizeLiquid(effects, ok, nok, isFlow, interrupt) {
     /** @type {typeof codes.rightCurlyBrace | typeof codes.percentSign} */
     let closingCode;
 
@@ -74,7 +96,7 @@ function liquidSyntax() {
                 ? codes.percentSign
                 : codes.rightCurlyBrace;
             effects.consume(code);
-            return inside;
+            return isFlow && interrupt ? ok : inside;
           default:
             return nok(code);
         }
@@ -92,6 +114,13 @@ function liquidSyntax() {
         default:
           if (markdownLineEnding(code)) {
             effects.exit(types.data);
+            if (isFlow) {
+              return effects.attempt(
+                nonLazyContinuation,
+                afterLineEnding,
+                nok,
+              )(code);
+            }
             effects.enter(types.lineEnding);
             effects.consume(code);
             effects.exit(types.lineEnding);
@@ -104,16 +133,78 @@ function liquidSyntax() {
     }
 
     /** @type {State} */
+    function afterLineEnding(code) {
+      if (markdownLineEnding(code)) {
+        return effects.attempt(nonLazyContinuation, afterLineEnding, nok)(code);
+      }
+
+      effects.enter(types.data);
+      return inside(code);
+    }
+
+    /** @type {State} */
     function mayClose(code) {
       if (code === codes.rightCurlyBrace) {
         effects.consume(code);
         effects.exit(types.data);
         effects.exit(nodeType);
-        return ok;
+        return isFlow ? afterClose : ok;
       }
 
       return inside;
     }
+
+    /** @type {State} */
+    function afterClose(code) {
+      if (markdownSpace(code)) {
+        effects.enter(types.whitespace);
+        effects.consume(code);
+        return afterWhitespace;
+      }
+
+      return after(code);
+    }
+
+    /** @type {State} */
+    function afterWhitespace(code) {
+      if (markdownSpace(code)) {
+        effects.consume(code);
+        return afterWhitespace;
+      }
+
+      effects.exit(types.whitespace);
+      return after(code);
+    }
+
+    /** @type {State} */
+    function after(code) {
+      return code === codes.eof || markdownLineEnding(code)
+        ? ok(code)
+        : nok(code);
+    }
+  }
+}
+
+/** @this {TokenizeContext} */
+function tokenizeNonLazyContinuation(effects, ok, nok) {
+  const { now, parser } = this;
+  return start;
+
+  /** @type {State} */
+  function start(code) {
+    if (code === codes.eof) {
+      return ok(code);
+    }
+
+    effects.enter(types.lineEnding);
+    effects.consume(code);
+    effects.exit(types.lineEnding);
+    return lineStart;
+  }
+
+  /** @type {State} */
+  function lineStart(code) {
+    return parser.lazy[now().line] ? nok(code) : ok(code);
   }
 }
 
