b36d30b16ac1f62c7d6cf4ebca59eb84900faaa9
fisker <lionkay@gmail.com>
2026-07-30 16:12:23 +0800

    Test

---
+++ tests/format/mdx/_errors_/attribute-names/format.test.js
@@ -0,0 +1,21 @@
+runFormatTest(
+  {
+    importMeta: import.meta,
+    snippets: [
+      // `name` can't contain `"`, `'`, or `=`
+      "<Foo \"='' />",
+      "<Foo &quot;='' />",
+      "<Foo &34;='' />",
+      "<Foo '='' />",
+      "<Foo &apos;='' />",
+      "<Foo &39;='' />",
+      "<Foo =='' />",
+      "<Foo &equals;='' />",
+      "<Foo &#61;='' />",
+
+      // `value` must be quoted
+      "<Foo name=value />",
+    ],
+  },
+  ["mdx"],
+);
