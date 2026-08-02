128cd599825bc31b05b39e266a3f53158df22dd0
fisker <lionkay@gmail.com>
2026-08-02 11:17:19 +0800

    Improve "flow-ambient" test

--- tests/format/misc/filename-matters/flow-ambient/format.test.js
+++ tests/format/misc/filename-matters/flow-ambient/format.test.js
@@ -1,11 +1,26 @@
+const tests = [
+  {
+    code: "const foo: string;",
+    baseFilename: "const-without-initializer",
+  },
+].flatMap(({ code, baseFilename }) =>
+  [undefined, ".js.flow", ".unknown"].map((extension) => {
+    const filename =
+      extension === undefined ? undefined : `${baseFilename}${extension}`;
+    return { name: filename ?? "(no name)", code, filename };
+  }),
+);
+
 runFormatTest(
   {
-    snippets: [
-      // Unknown filename
-      "const foo: string;",
-    ],
     importMeta: import.meta,
+    snippets: tests,
   },
   ["flow"],
-  { errors: { hermes: true } },
+  {
+    errors: {
+      flow: ["const-without-initializer.unknown"],
+      hermes: true,
+    },
+  },
 );
