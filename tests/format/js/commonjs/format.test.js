c2d165e32b1e42d9c887d72696cacdfafe2e6da4
fisker <lionkay@gmail.com>
2026-08-03 10:24:47 +0800

    return in "static block"

--- tests/format/js/commonjs/format.test.js
+++ tests/format/js/commonjs/format.test.js
@@ -1,7 +1,12 @@
 const tests = [
   {
     code: "return",
-    baseFilename: "return-top-level",
+    baseFilename: "return/top-level",
+  },
+  // https://github.com/acornjs/acorn/issues/1376#issuecomment-2960924476
+  {
+    code: "class X { static { return; } }",
+    baseFilename: "return/in-static-block",
   },
   {
     code: "new.target",
@@ -23,9 +28,42 @@ runFormatTest(
   ["babel", "flow", "typescript"],
   {
     errors: {
-      acorn: ["new-target.mjs"],
-      espree: ["return-top-level.mjs", "new-target.mjs"],
-      meriyah: ["using.cjs", "return-top-level.mjs", "new-target.mjs"],
+      babel: [
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      "babel-ts": [
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      __babel_estree: [
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      acorn: [
+        "new-target.mjs",
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      espree: [
+        "return/top-level.mjs",
+        "new-target.mjs",
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      meriyah: [
+        "using.cjs",
+        "return/top-level.mjs",
+        "new-target.mjs",
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
       flow: [
         "new-target.cjs",
         "new-target.mjs",
@@ -35,10 +73,30 @@ runFormatTest(
         "using.unknown",
       ],
       hermes: ["new-target.cjs", "new-target.mjs", "new-target.unknown"],
-      oxc: ["new-target.mjs"],
-      "oxc-ts": ["new-target.mjs"],
-      yuku: ["return-top-level.mjs"],
-      "yuku-ts": ["return-top-level.mjs"],
+      oxc: [
+        "new-target.mjs",
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      "oxc-ts": [
+        "new-target.mjs",
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      yuku: [
+        "return/top-level.mjs",
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
+      "yuku-ts": [
+        "return/top-level.mjs",
+        "return/in-static-block.cjs",
+        "return/in-static-block.mjs",
+        "return/in-static-block.unknown",
+      ],
     },
   },
 );
