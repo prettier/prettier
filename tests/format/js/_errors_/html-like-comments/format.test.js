bcd435c2032c0c29b34c355a1348f488ffe41508
fisker <lionkay@gmail.com>
2026-08-13 17:53:57 +0800

    Use file

--- tests/format/js/_errors_/html-like-comments/format.test.js
+++ tests/format/js/_errors_/html-like-comments/format.test.js
@@ -1,29 +1,14 @@
-import { outdent } from "outdent";
-
-runFormatTest(
-  {
-    importMeta: import.meta,
-    snippets: [
-      outdent`
-        alert(
-        <!-- comment
-        'hello world'
-        )
-      `,
-    ],
-  },
-  [
-    "babel",
-    // "acorn",
-    // "espree",
-    // "meriyah",
-    "typescript",
-    "babel-ts",
-    // "oxc",
-    // "oxc-ts",
-    // "yuku",
-    // "yuku-ts",
-    "flow",
-    "hermes",
-  ],
-);
+runFormatTest(import.meta, [
+  "babel",
+  // "acorn",
+  // "espree",
+  // "meriyah",
+  "typescript",
+  "babel-ts",
+  // "oxc",
+  // "oxc-ts",
+  // "yuku",
+  // "yuku-ts",
+  "flow",
+  "hermes",
+]);
