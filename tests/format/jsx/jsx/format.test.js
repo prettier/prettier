3eb24ceea9e74e7a6c60174295a9047686abe2f2
fisker <lionkay@gmail.com>
2026-08-15 17:22:55 +0800

    Improve tests for JSX quotes

--- tests/format/jsx/jsx/format.test.js
+++ tests/format/jsx/jsx/format.test.js
@@ -1,16 +1,1 @@
-runFormatTest(import.meta, ["babel", "typescript", "flow"], {
-  singleQuote: false,
-  jsxSingleQuote: false,
-});
-runFormatTest(import.meta, ["babel", "typescript", "flow"], {
-  singleQuote: false,
-  jsxSingleQuote: true,
-});
-runFormatTest(import.meta, ["babel", "typescript", "flow"], {
-  singleQuote: true,
-  jsxSingleQuote: false,
-});
-runFormatTest(import.meta, ["babel", "typescript", "flow"], {
-  singleQuote: true,
-  jsxSingleQuote: true,
-});
+runFormatTest(import.meta, ["babel", "typescript", "flow"]);
