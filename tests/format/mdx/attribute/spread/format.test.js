af7752ac4cd7d3a5b7f325e437cb0e20df262aee
fisker <lionkay@gmail.com>
2026-08-15 19:04:31 +0800

    Rewrite tests for options

--- tests/format/mdx/attribute/spread/format.test.js
+++ tests/format/mdx/attribute/spread/format.test.js
@@ -1,4 +1,1 @@
 runFormatTest(import.meta, ["mdx"]);
-runFormatTest(import.meta, ["mdx"], { semi: false });
-runFormatTest(import.meta, ["mdx"], { objectWrap: "preserve" });
-runFormatTest(import.meta, ["mdx"], { embeddedLanguageFormatting: "off" });
