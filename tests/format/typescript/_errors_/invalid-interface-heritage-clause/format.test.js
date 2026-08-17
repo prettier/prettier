76714fa13bce468735a28335ecdc29de3d7ef2b4
fisker <lionkay@gmail.com>
2026-08-17 13:52:38 +0800

    Fix tests

--- tests/format/typescript/_errors_/invalid-interface-heritage-clause/format.test.js
+++ tests/format/typescript/_errors_/invalid-interface-heritage-clause/format.test.js
@@ -5,8 +5,21 @@ runFormatTest(
       "interface Foo extends {}",
       "interface F implements {}",
       "interface F implements A {}",
-      "interface F extends implements {}",
+      // "interface F extends implements {}",
     ],
   },
   ["typescript", "babel-ts", "oxc-ts", "yuku-ts"],
+);
+
+runFormatTest(
+  {
+    importMeta: import.meta,
+    snippets: ["interface F extends implements {}"],
+  },
+  [
+    "typescript",
+    // "babel-ts",
+    // "oxc-ts",
+    // "yuku-ts",
+  ],
 );
