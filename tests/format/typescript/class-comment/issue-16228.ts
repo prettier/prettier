89ac5fa9cb97c894952884139bf579c4d23647df
koreahghg <koreahghg@gmail.com>
2026-08-20 18:15:39 +0900

    Remove redundant pre-formatted second class from issue-16228 test
    The format test framework already reformats the output and asserts
    stability, so manually including the already-formatted result as a
    second input case was unnecessary.

--- tests/format/typescript/class-comment/issue-16228.ts
+++ tests/format/typescript/class-comment/issue-16228.ts
@@ -1,9 +1,2 @@
 class MyClass1 extends MySuperClass implements /* foo */ ISomething, /* bar */ IOtherThing, /* baz */ IThirdThing {
 }
-
-class MyClass2
-  extends MySuperClass
-  implements
-    /* foo */ ISomething,
-    /* bar */ IOtherThing,
-    /* baz */ IThirdThing {}
