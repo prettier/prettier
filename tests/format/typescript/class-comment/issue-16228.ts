8574bada85c0b7f8cf6d51c2815e65f472522693
fisker <lionkay@gmail.com>
2026-08-20 18:31:35 +0800

    Test

--- tests/format/typescript/class-comment/issue-16228.ts
+++ tests/format/typescript/class-comment/issue-16228.ts
@@ -1,9 +1,9 @@
-class C1 extends S implements /* foo */ Foo, /* bar */ Bar {
+class C11 extends S implements /* foo */ Foo, /* bar */ Bar {
 }
-class C11 extends MySuperClass implements /* foo */ Foo, /* bar */ Bar, /* baz */ Baz__________________________________ {
+class C12 extends S implements /* foo */ Foo, /* bar */ Bar, /* baz */ Baz__________________________________ {
 }
 
-interface C1 extends /* foo */ Foo, /* bar */ Bar {
+interface I11 extends /* foo */ Foo, /* bar */ Bar {
 }
-interface C2 extends /* foo */ Foo, /* bar */ Bar, /* baz */ Baz__________________________________ {
+interface I12 extends /* foo */ Foo, /* bar */ Bar, /* baz */ Baz__________________________________ {
 }
