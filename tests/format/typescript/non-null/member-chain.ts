90ad0abf625eff490be2a3ca4b41777adceecd26
Konv Suu <hi@kovsu.com>
2026-08-24 10:06:59 +0800

    Use short comments in tests

--- tests/format/typescript/non-null/member-chain.ts
+++ tests/format/typescript/non-null/member-chain.ts
@@ -11,7 +11,7 @@ xyz.a(b!).a(b!).a(b!)
 xyz.a(/a/).a(/a/).a(/a/)
 xyz.a(/a/!).a(/a/!).a(/a/!)
 
-obj[l] /* This is a very very very very very long inline comment about something */!.substring(0);
-obj.aaa /* This is a very very very very very long inline comment about something */!.substring(0);
-wrapper.foo() /* comment aaaa */!.bar() /* comment bbbb */!.baz() /* comment cccc */!.qux();
-wrapper.foo() /* This is a very very very very long inline comment about something */!!.bar();
+obj[l] /* c */!.substring(0);
+obj.aaa /* c */!.substring(0);
+wrapper.foo() /* c1 */!.bar() /* c2 */!.baz() /* c3 */!.qux();
+wrapper.foo() /* c */!!.bar();
