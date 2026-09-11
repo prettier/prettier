8141a06acd5bfaaa2d1fbc1e752bd95ce30dd720
fisker <lionkay@gmail.com>
2026-09-11 15:31:05 +0800

    Async do

--- tests/format/js/parentheses/do-expression.js
+++ tests/format/js/parentheses/do-expression.js
@@ -1,31 +1,41 @@
 (do {});
+(async do {});
 (throw error);
 
 (do {}) || fallback;
+(async do {}) || fallback;
 (throw error) || fallback;
 
 (do {}).message;
+(async do {}).message;
 (throw error).message;
 
-((do {}), (throw error), other);
+((do {}), (async do {}), (throw error), other);
 
 (do {}) + 1;
+(async do {}) + 1;
 (throw error) + 1;
 
 1 + do {};
+1 + async do {};
 1 + throw error;
 
 () => do {};
+() => async do {};
 () => throw error;
 
 if (condition) (do {});
+if (condition) (async do {});
 if (condition) (throw error);
 
 for (;;) (do {});
+for (;;) (async do {});
 for (;;) (throw error);
 
 label: (do {});
+label: (async do {});
 label: (throw error);
 
 value = do {};
+value = async do {};
 value = throw error;
