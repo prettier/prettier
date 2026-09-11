ecd1968e6dcebe739afc0545f08c197f488eee50
fisker <lionkay@gmail.com>
2026-09-11 16:05:06 +0800

    Move files

--- tests/format/js/arrow-expression/arrow-call/format.test.js
+++ tests/format/js/arrow-expression/arrow-call/format.test.js
@@ -1,14 +1,9 @@
-const errors = {};
-
 runFormatTest(import.meta, ["babel", "flow", "typescript"], {
   trailingComma: "es5",
-  errors,
 });
 runFormatTest(import.meta, ["babel", "flow", "typescript"], {
   trailingComma: "all",
-  errors,
 });
 runFormatTest(import.meta, ["babel", "flow", "typescript"], {
   arrowParens: "always",
-  errors,
 });
