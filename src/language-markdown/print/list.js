54756273d7423282150c96f4651626669118b11f
Lazizbek Ergashev <lazerg2@gmail.com>
2026-09-12 06:55:29 +0500

    Fix HTML block indentation drift in markdown list items (#19987)
    Co-authored-by: fisker Cheung <lionkay@gmail.com>

--- src/language-markdown/print/list.js
+++ src/language-markdown/print/list.js
@@ -99,7 +99,7 @@ function printListItem(path, options, print, listPrefix) {
     prefix,
     printChildren(path, options, print, {
       processor({ node, isFirst }) {
-        if (isFirst && node.type !== "list") {
+        if ((isFirst && node.type !== "list") || node.type === "html") {
           return align(" ".repeat(prefix.length), print());
         }
 
