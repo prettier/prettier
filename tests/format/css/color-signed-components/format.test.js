cf442daf003a8a1d13a7349c90ba6d78588edf47
Grada <grada23333@outlook.com>
2026-09-13 16:00:18 +0800

    Consolidate color regression into CSS fixture

--- tests/format/css/color-signed-components/format.test.js
+++ tests/format/css/color-signed-components/format.test.js
@@ -1,12 +1,1 @@
-runFormatTest(
-  {
-    importMeta: import.meta,
-    snippets: [
-      {
-        code: "a { color: oklch(from red l c +130); }",
-        output: "a {\n  color: oklch(from red l c +130);\n}\n",
-      },
-    ],
-  },
-  ["css", "scss", "less"],
-);
+runFormatTest({ importMeta: import.meta }, ["css", "scss", "less"]);
