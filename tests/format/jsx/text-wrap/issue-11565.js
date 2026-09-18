ab25ee0e7be75db1632d40c0ab698f7b6fc04891
Konv Suu <hi@kovsu.com>
2026-09-18 16:50:12 +0800

    Fix unstable JSX single-character text before a self-closing element

---
+++ tests/format/jsx/text-wrap/issue-11565.js
@@ -0,0 +1,8 @@
+const Component = () => (
+  <div>
+    <strong>
+      Some random text
+    </strong>
+    : <div className="yoooooooooooooooooooo" data-test-a-long-property-name="aaaaaaaaaaaaaaaaaaaaaaa" />
+  </div>
+);
