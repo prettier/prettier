f2c83be8787b678734fdee26002d94f70213d550
fisker <lionkay@gmail.com>
2026-09-21 23:41:55 +0800

    Add test

---
+++ tests/unit/config-loader.js
@@ -0,0 +1,32 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { temporaryDirectory as getTemporaryDirectory } from "tempy";
+import loadConfig from "../../src/config/prettier-config/load-config.js";
+
+describe("Support BOM", () => {
+  const files = [
+    { filename: ".prettierrc", content: "tabWidth: 5" },
+    { filename: ".prettierrc.json", content: '{"tabWidth": 5}' },
+    { filename: ".prettierrc.yml", content: "tabWidth: 5" },
+    { filename: ".prettierrc.json5", content: "{tabWidth: 5}" },
+    { filename: ".prettierrc.toml", content: "tabWidth = 5" },
+  ];
+
+  const directory = getTemporaryDirectory();
+  const expected = { tabWidth: 5 };
+  beforeAll(async () => {
+    await Promise.all(
+      files.map(({ filename, content }) =>
+        fs.writeFile(path.join(directory, filename), `\ufeff${content}`),
+      ),
+    );
+  });
+
+  for (const { filename } of files) {
+    test(filename, async () => {
+      expect(await loadConfig(path.join(directory, filename))).toStrictEqual(
+        expected,
+      );
+    });
+  }
+});
