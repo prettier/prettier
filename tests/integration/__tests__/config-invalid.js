b49d6aced82897b88cc6e15800dbbf2fec73e707
fisker <lionkay@gmail.com>
2026-09-23 12:48:21 +0800

    Move some tests for invalid config to unit test

--- tests/integration/__tests__/config-invalid.js
+++ tests/integration/__tests__/config-invalid.js
@@ -1,33 +1,7 @@
 import fs from "node:fs/promises";
-import outdent from "outdent";
 import jestPathSerializer from "../path-serializer.js";
 
 expect.addSnapshotSerializer(jestPathSerializer);
-
-describe("throw error for unsupported extension", () => {
-  runCli("cli/config/invalid", [
-    "--config",
-    "file/.prettierrc.unsupported",
-  ]).test({
-    status: "non-zero",
-  });
-});
-
-describe("throw error with invalid config format", () => {
-  runCli("cli/config/invalid", ["--config", "file/.prettierrc"]).test({
-    status: "non-zero",
-    stderr: expect.stringMatching(/Cannot find package '--invalid--'/),
-  });
-});
-
-describe("throw error with invalid config format", () => {
-  runCli("cli/config/invalid", ["--config", "type-error/.prettierrc"]).test({
-    status: "non-zero",
-    stderr: expect.stringContaining(
-      "Config is only allowed to be an object, but received number in",
-    ),
-  });
-});
 
 describe("throw error with invalid config target (directory)", () => {
   runCli("cli/config/invalid", [
@@ -119,80 +93,6 @@ describe("show warning with kebab-case option key", () => {
     "babel",
   ]).test({
     status: 0,
-  });
-});
-
-// #8815, please make sure this error contains code frame
-describe("Invalid json file", () => {
-  runCli("cli/config/invalid", [
-    "--config",
-    "broken-json/.prettierrc.json",
-    "--parser",
-    "babel",
-  ]).test({
-    status: 2,
-    stdout: "",
-    write: [],
-    stderr: expect.stringContaining(
-      outdent`
-        > 1 | {a':}
-            |  ^
-          2 |
-      `
-        .split("\n")
-        .map((line) => `[error] ${line}`)
-        .join("\n"),
-    ),
-  });
-});
-
-describe("Invalid toml file", () => {
-  runCli("cli/config/invalid", [
-    "--config",
-    "broken-toml/.prettierrc.toml",
-    "--parser",
-    "babel",
-  ]).test({
-    status: 2,
-    stdout: "",
-    write: [],
-    stderr: expect.stringContaining(
-      outdent`
-        Invalid TOML document: incomplete declaration: value expected
-
-        1:  a=
-              ^
-        2:    b!=
-      `
-        .split("\n")
-        .map((line) => `[error] ${line}`)
-        .join("\n"),
-    ),
-  });
-});
-
-describe("Invalid yaml file", () => {
-  runCli("cli/config/invalid", [
-    "--config",
-    "broken-yaml/.prettierrc.yaml",
-    "--parser",
-    "babel",
-  ]).test({
-    status: 2,
-    stdout: "",
-    write: [],
-    stderr: expect.stringContaining(
-      // Keep the outdent, since error message changes between versions
-      outdent`
-        Map keys must be unique at line 1, column 3:
-
-        a:
-          ^
-      `
-        .split("\n")
-        .map((line) => `[error] ${line}`)
-        .join("\n"),
-    ),
   });
 });
 
